29475e20327c1f83df9b4760af50d7268cf67076
hannut91 <hannut91@gmail.com>
2020-06-27 21:11:21 +0900

    First commit

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  setupFilesAfterEnv: [
+    'jest-plugin-context/setup',
+    './jest.setup',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
---
+++ test/restaurants_test.js
@@ -0,0 +1,14 @@
+Feature('Restaurant');
+
+Scenario('카테고리와 지역을 모두 선택하면 레스토랑 목록을 볼 수 있습니다.', (I) => {
+  I.amOnPage('/');
+
+  I.click('서울');
+  I.click('한식');
+
+  I.see('양천주가');
+  I.see('한국식 초밥');
+
+  I.click('부산');
+  I.see('밀면넘어져요');
+});
