70f4c5001771d6ffe15b9e8855f8ff7447cebcbf
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-29 17:27:47 +0900

    feat(Regions): extract component
    - 지역 목록을 보여줄 컴포넌트 관심사 추출

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Regions from './Regions';
 
 export default function App() {
   return (
-    <></>
+    <>
+      <Regions />
+    </>
   );
 }
---
+++ src/Regions.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Regions() {
+  return (
+    <div>
+      서울
+    </div>
+  );
+}
