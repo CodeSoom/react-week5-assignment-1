ee334853e9a4ca277472c2f706e2f75f29c6c8b2
eonsang <2eonhworking@gmail.com>
2020-06-30 00:15:49 +0900

    Test: 검색된 리스트가 없을경우 관련 텍스트 노출 테스트

--- src/Restaurants.test.jsx
+++ src/Restaurants.test.jsx
@@ -13,4 +13,10 @@ describe('Restaurants', () => {
     expect(getByText('양천주가')).not.toBeNull();
     expect(getByText('한국식 초밥')).not.toBeNull();
   });
+
+  it('props로 받은 음식점 리스트가 없을 경우, 관련 텍스트를 보여준다..', () => {
+    const { getByText } = render(<Restaurants restaurants={[]} />);
+
+    expect(getByText('검색된 리스트가 없습니다.')).not.toBeNull();
+  });
 });
