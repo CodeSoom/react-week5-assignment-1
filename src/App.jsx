b1e4ccafea8073c46dac14ede4123719e49ab634
eonsang <2eonhworking@gmail.com>
2020-06-29 16:39:51 +0900

    Feat: 지역 리스트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 export default function App() {
+  const regions = ['서울', '대전', '대구'];
   return (
-    <></>
+    <>
+      <div>
+        {regions.map((region) => (
+          <button type="button" key={region}>
+            {region}
+          </button>
+        ))}
+      </div>
+    </>
   );
 }
