ca46af7c72e68f02a1ae6bf33b3d14acfd796f8e
eonsang <2eonhworking@gmail.com>
2020-06-30 00:16:30 +0900

    Feat: 검색된 리스트가 없을경우 관련 텍스트 추가

--- src/Restaurants.jsx
+++ src/Restaurants.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export default function Restaurants({ restaurants }) {
+  if (restaurants.length === 0) {
+    return <p>검색된 리스트가 없습니다.</p>;
+  }
+
   return (
     <>
       <div>
