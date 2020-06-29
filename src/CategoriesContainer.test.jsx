35b871b43cbc1df9617a926b03dfd4beb5be6282
eonsang <2eonhworking@gmail.com>
2020-06-29 23:03:33 +0900

    Test: categoriesContainer에서 reducer state에 등록되어잇는 음식점 카테고리 노출 테스트

---
+++ src/CategoriesContainer.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import CategoriesContainer from './CategoriesContainer';
+
+import { categories } from '../fixtures/categories';
+
+jest.mock('react-redux');
+
+describe('CategoriesContainer', () => {
+  it('지역 리스트가 나타난다.', () => {
+    useSelector.mockImplementation((selector) =>
+      selector({
+        categories,
+      }),
+    );
+    const { getByText } = render(<CategoriesContainer />);
+
+    expect(getByText('한식')).not.toBeNull();
+    expect(getByText('중식')).not.toBeNull();
+    expect(getByText('일식')).not.toBeNull();
+  });
+});
