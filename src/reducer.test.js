0484ce2ac5719a76e958ea67ae5023881537d0cc
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-29 21:09:27 +0900

    test(reducer): setRegions
    - regions 상태를 변경하는 리듀서 테스트 케이스 추가

---
+++ src/reducer.test.js
@@ -0,0 +1,19 @@
+import reducer from './reducer';
+
+import { setRegions } from './actions';
+
+import regions from '../fixtures/regions';
+
+describe('reducer', () => {
+  describe('setRegions', () => {
+    it('set new regions state', () => {
+      const previousState = {
+        regions: [],
+      };
+
+      const state = reducer(previousState, setRegions(regions));
+
+      expect(state.regions).toHaveLength(2);
+    });
+  });
+});
