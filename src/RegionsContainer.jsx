0f4217b6c127eab340f018c75112317d21d21a71
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-29 17:49:15 +0900

    test(RegionsContainer): add redux case
    - 리덕스를 통해 상태 관리하는 테스트 코드 추가

--- src/RegionsContainer.jsx
+++ src/RegionsContainer.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import Regions from './Regions';
 
-import regions from '../fixtures/regions';
-
 export default function RegionsContainer() {
   return (
     <Regions regions={regions} />
