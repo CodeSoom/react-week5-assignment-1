8003e9ca2b255f588c601209cfd6c4f459dd466e
daadaadaah <daadaadaah@gmail.com>
2020-06-30 23:24:02 +0900

    [Refactor] Refactor a App for adding a RegionsContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { regions } from '../__fixture__/data';
+import RegionsContainer from './RegionsContainer';
 
 export default function App() {
-  const [selectRegion, setSelectRegion] = useState('');
-
-  function handleSelectRegion(newSelectRegion) {
-    setSelectRegion(newSelectRegion);
-  }
-
   return (
     <div>
-      {regions.map((region) => (
-        <button key={region} onClick={() => handleSelectRegion(region)} type="button">
-          {region === selectRegion ? `${region}(V)` : region}
-        </button>
-      ))}
+      <RegionsContainer />
     </div>
   );
 }
