de48f963488b529ba07f890a3a0c34aa1ff4ecd1
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-30 19:22:53 +0900

    Implement reducer
    Use initialState with example data
    Return initialState for all cases

---
+++ src/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  categories: [
+    { id: 1, name: '한식' },
+    { id: 2, name: '중식' },
+  ],
+};
+
+function reducer(state = initialState, action) {
+  return state;
+}
+
+export default reducer;
