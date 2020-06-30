d44556371f338a4f56553200b0cafeaaa14300a3
sogoagain <imyong0@gmail.com>
2020-07-01 00:07:08 +0900

    Test - fetch categories 테스트 작성

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -1,6 +1,7 @@
-import fetchRegions from './api';
+import { fetchRegions, fetchCategories } from './api';
 
 import regionsFixture from '../__fixtures__/regions';
+import categoriesFixture from '../__fixtures__/categories';
 
 global.fetch = jest.fn();
 
@@ -15,5 +16,17 @@ describe('api', () => {
     const regions = await fetchRegions();
     // then
     expect(regions).toEqual(regionsFixture);
+  });
+
+  it('fetch categories', async () => {
+    // given
+    fetch.mockClear();
+    fetch.mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(categoriesFixture),
+    }));
+    // when
+    const categories = await fetchCategories();
+    // then
+    expect(categories).toEqual(categoriesFixture);
   });
 });
