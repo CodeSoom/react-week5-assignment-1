4595f65f8346155a2478fc739db875fac2e52a1a
sogoagain <imyong0@gmail.com>
2020-07-01 00:11:24 +0900

    Refactor - GET 요청의 중복되는 부분을 메서드로 추출

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,18 @@
+async function getDataFromServer(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
 const fetchRegions = async () => {
   const url = 'https://eatgo-customer-api.ahastudio.com/regions';
-  const response = await fetch(url);
-  const regions = await response.json();
+  const regions = await getDataFromServer(url);
   return regions;
 };
 
 const fetchCategories = async () => {
   const url = 'https://eatgo-customer-api.ahastudio.com/categories';
-  const response = await fetch(url);
-  const categories = await response.json();
+  const categories = await getDataFromServer(url);
   return categories;
 };
 
