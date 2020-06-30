7f1b679aff5bec9093f7397076c8cfe9cc5aa015
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-30 20:03:53 +0900

    Add useEffect to set initial categories
    Add dispatch
    Add loadCategories

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { setCategories } from './actions';
+
+function loadCategories(dispatch) {
+  const categories = [];
+
+  dispatch(setCategories(categories));
+}
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    loadCategories(dispatch);
+  }, []);
+
   const { categories } = useSelector((state) => ({
     categories: state.categories,
   }));
