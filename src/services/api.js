68d047009edbd7b426b8d4e5833c889f40dc82f5
newoo <newoo4297@naver.com>
2020-06-30 12:12:52 +0900

    Update api.jsx to pass test about fetching restaurants

--- src/services/api.js
+++ src/services/api.js
@@ -11,3 +11,10 @@ export async function fetchCategories() {
   const categories = await response.json();
   return categories;
 }
+
+export async function fetchRestaurants(regionName, categoryId) {
+  const url = 'https://eatgo-customer-api.ahastudio.com/restaurants?region=${regionName}&category=${categoryId}';
+  const response = await fetch(url);
+  const restaurants = await response.json();
+  return restaurants;
+}
