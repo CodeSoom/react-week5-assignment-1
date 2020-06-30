061b103e1f754c669a66285166ee482686263b41
jiunkoo <jiunkoo0106@gmail.com>
2020-06-30 23:58:09 +0900

    App - RestaurantsContainer 컴포넌트 붙이기

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
 
-import Restaurants from './Restaurants';
+import RestaurantsContainer from './RestaurantsContainer';
 
 export default function App() {
-  const restaurants = [
-    {
-      id: 1,
-      name: '비동기처리',
-      category: '코딩',
-      address: '서울시 강남구 역삼동',
-    },
-  ];
-
   return (
     <div>
       <h1>Restaurants</h1>
-      <Restaurants
-        restaurants={restaurants}
-      />
+      <RestaurantsContainer />
     </div>
   );
 }
