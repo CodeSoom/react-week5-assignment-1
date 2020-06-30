1babf97d01d7cd8928e60451de35dbed13bc74b3
sogoagain <imyong0@gmail.com>
2020-06-30 23:58:22 +0900

    Test - regions를 api를 통해 가져오는 로직 테스트

---
+++ src/services/api.test.js
@@ -0,0 +1,19 @@
+import fetchRegions from './api';
+
+import regionsFixture from '../__fixtures__/regions';
+
+global.fetch = jest.fn();
+
+describe('api', () => {
+  it('fetch regions', async () => {
+    // given
+    fetch.mockClear();
+    fetch.mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(regionsFixture),
+    }));
+    // when
+    const regions = fetchRegions();
+    // then
+    expect(regions).toEqual(regionsFixture);
+  });
+});
