5052a6dc6f18f591ccfe037ddb5c6a2093000b9f
jiunkoo <jiunkoo0106@gmail.com>
2020-06-30 23:47:17 +0900

    RestaurantsContainer - 레스토랑 목록 테스트 구현

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -1,4 +1,17 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
+import Restaurants from './Restaurants';
+
 export default function RestaurantsContainer() {
+  const { restaurants } = useSelector((state) => ({
+    restaurants: state.restaurants,
+  }));
+
+  return (
+    <Restaurants
+      restaurants={restaurants}
+    />
+  );
 }
