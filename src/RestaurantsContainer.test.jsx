4a283da0cb40c91bbf2fedb78159d556f9c93e8f
jiunkoo <jiunkoo0106@gmail.com>
2020-06-30 23:24:16 +0900

    RestaurantsContainer.jsx, RestaurantsContainer.test.jsx 파일 생성

---
+++ src/RestaurantsContainer.test.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import RestaurantsContainer from './RestaurantsContainer';
+
+test('RestaurantsContainer', () => {
+});
