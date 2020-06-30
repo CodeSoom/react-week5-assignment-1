1218681e3e72b9f8b031a402b9673dc5684b412e
Minwook Park <mwpark0975@gmail.com>
2020-07-01 00:51:20 +0900

    App, RegionContainer test refactor for useSelector

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
+
+jest.mock('react-redux');
 
 describe('App', () => {
   it('should display regions', () => {
+    useSelector.mockImplementation((selector) => selector({
+      regions: [],
+    }));
+
     const { getByText } = render((
       <App />
     ));
--- src/RegionContainer.jsx
+++ src/RegionContainer.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 export default function RegionContainer() {
+  const { regions } = useSelector((state) => ({
+    regions: state.regions,
+  }));
   return (
     <ul>
       <li>서울</li>
