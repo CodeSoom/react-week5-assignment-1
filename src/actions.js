90bf916d0e31a19dfa93bc878dae877d505a068c
aumilkyeong <aumilkyeong@gmail.com>
2020-06-30 18:21:14 +0900

    Feat: 서버에서 지역 정보 가져와서 갱신하는 액션

---
+++ src/actions.js
@@ -0,0 +1,17 @@
+import { fetchRegions } from './services/api';
+
+export function setRegions(regions) {
+  return {
+    type: 'setRegions',
+    payload: {
+      regions,
+    },
+  };
+}
+
+export function loadRegions() {
+  return async (dispatch) => {
+    const regions = await fetchRegions();
+    dispatch(setRegions(regions));
+  };
+}
