7d0ed9f7a2f1a891d03c8727be3af50a2f5ce30a
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-30 19:48:26 +0900

    Update test for app to add useSelector

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import { render } from '@testing-library/react';
 
 import App from './App';
+import { categories } from '../fixture/test-data';
+
+jest.mock('react-redux');
 
 test('App', () => {
   const { getByText } = render((
     <App />
   ));
+
+  useSelector.mockImplementation((selector) => selector({
+    categories,
+  }));
 
   expect(getByText(/한식/)).not.toBeNull();
   expect(getByText(/중식/)).not.toBeNull();
