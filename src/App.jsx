d0916863b03a717cba55e38e322c1bb5c1507c6b
sogoagain <imyong0@gmail.com>
2020-07-01 00:45:14 +0900

    Implement - <App />에서 category 목록을 출력한다

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
 import regions from './__fixtures__/regions';
+import categories from './__fixtures__/categories';
 
 export default function App() {
   return (
     <>
       <li>
         {regions.map(({ id, name }) => (
+          <button key={id} type="button">
+            {name}
+          </button>
+        ))}
+      </li>
+      <li>
+        {categories.map(({ id, name }) => (
           <button key={id} type="button">
             {name}
           </button>
