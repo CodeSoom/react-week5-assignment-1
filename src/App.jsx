3cdbdea0bd9e4ab34b34140479f8c8825f64f3bc
daadaadaah <daadaadaah@gmail.com>
2020-06-30 19:49:13 +0900

    [Green] Implement a App - context : render App - it : shows regions

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 export default function App() {
   return (
-    <></>
+    <div>
+      <button type="button">서울</button>
+      <button type="button">대전</button>
+      <button type="button">대구</button>
+      <button type="button">부산</button>
+      <button type="button">광주</button>
+      <button type="button">강원도</button>
+      <button type="button">인천</button>
+    </div>
   );
 }
