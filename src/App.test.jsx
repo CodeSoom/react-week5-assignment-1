28a8e3c247e23fbfdc2882b4d0b06e3e6ba5120b
Minwook Park <mwpark0975@gmail.com>
2020-06-30 23:51:22 +0900

    App - test

---
+++ src/App.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  it('should display regions', () => {
+    const { getByText } = render((
+      <App />
+    ));
+
+    expect(getByText('서울')).not.toBeNull();
+  });
+});
