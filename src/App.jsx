5c12f3c23ceee7c80e40886268ec4b6d437e361a
jiunkoo <jiunkoo0106@gmail.com>
2020-06-30 09:52:42 +0900

    App에서 레스토랑 목록 Restaurants로 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
+import Restaurants from './Restaurants';
+
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
-      <ul>
-        <li>
-          비동기처리
-        </li>
-      </ul>
+      <Restaurants />
     </div>
   );
 }
--- src/Restaurants.jsx
+++ src/Restaurants.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
 
 export default function Restaurants() {
+  return (
+    <ul>
+      <li>
+        비동기처리
+      </li>
+    </ul>
+  );
 }
