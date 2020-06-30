f8ca13c14ba42d2d34555d13fee3083ee2327164
jiunkoo <jiunkoo0106@gmail.com>
2020-06-30 09:44:21 +0900

    App - 테스트 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
+import { render } from '@testing-library/react';
+
 import App from './App';
 
 test('App', () => {
+  const { getByText } = render((
+    <App />
+  ));
+
+  expect(getByText(/비동기처리/)).not.toBeNull();
 });
