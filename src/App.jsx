0379258ebb4a68c8964f44878422804715687ac5
daadaadaah <daadaadaah@gmail.com>
2020-06-30 22:35:36 +0900

    [Green] Implement a App - context : with a selected region - it : shows a mark for a selected region

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { regions } from '../__fixture__/data';
 
 export default function App() {
+
+  const [selectRegion, setSelectRegion] = useState('');
+
+  function handleSelectRegion(newSelectRegion) {
+    setSelectRegion(newSelectRegion);
+  }
+
   return (
     <div>
-      {regions.map((region) => (
-        <button key={region} type="button">
-          {region}
-        </button>
-      ))}
+      {regions.map((region) => {
+
+        if (region === selectRegion) {
+          return (
+            <button key={region} onClick={() => handleSelectRegion(region)} type="button">
+              {`${region}(V)`}
+            </button>
+          );
+        }
+        return (
+          <button key={region} onClick={() => handleSelectRegion(region)} type="button">
+            {region}
+          </button>
+        );
+      })}
     </div>
   );
 }
