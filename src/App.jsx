7c912859a9fe3a29783fcb4be53e5b619cb552d6
naraekn <34229459+naraekn@users.noreply.github.com>
2020-06-30 19:10:01 +0900

    Refactor App
    Use buttons instead of paragraph
    Use categories object lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 export default function App() {
+  const categories = [
+    { id: 1, name: '한식' },
+    { id: 2, name: '중식' },
+  ];
+
   return (
     <div>
-      <p>한식</p>
-      <p>중식</p>
+      {
+        categories.map((category) => (
+          <button type="button" key={category.id}>
+            { category.name }
+          </button>
+        ))
+      }
     </div>
   );
 }
