d0a941e4332241be993b3f7d5c8a6eb0c1a3266a
jiunkoo <jiunkoo0106@gmail.com>
2020-06-30 09:45:50 +0900

    App - 테스트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 
 export default function App() {
   return (
-    <></>
+    <div>
+      <h1>Restaurants</h1>
+      <ul>
+        <li>
+          비동기처리
+        </li>
+      </ul>
+    </div>
   );
 }
