d6caf3fde84cfab5303b40efa0ccf108c784880e
daadaadaah <daadaadaah@gmail.com>
2020-06-30 20:02:14 +0900

    [Refactor] Refactor a App - context : render App - it : shows regions

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
+import { regions } from '../__fixture__/data';
+
 export default function App() {
   return (
     <div>
-      <button type="button">서울</button>
-      <button type="button">대전</button>
-      <button type="button">대구</button>
-      <button type="button">부산</button>
-      <button type="button">광주</button>
-      <button type="button">강원도</button>
-      <button type="button">인천</button>
+      {regions.map((region) => (
+        <button key={region} type="button">
+          {region}
+        </button>
+      ))}
     </div>
   );
 }
