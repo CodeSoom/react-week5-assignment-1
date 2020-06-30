5592eae030dae679152d5aebdb1ccb490cecbefe
jiunkoo <jiunkoo0106@gmail.com>
2020-06-30 09:57:58 +0900

    Restaurants - 레스토랑 목록 테스트 추가

--- src/Restaurants.test.jsx
+++ src/Restaurants.test.jsx
@@ -5,4 +5,17 @@ import { render } from '@testing-library/react';
 import Restaurants from './Restaurants';
 
 test('Restaurants', () => {
+  const restaurants = [
+    {
+      id: 1, name: '김밥제국', category: '분식', address: '서울시 강남구 역삼동',
+    },
+  ];
+
+  const { getByText } = render((
+    <Restaurants
+      restaurants={restaurants}
+    />
+  ));
+
+  expect(getByText(/김밥제국/)).not.toBeNull();
 });
