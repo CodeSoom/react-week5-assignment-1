2e16ef0991da47bbf1dc850b7761e758095bc56a
Kwakcena <rhkrgudwh@gmail.com>
2020-07-01 21:57:52 +0900

    test(red): 서버로부터 데이터를 가져오기를 원함
    App 컴포넌트에서 useEffect를 이용해 서버로부터 데이터를 가져오기를
    원한다. 이를 구현체에 넣었고 이것이 가능하도록 reducer에서 테스트를
    진행해야 한다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
 
 import RestaurantRegionsContainer from './RestaurantRegionsContainer';
 
+import {
+  loadRestaurantRegions,
+} from './actions';
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRestaurantRegions());
+  }, []);
+
   return (
     <>
       <RestaurantRegionsContainer />
