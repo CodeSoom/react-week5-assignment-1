dec6a9b2fcce494360d2b253eb745964609e7df8
Kwakcena <rhkrgudwh@gmail.com>
2020-07-01 21:33:30 +0900

    Refactor: 지역 목록은 버튼으로 구성되어 있음
    버튼으로 구성된 지역 목록으로 수정

--- src/RestaurantRegions.jsx
+++ src/RestaurantRegions.jsx
@@ -4,7 +4,11 @@ export default function RestaurantRegions({ regions }) {
   return (
     <ul>
       {regions.map(({ id, name }) => (
-        <li key={id}>{name}</li>
+        <li key={id}>
+          <button type="button">
+            {name}
+          </button>
+        </li>
       ))}
     </ul>
   );
