8c4241a2131993e2cff3aa1cc56214cac44e56cc
Kwakcena <rhkrgudwh@gmail.com>
2020-07-01 21:30:30 +0900

    test(Refactor): fixtures 생성
    테스트용 데이터 생성

--- src/RestaurantRegions.test.jsx
+++ src/RestaurantRegions.test.jsx
@@ -4,18 +4,10 @@ import { render } from '@testing-library/react';
 
 import RestaurantRegions from './RestaurantRegions';
 
+import { regions } from '../fixtures/restaurants';
+
 describe('RestaurantRegions', () => {
   it('레스토랑 지역 목록을 보여준다.', () => {
-    const regions = [
-      { id: 1, name: '서울' },
-      { id: 2, name: '대전' },
-      { id: 3, name: '대구' },
-      { id: 4, name: '부산' },
-      { id: 5, name: '광주' },
-      { id: 6, name: '강원도' },
-      { id: 7, name: '인천' },
-    ];
-
     const { getByText } = render((
       <RestaurantRegions regions={regions} />
     ));
