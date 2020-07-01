3211d94c74ed81aa658e2653c1336b143cb45c53
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-01 23:08:39 +0900

    Update test for CategoriesContainer for selectCategory action
    Test if selectCategory works

--- src/CategoriesContainer.test.jsx
+++ src/CategoriesContainer.test.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import CategoriesContainer from './CategoriesContainer';
 
+import { selectCategory } from './actions';
 import { categories } from '../fixture/test-data';
 
 jest.mock('react-redux');
 
 test('CategoriesContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
   useSelector.mockImplementation((selector) => selector({ categories }));
 
   const { getByText } = render((
     <CategoriesContainer />
   ));
+
+  fireEvent.click(getByText(categories[0].name));
+
+  expect(dispatch).toBeCalledWith(selectCategory(categories[0].id));
 });
