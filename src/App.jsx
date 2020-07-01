e1bc6d1d45e9549c9c0f731d03c8a972c29f8d53
eonsang <2eonhworking@gmail.com>
2020-07-01 16:14:12 +0900

    Test: App이 실행되면 dispatch 발생 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+
 import { useDispatch, useSelector } from 'react-redux';
 
 import RegionContainer from './RegionContainer';
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,6 +5,7 @@ import { render } from '@testing-library/react';
 
 import App from './App';
 
+import defaultState from '../fixtures/defaultState';
 import { regions } from '../fixtures/regions';
 import { categories } from '../fixtures/categories';
 
@@ -14,20 +15,14 @@ jest.mock('./services/api.js');
 describe('App', () => {
   const dispatch = jest.fn();
   useDispatch.mockImplementation(() => dispatch);
-  useSelector.mockImplementation((selector) => selector({
-    regions,
-    categories,
-    restaurants: [{ id: 1, name: '홍콩반점' }],
-  }));
+  useSelector.mockImplementation((selector) => selector(defaultState));
 
   it('지역 리스트가 나타난다.', () => {
-    useDispatch.mockImplementation(() => dispatch);
-
     const { getByText } = render(<App />);
 
-    expect(getByText('서울')).not.toBeNull();
-    expect(getByText('대전')).not.toBeNull();
-    expect(getByText('대구')).not.toBeNull();
+    expect(getByText(/서울/)).not.toBeNull();
+    expect(getByText(/대전/)).not.toBeNull();
+    expect(getByText(/대구/)).not.toBeNull();
   });
 
   it('카테고리 리스트가 나타난다.', () => {
@@ -42,5 +37,32 @@ describe('App', () => {
     const { getByText } = render(<App />);
 
     expect(getByText('홍콩반점')).not.toBeNull();
+  });
+
+  it('컴포넌트가 렌더링되면 dispatch가 발생한다.', () => {
+    // dispatch 호출 횟수 확인용
+    const dispatch2 = jest.fn();
+    useDispatch.mockImplementation(() => dispatch2);
+    useSelector.mockImplementation((selector) =>
+      selector({
+        regions,
+        categories,
+        restaurants: [{ id: 1, name: '홍콩반점' }],
+      }),
+    );
+    render(<App />);
+
+    expect(dispatch2).toBeCalledTimes(2);
+  });
+
+  it('selectedRegion, selectedCategory이 잇으면, dispatch가 발생한다.', () => {
+    // dispatch 호출 횟수 확인용
+    const dispatch2 = jest.fn();
+    useDispatch.mockImplementation(() => dispatch2);
+    useSelector.mockImplementation((selector) => selector(defaultState));
+
+    render(<App />);
+
+    expect(dispatch2).toBeCalledTimes(3);
   });
 });
