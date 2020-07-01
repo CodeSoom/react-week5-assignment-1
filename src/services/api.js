23cc7108ba71bc361c651563c07efa8a59cf5828
happytalk-gi <>
2020-07-01 10:09:15 +0900

    fetch를 통한 API services 작성

---
+++ src/services/api.js
@@ -0,0 +1,23 @@
+export const fetchRestaurantRegions = async () => {
+    const API_REGION_PATH = "https://eatgo-customer-api.ahastudio.com/regions"    
+    return fetch(API_REGION_PATH)
+            .then(response=>response.json())
+            .catch(error => console.log(error))
+}
+export const fetchRestaurantInfo = async ({regionName, categoryId})=>{
+    const API_RESTAURANT_INFO_PATH = "https://eatgo-customer-api.ahastudio.com/restaurants?region=${regionName}&category=${categoryId}"
+    return fetch(API_RESTAURANT_INFO_PATH
+                    .replace(/\${regionName}/, regionName)
+                    .replace(/\${categoryId}/, categoryId)
+            )
+            .then(response => response.json())
+            .catch(error => console.log(error))
+
+}
+export const fetchCategories = async () =>{
+    const API_RESTAURANTS_CATERGORY_PATH = "https://eatgo-customer-api.ahastudio.com/categories";
+    return fetch(API_RESTAURANTS_CATERGORY_PATH)
+            .then(response=>response.json())
+            .catch(error => console.log(error))
+}
+
