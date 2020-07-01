e2ee27b22628b59de7fec4bb43287c549cb67360
daadaadaah <daadaadaah@gmail.com>
2020-07-01 23:07:04 +0900

    [Green] Implement a RegionsContainer with Redux

--- src/RegionsContainer.jsx
+++ src/RegionsContainer.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 
+import { useSelector } from 'react-redux';
+
 export default function RegionsContainer() {
+  const { regions } = useSelector((state) => ({
+    regions: state.regions,
+  }));
+
   const [selectRegion, setSelectRegion] = useState('');
 
   function handleSelectRegion(newSelectRegion) {
