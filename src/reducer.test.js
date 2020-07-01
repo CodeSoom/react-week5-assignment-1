ea5ad73a2df95ccc030dc9a0f542b185072e2c29
daadaadaah <daadaadaah@gmail.com>
2020-07-01 18:46:41 +0900

    [Refactor] Refactor a reducer test for setInitRegions

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -2,6 +2,8 @@ import { regions } from '../__fixture__/data';
 
 import reducer from './reducer';
 
+import { setInitRegions } from './action';
+
 describe('reducer', () => {
   context('setInitRegions', () => {
     it('set a regions', () => {
@@ -9,14 +11,7 @@ describe('reducer', () => {
         regions: [],
       };
 
-      const action = {
-        type: 'setInitRegions',
-        payload: {
-          regions,
-        },
-      };
-
-      const newState = reducer(prevState, action);
+      const newState = reducer(prevState, setInitRegions(regions));
 
       expect(newState.regions).not.toHaveLength(0);
     });
