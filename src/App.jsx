4b2d2bde02f753d5c5897cdc9c585f48fea2b48c
sogoagain <imyong0@gmail.com>
2020-07-02 02:32:19 +0900

    Refactor - <App />에서 <RestaurantListContainer />를 사용하도록 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 
-import TextList from './TextList';
 import RegionButtonListContainer from './RegionButtonListContainer';
 import CategoryButtonListContainer from './CategoryButtonListContainer';
+import RestaurantListContainer from './RestaurantListContainer';
 
 export default function App() {
-  const { restaurants } = useSelector((state) => ({
-    restaurants: state.restaurants,
-  }));
-
   return (
     <>
       <RegionButtonListContainer />
       <CategoryButtonListContainer />
-      <TextList
-        elements={restaurants}
-      />
+      <RestaurantListContainer />
     </>
   );
 }
