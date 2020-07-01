98b28d46df6a4ecdd6d1e604b6f564657ca9d6cd
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-01 13:15:56 +0900

    test(RegionContainer): selected region case
    - 렌더링 대상이 선택된 지역일 경우 테스트 케이스 작성

--- src/RegionContainer.test.jsx
+++ src/RegionContainer.test.jsx
@@ -2,33 +2,62 @@ import React from 'react';
 
 import { render, fireEvent } from '@testing-library/react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import RegionContainer from './RegionContainer';
 
 jest.mock('react-redux');
 
-test('RegionContainer', () => {
-  const dispatch = jest.fn();
+const region = {
+  id: 1,
+  name: '부산',
+};
 
-  useDispatch.mockImplementation(() => dispatch);
-
-  const region = {
-    id: 1,
-    name: '부산',
-  };
-
-  const { getByText } = render((
+function renderMockRegion() {
+  return render((
     <RegionContainer region={region} />
   ));
+}
 
-  expect(getByText('부산')).not.toBeNull();
+describe('RegionContainer', () => {
+  context('when not selected', () => {
+    it('can see normal button', () => {
+      const { getByText } = renderMockRegion();
 
-  fireEvent.click(getByText('부산'));
-  expect(dispatch).toBeCalledWith({
-    type: 'setSelectedRegion',
-    payload: {
-      id: 1,
-    },
+      expect(getByText('부산')).not.toBeNull();
+    });
+  });
+
+  context('when selected', () => {
+    it('can see selected button', () => {
+      useSelector.mockImplementation((selector) => selector({
+        selectedRegion: 1,
+      }));
+
+      const { queryByText } = renderMockRegion();
+
+      expect(queryByText('부산')).toBeNull();
+      expect(queryByText('부산(v)')).not.toBeNull();
+    });
+  });
+
+  describe('click button', () => {
+    it('can select region', () => {
+      const dispatch = jest.fn();
+
+      useDispatch.mockImplementation(() => dispatch);
+
+      const { getByText } = renderMockRegion();
+
+      expect(getByText('부산')).not.toBeNull();
+
+      fireEvent.click(getByText('부산'));
+      expect(dispatch).toBeCalledWith({
+        type: 'setSelectedRegion',
+        payload: {
+          id: 1,
+        },
+      });
+    });
   });
 });
