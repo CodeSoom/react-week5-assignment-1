4cf80667b9d8dd8403775aff2faf8a6c6c148144
daadaadaah <daadaadaah@gmail.com>
2020-07-01 18:42:17 +0900

    [Green] Implement a App with a Redux for setInitRegions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
 
 import RegionsContainer from './RegionsContainer';
 
+import { regions } from '../__fixture__/data'; // TODO : API 통신 구현 완료 후 삭제 예정
+
+import { setInitRegions } from './action';
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setInitRegions(regions));
+  }, []);
+
   return (
     <div>
       <RegionsContainer />
