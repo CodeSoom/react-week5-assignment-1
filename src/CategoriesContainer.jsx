784960670fe8b54a36af00cd7feb6f08fda904cf
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-01 23:19:55 +0900

    Update CategoriesContainer to add handleClick

--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Categories from './Categories';
+
+import { selectCategory } from './actions';
 
 export default function CategoriesContainer() {
   const { categories } = useSelector((state) => ({
     categories: state.categories,
   }));
 
+  const dispatch = useDispatch();
+
+  function handleClick(categoryId) {
+    dispatch(selectCategory(categoryId));
+  }
+
   return (
     <Categories
       categories={categories}
+      onClick={handleClick}
     />
   );
 }
