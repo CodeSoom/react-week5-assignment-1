b5dc27b0e2baf79d1ae2255b467645ee019a7345
daadaadaah <daadaadaah@gmail.com>
2020-07-01 23:04:09 +0900

    [Red] Add a test for a RegionsContainer with Redux

--- src/RegionsContainer.test.jsx
+++ src/RegionsContainer.test.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { render, fireEvent } from '@testing-library/react';
 
 import RegionsContainer from './RegionsContainer';
 
 import { regions } from '../__fixture__/data';
 
+jest.mock('react-redux');
+
 describe('<RegionsContainer />', () => {
+  context('render RegionsContainer', () => {
+    it('shows regions', () => {
+      useSelector.mockImplementation((selector) => selector({
+        regions,
+      }));
+
+      const { queryByRole } = render(<RegionsContainer />);
+
+      regions.forEach((region) => {
+        expect(queryByRole('button', { name: region })).not.toBeNull();
+      });
+    });
+  });
+
   context('with a selected region', () => {
     it('shows a mark for a selected region', () => {
       const { getByRole, queryByRole } = render(<RegionsContainer />);
