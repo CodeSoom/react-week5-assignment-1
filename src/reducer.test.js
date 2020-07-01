9936fec630d8ee452f1b21a3c20e7de44047fcc9
sogoagain <imyong0@gmail.com>
2020-07-01 22:38:52 +0900

    Test - setCategories reduce 테스트

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,7 +1,8 @@
 import reducer from './reducer';
-import { setRegions } from './actions';
+import { setRegions, setCategories } from './actions';
 
 import regionsFixture from './__fixtures__/regions';
+import categoriesFixture from './__fixtures__/categories';
 
 describe('reducer', () => {
   const previousState = {
@@ -17,5 +18,15 @@ describe('reducer', () => {
     const state = reducer(previousState, action);
     // then
     expect(state.regions).toEqual(regions);
+  });
+
+  it('setCategories', () => {
+    // given
+    const categories = categoriesFixture;
+    const action = setCategories(categories);
+    // when
+    const state = reducer(previousState, action);
+    // then
+    expect(state.categories).toEqual(categories);
   });
 });
