694cb3e2084fb9b01ce44a6c1d9c3b6c69761962
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-01 14:53:11 +0900

    test(CategoryContainer): not selected render case
    - 선택되지 않는 경우와 선택된 경우에 따라 변경되는 버튼 렌더링 테스트 케이스 추가

--- src/CategoryContainer.test.jsx
+++ src/CategoryContainer.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { render, fireEvent } from '@testing-library/react';
 
@@ -8,27 +8,66 @@ import CategoryContainer from './CategoryContainer';
 
 jest.mock('react-redux');
 
-test('CategoryContainer', () => {
-  const dispatch = jest.fn();
+const category = {
+  id: 1,
+  name: '한식',
+};
 
-  useDispatch.mockImplementation(() => dispatch);
-
-  const category = {
-    id: 1,
-    name: '한식',
-  };
-
-  const { getByText } = render((
+function renderMockCategoryContainer() {
+  return render((
     <CategoryContainer category={category} />
   ));
+}
 
-  expect(getByText('한식')).not.toBeNull();
+describe('CategoryContainer', () => {
+  context('when not selected', () => {
+    it('can see normal button', () => {
+      const selectedCategoryId = 2;
 
-  fireEvent.click(getByText('한식'));
-  expect(dispatch).toBeCalledWith({
-    type: 'setSelectedCategoryId',
-    payload: {
-      id: 1,
-    },
+      useSelector.mockImplementation((selector) => selector({
+        selectedCategoryId,
+      }));
+
+      const { getByText } = renderMockCategoryContainer();
+
+      expect(getByText('한식')).not.toBeNull();
+    });
+  });
+
+  context('when selected', () => {
+    it('can see selectedd button', () => {
+      const selectedCategoryId = 1;
+
+      useSelector.mockImplementation((selector) => selector({
+        selectedCategoryId,
+      }));
+
+      const { getByText } = renderMockCategoryContainer();
+
+      expect(getByText('한식')).toBeNull();
+      expect(getByText('한식(v)')).not.toBeNull();
+    });
+  });
+
+  it('can click', () => {
+    const dispatch = jest.fn();
+
+    const selectedCategoryId = 2;
+
+    useSelector.mockImplementation((selector) => selector({
+      selectedCategoryId,
+    }));
+
+    useDispatch.mockImplementation(() => dispatch);
+
+    const { getByText } = renderMockCategoryContainer();
+
+    fireEvent.click(getByText('한식'));
+    expect(dispatch).toBeCalledWith({
+      type: 'setSelectedCategoryId',
+      payload: {
+        id: 1,
+      },
+    });
   });
 });
