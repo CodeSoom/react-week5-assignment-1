c4bce17be88ac72651a27a1d4ff4a82c76f09c28
sogoagain <imyong0@gmail.com>
2020-07-02 00:11:58 +0900

    Test - <App />에서 식당 리스트를 출력한다

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import regionsFixture from './__fixtures__/regions';
 import categoriesFixture from './__fixtures__/categories';
+import restaurantsFixture from './__fixtures__/restaurants';
 
 import App from './App';
 
@@ -15,6 +16,7 @@ function renderApp() {
   render(<App />);
   return {
     getButtonByName: (name) => screen.getByRole('button', { name }),
+    getByText: (text) => screen.getByText(text),
   };
 }
 
@@ -24,6 +26,7 @@ describe('<App />', () => {
   useSelector.mockImplementation((selector) => selector({
     regions: regionsFixture,
     categories: categoriesFixture,
+    restaurants: restaurantsFixture,
   }));
 
   beforeEach(() => {
@@ -34,16 +37,25 @@ describe('<App />', () => {
     // when
     const { getButtonByName } = renderApp();
     // then
-    expect(getButtonByName('서울'));
-    expect(getButtonByName('대전'));
+    expect(getButtonByName('서울')).toBeInTheDocument();
+    expect(getButtonByName('대전')).toBeInTheDocument();
   });
 
   it('renders category buttons', () => {
     // when
     const { getButtonByName } = renderApp();
     // then
-    expect(getButtonByName('한식'));
-    expect(getButtonByName('중식'));
+    expect(getButtonByName('한식')).toBeInTheDocument();
+    expect(getButtonByName('중식')).toBeInTheDocument();
+  });
+
+  it('renders restaurant list', () => {
+    // when
+    const { getByText } = renderApp();
+    // then
+    restaurantsFixture.forEach(({ name }) => {
+      expect(getByText(name)).toBeInTheDocument();
+    });
   });
 
   it('check when button clicked', () => {
