c266b2e8f8fa6da862966a170b5842343403a361
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-01 23:22:08 +0900

    Update test for Categories
    Test if handleClick is called when button is clicked

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Categories from './Categories';
 
 import { categories } from '../fixture/test-data';
 
 test('Categories', () => {
+  const handleClick = jest.fn();
+
   const { getByText } = render((
     <Categories
       categories={categories}
+      onClick={handleClick}
     />
   ));
 
   categories.forEach((cateogory) => {
     expect(getByText(cateogory.name)).not.toBeNull();
   });
+
+  fireEvent.click(getByText(categories[0].name));
+
+  expect(handleClick).toBeCalled();
 });
