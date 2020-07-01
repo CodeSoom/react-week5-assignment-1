689975c455c4889cd974073540997561aef3e741
daadaadaah <daadaadaah@gmail.com>
2020-07-02 03:18:36 +0900

    [Red] Add a test for Regions

---
+++ src/Regions.test.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import Regions from './Regions';
+
+import { regions } from '../__fixture__/data';
+
+describe('<Regions />', () => {
+  describe('render Regions', () => {
+    context('without newSelectRegion', () => {
+      it('does not show a mark for a selected region ', () => {
+        const { queryByRole } = render(
+          <Regions regions={regions} newSelectRegion="" />,
+        );
+
+        regions.forEach((region) => {
+          expect(queryByRole('button', { name: region.name })).not.toBeNull();
+        });
+      });
+    });
+
+    // context('with newSelectRegion', () => {
+    //   it('shows a mark for a selected region', () => {
+    //     const { queryByRole } = render(
+    //       <Regions regions={regions} newSelectRegion="서울" />,
+    //     );
+
+    //     expect(queryByRole('button', { name: '서울(V)' })).not.toBeNull();
+    //   });
+    // });
+  });
+
+  // context('with a selected region', () => {
+  //   it('run handleSelectRegion', () => {
+  //     const handleSelectRegion = jest.fn();
+
+  //     const { getByRole } = render(<Regions regions={regions} newSelectRegion="서울" onSelectRegions={handleSelectRegion} />);
+
+  //     regions.forEach((region) => {
+  //       fireEvent.click(getByRole('button', { name: region.name }));
+  //     });
+  //     expect(handleSelectRegion).toBeCalledTimes(regions.length);
+  //   });
+  // });
+});
