d87de01524ba4fcca3453069eb64c3167ddbaf32
Kwakcena <rhkrgudwh@gmail.com>
2020-07-01 22:22:55 +0900

    test(green): setRegions 테스트 통과

--- src/reducer.js
+++ src/reducer.js
@@ -3,5 +3,12 @@ const initialState = {
 };
 
 export default function reducer(state = initialState, action) {
+  if (action.type === 'setRegions') {
+    const { regions } = action.payload;
+    return {
+      ...state,
+      regions,
+    };
+  }
   return state;
 }
