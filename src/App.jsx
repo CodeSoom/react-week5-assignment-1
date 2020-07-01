95337164188c5dc217c24afaac53026fbf817eed
Kwakcena <rhkrgudwh@gmail.com>
2020-07-01 21:10:01 +0900

    test(green): 레스토랑 지역 목록 보여주기
    하드코딩으로 통과시킴

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 
 export default function App() {
   return (
-    <></>
+    <>
+      <ul>
+        <li>서울</li>
+        <li>대전</li>
+        <li>대구</li>
+        <li>부산</li>
+        <li>광주</li>
+        <li>강원도</li>
+        <li>인천</li>
+      </ul>
+    </>
   );
 }
