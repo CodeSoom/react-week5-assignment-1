759a5b43a80eb701baf6e98ded99bef5b7dec7b4
Kwakcena <rhkrgudwh@gmail.com>
2020-07-01 21:14:51 +0900

    test(Refactor): 레스토랑 지역 목록 컴포넌트 분리
    레스토랑 지역 목록을 보여주는 컴포넌트를 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 
+import RestaurantRegionsContainer from './RestaurantRegionsContainer';
+
 export default function App() {
   return (
     <>
-      <ul>
-        <li>서울</li>
-        <li>대전</li>
-        <li>대구</li>
-        <li>부산</li>
-        <li>광주</li>
-        <li>강원도</li>
-        <li>인천</li>
-      </ul>
+      <RestaurantRegionsContainer />
     </>
   );
 }
