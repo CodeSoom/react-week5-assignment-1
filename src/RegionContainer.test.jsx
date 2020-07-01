8bf16082b6bd5db4374a2a5c38cbefc29c7122c0
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-01 12:46:25 +0900

    test(RegionContainer): click event test
    - 버튼을 클릭했을 때 원하는 기능 테스트 케이스 구현

--- src/RegionContainer.test.jsx
+++ src/RegionContainer.test.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+
+import { useDispatch } from 'react-redux';
 
 import RegionContainer from './RegionContainer';
 
 jest.mock('react-redux');
 
 test('RegionContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   const region = {
     id: 1,
     name: '부산',
@@ -17,4 +23,12 @@ test('RegionContainer', () => {
   ));
 
   expect(getByText('부산')).not.toBeNull();
+
+  fireEvent.click(getByText('부산'));
+  expect(dispatch).toBeCalledWith({
+    type: 'setSelectedRegion',
+    payload: {
+      id: 1,
+    },
+  });
 });
