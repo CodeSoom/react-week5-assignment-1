e2d04083ecdd1d3ccb94bbf5d8762c46b6509c64
eonsang <2eonhworking@gmail.com>
2020-07-01 15:26:21 +0900

    Refactor: 테스트 구조를 GIVEN-WHEN-THEN 구조로 변경

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -10,29 +10,34 @@ import { categories } from '../fixtures/categories';
 jest.mock('react-redux');
 
 describe('Categories', () => {
+  // GIVEN
+  const dispatch = jest.fn();
+  const handleClick = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+  useSelector.mockImplementation((selector) => selector({ categories }));
+
   it('category를 보여준다.', () => {
+    // WHEN
     const { getByText } = render(<Categories categories={categories} />);
+
+    // THEN
     expect(getByText('한식')).not.toBeNull();
     expect(getByText('중식')).not.toBeNull();
     expect(getByText('일식')).not.toBeNull();
   });
 
-  it('카테고리 버튼을 클릭 할 수 있다.', () => {
-    const dispatch = jest.fn();
-    const handleClick = jest.fn();
-    useDispatch.mockImplementation(() => dispatch);
-    useSelector.mockImplementation((selector) => selector({ categories }));
-
+  it('카테고리 버튼을 클릭하면 클릭 이벤트가 발생한다.', () => {
+    // WHEN
     const { getByText } = render(
       <Categories categories={categories} onClick={handleClick} />,
     );
+
+    // THEN
     fireEvent.click(getByText('한식'));
     expect(handleClick).toBeCalled();
   });
 
   it('선택된 카테고리는 V 표시가 있다.', () => {
-    useSelector.mockImplementation((selector) => selector({ categories }));
-
     const { getByText } = render(
       <Categories categories={categories} selectedCategory={1} />,
     );
