3dd8e8e2555a64e78043972f022e13cd03633bc7
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-01 12:22:03 +0900

    test(RegionContainer): extract component
    - Region의 상태를 관리할 컴포넌트 테스트 케이스 추출

---
+++ src/RegionContainer.test.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import RegionContainer from './RegionContainer';
+
+test('RegionContainer', () => {
+  const region = {
+    id: 1,
+    name: '부산',
+  };
+
+  const { getByText } = render((
+    <RegionContainer region={region} />
+  ));
+
+  expect(getByText('부산')).not.toBeNull();
+});
