753a831967db13196662e6899e21c4b76f0f6965
eonsang <2eonhworking@gmail.com>
2020-07-01 15:29:12 +0900

    Refactor: 테스트 케이스에서 중복되는 Given 제거

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -12,6 +12,7 @@ import { regions } from '../fixtures/regions';
 import { categories } from '../fixtures/categories';
 
 describe('reducer', () => {
+  const initialState = {};
   it('액션이 없으면 initialState를 반환한다.', () => {
     const expectState = {
       regions: [],
@@ -27,21 +28,13 @@ describe('reducer', () => {
 
   describe('SELECT_REGION', () => {
     it('선택된 지역이 변경된다.', () => {
-      const state = reducer(
-        {
-          selectedRegion: '',
-        },
-        selectRegion('서울'),
-      );
+      const state = reducer(initialState, selectRegion('서울'));
       expect(state.selectedRegion).toBe('서울');
     });
   });
 
   describe('SET_REGIONS', () => {
     it('지역리스트가 변경된다.', () => {
-      const initialState = {
-        regions: [],
-      };
       const state = reducer(initialState, setRegions(regions));
       expect(state.regions).toHaveLength(3);
     });
@@ -61,9 +54,6 @@ describe('reducer', () => {
 
   describe('SET_CATEGORIES', () => {
     it('카테고리 리스트가 변경된다,', () => {
-      const initialState = {
-        categories: [],
-      };
       const state = reducer(initialState, setCategories(categories));
       expect(state.categories).toHaveLength(3);
     });
@@ -71,9 +61,6 @@ describe('reducer', () => {
 
   describe('SET_RESTAURANTS', () => {
     it('레스토랑 리스트가 변경된다.', () => {
-      const initialState = {
-        restaurants: [],
-      };
       const state = reducer(
         initialState,
         setRestaurants([
