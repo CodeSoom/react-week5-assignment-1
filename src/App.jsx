0ca9710e7c2df1c99ec32d03ad3b38fd3956f477
Minwook Park <mwpark0975@gmail.com>
2020-07-01 23:45:08 +0900

    prepare for api usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
+
+import { setRegions } from './actions';
 
 import RegionContainer from './RegionContainer';
 
+function loadRegions(dispatch) {
+  const regions = [];
+  // TODO : load regions
+  // 1. API server
+  // 2. fetch
+  dispatch(setRegions(regions));
+}
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    loadRegions({ dispatch });
+  }, []);
+
   return (
     <div>
       <RegionContainer />
