9e5aa937573bac2b98e2b5cc35b66543f35e446e
daadaadaah <daadaadaah@gmail.com>
2020-07-02 00:40:07 +0900

    [Refactor] Refactor a fetchInitRegions for adding url

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,6 @@
 export async function fetchInitRegions() {
-  return [];
+  const url = 'https://eatgo-customer-api.ahastudio.com/regions';
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
