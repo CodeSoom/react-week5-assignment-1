358644eba8f1d236f5d8d405a8b52022dbcb03ee
Minwook Park <mwpark0975@gmail.com>
2020-07-02 00:27:20 +0900

    Regions - test for button

--- src/Regions.test.jsx
+++ src/Regions.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Regions from './Regions';
 
@@ -8,16 +8,21 @@ jest.mock('react-redux');
 
 describe('App', () => {
   it('should display regions', () => {
+    const handleClick = jest.fn();
+
     const regions = [
       { id: 101, name: '서울' },
     ];
 
-    const { container } = render((
+    const { container, getByText } = render((
       <Regions
         regions={regions}
       />
     ));
 
     expect(container).toHaveTextContent('서울');
+    fireEvent.click(getByText('서울'));
+
+    expect(handleClick).toBeCalled();
   });
 });
