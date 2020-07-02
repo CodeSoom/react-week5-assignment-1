3208cdf60875637c1a3a4c8fcd29bce360f55b56
daadaadaah <daadaadaah@gmail.com>
2020-07-03 00:29:12 +0900

    [Red] Add a aync action test for loadRegions

---
+++ src/action.test.js
@@ -0,0 +1,27 @@
+import configureStore from 'redux-mock-store';
+
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk]; // add your middlewares like `redux-thunk`
+const mockStore = configureStore(middlewares);
+
+global.fetch = jest.fn(() => Promise.resolve({
+  json: () => Promise.resolve(regions),
+}));
+
+describe('acton', () => {
+  beforeEach(() => {
+    fetch.mockClear();
+  });
+
+  describe('loadRegions', () => {
+    it('load Regions', () => {
+      const store = mockStore({});
+
+      return store.dispatch(loadRegions()).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual(setInitRegions(regions));
+      });
+    });
+  });
+});
