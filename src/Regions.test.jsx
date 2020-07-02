12d0d6951b1306d7ce58ff4db9b61510dff60ecf
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-02 23:17:36 +0900

    Add test for Regions

---
+++ src/Regions.test.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import Regions from './Regions';
+
+import { regions } from '../fixture/test-data';
+
+describe('Regions', () => {
+  const handleClick = jest.fn();
+
+  const renderRegions = (regionName) => render((
+    <Regions
+      regions={regions}
+      regionName={regionName}
+      onClick={handleClick}
+    />
+  ));
+
+  it('renders regions buttons', () => {
+    const { getByText } = renderRegions();
+
+    regions.forEach((region) => {
+      expect(getByText(RegExp(region.name))).not.toBeNull();
+    });
+  });
+
+  context('when button is clicked', () => {
+    it('handleClick is called', () => {
+      const { getByText } = renderRegions();
+
+      fireEvent.click(getByText(regions[0].name));
+
+      expect(handleClick).toBeCalled();
+    });
+  });
+
+  context('with selected region name', () => {
+    it('renders V sign on the clicked button', () => {
+      const { getByText } = renderRegions('서울');
+
+      expect(getByText(/V/)).not.toBeNull();
+    });
+  });
+
+  context('without selected region name', () => {
+    it('does not render V sign on all buttons', () => {
+      const { queryByText } = renderRegions(undefined);
+
+      expect(queryByText(/V/)).toBeNull();
+    });
+  });
+});
