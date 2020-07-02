0bc697c901543afce3e76dd3b6ba7b682714bddb
Kwakcena <rhkrgudwh@gmail.com>
2020-07-02 10:31:01 +0900

    test(red): 테스트 수정
    categories를 props로 넘겨주는 작업 필요

--- src/RestaurantCategories.test.jsx
+++ src/RestaurantCategories.test.jsx
@@ -6,8 +6,11 @@ import RestaurantCategories from './RestaurantCategories';
 
 describe('RestaurantCategories', () => {
   it('카테고리 목록을 화면에 보여준다.', () => {
+    const categories = [
+      { id: 1, name: '한식' },
+    ];
     const { getByText } = render((
-      <RestaurantCategories />
+      <RestaurantCategories categories={categories} />
     ));
 
     expect(getByText('한식')).not.toBeNull();
