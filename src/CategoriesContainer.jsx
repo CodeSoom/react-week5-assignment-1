5e3dfa3ea6f08b96f310a47c041e134d6448bda6
daadaadaah <daadaadaah@gmail.com>
2020-07-03 06:51:57 +0900

    [Refactor] Refactor a CategoriesContainer for adding Categories

--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
-import RegionsContainer from './RegionsContainer';
+import Categories from './Categories';
 
-import {
-  selectCategory, loadRestaurants,
-} from './action';
+import { selectCategory, loadRestaurants } from './action';
 
 export default function CategoriesContainer() {
-  const {
-    selectedRegion, categories, selectedCategory,
-  } = useSelector(
+  const { selectedRegion, categories, selectedCategory } = useSelector(
     (state) => ({
       selectedRegion: state.selectedRegion,
       categories: state.categories,
@@ -27,18 +23,10 @@ export default function CategoriesContainer() {
   }
 
   return (
-    <div>
-      {categories.map((category) => (
-        <button
-          key={category.id}
-          onClick={() => handleSelectCategory(category.name, category.id)}
-          type="button"
-        >
-          {category.name === selectedCategory
-            ? `${category.name}(V)`
-            : category.name}
-        </button>
-      ))}
-    </div>
+    <Categories
+      categories={categories}
+      selectedCategory={selectedCategory}
+      onSelectCategory={handleSelectCategory}
+    />
   );
 }
