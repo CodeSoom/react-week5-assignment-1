745d3566af4b15b4eceff3e9dd190666f681c824
morykwon <kwonmory@gmail.com>
2020-07-02 23:55:34 +0900

    App 에서 setXXX dispatch 해보기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
+
+import RestaurantCategoriesContainer from './RestaurantCategoriesContainer';
+import RestaurantRegionsContainer from './RestaurantRegionsContainer';
+
+import { setRegions, setCategories } from './actions';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // TODO 지역, 카테고리 불러오기
+    const regions = [
+      {
+        id: 1,
+        name: '서울',
+      },
+      {
+        id: 2,
+        name: '대전',
+      },
+    ];
+
+    dispatch(setRegions(regions));
+    dispatch(setCategories(regions));
+  }, []);
+
   return (
-    <></>
+    <>
+      <RestaurantRegionsContainer />
+      <RestaurantCategoriesContainer />
+    </>
   );
 }
