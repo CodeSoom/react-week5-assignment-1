d541c0f76331f595abe364431afad6d8a3c4e53f
Kwakcena <rhkrgudwh@gmail.com>
2020-07-02 11:08:22 +0900

    test(green): 리덕스 thunk 사용 전
    사용 전에는 fixtures 의 데이터를 setCategories에 넣어 확인한다

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,23 @@ import { useDispatch } from 'react-redux';
 import RestaurantRegionsContainer from './RestaurantRegionsContainer';
 import RestaurantCategoriesContainer from './RestaurantCategoriesContainer';
 
+import { categories } from '../fixtures/restaurants';
+
 import {
   loadRestaurantRegions,
+  setCategories,
 } from './actions';
+
+function loadRestaurantCategores({ dispatch }) {
+  dispatch(setCategories(categories));
+}
 
 export default function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadRestaurantRegions());
+    loadRestaurantCategores({ dispatch });
   }, []);
 
   return (
