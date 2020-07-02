53e7d55d9eefb3716ba7b7d0207d3499abc8e592
Kwakcena <rhkrgudwh@gmail.com>
2020-07-02 10:19:15 +0900

    test(red): reducer setCategories test
    레스토랑의 카테고리를 categories 배열에 넣어주는 테스트 작성

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -2,14 +2,16 @@ import reducer from './reducer';
 
 import {
   setRegions,
+  setCategories,
 } from './actions';
 
 const initialState = {
   regions: [],
+  categories: [],
 };
 
 describe('reducer', () => {
-  it('카테고리 정보 설정', () => {
+  it('set restaurant region into regions', () => {
     const regions = [
       { id: 1, name: '서울' },
     ];
@@ -17,5 +19,14 @@ describe('reducer', () => {
     const state = reducer(initialState, setRegions(regions));
 
     expect(state.regions).toHaveLength(1);
+  });
+
+  it('set restaurant category into categorys', () => {
+    const categories = [
+      { id: 1, name: '한식' },
+    ];
+
+    const state = reducer(initialState, setCategories(categories));
+    expect(state.categories).toHaveLength(1);
   });
 });
