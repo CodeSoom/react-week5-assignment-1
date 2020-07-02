3c0a107ade6583c022e3d20866fba7015aaa2dba
KiChang Shin <kc7851@gmail.com>
2020-07-02 22:54:00 +0900

    Regions test 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 
+import Regions from './Regions';
+
 export default function App() {
   return (
     <div>
-      <p>
-        <ul>
-          <li><button type="button">서울</button></li>
-          <li><button type="button">대전</button></li>
-          <li><button type="button">대구</button></li>
-          <li><button type="button">부산</button></li>
-          <li><button type="button">광주</button></li>
-          <li><button type="button">강원도</button></li>
-          <li><button type="button">인천</button></li>
-        </ul>
-      </p>
-      <p>
+      <Regions />
+      <div>
         <ul>
           <li><button type="button">한식</button></li>
           <li><button type="button">중식</button></li>
@@ -22,7 +14,7 @@ export default function App() {
           <li><button type="button">양식</button></li>
           <li><button type="button">분식</button></li>
         </ul>
-      </p>
+      </div>
     </div>
   );
 }
---
+++ src/Regions.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Regions() {
+  return (
+    <div>
+      <ul>
+        <li><button type="button">서울</button></li>
+        <li><button type="button">대전</button></li>
+        <li><button type="button">대구</button></li>
+        <li><button type="button">부산</button></li>
+        <li><button type="button">광주</button></li>
+        <li><button type="button">강원도</button></li>
+        <li><button type="button">인천</button></li>
+      </ul>
+    </div>
+  );
+}
