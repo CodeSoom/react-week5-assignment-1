83885cd40f6db2db264c5b3f049ce75a8c7422a2
morykwon <kwonmory@gmail.com>
2020-07-02 22:38:06 +0900

    RestaurantRegions 테스트 수정

--- src/RestaurantRegions.test.jsx
+++ src/RestaurantRegions.test.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import RestaurantRegions from './RestaurantRegions';
 
 describe('RestaurantRegions', () => {
   context('with fetch data', () => {
     it('renders regions', () => {
+      const handleClick = jest.fn();
+
       const regions = [
         {
           id: 1,
@@ -18,7 +20,11 @@ describe('RestaurantRegions', () => {
         },
       ];
 
-      const { getByText } = render(<RestaurantRegions regions={regions} />);
+      const { getByText } = render(<RestaurantRegions regions={regions} onClick={handleClick} />);
+
+      fireEvent.click(getByText('서울'));
+
+      expect(handleClick).toBeCalled();
 
       expect(getByText('서울')).not.toBeNull();
       expect(getByText('대전')).not.toBeNull();
