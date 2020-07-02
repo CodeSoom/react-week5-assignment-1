b0696581ac22736dada654c8f8de852db19f7592
morykwon <kwonmory@gmail.com>
2020-07-02 23:25:03 +0900

    RestaurantRegionsContainer를 수정한다

--- src/RestaurantRegionsContainer.test.jsx
+++ src/RestaurantRegionsContainer.test.jsx
@@ -27,45 +27,44 @@ describe('RestaurantRegionsContainer', () => {
         ],
       }));
 
-      const { getByText, queryByText } = render(<RestaurantRegionsContainer />);
-
-      expect(getByText('서울')).not.toBeNull();
-      expect(getByText('대전')).not.toBeNull();
-      });
-    });
-  });
-  
-  context('when click region value', () => {
-    it('change region', () => {
-      const dispatch = jest.fn();
-
-      useDispatch.mockImplementation(() => dispatch);
-
-      useSelector.mockImplementation((selector) => selector({
-        regions: [
-          {
-            id: 1,
-            name: '서울',
-          },
-          {
-            id: 2,
-            name: '대전',
-          },
-        ],
-      }));
-
       const { getByText } = render(<RestaurantRegionsContainer />);
 
       expect(getByText('서울')).not.toBeNull();
+      expect(getByText('대전')).not.toBeNull();
+    });
+  });
+});
 
-      fireEvent.click(getByText('서울'));
+context('when click region value', () => {
+  it('change region', () => {
+    const dispatch = jest.fn();
 
-      expect(dispatch).toBeCalledWith({
-        type: 'changeRegion',
-        payload: {
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      regions: [
+        {
+          id: 1,
           name: '서울',
         },
-      });
+        {
+          id: 2,
+          name: '대전',
+        },
+      ],
+    }));
+
+    const { getByText } = render(<RestaurantRegionsContainer />);
+
+    expect(getByText('서울')).not.toBeNull();
+
+    fireEvent.click(getByText('서울'));
+
+    expect(dispatch).toBeCalledWith({
+      type: 'changeRegion',
+      payload: {
+        name: '서울',
+      },
     });
   });
 });
