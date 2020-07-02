52964fe557d9d04de0e01ff7095426cedab251e2
KiChang Shin <kc7851@gmail.com>
2020-07-03 00:31:25 +0900

    Restaurants test 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Regions from './Regions';
 import Categories from './Categories';
+import Restaurants from './Restaurants';
 
 export default function App() {
   return (
     <div>
       <Regions />
       <Categories />
+      <Restaurants />
     </div>
   );
 }
---
+++ src/Restaurants.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Regions() {
+  return (
+    <div>
+      <ul>
+        <li>밀면넘어져요</li>
+      </ul>
+    </div>
+  );
+}
