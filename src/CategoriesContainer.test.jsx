ba5d3a689981e1725727d2bfc631f05e47bb094e
daadaadaah <daadaadaah@gmail.com>
2020-07-03 06:08:57 +0900

    [Red] Add a CategoriesContainer test for init render

---
+++ src/CategoriesContainer.test.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { useSelector } from 'react-redux';
+
+import { render } from '@testing-library/react';
+
+import { categories } from '../__fixture__/data';
+
+jest.mock('react-redux');
+
+describe('<CategoriesContainer />', () => {
+  describe('render CategoriesContainer', () => {
+    it('shows categories', () => {
+      useSelector.mockImplementation((selector) => selector({
+        categories,
+        selectedCategory: '',
+      }));
+
+      const { queryByRole } = render(<CategoriesContainer />);
+
+      categories.forEach((category) => {
+        expect(queryByRole('button', { name: category.name })).not.toBeNull();
+      });
+    });
+  });
+
+  // context('when the user selects category', () => {
+  //   it('run selectCategory action', () => {});
+  // });
+});
