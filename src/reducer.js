1a000916fc269d51f7512ab5e041fae13614164a
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-02 22:48:35 +0900

    Update reducer to cover setRegions action

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,18 @@
 const initialState = {
+  regions: [],
   categories: [],
 };
 
 function reducer(state = initialState, action) {
+  if (action.type === 'setRegions') {
+    const { regions } = action.payload;
+
+    return {
+      ...state,
+      regions,
+    };
+  }
+
   if (action.type === 'setCategories') {
     const { categories } = action.payload;
 
