f54d8450df1acf4e9a48e200938501d804e4394b
KiChang Shin <kc7851@gmail.com>
2020-07-02 22:44:39 +0900

    App test 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 
 export default function App() {
   return (
-    <></>
+    <div>
+      <ul>
+        <li><button type="button">서울</button></li>
+        <li><button type="button">대전</button></li>
+        <li><button type="button">대구</button></li>
+        <li><button type="button">부산</button></li>
+        <li><button type="button">광주</button></li>
+        <li><button type="button">강원도</button></li>
+        <li><button type="button">인천</button></li>
+      </ul>
+    </div>
   );
 }
