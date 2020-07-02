ac3761e064a1af7ffcb4ef6e35890fb17d29da7b
daadaadaah <daadaadaah@gmail.com>
2020-07-03 06:11:52 +0900

    Remove a comment

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,14 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import RegionsContainer from './RegionsContainer';
 
-import { loadRegions, selectCategory, loadRestaurants, loadCategories } from './action';
-
-// const categories = [
-//   { id: 1, name: '한식' },
-//   { id: 2, name: '중식' },
-//   { id: 3, name: '일식' },
-//   { id: 4, name: '양식' },
-//   { id: 5, name: '분식' },
-// ];
+import {
+  loadRegions, selectCategory, loadRestaurants, loadCategories,
+} from './action';
 
 export default function App() {
-  const { selectedRegion, categories, selectedCategory, restaurants } = useSelector(
+  const {
+    selectedRegion, categories, selectedCategory, restaurants,
+  } = useSelector(
     (state) => ({
       selectedRegion: state.selectedRegion,
       categories: state.categories,
