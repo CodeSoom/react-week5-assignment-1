e2e76bca97d0c8ba3b9e356b898fab07c0dc9b57
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-02 21:24:24 +0900

    test(App): can see restaurants
    - 지역과 카테고리가 선택된 경우 레스토랑 목록을 조회하는 테스트 케이스 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,25 +6,56 @@ import { render } from '@testing-library/react';
 
 import App from './App';
 
+import regions from '../fixtures/regions';
+import categories from '../fixtures/categories';
+import restaurants from '../fixtures/restaurants';
+
 jest.mock('react-redux');
 jest.mock('./services/api');
 
-test('App', () => {
-  const dispatch = jest.fn();
+describe('App', () => {
+  it('can see', () => {
+    const dispatch = jest.fn();
 
-  useDispatch.mockImplementation(() => dispatch);
+    useDispatch.mockImplementation(() => dispatch);
 
-  useSelector.mockImplementation((selector) => (selector({
-    regions: [],
-    categories: [],
-  })));
+    useSelector.mockImplementation((selector) => (selector({
+      regions: [],
+      categories: [],
+    })));
 
-  const { queryByText } = render((
-    <App />
-  ));
+    const { queryByText } = render((
+      <App />
+    ));
 
-  expect(queryByText('서울')).toBeNull();
-  expect(queryByText('한식')).toBeNull();
+    expect(queryByText('서울')).toBeNull();
+    expect(queryByText('한식')).toBeNull();
 
-  expect(dispatch).toBeCalledTimes(2);
+    expect(dispatch).toBeCalledTimes(2);
+  });
+
+  context('region and category selected', () => {
+    it('can see restaurants', () => {
+      const dispatch = jest.fn();
+
+      useDispatch.mockImplementation(() => dispatch);
+
+      useSelector.mockImplementation((selector) => (selector({
+        regions,
+        categories,
+        selectedRegion: '서울',
+        selectedCategoryId: 1,
+      })));
+
+      const { queryByText } = render((
+        <App />
+      ));
+
+      expect(queryByText('서울(v)')).not.toBeNull();
+      expect(queryByText('한식(v)')).not.toBeNull();
+      expect(queryByText('베니스')).not.toBeNull();
+
+      expect(dispatch).toBeCalledTimes(3);
+    });
+  });
 });
