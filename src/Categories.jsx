68e2bb9b903a1e38e440a3f53c063c3dda09bcdc
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-02 21:34:02 +0900

    Update Categories to render check sign

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 
-export default function Categories({ categories, onClick }) {
+export default function Categories({
+  categories, categoryId, onClick,
+}) {
+  const isSelected = (id) => id === categoryId;
+
   return (
-    <div>
+    <ul>
       {
         categories.map((category) => (
-          <button
-            type="button"
-            key={category.id}
-            onClick={() => onClick(category.id)}
-          >
-            { category.name }
-          </button>
+          <li key={category.id}>
+            <button
+              type="button"
+              onClick={() => onClick(category.id)}
+            >
+              { category.name }
+              { isSelected(category.id) && '(V)' }
+            </button>
+          </li>
         ))
       }
-    </div>
+    </ul>
   );
 }
