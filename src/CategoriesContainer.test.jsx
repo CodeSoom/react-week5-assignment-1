7b5c1f112045f5575a49c3c927e2598d7620e177
KiChang Shin <kc7851@gmail.com>
2020-07-03 00:57:46 +0900

    CategoriesContainer test 작성

---
+++ src/CategoriesContainer.test.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import CategoriesContainer from './CategoriesContainer';
+
+describe('CategoriesContainer', () => {
+  context('with categories', () => {
+    it('카테고리 항목들이 보인다.', () => {
+      const { getByText } = render(
+        <CategoriesContainer />,
+      );
+
+      expect(getByText('한식')).not.toBeNull();
+      expect(getByText('중식')).not.toBeNull();
+      expect(getByText('일식')).not.toBeNull();
+      expect(getByText('양식')).not.toBeNull();
+      expect(getByText('분식')).not.toBeNull();
+    });
+  });
+});
