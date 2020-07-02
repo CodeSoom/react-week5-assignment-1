1f731167a2b7827c70167a22f4083e3bc9ea09ce
SeaFalcon <seafalcon323@gmail.com>
2020-07-03 02:55:00 +0900

    AddressListContainer selectAddress (버튼 클릭)  테스트 추가

--- src/AddressListContainer.test.jsx
+++ src/AddressListContainer.test.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import AddressListContainer from './AddressListContainer';
 
@@ -11,6 +11,13 @@ import { addressList } from '../__fixture__/restaurants';
 jest.mock('react-redux');
 
 describe('AddressListContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    useDispatch.mockImplementation(() => dispatch);
+    dispatch.mockClear();
+  });
+
   it('레스토랑 지역 목록이 로딩된다.', () => {
     useSelector.mockImplementation((selector) => selector({
       addressList,
@@ -21,5 +28,17 @@ describe('AddressListContainer', () => {
     ));
 
     expect(getByText('서울')).toBeInTheDocument();
+  });
+
+  context('지역 목록 중 하나의 버튼을 클릭하면', () => {
+    it('selectAddress 액션이 전달된다.', () => {
+      const { getByText } = render((
+        <AddressListContainer />
+      ));
+
+      fireEvent.click(getByText('서울'));
+
+      expect(dispatch).toBeCalledWith(selectAddress());
+    });
   });
 });
