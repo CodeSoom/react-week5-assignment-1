0b6820a4dd2f3a9aaa3f96e4d7a1beb5cb147bf1
Kwakcena <rhkrgudwh@gmail.com>
2020-07-02 13:44:45 +0900

    test(green): 선택한 region 담기, dispatch 호출
    선택한 region을 담는 테스트와 dispatch가 호출되는 테스트를 통과한다.

--- src/RestaurantRegions.jsx
+++ src/RestaurantRegions.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
-export default function RestaurantRegions({ regions }) {
+export default function RestaurantRegions(
+  { regions, onClick },
+) {
   return (
     <ul>
       {regions.map(({ id, name }) => (
         <li key={id}>
-          <button type="button">
-            {name}
-          </button>
+          <input
+            type="button"
+            onClick={onClick}
+            value={name}
+          />
         </li>
       ))}
     </ul>
--- src/RestaurantRegionsContainer.jsx
+++ src/RestaurantRegionsContainer.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import RestaurantRegions from './RestaurantRegions';
 
+import {
+  selectRegion,
+} from './actions';
+
 export default function RestaurantRegionsContainer() {
+  const dispatch = useDispatch();
+
   const { regions } = useSelector((state) => ({
     regions: state.regions,
   }));
 
+  function handleClick(event) {
+    dispatch(selectRegion(event.target.value));
+  }
+
   return (
     <>
-      <RestaurantRegions regions={regions} />
+      <RestaurantRegions
+        regions={regions}
+        onClick={handleClick}
+      />
     </>
   );
 }
