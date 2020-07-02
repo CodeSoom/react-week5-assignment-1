017671eb7697ee8a1f3fbb2977f514788b3a309f
morykwon <kwonmory@gmail.com>
2020-07-02 22:46:15 +0900

    RestaurantRegions 코드 정렬

--- src/RestaurantRegions.jsx
+++ src/RestaurantRegions.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-export default function RestaurantRegions({ regions }) {
+export default function RestaurantRegions({ regions, onClick }) {
   return (
     <ul>
-      {regions.map((region) => (<li key={region.id}><button type="button">{region.name}</button></li>))}
+      {regions.map((region) => (
+        <li key={region.id}>
+          <button type="button" onClick={onClick}>{region.name}</button>
+        </li>
+      ))}
     </ul>
   );
 }
