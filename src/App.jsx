5d62d5a6378c362b48fe70371adcc7dd486f215b
daadaadaah <daadaadaah@gmail.com>
2020-07-03 07:31:01 +0900

    [Refactor] Refactor a App for adding RestaurantsContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import React, { useEffect } from 'react';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import RegionsContainer from './RegionsContainer';
 import CategoriesContainer from './CategoriesContainer';
+import RestaurantsContainer from './RestaurantsContainer';
 
 import { loadRegions, loadCategories } from './action';
 
 export default function App() {
-  const { restaurants } = useSelector((state) => ({
-    selectedRegion: state.selectedRegion,
-    restaurants: state.restaurants,
-  }));
-
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -24,11 +20,7 @@ export default function App() {
     <div>
       <RegionsContainer />
       <CategoriesContainer />
-      <ul>
-        {restaurants.map((restaurant) => (
-          <li key={restaurant.id}>{restaurant.name}</li>
-        ))}
-      </ul>
+      <RestaurantsContainer />
     </div>
   );
 }
