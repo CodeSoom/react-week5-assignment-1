0a33465102bca3c2b8d487a9b47568d6db795017
Kwakcena <rhkrgudwh@gmail.com>
2020-07-02 20:35:10 +0900

    test(green): RestaurantCategoriesContainer
    컨테이너에서 핸들러 함수를 전달하여 dispatch가 호출되는지에 대한
    테스트를 통과시킴.

--- src/RestaurantCategoriesContainer.jsx
+++ src/RestaurantCategoriesContainer.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import RestaurantCategories from './RestaurantCategories';
 
+import {
+  selectCategory,
+} from './actions';
+
 export default function RestaurantCategoriesContainer() {
+  const dispatch = useDispatch();
+
   const { categories } = useSelector((state) => ({
     categories: state.categories,
   }));
 
+  function handleClick(event) {
+    dispatch(selectCategory(event.target.value));
+  }
+
   return (
-    <RestaurantCategories categories={categories} />
+    <RestaurantCategories
+      categories={categories}
+      onClick={handleClick}
+    />
   );
 }
