d9c19bd3003e39caae21dd0181f15167efae84f7
morykwon <kwonmory@gmail.com>
2020-07-03 01:30:15 +0900

    RestaurantCategories  내용 구현(V 표시)

--- src/RestaurantCategories.jsx
+++ src/RestaurantCategories.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-export default function RestaurantCategories({ categories, onClick }) {
+export default function RestaurantCategories({ categories, onClick, selected }) {
   return (
     <ul>
       {categories.map((category) => (
         <li key={category.id}>
-          <button type="button" onClick={onClick} data-name={category.name}>{category.name}</button>
+          <button type="button" onClick={onClick} data-name={category.name}>
+            {category.name}
+            {selected && category.name === selected && '(V)'}
+          </button>
         </li>
       ))}
     </ul>
