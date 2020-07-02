eb04a63fd5825a9425c3365e0e720f37b033d515
morykwon <kwonmory@gmail.com>
2020-07-03 01:21:53 +0900

    App을 구현한다

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,17 @@ import { useDispatch } from 'react-redux';
 import RestaurantCategoriesContainer from './RestaurantCategoriesContainer';
 import RestaurantRegionsContainer from './RestaurantRegionsContainer';
 
-import { setRegions, setCategories } from './actions';
+import {
+  loadRegions, loadCategories,
+} from './actions';
 
 export default function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     // TODO 지역, 카테고리 불러오기
-    const regions = [
-      {
-        id: 1,
-        name: '서울',
-      },
-      {
-        id: 2,
-        name: '대전',
-      },
-    ];
-
-    dispatch(setRegions(regions));
-    dispatch(setCategories(regions));
+    dispatch(loadRegions());
+    dispatch(loadCategories());
   }, []);
 
   return (
