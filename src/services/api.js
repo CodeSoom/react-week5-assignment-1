9f6a00c86fd4dad0a1ed1e01e627256e443c7734
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-02 22:14:02 +0900

    Fix fetchCategories url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 export async function fetchCategories() {
-  const url = 'https://eatgo-customer-api.ahastudio.com/regions';
+  const url = 'https://eatgo-customer-api.ahastudio.com/categories';
   const response = await fetch(url);
   const data = await response.json();
   return data;
