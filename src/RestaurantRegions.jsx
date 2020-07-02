d64229db21d8a7dbbd196d8d21b87f957313585c
morykwon <kwonmory@gmail.com>
2020-07-03 01:34:19 +0900

    RestaurantRegions 내용 수정(V 표시)

--- src/RestaurantRegions.jsx
+++ src/RestaurantRegions.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-export default function RestaurantRegions({ regions, onClick }) {
+export default function RestaurantRegions({ regions, onClick, selected }) {
   return (
     <ul>
       {regions.map((region) => (
         <li key={region.id}>
-          <button type="button" onClick={onClick} data-name={region.name}>{region.name}</button>
+          <button
+            type="button"
+            onClick={onClick}
+            data-name={region.name}
+          >
+            {region.name}
+            {selected && selected === region.name && '(V)'}
+          </button>
         </li>
       ))}
     </ul>
