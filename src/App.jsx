7967c5d300c470e32f759a1b539dbb920298b912
KiChang Shin <kc7851@gmail.com>
2020-07-02 23:02:28 +0900

    App 리팩토링
    Categories component를 사용합니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 
 import Regions from './Regions';
+import Categories from './Categories';
 
 export default function App() {
   return (
     <div>
       <Regions />
-      <div>
-        <ul>
-          <li><button type="button">한식</button></li>
-          <li><button type="button">중식</button></li>
-          <li><button type="button">일식</button></li>
-          <li><button type="button">양식</button></li>
-          <li><button type="button">분식</button></li>
-        </ul>
-      </div>
+      <Categories />
     </div>
   );
 }
