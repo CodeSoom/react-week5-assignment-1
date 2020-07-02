ed5f5e94754bd42b03353cdb35120734645cd40c
daadaadaah <daadaadaah@gmail.com>
2020-07-03 08:20:53 +0900

    Remove a comment

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,16 +1,3 @@
-/**
- * # 과제 5의 요구사항
- * 1. 카테고리
- * - 카테고리 목록 보기
- * - 카테고리 선택
- *
- * 2. 지역
- * - 지역 목록 보기
- * - 지역 선택
- *
- * 3. 레스토랑
- * - 카테고리 및 지역에 따른 레스토랑 목록 보기
- */
 import React from 'react';
 
 import { useSelector, useDispatch } from 'react-redux';
@@ -147,29 +134,3 @@ describe('<App />', () => {
     });
   });
 });
-
-// TODO : App TEST
-// context('with a selected region &  with a selected category', () => {
-//   it('shows a mark for a selected category', () => {});
-//   it('shows restaurants', () => {});
-// });
-// ----- end : App
-
-// describe('<Categories />', () => {
-//   context('render Categories', () => {
-//     it('shows categories', () => {});
-//   });
-
-//   context('with a selected category', () => {
-//     it('shows a mark for a selected category', () => {});
-//   });
-// });
-
-// describe('<Restaurants />', () => {
-//   context('render Restaurants', () => {
-//     it('does not show restaurants', () => {});
-//   });
-//   context('with a selected region &  with a selected category', () => {
-//     it('shows restaurants', () => {});
-//   });
-// });
