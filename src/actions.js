998fd39271e5a9961fa541fbb587316ab8c2970d
morykwon <kwonmory@gmail.com>
2020-07-03 01:15:29 +0900

    thunk 액션 함수 추가

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,5 @@
+import { fetchRegions, fetchCategories } from './services/api';
+
 export const changeRegion = ({ name }) => ({
   type: 'changeRegion',
   payload: {
@@ -21,3 +23,13 @@ export const setRegions = (regions) => ({
   type: 'setRegions',
   payload: regions,
 });
+
+export const loadCategories = () => async (dispatch) => {
+  const categories = await fetchCategories();
+  dispatch(setCategories(categories));
+};
+
+export const loadRegions = () => async (dispatch) => {
+  const regions = await fetchRegions();
+  dispatch(setRegions(regions));
+};
