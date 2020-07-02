e0233d22922592bb1703b330e8b7eb28e4f9e38e
SeaFalcon <seafalcon323@gmail.com>
2020-07-02 16:46:04 +0900

    App 초기화면 로딩 테스트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,20 @@ import React from 'react';
 
 export default function App() {
   return (
-    <></>
+    <div>
+      {/* Address List */}
+      <ul>
+        <li>
+          <button type="button">서울</button>
+        </li>
+      </ul>
+
+      {/* Category List */}
+      <ul>
+        <li>
+          <button type="button">한식</button>
+        </li>
+      </ul>
+    </div>
   );
 }
