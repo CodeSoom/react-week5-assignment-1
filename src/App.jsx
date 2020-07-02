2821519571d470fb1fb6955599c6ba31ce76515a
newoo <newoo4297@naver.com>
2020-07-03 00:17:12 +0900

    Update App to pass test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import RegionsContainer from './RegionsContainer';
+import CategoriesContainer from './CategoriesContainer';
+
 export default function App() {
   return (
-    <></>
+    <div>
+      <h1>Restaurants</h1>
+      <RegionsContainer />
+      <CategoriesContainer />
+    </div>
   );
 }
