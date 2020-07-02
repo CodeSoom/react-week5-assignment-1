b690740a6706b480da671c70a973dd82e766b952
KiChang Shin <kc7851@gmail.com>
2020-07-02 23:01:52 +0900

    Categories test 구현

---
+++ src/Categories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Categories() {
+  return (
+    <div>
+      <ul>
+        <li><button type="button">한식</button></li>
+        <li><button type="button">중식</button></li>
+        <li><button type="button">일식</button></li>
+        <li><button type="button">양식</button></li>
+        <li><button type="button">분식</button></li>
+      </ul>
+    </div>
+  );
+}
