f28bfe765079a49f72fcf0908be7aece0fe1c1b3
Kwakcena <rhkrgudwh@gmail.com>
2020-07-02 13:24:46 +0900

    test(red): select region
    지역 선택에 따라 체크 표시가 되는 테스트를 하기 전에 reducer로 선택된
    지역을 갖게 되는 테스트를 해야 한다고 생각함

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -35,4 +35,10 @@ describe('reducer', () => {
     const state = reducer(initialState, setCategories(categories));
     expect(state.categories).toHaveLength(2);
   });
+
+  it('select region', () => {
+    const region = '서울';
+    const state = reducer(initialState, selectRegion(region));
+    expect(state.region).toBe('서울');
+  });
 });
