636847ead8fff44dda9bd2d7058405deb92e1917
daadaadaah <daadaadaah@gmail.com>
2020-07-03 01:49:44 +0900

    [Green] Implement a action for loadCategories

--- src/action.test.js
+++ src/action.test.js
@@ -6,21 +6,43 @@ import { loadRegions, setInitRegions } from './action';
 
 import { regions, categories } from '../__fixture__/data';
 
+import { fetchInitCategories } from './services/api';
+
 const middlewares = [thunk]; // add your middlewares like `redux-thunk`
 const mockStore = configureStore(middlewares);
 
-global.fetch = jest.fn(() => Promise.resolve({
-  json: () => Promise.resolve(regions),
-}));
+function mockfetch(data) {
+  global.fetch = jest.fn(() => Promise.resolve({
+    json: () => Promise.resolve(data),
+  }));
 
-describe('acton', () => {
   beforeEach(() => {
     fetch.mockClear();
   });
+}
 
+function setInitCategories(initCategories) {
+  return {
+    type: 'setInitCategories',
+    payload: {
+      regions: initCategories,
+    },
+  };
+}
+
+function loadCategories() {
+  return async (dispatch) => {
+    const initCategories = await fetchInitCategories();
+    dispatch(setInitCategories(initCategories));
+  };
+}
+
+describe('acton', () => {
   describe('loadRegions', () => {
     it('load Regions', () => {
       const store = mockStore({});
+
+      mockfetch(regions);
 
       return store.dispatch(loadRegions()).then(() => {
         const actions = store.getActions();
@@ -33,15 +55,12 @@ describe('acton', () => {
     it('load Categories', () => {
       const store = mockStore({});
 
+      mockfetch(categories);
+
       return store.dispatch(loadCategories()).then(() => {
         const actions = store.getActions();
         expect(actions[0]).toEqual(setInitCategories(categories));
       });
     });
   });
-
-
-
-
-
 });
