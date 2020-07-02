02f5168da0b520dfa9a21115c24abf18cace81dd
morykwon <kwonmory@gmail.com>
2020-07-02 22:53:14 +0900

    RestaurantRegionsContainer에 대한 테스트를 추가한다

---
+++ src/RestaurantRegionsContainer.test.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import { useDispatch, useSelector } from 'react-redux';
+import RestaurantRegionsContainer from './RestaurantRegionsContainer';
+
+jest.mock('react-redux');
+describe('RestaurantRegionsContainer', () => {
+  context('with fetch data', () => {
+    it('renders regions', () => {
+      const dispatch = jest.fn();
+
+      useDispatch.mockImplementation(() => dispatch);
+
+      useSelector.mockImplementation((selector) => selector({
+        regions: [
+          {
+            id: 1,
+            name: '서울',
+          },
+          {
+            id: 2,
+            name: '대전',
+          },
+        ],
+      }));
+
+      const { getByText } = render(<RestaurantRegionsContainer />);
+
+      expect(getByText('서울')).not.toBeNull();
+
+      fireEvent.click(getByText('서울'));
+
+      expect(dispatch).toBeCalledWith({
+        type: 'changeRegion',
+        payload: {
+          name: '서울',
+        },
+      });
+    });
+  });
+});
