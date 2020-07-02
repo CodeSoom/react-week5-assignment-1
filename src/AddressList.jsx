5ab9269b4eba9657948f909c0af5a0b9cf381711
SeaFalcon <seafalcon323@gmail.com>
2020-07-03 02:43:14 +0900

    AddressList 버튼 클릭에 대한 테스트 구현

--- src/AddressList.jsx
+++ src/AddressList.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function AddressList({ addressList }) {
+export default function AddressList({ addressList, onClick }) {
   return (
     <ul>
       {addressList.map((address) => (
         <li key={address.id}>
-          <button type="button">{address.name}</button>
+          <button type="button" onClick={onClick}>{address.name}</button>
         </li>
       ))}
     </ul>
