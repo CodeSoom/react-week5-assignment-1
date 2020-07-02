e277dcaacf74280986723ffac6118b7074ac6470
daadaadaah <daadaadaah@gmail.com>
2020-07-03 02:15:51 +0900

    [Green] Add a App for loadCategories

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,11 @@ export default function App() {
   return (
     <div>
       <RegionsContainer />
+      <button type="button">한식</button>
+      <button type="button">중식</button>
+      <button type="button">일식</button>
+      <button type="button">양식</button>
+      <button type="button">분식</button>
     </div>
   );
 }
