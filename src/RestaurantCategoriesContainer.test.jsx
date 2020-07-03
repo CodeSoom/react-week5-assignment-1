fd60edf6d679aa8b25796ccdb4099c5c84be53e7
Kwakcena <rhkrgudwh@gmail.com>
2020-07-04 05:04:53 +0900

    test(red)-카테고리 컨테이너 dispatch 호출 테스트
    카테고리 컨테이너에서 분류를 클릭시 dispatch가 호출되는지 테스트 한다.

--- src/RestaurantCategoriesContainer.test.jsx
+++ src/RestaurantCategoriesContainer.test.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import RestaurantCategoriesContainer from './RestaurantCategoriesContainer';
 
 import { categories } from '../fixtures/restaurants';
+import {
+  setCategory,
+} from './actions';
+
+jest.mock('react-redux');
 
 describe('RestaurantCategoriesContainer', () => {
+  const dispatch = jest.fn();
   beforeEach(() => {
+    useDispatch.mockImplementation(() => dispatch);
     useSelector.mockImplementation((selector) => selector({
       categories,
     }));
@@ -23,5 +30,14 @@ describe('RestaurantCategoriesContainer', () => {
     categories.forEach(({ name }) => {
       expect(getByText(name)).toBeInTheDocument();
     });
+  });
+
+  it('분류 중 하나를 클릭 시 dispatch가 호출된다.', () => {
+    const { getByText } = render((
+      <RestaurantCategoriesContainer />
+    ));
+
+    fireEvent.click(getByText('한식'));
+    expect(dispatch).toBeCalledWith(setCategory('한식'));
   });
 });
