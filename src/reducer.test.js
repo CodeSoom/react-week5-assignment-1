60a0cd9f3a068d5d393e761030499316f820ad61
jiunkoo <jiunkoo0106@gmail.com>
2020-07-04 05:03:48 +0900

    reducer - setCategories 테스트 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,4 +1,19 @@
 import reducer from './reducer';
 
-test('reducer', () => {
+import {
+  setCategories,
+} from './actions';
+
+import categories from '../fixtures/categories';
+
+describe('setCategories', () => {
+  it('changes categories', () => {
+    const initialState = {
+      categories: [],
+    };
+
+    const state = reducer(initialState, setCategories(categories));
+
+    expect(state.categories).toHaveLength(1);
+  });
 });
