8ff72f2b394cc418cb998b79dadc7396f20a0164
SeaFalcon <seafalcon323@gmail.com>
2020-07-03 11:24:36 +0900

    CategoryList 버튼 클릭 이벤트 테스트 추가

--- src/CategoryList.test.jsx
+++ src/CategoryList.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import CategoryList from './CategoryList';
 
@@ -13,5 +13,20 @@ describe('CategoryList', () => {
     ));
 
     expect(getByText('한식')).toBeInTheDocument();
+  });
+
+  it('각각의 카테고리 버튼을 클릭할 수 있다.', () => {
+    const handleClick = jest.fn();
+
+    const { getByText } = render((
+      <CategoryList
+        categoryList={categoryList}
+        onClick={handleClick}
+      />
+    ));
+
+    fireEvent.click(getByText(/한식/));
+
+    expect(handleClick).toBeCalled();
   });
 });
