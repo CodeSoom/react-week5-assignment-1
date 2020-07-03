f264c80cd5401c555de2051059de3ad505ccc08f
jiunkoo <jiunkoo0106@gmail.com>
2020-07-04 07:28:56 +0900

    api - fetchRegions api 구현

--- src/services/api.js
+++ src/services/api.js
@@ -4,3 +4,10 @@ export async function fetchCategories() {
   const data = await response.json();
   return data;
 }
+
+export async function fetchRegions() {
+  const url = 'https://eatgo-customer-api.ahastudio.com/regions';
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
