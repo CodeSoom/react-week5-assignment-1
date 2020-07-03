5c01a95e6c0e9374a053ecf4f350a00855f89c04
Kwakcena <rhkrgudwh@gmail.com>
2020-07-03 17:13:07 +0900

    가짜 useSelector를 사용해서 테스트를 통과함.
    현재 RestaurantRegionsContainer는 useSelector를 사용하도록 구현되어 있지
    않다. 그래서 이를 사용하기 위해 테스트에서 먼저 useSelector에 대해
    사용하는 코드를 작성하고 가짜를 가져다가 썻다.
    
    내 생각에는 이 테스트가 깨져야 하는데 왜 통과되는것인지 모르겠다.

--- src/RestaurantRegionsContainer.test.jsx
+++ src/RestaurantRegionsContainer.test.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import RestaurantRegionsContainer from './RestaurantRegionsContainer';
 
 import { regions } from '../fixtures/restaurants';
 
+jest.mock('react-redux');
+
 describe('RestaurantRegionsContainer', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      regions,
+    }));
+  });
+
   it('show restaurant regions', () => {
     const { getByText } = render((
       <RestaurantRegionsContainer />
