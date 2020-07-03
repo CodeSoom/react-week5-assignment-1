16a8aea126162fc5f36402b7151412fea1bc3a30
KiChang Shin <kc7851@gmail.com>
2020-07-03 23:39:52 +0900

    App test 리팩토링
    useDispatch, useSelector를 mocking하도록 하였습니다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import App from './App';
 
+jest.mock('react-redux');
+
 describe('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    selected: {
+
+    },
+  }));
+
   it('지역이 보인다.', () => {
     const { getByText } = render(
       <App />,
