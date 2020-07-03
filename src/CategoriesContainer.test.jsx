b09316018b814526cf1fcb4f07eeae2f498105e3
KiChang Shin <kc7851@gmail.com>
2020-07-03 23:17:28 +0900

    CategoriesContainer test 작성

--- src/CategoriesContainer.test.jsx
+++ src/CategoriesContainer.test.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { useDispatch } from 'react-redux';
+
+import { render, fireEvent } from '@testing-library/react';
 
 import CategoriesContainer from './CategoriesContainer';
 
+jest.mock('react-redux');
+
 describe('CategoriesContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   context('with categories', () => {
     it('카테고리 항목들이 보인다.', () => {
       const { getByText } = render(
@@ -16,6 +24,18 @@ describe('CategoriesContainer', () => {
       expect(getByText(/일식/)).not.toBeNull();
       expect(getByText(/양식/)).not.toBeNull();
       expect(getByText(/분식/)).not.toBeNull();
+    });
+  });
+
+  context('when click category', () => {
+    it('dispatch가 실행된다.', () => {
+      const { getByText } = render(
+        <CategoriesContainer />,
+      );
+
+      fireEvent.click(getByText(/한식/));
+
+      expect(dispatch).toBeCalled();
     });
   });
 });
