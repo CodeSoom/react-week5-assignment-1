e7cc02628520d59ab61a5f9005ef258cf0de0f3e
Kwakcena <rhkrgudwh@gmail.com>
2020-07-04 02:36:07 +0900

    test(green)-App: 분류 보이기 통과

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,13 @@ export default function App() {
   return (
     <>
       <RestaurantRegionsContainer />
+      <ul>
+        <li>한식</li>
+        <li>중식</li>
+        <li>일식</li>
+        <li>양식</li>
+        <li>분식</li>
+      </ul>
     </>
   );
 }
