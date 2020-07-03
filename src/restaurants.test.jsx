3557e8022e46df1199f41a6c03e6994b646ea677
SeaFalcon <seafalcon323@gmail.com>
2020-07-03 14:18:47 +0900

    restaurants  레스토랑 데이터 유무에 따라 목록 표시 테스트 추가, 구현

--- src/restaurants.test.jsx
+++ src/restaurants.test.jsx
@@ -5,20 +5,34 @@ import { render } from '@testing-library/react';
 import Restaurants from './Restaurants';
 
 describe('Restaurants', () => {
-  it('레스토랑 목록이 표시된다.', () => {
-    const restaurants = [
-      {
-        id: 1,
-        categoryId: 1,
-        name: '양천주가',
-        address: '서울 강남구',
-      },
-    ];
+  context('레스토랑이 있으면', () => {
+    it('레스토랑 목록이 표시된다.', () => {
+      const restaurants = [
+        {
+          id: 1,
+          categoryId: 1,
+          name: '양천주가',
+          address: '서울 강남구',
+        },
+      ];
 
-    const { getByText } = render((
-      <Restaurants restaurants={restaurants} />
-    ));
+      const { getByText } = render((
+        <Restaurants restaurants={restaurants} />
+      ));
 
-    expect(getByText('양천주가')).toBeInTheDocument();
+      expect(getByText('양천주가')).toBeInTheDocument();
+    });
+  });
+
+  context('레스토랑이 없으면', () => {
+    it('레스토랑 목록이 표시되지 않는다.', () => {
+      const restaurants = [];
+
+      const { container } = render((
+        <Restaurants restaurants={restaurants} />
+      ));
+
+      expect(container).toHaveTextContent('');
+    });
   });
 });
