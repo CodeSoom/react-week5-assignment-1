d803931d37dd16cbea50d56f7371da250ee97d45
KiChang Shin <kc7851@gmail.com>
2020-07-04 01:14:23 +0900

    RegionsContainer test 구현
    Container에서 Rigions에 넘겨주는 regions를 useSelector로 store에서 받도록 변경했습니다.

--- src/RegionsContainer.jsx
+++ src/RegionsContainer.jsx
@@ -11,19 +11,10 @@ import {
 export default function RegionsContainer() {
   const dispatch = useDispatch();
 
-  const { region } = useSelector((state) => ({
+  const { region, regions } = useSelector((state) => ({
     region: state.selected.region,
+    regions: state.regions,
   }));
-
-  const regions = [
-    { id: 1, name: '서울' },
-    { id: 2, name: '대전' },
-    { id: 3, name: '대구' },
-    { id: 4, name: '부산' },
-    { id: 5, name: '광주' },
-    { id: 6, name: '강원도' },
-    { id: 7, name: '인천' },
-  ];
 
   function handleClick(type, name) {
     dispatch(updateSelected(type, name));
--- src/reducer.js
+++ src/reducer.js
@@ -10,6 +10,15 @@ const initialState = {
     { id: 4, name: '양식' },
     { id: 5, name: '분식' },
   ],
+  regions: [
+    { id: 1, name: '서울' },
+    { id: 2, name: '대전' },
+    { id: 3, name: '대구' },
+    { id: 4, name: '부산' },
+    { id: 5, name: '광주' },
+    { id: 6, name: '강원도' },
+    { id: 7, name: '인천' },
+  ],
 };
 
 export default function reducer(state = initialState, action) {
