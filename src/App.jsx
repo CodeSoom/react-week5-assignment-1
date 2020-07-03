efd1197e7d2465be05ffa701c044ad2c9caa3daa
Kwakcena <rhkrgudwh@gmail.com>
2020-07-04 02:28:35 +0900

    test(green)-reducer: 비동기 액션 테스트 통과

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import { useDispatch } from 'react-redux';
 import RestaurantRegionsContainer from './RestaurantRegionsContainer';
 
+import {
+  loadRegions,
+} from './actions';
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRegions());
+  }, []);
+
   return (
     <>
       <RestaurantRegionsContainer />
