eb63c2d3f6cbc0210365022ec08f700f95d8c016
SeaFalcon <seafalcon323@gmail.com>
2020-07-03 14:02:42 +0900

    restaurants  레스토랑 목록 표시 테스트 추가

---
+++ src/restaurants.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+describe('Restaurants', () => {
+  it('레스토랑 목록이 표시된다.', () => {
+    const { getByText } = render((
+      <Restaurants />
+    ));
+
+    expect(getByText('양천주가')).toBeInTheDocument();
+  });
+});
