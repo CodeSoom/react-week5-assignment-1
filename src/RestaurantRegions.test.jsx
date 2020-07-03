066103884d99179002441c105baf7cd7cf0e41b5
Kwakcena <rhkrgudwh@gmail.com>
2020-07-04 03:29:37 +0900

    test(red)-RestaurantRegions
    지역 목록 중 하나를 클릭하면 onClick 이벤트가 호출되는지 테스트 함

--- src/RestaurantRegions.test.jsx
+++ src/RestaurantRegions.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import RestaurantRegions from './RestaurantRegions';
 
@@ -15,5 +15,17 @@ describe('RestaurantRegions', () => {
     regions.forEach(({ name }) => {
       expect(getByText(name)).toBeInTheDocument();
     });
+  });
+
+  it('지역 클릭 시 이벤트 함수가 호출된다.', () => {
+    const handleClick = jest.fn();
+    const { getByText } = render((
+      <RestaurantRegions
+        regions={regions}
+        onClick={handleClick}
+      />
+    ));
+    fireEvent.click(getByText('서울'));
+    expect(handleClick).toBeCalled();
   });
 });
