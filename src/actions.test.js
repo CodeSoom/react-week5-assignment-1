522becea04e35ed5e17ba7df7f07ae93d1a910e7
jiunkoo <jiunkoo0106@gmail.com>
2020-07-04 08:01:34 +0900

    actions - setRegions, loadRegions 테스트 추가

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,14 +1,32 @@
 import {
+  setRegions,
+  loadRegions,
   setCategories,
   loadCategories,
 } from './actions';
 
 import categories from '../fixtures/categories';
+import regions from '../fixtures/regions';
 
 jest.mock('react-redux');
 jest.mock('./services/api');
 
 describe('actions', () => {
+  test('setRegions', () => {
+    const action = setRegions(regions);
+
+    expect(action.type).toBe('setRegions');
+    expect(action.payload.regions).toBe(regions);
+  });
+
+  test('loadRegions', async () => {
+    const dispatch = jest.fn();
+    
+    await loadRegions()(dispatch);
+
+    expect(dispatch).toBeCalledTimes(1);
+  });
+
   test('setCategories', () => {
     const action = setCategories(categories);
 
