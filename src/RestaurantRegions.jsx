e379bf2a5b75264d3d56bb8ac931cfb8fdc8256c
Kwakcena <rhkrgudwh@gmail.com>
2020-07-04 03:31:01 +0900

    test(green)-onClick 이벤트 호출 테스트 통과

--- src/RestaurantRegions.jsx
+++ src/RestaurantRegions.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-export default function RestaurantRegions({ regions }) {
+export default function RestaurantRegions({ regions, onClick }) {
   return (
     <ul>
       {regions.map(({ id, name }) => (
         <li key={id}>
           <input
             type="button"
+            onClick={onClick}
             value={name}
           />
         </li>
