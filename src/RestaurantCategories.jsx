f85e3a2910d7d5c3f079f4f5f3e8541e3bcc2e1e
Kwakcena <rhkrgudwh@gmail.com>
2020-07-04 05:02:30 +0900

    test(green)- 이벤트 핸들러 호출 테스트 통과

--- src/RestaurantCategories.jsx
+++ src/RestaurantCategories.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-export default function RestaurantCategories({ categories }) {
+export default function RestaurantCategories({ categories, onClick }) {
   return (
     <ul>
       {categories.map(({ id, name }) => (
         <li key={id}>
           <input
             type="button"
+            onClick={() => onClick(id)}
             value={name}
           />
         </li>
