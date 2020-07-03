b9606df60e391bb6e3acacd337f89c0ce45c6db3
SeaFalcon <seafalcon323@gmail.com>
2020-07-03 11:26:27 +0900

    CategoryList 버튼 클릭 이벤트 테스트 구현

--- src/CategoryList.jsx
+++ src/CategoryList.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default function CategoryList({ categoryList }) {
+export default function CategoryList({ categoryList, onClick }) {
   return (
     <ul>
       {categoryList.map((category) => (
         <li key={category.id}>
-          <button type="button">{category.name}</button>
+          <button type="button" onClick={onClick}>{category.name}</button>
         </li>
       ))}
     </ul>
