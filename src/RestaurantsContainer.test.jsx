199a2230259971cf5c8bd32dfef80af1c4013cda
SeaFalcon <seafalcon323@gmail.com>
2020-07-03 14:27:38 +0900

    RestaurantsContainer 레스토랑 목록 표시 테스트 구현

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
+jest.mock('react-redux');
+
 describe('RestaurantsContainer', () => {
   it('레스토랑 목록이 표시된다.', () => {
+    useSelector.mockImplementation((selector) => selector({
+      restaurants: [{
+        id: 1,
+        categoryId: 1,
+        name: '양천주가',
+        address: '서울 강남구',
+      }],
+    }));
+
     const { getByText } = render((
       <RestaurantsContainer />
     ));
