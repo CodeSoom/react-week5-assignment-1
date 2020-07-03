17ab8f7ce2a9b9f9963b154615c4f101ac542181
Kwakcena <rhkrgudwh@gmail.com>
2020-07-04 03:08:34 +0900

    test(green)-loadCategories
    서버로부터 분류 목록 불러오기 테스트 통과

--- src/services/api.js
+++ src/services/api.js
@@ -4,3 +4,10 @@ export async function fetchRegions() {
   const data = await response.json();
   return data;
 }
+
+export async function fetchCategories() {
+  const url = 'https://eatgo-customer-api.ahastudio.com/categories';
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
