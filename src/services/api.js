f2f00f51616d2a16daf31b234fc8065ca1d33fb6
Kwakcena <rhkrgudwh@gmail.com>
2020-07-04 02:29:43 +0900

    services 빼먹은 커밋

---
+++ src/services/api.js
@@ -0,0 +1,6 @@
+export async function fetchRegions() {
+  const url = 'https://eatgo-customer-api.ahastudio.com/regions';
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
