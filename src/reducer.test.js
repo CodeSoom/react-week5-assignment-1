4d58ebb830ce96ebc6aa93af5fc10c5b8b2eb1e3
Kwakcena <rhkrgudwh@gmail.com>
2020-07-03 18:23:29 +0900

    test(red)-reducer: 지역 정보를 set 한다.
    서버로부터 지역 목록을 받아오면 이를 regions 배열에 담는 setRegions
    액션에 대한 테스트를 진행한다.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,10 +1,20 @@
 import reducer from './reducer';
 
-import { initialState } from '../fixtures/restaurants';
+import { regions, initialState } from '../fixtures/restaurants';
 
 describe('reducer', () => {
   it('처음에는 초기 상태를 반환한다.', () => {
     const state = reducer(undefined);
     expect(state).toEqual(initialState);
+  });
+
+  it('지역 정보를 추가한다.', () => {
+    const state = reducer(initialState, {
+      type: 'setRegions',
+      payload: {
+        regions,
+      },
+    });
+    expect(state.regions).toEqual(regions);
   });
 });
