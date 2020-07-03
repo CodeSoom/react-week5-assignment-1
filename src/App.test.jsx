3840980bb7a8a1deca310537dd59b88719a63138
KiChang Shin <kc7851@gmail.com>
2020-07-04 01:14:48 +0900

    App test 오류 수정
    기본 값을 변경하였습니다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -14,9 +14,23 @@ describe('App', () => {
   useDispatch.mockImplementation(() => dispatch);
 
   useSelector.mockImplementation((selector) => selector({
-    selected: {
-
-    },
+    selected: {},
+    categories: [
+      { id: 1, name: '한식' },
+      { id: 2, name: '중식' },
+      { id: 3, name: '일식' },
+      { id: 4, name: '양식' },
+      { id: 5, name: '분식' },
+    ],
+    regions: [
+      { id: 1, name: '서울' },
+      { id: 2, name: '대전' },
+      { id: 3, name: '대구' },
+      { id: 4, name: '부산' },
+      { id: 5, name: '광주' },
+      { id: 6, name: '강원도' },
+      { id: 7, name: '인천' },
+    ],
   }));
 
   it('지역이 보인다.', () => {
