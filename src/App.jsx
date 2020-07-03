653c459f9420e49b1876ae2c2e548f7c2be0196d
jiunkoo <jiunkoo0106@gmail.com>
2020-07-04 05:21:36 +0900

    App - loadCategories 테스트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
 
 import CategoriesContainer from './CategoriesContainer';
 
+import {
+  setCategories,
+} from './actions';
+
+function loadCategories({ dispatch }) {
+  const categories = [];
+  dispatch(setCategories(categories));
+}
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    loadCategories({ dispatch });
+  }, []);
+
   return (
     <div>
       <CategoriesContainer />
