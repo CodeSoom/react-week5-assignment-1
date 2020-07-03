6b9ebe1fd732a870db2df257604d3a536d264b0b
jiunkoo <jiunkoo0106@gmail.com>
2020-07-04 05:48:03 +0900

    api - fetchCategories api 구현

--- src/services/api.js
+++ src/services/api.js
@@ -1,2 +1,6 @@
-export default function api(){
+export async function fetchCategories() {
+  const url = 'https://eatgo-customer-api.ahastudio.com/categories';
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
