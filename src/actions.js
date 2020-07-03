345efdfbf46f65c58ae300efbe7daa405a528a8b
KiChang Shin <kc7851@gmail.com>
2020-07-03 23:57:38 +0900

    actions 작성

---
+++ src/actions.js
@@ -0,0 +1,12 @@
+export function updateSelected(id) {
+  return {
+    type: 'updateSelected',
+    payload: {
+      categoryId: id,
+    },
+  };
+}
+
+export function xxx() {
+  // TODO: delete this
+}
