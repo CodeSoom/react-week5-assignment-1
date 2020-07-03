399742f24936c06b38cc5c6a46fea1a38eb44fc1
SeaFalcon <seafalcon323@gmail.com>
2020-07-04 03:19:09 +0900

    원격저장소에서 파일명 소문자로 나타나는 현상 수정하기 위한 커밋

--- src/restaurants.jsx
+++ src/restaurants.jsx
@@ -4,7 +4,9 @@ export default function Restaurants({ restaurants }) {
   return (
     <ul>
       {restaurants.map((restaurant) => (
-        <li key={restaurant.id}>{restaurant.name}</li>
+        <li key={restaurant.id}>
+          {restaurant.name}
+        </li>
       ))}
     </ul>
   );
