ff7c4795ddbe8ee94971ca60f304d6ab17da23fd
Kwakcena <rhkrgudwh@gmail.com>
2020-07-04 03:32:49 +0900

    Refactor: props에 onClick 추가

--- src/RestaurantRegionsContainer.jsx
+++ src/RestaurantRegionsContainer.jsx
@@ -5,11 +5,18 @@ import { useSelector } from 'react-redux';
 import RestaurantRegions from './RestaurantRegions';
 
 export default function RestaurantRegionsContainer() {
+  function handleClick() {
+    return {};
+  }
+
   const { regions } = useSelector((state) => ({
     regions: state.regions,
   }));
 
   return (
-    <RestaurantRegions regions={regions} />
+    <RestaurantRegions
+      regions={regions}
+      onClick={handleClick}
+    />
   );
 }
