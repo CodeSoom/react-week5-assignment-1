5ebdfc666d441f7535596389b9aec84d6faa57fb
Kwakcena <rhkrgudwh@gmail.com>
2020-07-04 02:54:54 +0900

    test(green)-RestaurantCategoriesContainer
    레스토랑 카테고리 컨테이너 컴포넌트 구현

---
+++ src/RestaurantCategoriesContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { useSelector } from 'react-redux';
+
+import RestaurantCategories from './RestaurantCategories';
+
+export default function RestaurantCategoriesContainer() {
+  const { categories } = useSelector((state) => ({
+    categories: state.categories,
+  }));
+
+  return (
+    <RestaurantCategories categories={categories} />
+  );
+}
