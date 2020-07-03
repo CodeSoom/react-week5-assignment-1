adf21cfea2731e8cceee782966d4720dbec07cc1
Kwakcena <rhkrgudwh@gmail.com>
2020-07-03 18:26:34 +0900

    test(green)-reducer: set regions
    레스토랑 지역 목록을 regions에 담기

--- src/reducer.js
+++ src/reducer.js
@@ -3,5 +3,11 @@ const initialState = {
 };
 
 export default function reducer(state = initialState, action) {
+  if (action.type === 'setRegions') {
+    return {
+      ...state,
+      regions: action.payload.regions,
+    };
+  }
   return state;
 }
