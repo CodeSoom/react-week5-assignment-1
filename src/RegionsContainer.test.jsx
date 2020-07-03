34b998ec4132db76e31808bef134bd057136808a
KiChang Shin <kc7851@gmail.com>
2020-07-04 01:03:15 +0900

    RegionsContainer test 오류 수정

--- src/RegionsContainer.test.jsx
+++ src/RegionsContainer.test.jsx
@@ -15,7 +15,7 @@ describe('RegionsContainer', () => {
 
   useSelector.mockImplementation((selector) => selector({
     selected: {
-      region: 1,
+      region: '서울',
     },
   }));
 
@@ -25,13 +25,13 @@ describe('RegionsContainer', () => {
         <RegionsContainer />,
       );
 
-      expect(getByText('서울')).not.toBeNull();
-      expect(getByText('대전')).not.toBeNull();
-      expect(getByText('대구')).not.toBeNull();
-      expect(getByText('부산')).not.toBeNull();
-      expect(getByText('광주')).not.toBeNull();
-      expect(getByText('강원도')).not.toBeNull();
-      expect(getByText('인천')).not.toBeNull();
+      expect(getByText(/서울/)).not.toBeNull();
+      expect(getByText(/대전/)).not.toBeNull();
+      expect(getByText(/대구/)).not.toBeNull();
+      expect(getByText(/부산/)).not.toBeNull();
+      expect(getByText(/광주/)).not.toBeNull();
+      expect(getByText(/강원도/)).not.toBeNull();
+      expect(getByText(/인천/)).not.toBeNull();
     });
   });
 
