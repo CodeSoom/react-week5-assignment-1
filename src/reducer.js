43debf3fa2184fbdcaa3d3774d322fc65aea1c7e
jiunkoo <jiunkoo0106@gmail.com>
2020-07-04 08:02:50 +0900

    reducer -setRegions 테스트 구현

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,17 @@
 const initialState = {
+  regions: [],
   categories: [],
 };
 
 export default function reducer(state=initialState, action) {
+  if(action.type === 'setRegions') {
+    const { regions } = action.payload;
+    return {
+      ...state,
+      regions,
+    };
+  }
+
   if(action.type === 'setCategories') {
     const { categories } = action.payload;
     return {
