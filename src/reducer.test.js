f85e8ef64b376e8a068b8adb01ea95b0d2cd30d9
jiunkoo <jiunkoo0106@gmail.com>
2020-07-04 08:02:38 +0900

    reducer - setRegions 테스트 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,19 +1,35 @@
 import reducer from './reducer';
 
 import {
+  setRegions,
   setCategories,
 } from './actions';
 
 import categories from '../fixtures/categories';
+import regions from '../fixtures/regions';
 
-describe('setCategories', () => {
-  it('changes categories', () => {
-    const initialState = {
-      categories: [],
-    };
-
-    const state = reducer(initialState, setCategories(categories));
-
-    expect(state.categories).toHaveLength(1);
+describe('reducer', () => {
+  describe('setRegions', () => {
+    it('changes regions', () => {
+      const initialState = {
+        regions: [],
+      };
+  
+      const state = reducer(initialState, setRegions(regions));
+  
+      expect(state.regions).toHaveLength(1);
+    });
   });
+
+  describe('setCategories', () => {
+    it('changes categories', () => {
+      const initialState = {
+        categories: [],
+      };
+  
+      const state = reducer(initialState, setCategories(categories));
+  
+      expect(state.categories).toHaveLength(1);
+    });
+  });  
 });
