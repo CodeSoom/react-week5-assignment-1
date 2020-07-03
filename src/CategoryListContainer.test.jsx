480f71aa36ab3b398fbc33ef1b8832147e53eff2
SeaFalcon <seafalcon323@gmail.com>
2020-07-03 13:54:07 +0900

    ListContainer 테스트 오류 통과 (useSelector 사용)

--- src/CategoryListContainer.test.jsx
+++ src/CategoryListContainer.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { render, fireEvent } from '@testing-library/react';
 
@@ -8,9 +8,23 @@ import CategoryListContainer from './CategoryListContainer';
 
 import { selectCategory } from './actions';
 
+import { categoryList } from '../__fixture__/restaurants';
+
 jest.mock('react-redux');
 
 describe('CategoryListContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      categoryList,
+    }));
+
+    dispatch.mockClear();
+  });
+
   it('레스토랑 카테고리 목록이 로딩된다.', () => {
     const { getByText } = render((
       <CategoryListContainer />
@@ -21,10 +35,6 @@ describe('CategoryListContainer', () => {
 
   describe('레스토랑 카테고리 목록 중 하나를 클릭하면', () => {
     it('selectCategory 액션이 전달된다.', () => {
-      const dispatch = jest.fn();
-
-      useDispatch.mockImplementation(() => dispatch);
-
       const { getByText } = render((
         <CategoryListContainer />
       ));
