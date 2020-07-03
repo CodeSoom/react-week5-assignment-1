54f290b1e59ecd07623f4400740039bf87e52741
Kwakcena <rhkrgudwh@gmail.com>
2020-07-03 18:11:18 +0900

    test(refactor)-App: App 테스트에서 useSelector 사용
    App 테스트에서도 useSelector를 사용해야 테스트가 통과된다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import App from './App';
+import { useSelector } from 'react-redux';
 
 import { regions } from '../fixtures/restaurants';
 
+import App from './App';
+
 describe('App', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      regions,
+    }));
+  });
+
   it('show restaurant regions', () => {
     const { getByText } = render((
       <App />
