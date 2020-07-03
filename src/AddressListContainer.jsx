9dcdbf658c61f8eb0e801c801c8092fff080b7cf
SeaFalcon <seafalcon323@gmail.com>
2020-07-03 16:40:55 +0900

    addressListContainer에 useEffect 사용하여, loadRestaurants 호출 => 기능 완성

--- src/AddressListContainer.jsx
+++ src/AddressListContainer.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { useSelector, useDispatch } from 'react-redux';
 
 import AddressList from './AddressList';
 
-import { selectAddress } from './actions';
+import { selectAddress, loadRestaurants } from './actions';
+import { categoryList } from '../__fixture__/restaurants';
 
 export default function AddressListContainer() {
   const dispatch = useDispatch();
 
-  const { addressList, selectedAddress } = useSelector((state) => ({
+  const { addressList, selectedAddress, selectedCategory } = useSelector((state) => ({
     addressList: state.addressList,
+    categoryList: state.categoryList,
     selectedAddress: state.selectedAddress,
+    selectedCategory: state.selectedCategory,
   }));
+
+  useEffect(() => {
+    if (selectedAddress && selectedCategory) {
+      dispatch(loadRestaurants({
+        addressName: selectedAddress,
+        categoryId: categoryList.filter((category) => category.name === selectedCategory)[0].id,
+      }));
+    }
+  }, [selectedAddress]);
 
   function handleClick(event) {
     const { target: { id } } = event;
