08b894910df814c11f9a4aab0fc61c7fc0d4e109
KiChang Shin <kc7851@gmail.com>
2020-07-04 17:14:24 +0900

    reducer test 구현

--- src/reducer.js
+++ src/reducer.js
@@ -25,6 +25,7 @@ const initialState = {
 
 export default function reducer(state = initialState, action) {
   const { selected } = state;
+
   if (action.type === 'updateSelected') {
     return {
       ...state,
@@ -35,8 +36,18 @@ export default function reducer(state = initialState, action) {
     };
   }
 
+  if (action.type === 'setRegions') {
+    const { regions } = action.payload;
+
+    return {
+      ...state,
+      regions,
+    };
+  }
+
   if (action.type === 'setCategories') {
     const { categories } = action.payload;
+
     return {
       ...state,
       categories,
