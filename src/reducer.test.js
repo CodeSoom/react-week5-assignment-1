1df0bb393f4a0c645bc376d5b42a2500d223d5e2
Kwakcena <rhkrgudwh@gmail.com>
2020-07-04 13:32:15 +0900

    Refactor: reducer test refactoring

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -23,76 +23,71 @@ describe('reducer', () => {
     expect(state).toEqual(initialState);
   });
 
-  it('지역 정보를 추가한다.', () => {
-    const state = reducer(initialState, setRegions(regions));
-    expect(state.regions).toEqual(regions);
-  });
-
-  it('지역 중 하나를 선택하면 region에 담는다', () => {
-    const state = reducer(initialState, setRegion({
-      id: 1,
-      name: '서울',
-    }));
-    expect(state.region).toStrictEqual({
-      id: 1,
-      name: '서울',
+  context('region', () => {
+    it('지역 정보를 추가한다.', () => {
+      const state = reducer(initialState, setRegions(regions));
+      expect(state.regions).toEqual(regions);
     });
-  });
 
-  it('지역을 선택하면 해당 지역은 체크 표시가 된다.', () => {
-    const state = reducer({
-      regions,
-      region: '',
-    }, setRegion({
-      id: 1,
-      name: '서울',
-    }));
-    expect(state.regions).toEqual(
-      expect.arrayContaining([{ id: 1, name: '서울(V)' }]),
-    );
-  });
-
-  it('should execute fetch regions', () => {
-    const store = mockStore({
-      regions: [],
+    it('지역 중 하나를 선택하면 region에 담는다', () => {
+      const state = reducer(initialState, setRegion(regions[0]));
+      expect(state.region).toStrictEqual(regions[0]);
     });
-    return store.dispatch(loadRegions())
-      .then(() => {
-        const actions = store.getActions();
-        expect(actions[0].type).toEqual('setRegions');
+
+    it('지역을 선택하면 해당 지역은 체크 표시가 된다.', () => {
+      const state = reducer({
+        regions,
+        region: '',
+      }, setRegion(regions[0]));
+      expect(state.regions).toEqual(
+        expect.arrayContaining([{ id: 1, name: '서울(V)' }]),
+      );
+    });
+
+    it('should execute fetch regions', () => {
+      const store = mockStore({
+        regions: [],
       });
-  });
-
-  it('분류 정보를 추가한다', () => {
-    const state = reducer(initialState, setCategories(categories));
-    expect(state.categories).toEqual(categories);
-  });
-
-  it('분류 중 하나를 선택하면 category에 담는다', () => {
-    const state = reducer(initialState, setCategory('한식'));
-    expect(state.category).toBe('한식');
-  });
-
-  it('분류중 선택하면 해당 분류는 체크 표시가 된다.', () => {
-    const state = reducer({
-      categories,
-      category: '',
-    }, setCategory({
-      id: 1,
-      name: '한식',
-    }));
-    expect(state.categories).toEqual(
-      expect.arrayContaining([{ id: 1, name: '한식(V)' }]),
-    );
-  });
-  it('should execute fetch categories', () => {
-    const store = mockStore({
-      categories: [],
+      return store.dispatch(loadRegions())
+        .then(() => {
+          const actions = store.getActions();
+          expect(actions[0].type).toEqual(setRegions().type);
+        });
     });
-    return store.dispatch(loadCategories())
-      .then(() => {
-        const actions = store.getActions();
-        expect(actions[0].type).toEqual('setCategories');
+  });
+
+  context('category', () => {
+    it('분류 정보를 추가한다', () => {
+      const state = reducer(initialState, setCategories(categories));
+      expect(state.categories).toEqual(categories);
+    });
+
+    it('분류 중 하나를 선택하면 category에 담는다', () => {
+      const state = reducer(initialState, setCategory('한식'));
+      expect(state.category).toBe('한식');
+    });
+
+    it('분류중 선택하면 해당 분류는 체크 표시가 된다.', () => {
+      const state = reducer({
+        categories,
+        category: '',
+      }, setCategory({
+        id: 1,
+        name: '한식',
+      }));
+      expect(state.categories).toEqual(
+        expect.arrayContaining([{ id: 1, name: '한식(V)' }]),
+      );
+    });
+    it('should execute fetch categories', () => {
+      const store = mockStore({
+        categories: [],
       });
+      return store.dispatch(loadCategories())
+        .then(() => {
+          const actions = store.getActions();
+          expect(actions[0].type).toEqual(setCategories().type);
+        });
+    });
   });
 });
