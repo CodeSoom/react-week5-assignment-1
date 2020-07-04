ce6f0026f63fc3f2fe64247ed9b919527596e465
daadaadaah <daadaadaah@gmail.com>
2020-07-04 18:13:58 +0900

    [Red] Add a action test for loadRestaurants2

--- src/action.test.js
+++ src/action.test.js
@@ -4,6 +4,7 @@ import {
   loadCategories,
   setInitCategories,
   loadRestaurants,
+  loadRestaurants2,
   setRestaurants,
 } from './action';
 
@@ -87,7 +88,7 @@ describe('acton', () => {
 
     beforeEach(() => {
       dispatch.mockClear();
-      fetchRestaurants.mockResolvedValue(restaurants);
+      fetchRestaurants.mockResolvedValue(restaurants); // TODO : 삭제 필요
     });
 
     context('when successfully fetch data', () => {
@@ -113,5 +114,38 @@ describe('acton', () => {
         expect(dispatch).not.toBeCalled();
       });
     });
+  });
+
+  describe('loadRestaurants2', () => {
+    const dispatch = jest.fn();
+    const getState = jest.fn();
+
+    beforeEach(() => {
+      dispatch.mockClear();
+    });
+
+    context('when successfully fetch data', () => {
+      beforeEach(() => {
+        fetchRestaurants.mockResolvedValue(restaurants);
+      });
+
+      it('dispatch loadCategories', async () => {
+        await loadRestaurants2()(dispatch, getState);
+
+        expect(dispatch).toBeCalledWith(setRestaurants(restaurants));
+      });
+    });
+
+    // context('when fail to fetch data', () => {
+    //   beforeEach(() => {
+    //     fetchRestaurants.mockRejectedValue(new Error('some error'));
+    //   });
+
+    //   it('dispatch loadRestaurants', async () => {
+    //     await loadRestaurants()(dispatch);
+
+    //     expect(dispatch).not.toBeCalled();
+    //   });
+    // });
   });
 });
