2c3077402e298db8227bad955100597aae61cb97
KiChang Shin <kc7851@gmail.com>
2020-07-04 16:53:22 +0900

    App 리팩토링
    initialState에 있는 categories를 없애고 외부의 값으로 categories를 업데이트하도록 변경합니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
 
 import RegionsContainer from './RegionsContainer';
 import CategoriesContainer from './CategoriesContainer';
 import RestaurantsContainer from './RestaurantsContainer';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({
+      type: 'setCategories',
+      payload: {
+        categories: [],
+      },
+    });
+  }, []);
+
   return (
     <div>
       <RegionsContainer />
