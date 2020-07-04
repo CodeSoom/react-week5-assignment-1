d7449315ebbca68fa9eabf51bd6a98cdc4eb0d19
KiChang Shin <kc7851@gmail.com>
2020-07-04 21:58:30 +0900

    api mock 작성
    fetchRestaurants에 대한 mock 함수를 만들었습니다.

--- src/services/__mocks__/api.js
+++ src/services/__mocks__/api.js
@@ -19,3 +19,22 @@ export async function fetchCategories() {
     { id: 5, name: '분식' },
   ];
 }
+
+export async function fetchRestaurants() {
+  return [
+    {
+      id: 1,
+      categoryId: 1,
+      name: '양천주가',
+      address: '서울 강남구',
+      information: '양천주가 in 서울 강남구',
+    },
+    {
+      id: 6,
+      categoryId: 1,
+      name: '한국식 초밥',
+      address: '서울 강남구',
+      information: '한국식 초밥 in 서울 강남구',
+    },
+  ];
+}
