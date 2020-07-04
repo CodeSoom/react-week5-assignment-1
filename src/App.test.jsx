e810a072116e1115c8d19c6c2e0947e1e4be068c
KiChang Shin <kc7851@gmail.com>
2020-07-04 15:15:26 +0900

    App test 리팩토링
    1. fixture를 사용하도록 변경했습니다.
    
    2. 배열의 forEach를 이용해 테스트하도록 변경했습니다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,6 +6,9 @@ import { render } from '@testing-library/react';
 
 import App from './App';
 
+import categories from '../fixtures/categories';
+import regions from '../fixtures/regions';
+
 jest.mock('react-redux');
 
 describe('App', () => {
@@ -14,23 +17,12 @@ describe('App', () => {
   useDispatch.mockImplementation(() => dispatch);
 
   useSelector.mockImplementation((selector) => selector({
-    selected: {},
-    categories: [
-      { id: 1, name: '한식' },
-      { id: 2, name: '중식' },
-      { id: 3, name: '일식' },
-      { id: 4, name: '양식' },
-      { id: 5, name: '분식' },
-    ],
-    regions: [
-      { id: 1, name: '서울' },
-      { id: 2, name: '대전' },
-      { id: 3, name: '대구' },
-      { id: 4, name: '부산' },
-      { id: 5, name: '광주' },
-      { id: 6, name: '강원도' },
-      { id: 7, name: '인천' },
-    ],
+    selected: {
+      region: '',
+      categoryId: '',
+    },
+    categories,
+    regions,
   }));
 
   it('지역이 보인다.', () => {
@@ -38,13 +30,11 @@ describe('App', () => {
       <App />,
     );
 
-    expect(getByText(/서울/)).not.toBeNull();
-    expect(getByText(/대전/)).not.toBeNull();
-    expect(getByText(/대구/)).not.toBeNull();
-    expect(getByText(/부산/)).not.toBeNull();
-    expect(getByText(/광주/)).not.toBeNull();
-    expect(getByText(/강원도/)).not.toBeNull();
-    expect(getByText(/인천/)).not.toBeNull();
+    categories.forEach((category) => {
+      const { name } = category;
+      const regExp = new RegExp(name);
+      expect(getByText(regExp)).not.toBeNull();
+    });
   });
 
   it('카테고리가 보인다.', () => {
@@ -52,10 +42,10 @@ describe('App', () => {
       <App />,
     );
 
-    expect(getByText(/한식/)).not.toBeNull();
-    expect(getByText(/중식/)).not.toBeNull();
-    expect(getByText(/일식/)).not.toBeNull();
-    expect(getByText(/양식/)).not.toBeNull();
-    expect(getByText(/분식/)).not.toBeNull();
+    regions.forEach((region) => {
+      const { name } = region;
+      const regExp = new RegExp(name);
+      expect(getByText(regExp)).not.toBeNull();
+    });
   });
 });
