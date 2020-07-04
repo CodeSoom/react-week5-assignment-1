536680834d944f9a020781dc8554d1ca3d3fd16d
KiChang Shin <kc7851@gmail.com>
2020-07-04 21:00:43 +0900

    recuder 리팩토링
    initialState restaurants을 빈 배열을 주도록 변경하였습니다.

--- src/reducer.js
+++ src/reducer.js
@@ -5,22 +5,7 @@ const initialState = {
   },
   categories: [],
   regions: [],
-  restaurants: [
-    {
-      id: 1,
-      categoryId: 1,
-      name: '양천주가',
-      address: '서울 강남구',
-      information: '양천주가 in 서울 강남구',
-    },
-    {
-      id: 6,
-      categoryId: 1,
-      name: '한국식 초밥',
-      address: '서울 강남구',
-      information: '한국식 초밥 in 서울 강남구',
-    },
-  ],
+  restaurants: [],
 };
 
 export default function reducer(state = initialState, action) {
