e671907055bea5b877d6f6e66ac1e0bbcf3ba263
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-04 12:40:06 +0900

    Add parameter to loadRestaurants

--- src/services/api.js
+++ src/services/api.js
@@ -15,8 +15,7 @@ export async function fetchCategories() {
 }
 
 export async function fetchRestaurants({ regionName, categoryId }) {
-  const url = `https://eatgo-customer-api.ahastudio.com/restaurants
-    ?region=${regionName}&category=${categoryId}`;
+  const url = `https://eatgo-customer-api.ahastudio.com/restaurants?region=${regionName}&category=${categoryId}`;
 
   const response = await fetch(url);
   const data = await response.json();
