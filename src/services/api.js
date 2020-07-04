7834aa4ba6360547c9ac7ca5e4aa0240ea5ca830
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-05 00:30:48 +0900

    Add line break to url in fetchRestaurants

--- src/services/api.js
+++ src/services/api.js
@@ -15,7 +15,8 @@ export async function fetchCategories() {
 }
 
 export async function fetchRestaurants({ regionName, categoryId }) {
-  const url = `https://eatgo-customer-api.ahastudio.com/restaurants?region=${regionName}&category=${categoryId}`;
+  const url = 'https://eatgo-customer-api.ahastudio.com/restaurants?'
+    + `region=${regionName}&category=${categoryId}`;
 
   const response = await fetch(url);
   const data = await response.json();
