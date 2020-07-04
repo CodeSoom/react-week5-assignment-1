f3fefcca62244d8283b4d6f5e807454b99daf3ab
KiChang Shin <kc7851@gmail.com>
2020-07-04 23:40:55 +0900

    App test case 추가
    비동기액션 테스트를 추가하였습니다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
 import { useDispatch, useSelector } from 'react-redux';
 
 import { render } from '@testing-library/react';
 
 import App from './App';
+
+import {
+  loadRegions,
+  loadCategories,
+  loadRestaurants,
+} from './actions';
 
 import categories from '../fixtures/categories';
 import regions from '../fixtures/regions';
@@ -14,6 +23,9 @@ jest.mock('react-redux');
 jest.mock('./services/api');
 
 describe('App', () => {
+  const middlewares = [thunk];
+  const mockStore = configureStore(middlewares);
+
   const dispatch = jest.fn();
 
   useDispatch.mockImplementation(() => dispatch);
@@ -27,6 +39,10 @@ describe('App', () => {
     regions,
     restaurants,
   }));
+
+  beforeEach(() => {
+    dispatch.mockClear();
+  });
 
   it('지역이 보인다.', () => {
     const { getByText } = render(
@@ -61,6 +77,78 @@ describe('App', () => {
       const { name } = restaurant;
       const regExp = new RegExp(name);
       expect(getByText(regExp)).not.toBeNull();
+    });
+  });
+
+  describe('loadRegions', () => {
+    const store = mockStore({});
+
+    it('regions를 fetch 한다.', async () => {
+      await store.dispatch(loadRegions());
+      const actions = await store.getActions();
+
+      expect(actions[0].type).toBe('setRegions');
+    });
+  });
+
+  describe('loadCategories', () => {
+    const store = mockStore({});
+
+    it('categories를 fetch 한다.', async () => {
+      await store.dispatch(loadCategories());
+      const actions = await store.getActions();
+
+      expect(actions[0].type).toBe('setCategories');
+    });
+  });
+
+  describe('loadRestaurants', () => {
+    const withoutRegionStore = mockStore({
+      selected: {
+        region: '',
+        categoryId: 1,
+      },
+    });
+
+    context('without region', () => {
+      it('동작하지 않는다.', async () => {
+        await withoutRegionStore.dispatch(loadRestaurants());
+        const actions = withoutRegionStore.getActions();
+
+        expect(actions).toHaveLength(0);
+      });
+    });
+
+    const withoutCategoryIdStore = mockStore({
+      selected: {
+        region: '서울',
+        categoryId: '',
+      },
+    });
+
+    context('without categoryId', () => {
+      it('동작하지 않는다.', async () => {
+        await withoutCategoryIdStore.dispatch(loadRestaurants());
+        const actions = withoutCategoryIdStore.getActions();
+
+        expect(actions).toHaveLength(0);
+      });
+    });
+
+    const withSelectedStore = mockStore({
+      selected: {
+        region: '서울',
+        categoryId: '1',
+      },
+    });
+
+    context('with region and categoryId', () => {
+      it('동작하지 않는다.', async () => {
+        await withSelectedStore.dispatch(loadRestaurants());
+        const actions = withSelectedStore.getActions();
+
+        expect(actions[0].type).toBe('setRestaurants');
+      });
     });
   });
 });
