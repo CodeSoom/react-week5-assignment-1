7c04bdd4d2b527e51b596207e75f8e4e3950588b
Kwakcena <rhkrgudwh@gmail.com>
2020-07-05 22:22:33 +0900

    막힌 부분
    원래는 App 컴포넌트에서 loadRestaurants 를 이용해서 데이터를 api 서버에
    요청하려 했으나, App 컴포넌트에서는 상태를 가져오지 않으므로
    loadRestaurants에 인자를 전달하지 못함을 깨닫고
    
    RestaurantsContainer에서 이를 처리하려 했습니다. 근데 regionName과
    categoryId가 비어있을때에 대한 처리를 어떻게 테스하고 적용해야 할지
    모르겠어요.
    
    일단 RestaurantsContainer에서 상태를 가져오는 것이 맞는지부터
    햇갈립니다.

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Restaurants from './Restaurants';
+import { loadRestaurants } from './actions';
 
 export default function RestaurantsContainer() {
-  const { restaurants } = useSelector((state) => ({
+  const dispatch = useDispatch();
+
+  const { restaurants, region, category } = useSelector((state) => ({
     restaurants: state.restaurants,
+    region: state.region,
+    category: state.category,
   }));
 
+  useEffect(() => {
+    dispatch(loadRestaurants(region.name, category.id));
+  });
   return (
     <Restaurants restaurants={restaurants} />
   );
