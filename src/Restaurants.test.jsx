69ebd8a120a310a6d4d43eda7e3ff5eb664b31a4
Kwakcena <rhkrgudwh@gmail.com>
2020-07-05 20:49:04 +0900

    Refactor: fixtures 데이터 사용
    테스트에 fixtures 데이터를 사용한다.

--- src/Restaurants.test.jsx
+++ src/Restaurants.test.jsx
@@ -4,25 +4,10 @@ import { render } from '@testing-library/react';
 
 import Restaurants from './Restaurants';
 
+import { restaurants } from '../fixtures/restaurants';
+
 describe('Restaurants', () => {
   it('분류 목록을 받아서 화면에 보여준다.', () => {
-    const restaurants = [
-      {
-        id: 9,
-        categoryId: 2,
-        name: '호신각',
-        address: '서울 강남구',
-        information: '호신각 in 서울 강남구',
-      },
-      {
-        id: 10,
-        categoryId: 2,
-        name: '홍콩반점',
-        address: '서울시 서대문구',
-        information: '홍콩반점 in 서울시 서대문구',
-      },
-    ];
-
     const { getByText } = render((
       <Restaurants restaurants={restaurants} />
     ));
--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -6,26 +6,11 @@ import { useSelector } from 'react-redux';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
+import { restaurants } from '../fixtures/restaurants';
+
 jest.mock('react-redux');
 
 describe('RestaurantsContainer', () => {
-  const restaurants = [
-    {
-      id: 9,
-      categoryId: 2,
-      name: '호신각',
-      address: '서울 강남구',
-      information: '호신각 in 서울 강남구',
-    },
-    {
-      id: 10,
-      categoryId: 2,
-      name: '홍콩반점',
-      address: '서울시 서대문구',
-      information: '홍콩반점 in 서울시 서대문구',
-    },
-  ];
-
   beforeEach(() => {
     useSelector.mockImplementation((selector) => selector(({
       restaurants,
