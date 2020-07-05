0abee54723388ce7e84b6bb150e96aa1e87882a0
aumilkyeong <aumilkyeong@gmail.com>
2020-07-05 19:29:57 +0900

    Feat: 카테고리 api 호출 기능 구현 및 목업 생성

--- src/services/__mocks__/api.js
+++ src/services/__mocks__/api.js
@@ -2,7 +2,6 @@ export async function fetchRegions() {
   return [];
 }
 
-// TODO: delete this!! - dummy to avoid lint error
-export function xxx() {
-
+export async function fetchCategories() {
+  return [];
 }
--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,9 @@ export async function fetchRegions() {
   return data;
 }
 
-// TODO: delete this!! - it just dummy to avoid lint error
-export function xxx() {
-
+export async function fetchCategories() {
+  const URL = ' https://eatgo-customer-api.ahastudio.com/categories';
+  const response = await fetch(URL);
+  const data = await response.json();
+  return data;
 }
