c56e0dff880f3854c2bac98b07892d82298c5b9c
jiunkoo <jiunkoo0106@gmail.com>
2020-07-05 14:34:49 +0900

    Categories - Button 컴포넌트 제거, categories 버튼 테스트 추가

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Categories from './Categories';
 
 import categories from '../fixtures/categories';
 
-test('Categories', () => {
-  const { getByText } = render((
-    <Categories
-      categories={categories}
-    />
-  ));
+describe('Categories', () => {
+  describe('상위 컴포넌트에서 받은 props 테스트', () => {
+    const onClick = jest.fn();
 
-  expect(getByText(/한식/)).not.toBeNull();
+    test('value를 버튼 이름으로 출력', () => {
+      const { getByText } = render((
+        <Categories
+          categories={categories}
+          onClick={onClick}
+        />
+      ));
+
+      expect(getByText(/한식/)).not.toBeNull();
+    });
+
+    test('버튼 동작 테스트', () => {
+      const { getByText } = render((
+        <Categories
+          categories={categories}
+          onClick={onClick}
+        />
+      ));
+
+      expect(onClick).not.toBeCalled();
+
+      fireEvent.click(getByText(/한식/));
+
+      expect(onClick).toBeCalledWith(1);
+    });
+  });
 });
