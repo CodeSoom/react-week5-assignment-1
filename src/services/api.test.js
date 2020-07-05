35a49247208b2a39359b0e38a3a4d977eb4b9c4e
jiunkoo <jiunkoo0106@gmail.com>
2020-07-05 18:40:09 +0900

    api - fetchRestaurants 테스트 추가

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -1,7 +1,8 @@
-import { fetchRegions, fetchCategories } from './api';
+import { fetchRegions, fetchCategories, fetchRestaurants } from './api';
 
 import { regions } from '../../fixtures/regions';
 import { categories } from '../../fixtures/categories';
+import { restaurants } from '../../fixtures/restaurants';
 
 describe('api', () => {
   const fetch = jest.fn();
@@ -14,7 +15,7 @@ describe('api', () => {
   };
 
   describe('fetchRegions 테스트', () => {
-    it('returns regions', async () => {
+    it('지역 목록을 반환한다', async () => {
       await mockFetch(regions);
       const data = await fetchRegions();
 
@@ -23,11 +24,23 @@ describe('api', () => {
   });
 
   describe('fetchCategories 테스트', () => {
-    it('returns regions', async () => {
+    it('카테고리 목록을 반환한다', async () => {
       await mockFetch(categories);
       const data = await fetchCategories();
 
       expect(data).toEqual(categories);
+    });
+  });
+
+  describe('fetchRestaurants 테스트', () => {
+    it('레스토랑 목록을 반환한다', async () => {
+      await mockFetch(restaurants);
+      const data = await fetchRestaurants({
+        regionName: '서울',
+        categoryId: 1,
+      });
+
+      expect(data).toEqual(restaurants);
     });
   });
 });
