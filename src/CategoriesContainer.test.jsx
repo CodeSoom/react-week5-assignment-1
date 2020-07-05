1934675fe20e86031957d10ecf0434e8cba36912
aumilkyeong <aumilkyeong@gmail.com>
2020-07-05 19:55:23 +0900

    Test: 카테고리 컨테이너 테스트 추가

---
+++ src/CategoriesContainer.test.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+import CategoriesContainer from './CategoriesContainer';
+
+const categories = [
+  { id: 1, name: '한식' },
+];
+useSelector.mockImplementation((selector) => selector({ categories }));
+
+describe('CategoriesContainer', () => {
+  it('contains category names', () => {
+    const { queryByText } = render(
+      <CategoriesContainer />,
+    );
+
+    expect(queryByText(/한식/)).not.toBeNull();
+  });
+
+  it('selects a region and update display', () => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementation(() => dispatch);
+
+    const { getByText } = render(
+      <CategoriesContainer />,
+    );
+
+    fireEvent.click(getByText(/한식/));
+
+    expect(dispatch).toBeCalledWith({
+      type: 'selectRegion',
+      payload: { regionId: 1 },
+    });
+  });
+});
