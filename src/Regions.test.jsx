434829c31dce0d2fd2480befe3924ab2ffff2342
jiunkoo <jiunkoo0106@gmail.com>
2020-07-05 14:41:30 +0900

    Regions - regions 버튼 테스트 추가

--- src/Regions.test.jsx
+++ src/Regions.test.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Regions from './Regions';
 
 import regions from '../fixtures/regions';
 
-test('Regions', () => {
-  const { getByText } = render((
-    <Regions
-      regions={regions}
-    />
-  ));
+describe('Regions', () => {
+  describe('상위 컴포넌트에서 받은 props 테스트', () => {
+    const onClick = jest.fn();
 
-  expect(getByText(/서울/)).not.toBeNull();
+    test('value를 버튼 이름으로 출력', () => {
+      const { getByText } = render((
+        <Regions
+          regions={regions}
+          onClick={onClick}
+        />
+      ));
+
+      expect(getByText(/서울/)).not.toBeNull();
+    });
+
+    test('버튼 동작 테스트', () => {
+      const { getByText } = render((
+        <Regions
+          regions={regions}
+          onClick={onClick}
+        />
+      ));
+
+      expect(onClick).not.toBeCalled();
+
+      fireEvent.click(getByText(/서울/));
+
+      expect(onClick).toBeCalledWith(1);
+    });
+  });
 });
