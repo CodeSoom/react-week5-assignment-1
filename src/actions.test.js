ad9e96771daee53d86b2a50d0ce13d82bccdcef3
jiunkoo <jiunkoo0106@gmail.com>
2020-07-05 19:20:51 +0900

    actions - setRestaurants 테스트 추가

--- src/actions.test.js
+++ src/actions.test.js
@@ -5,10 +5,13 @@ import {
   setCategories,
   loadCategories,
   selectCategory,
+  setRestaurants,
+  loadRestaurants,
 } from './actions';
 
-import categories from '../fixtures/categories';
-import regions from '../fixtures/regions';
+import { regions } from '../fixtures/regions';
+import { categories } from '../fixtures/categories';
+import restaurants from '../fixtures/restaurants';
 
 jest.mock('react-redux');
 jest.mock('./services/api');
@@ -61,6 +64,27 @@ describe('actions', () => {
 
       expect(action.type).toBe('selectCategory');
       expect(action.payload.categoryId).toBe(categoryId);
+    });
+  });
+
+  describe('Restaurants', () => {
+    it('setRestaurants', () => {
+      const action = setRestaurants(restaurants);
+
+      expect(action.type).toBe('setRestaurants');
+      expect(action.payload.restaurants).toBe(restaurants);
+    });
+
+    it('loadRestaurants', async () => {
+      const getState = jest.fn(() => ({
+        selectedRegion: '서울',
+        selectedCategory: 1,
+      }));
+      const dispatch = jest.fn();
+
+      await loadRestaurants()(dispatch, getState);
+
+      expect(dispatch).toBeCalledTimes(1);
     });
   });
 });
