3f4ea5d590b8f844567ae8f877c93a11bccd4e18
Kwakcena <rhkrgudwh@gmail.com>
2020-07-05 20:47:20 +0900

    test(green): 레스토랑 목록 useSelector 테스트
    컨테이너에 실제로 useSelector를 적용하여 test가 통과하도록 한다.

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import Restaurants from './Restaurants';
 
-import { restaurants } from '../fixtures/restaurants';
-
 export default function RestaurantsContainer() {
+  const { restaurants } = useSelector((state) => ({
+    restaurants: state.restaurants,
+  }));
+
   return (
     <Restaurants restaurants={restaurants} />
   );
