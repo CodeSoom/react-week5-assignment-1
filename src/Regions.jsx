1d29c33b8f5086f489ce3b7a21cd4d783049355f
jiunkoo <jiunkoo0106@gmail.com>
2020-07-05 16:23:24 +0900

    Regions - 선택한 regions 화면 출력 테스트 구현

--- src/Regions.jsx
+++ src/Regions.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-export default function Regions({ regions, onClick }) {
+export default function Regions({ regions, selectedRegion, onClick }) {
   return (
     <ul>
       {regions.map((region) => (
         <li key={region.id}>
           <button type="button" onClick={() => onClick(region.id)}>
             {region.name}
+            {selectedRegion && region.id === selectedRegion.id ? '(V)' : null }
           </button>
         </li>
       ))}
