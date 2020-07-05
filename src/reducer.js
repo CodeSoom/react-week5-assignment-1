31d14f6c2a943d2b663aeb6cfe2676783e121ace
jiunkoo <jiunkoo0106@gmail.com>
2020-07-05 19:22:35 +0900

    reducer - setRestaurants 테스트 구현

--- src/reducer.js
+++ src/reducer.js
@@ -39,5 +39,13 @@ export default function reducer(state = initialState, action) {
     };
   }
 
+  if (action.type === 'setRestaurants') {
+    const { restaurants } = action.payload;
+    return {
+      ...state,
+      restaurants,
+    };
+  }
+
   return state;
 }
