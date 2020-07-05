494378cd1e7be89ee0a040d2cefebd59dc5a6979
jiunkoo <jiunkoo0106@gmail.com>
2020-07-05 16:46:52 +0900

    Categories - 선택한 categories 화면 출력 테스트 구현

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-export default function Categories({ categories, onClick }) {
+export default function Categories({ categories, selectedCategory, onClick }) {
   return (
     <ul>
       {categories.map((category) => (
         <li key={category.id}>
           <button type="button" onClick={() => onClick(category.id)}>
             {category.name}
+            {selectedCategory && category.id === selectedCategory.id ? '(V)' : null }
           </button>
         </li>
       ))}
