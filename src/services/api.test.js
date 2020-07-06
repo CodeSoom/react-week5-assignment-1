2411d9d2ef65516e0356e1f8aaa0982ed6b50154
Paul An <anpaul0615@gmail.com>
2020-07-06 22:30:10 +0900

    ~/services/api 테스트 추가

---
+++ src/services/api.test.js
@@ -0,0 +1,16 @@
+import { fetchCategories } from './api';
+
+import CATEGORIES from '../__fixtures__/categories.json';
+
+describe('fetchCategories', () => {
+  global.fetch = jest.fn().mockResolvedValue({
+    json() {
+      return CATEGORIES;
+    },
+  });
+
+  it('returns categories', async () => {
+    const regions = await fetchCategories();
+    expect(regions).toEqual(CATEGORIES);
+  });
+});
