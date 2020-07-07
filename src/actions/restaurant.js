152a9788ca384ce82de1dba00be1a44db6998409
Paul An <anpaul0615@gmail.com>
2020-07-07 19:41:22 +0900

    actions/restaurant 테스트 작성 및 구현

---
+++ src/actions/restaurant.js
@@ -0,0 +1,28 @@
+import { fetchRestaurants } from '../services/api';
+
+/**
+ * Action Types
+ */
+export const SET_RESTAURANTS = Symbol('setRestaurants');
+
+/**
+ * Actions
+ */
+export function setRestaurants(restaurants) {
+  return {
+    type: SET_RESTAURANTS,
+    payload: { restaurants },
+  };
+}
+export function loadRestaurants() {
+  return async (dispatch, getState) => {
+    try {
+      const { selectedCategoryId, selectedRegionId, regions } = getState();
+      const selectedRegion = regions.find((region) => region.id === selectedRegionId);
+      const restaurants = await fetchRestaurants(selectedRegion.name, selectedCategoryId);
+      dispatch(setRestaurants(restaurants));
+    } catch (e) {
+      dispatch(setRestaurants([]));
+    }
+  };
+}
