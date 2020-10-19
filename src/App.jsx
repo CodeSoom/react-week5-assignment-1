182497303a17a0723cb2de0f598693fa6cdf6841
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-19 19:06:59 +0900

    [green] Implement dispatch in App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import RestaurantRegionsContainer from './RestaurantRegionsContainer';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch();
+  }, {});
+
   return (
     <div>
       <h1>Restaurants</h1>
