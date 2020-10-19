19cb398dd1cf03fc288004f76753c9ce848c823c
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-19 18:12:09 +0900

    [refactor] Add useSelector to App test.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
+import restaurantRegions from '../__fixtures__/restaurantRegions';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurantRegions,
+  }));
+
   const { container } = render((
     <App />
   ));
