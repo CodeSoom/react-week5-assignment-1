328cc43b9845f8680c2d04ba82b872861a7e10da
Dong A Kim <dongakim90@gmail.com>
2020-10-20 00:25:05 +0900

    RestaurantRegionsContainer 테스트 추가

--- src/RestaurantRegionsContainer.test.jsx
+++ src/RestaurantRegionsContainer.test.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import RestaurantRegionsContainer from './RestaurantRegionsContainer';
 
 describe('<RestaurantRegionsContainer />', () => {
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    regions: [{ id: 1, name: '서울' }],
+  }));
+
   const renderRestaurantRegionsContainer = () => render((
     <RestaurantRegionsContainer />
   ));
@@ -15,5 +24,13 @@ describe('<RestaurantRegionsContainer />', () => {
 
     // Then
     expect(getByRole('list')).toHaveTextContent('서울');
+  });
+
+  it('calls dispatch', () => {
+    // When
+    renderRestaurantRegionsContainer();
+
+    // Then
+    expect(dispatch).toBeCalled();
   });
 });
