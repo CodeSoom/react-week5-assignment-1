b03b4e7d07fd59c1c5365bc815b50be3212f8a89
Dong A Kim <dongakim90@gmail.com>
2020-10-20 06:27:55 +0900

    RestaurantsCategoriesContainer 테스트 추가

--- src/RestaurantsCategoriesContainer.test.jsx
+++ src/RestaurantsCategoriesContainer.test.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import RestaurantsCategoriesContainer from './RestaurantsCategoriesContainer';
 
 describe('<RestaurantsCategoriesContainer />', () => {
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    categories: [{ id: 1, name: '한식' }],
+  }));
+
   const renderRestaurantsCategoriesContainer = () => render((
     <RestaurantsCategoriesContainer />
   ));
@@ -15,5 +24,13 @@ describe('<RestaurantsCategoriesContainer />', () => {
 
     // Then
     expect(getByRole('list')).toHaveTextContent('한식');
+  });
+
+  it('calls dispach', () => {
+    // When
+    renderRestaurantsCategoriesContainer();
+
+    // Then
+    expect(dispatch).toBeCalled();
   });
 });
