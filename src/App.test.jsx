3e50d2ed259ebb83d1682594d1b06d27ee5f0bc6
oceanide <oceanide@gmail.com>
2020-10-20 08:07:31 +0900

    [REFACTOR] add useSelector mock in App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  useSelector.mockImplementation((selector) => selector({
+    regions: [
+      { id: 1, name: '서울' },
+    ],
+  }));
+
   const { getByText } = render(<App />);
 
   expect(getByText('서울')).not.toBeNull();
