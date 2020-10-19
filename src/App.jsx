d2d28fca789dc4af682a12267b040a708df9cd80
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-19 22:15:13 +0900

    Implement App component
    to call dispatch on mount

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
 
 import RegionsContainer from './RegionsContainer';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch();
+  }, []);
+
   return (
     <RegionsContainer />
   );
