05d9700e52f0c66d1b1129005dc4607e7cec206d
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-19 22:04:10 +0900

    Refactor App test
    to use react-redux

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import App from './App';
 
+import regions from '../fixtures/regions';
+
+jest.mock('react-redux');
+
 test('App', () => {
+  useSelector.mockImplementation((selector) => selector({ regions }));
+
   const { queryByText } = render(<App />);
 
   expect(queryByText(/서울/)).not.toBeNull();
