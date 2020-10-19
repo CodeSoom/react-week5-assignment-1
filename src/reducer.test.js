86c917be30bf3079969f9e7383c7efb77264d298
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-19 22:29:27 +0900

    Add test about reducer

---
+++ src/reducer.test.js
@@ -0,0 +1,17 @@
+import { setRegions } from './actions';
+
+import regions from '../fixtures/regions';
+
+describe('reducer', () => {
+  describe('setRegions', () => {
+    it('changes regions', () => {
+      const initialState = {
+        regions: [],
+      };
+
+      const state = reducer(initialState, setRegions(regions));
+
+      expect(state.regions).not.toHaveLength(0);
+    });
+  });
+});
