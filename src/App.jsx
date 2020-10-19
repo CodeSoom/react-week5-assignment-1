80d0322d1feb07792e423ffc22ad2d5e98f8e1c6
Dong A Kim <dongakim90@gmail.com>
2020-10-20 00:30:41 +0900

    RestaurantRegionsContainer 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import RestaurantRegionsContainer from './RestaurantRegionsContainer';
+
 export default function App() {
   return (
-    <></>
+    <>
+      <RestaurantRegionsContainer />
+    </>
   );
 }
