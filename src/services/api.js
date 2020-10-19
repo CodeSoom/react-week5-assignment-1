ef88afb7279bb8ad317d68627a50a1dd40d9d421
Dong A Kim <dongakim90@gmail.com>
2020-10-20 06:28:21 +0900

    api 추가

--- src/services/api.js
+++ src/services/api.js
@@ -4,5 +4,8 @@ export async function fetchRestaurantRegion() {
   return data;
 }
 
-// TODO: remove this fucntion!
-export function xxx() { }
+export async function fetchRestaurantCategories() {
+  const response = await fetch('https://eatgo-customer-api.ahastudio.com/categories');
+  const data = await response.json();
+  return data;
+}
