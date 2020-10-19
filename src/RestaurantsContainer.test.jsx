d6bfe0f521187a8d5bab61f04eb252f63ed70621
Dong A Kim <dongakim90@gmail.com>
2020-10-20 08:41:45 +0900

    RestaurantsContainer 테스트 추가

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
 describe('<RestaurantsContainer />', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: [{
+      id: 1,
+      categoryId: 1,
+      name: '양천주가',
+      address: '서울 강남구 123456',
+      information: '양천주가 in 서울 강남구 123456',
+    }],
+  }));
+
   const renderRestaurantsContainer = () => render((
     <RestaurantsContainer />
   ));
@@ -14,6 +26,6 @@ describe('<RestaurantsContainer />', () => {
     const { getByRole } = renderRestaurantsContainer();
 
     // Then
-    expect(getByRole('list')).toBeInTheDocument();
+    expect(getByRole('list')).toHaveTextContent('양천주가');
   });
 });
