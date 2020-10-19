4407a6cc05087d865aaf64a4db36f1b36aac94b8
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-19 22:00:51 +0900

    Refactor App component
    to call RegionsContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 
 import Regions from './Regions';
+import RegionsContainer from './RegionsContainer';
 
 export default function App() {
-  const regions = [
-    { id: 1, name: '서울' },
-    { id: 2, name: '부산' },
-    { id: 3, name: '대구' },
-    { id: 4, name: '대전' },
-  ];
-
   return (
-    <Regions regions={regions} />
+    <RegionsContainer />
   );
 }
