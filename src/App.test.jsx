7a0f0644bd9ccac34d3783e597c46d7ad10d3d65
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-19 21:26:18 +0900

    Add test about App component
    to show region name

---
+++ src/App.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+test('App', () => {
+  const { queryByText } = render(<App />);
+
+  expect(queryByText(/서울/)).not.toBeNull();
+});
