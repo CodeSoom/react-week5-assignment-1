c7e5e8831fdf881fe520973493b64149fd5a0db7
Dong A Kim <dongakim90@gmail.com>
2020-10-20 07:21:11 +0900

    RestaurantRegionsContainer 구현

--- src/RestaurantRegionsContainer.jsx
+++ src/RestaurantRegionsContainer.jsx
@@ -8,6 +8,13 @@ export default function RestaurantRegionsContainer() {
   const dispatch = useDispatch();
   const { regions } = useSelector((state) => state);
 
+  const handleClick = (event) => {
+    dispatch({
+      type: 'SET_REGION',
+      payload: event.target.name,
+    });
+  };
+
   useEffect(() => {
     dispatch(loadRestaurantRegion());
   }, []);
@@ -16,7 +23,13 @@ export default function RestaurantRegionsContainer() {
     <ul>
       {regions.map((region) => (
         <li key={region.id}>
-          {region.name}
+          <button
+            type="button"
+            name={region.name}
+            onClick={handleClick}
+          >
+            {region.name}
+          </button>
         </li>
       ))}
     </ul>
