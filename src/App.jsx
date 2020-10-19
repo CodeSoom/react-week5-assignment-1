e94cb6f456d114a6ab802832d114886dc6dc9468
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-19 21:36:45 +0900

    Implement App
    to pass constant regions to Regions component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import Regions from './Regions';
 
 export default function App() {
+  const regions = [
+    { id: 1, name: '서울' },
+    { id: 2, name: '부산' },
+    { id: 3, name: '대구' },
+    { id: 4, name: '대전' },
+  ];
+
   return (
-    <Regions />
+    <Regions regions={regions} />
   );
 }
