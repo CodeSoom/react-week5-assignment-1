a7116a2ab52980deba89708f1d0e3089bf6a1f3c
Dong A Kim <dongakim90@gmail.com>
2020-10-19 11:06:18 +0900

    reducer 테스트 추가

---
+++ src/reducer.test.js
@@ -0,0 +1,18 @@
+import reducer from './reducer';
+
+describe('reducer', () => {
+  describe('SET_REGIONS', () => {
+    it('sets restaurant regions', () => {
+      const previousState = {
+        regions: [],
+      };
+      const action = {
+        type: 'SET_REGIONS',
+        payload: [{ id: 1, name: '서울' }],
+      };
+      const state = reducer(previousState, action);
+
+      expect(state.regions).toMatchObject([{ id: 1, name: '서울' }]);
+    });
+  });
+});
