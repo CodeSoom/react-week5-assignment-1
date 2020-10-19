a2b8b00fe581ea2a8e1050b432627cfe7569cad6
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-19 16:54:14 +0900

    [red] Create test for App.

---
+++ src/App.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+test('App', () => {
+  const { container } = render((
+    <App />
+  ));
+
+  expect(container).toHaveTextContent('Restaurants');
+});
