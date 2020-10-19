bc9ee984cd9e56022b0db10cafde00f85b3cc55d
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-19 17:42:51 +0900

    [red] Create useSelector test for RestaurantRegionsContainer.

---
+++ src/RestaurantRegionsContainer.test.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { useSelector } from 'react-redux';
+
+import restaurantRegions from '../__fixtures__/restaurantRegions';
+
+import RestaurantRegionsContainer from './RestaurantRegionsContainer';
+
+test('App', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurantRegions,
+  }));
+
+  const { container } = render((
+    <RestaurantRegionsContainer />
+  ));
+
+  restaurantRegions.forEach(({ name }) => {
+    expect(container).toHaveTextContent(name);
+  });
+});
