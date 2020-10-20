a30dbc8fb7f733e2556d4cf934b15734db95c766
oceanide <oceanide@gmail.com>
2020-10-20 19:23:34 +0900

    [GREEN] add reducer for setRegions

--- src/reducer.js
+++ src/reducer.js
@@ -5,5 +5,14 @@ const initialState = {
 };
 
 export default function reducer(state = initialState, action) {
+  if (action.type === 'setRegions') {
+    const { regions } = action.payload;
+
+    return {
+      ...state,
+      regions,
+    };
+  }
+
   return state;
 }
