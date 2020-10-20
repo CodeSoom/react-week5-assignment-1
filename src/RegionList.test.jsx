47df39d777d2780ea0cf56ce8deb597bd3f26374
saseungmin <dbd02169@naver.com>
2020-10-20 20:53:34 +0900

    RegionList의 컴포넌트에 대한 테스트를 구현한다.
    - renders button to click a region

--- src/RegionList.test.jsx
+++ src/RegionList.test.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import RegionList from './RegionList';
 import { regionsFixture } from '../fixtures/fixtures';
 
-describe('List', () => {
-  const listRender = (regions) => render((
+describe('RegionList', () => {
+  const handleSelectClick = jest.fn();
+
+  const regionListRender = (regions) => render((
     <RegionList
       regions={regions}
+      onSelectRegionClick={handleSelectClick}
     />
   ));
 
   context('with regions', () => {
     it('see renders regions', () => {
-      const { getByText } = listRender(regionsFixture);
+      const { getByText } = regionListRender(regionsFixture);
 
       regionsFixture.forEach(({ name }) => {
         expect(getByText(name)).not.toBeNull();
+      });
+    });
+
+    it('renders button to click a region', () => {
+      const { getByText } = regionListRender(regionsFixture);
+
+      regionsFixture.forEach(({ name, id }) => {
+        fireEvent.click(getByText(name));
+
+        expect(handleSelectClick).toBeCalledWith(id);
       });
     });
   });
@@ -26,7 +39,7 @@ describe('List', () => {
     const regions = [];
 
     it('nothing render regions', () => {
-      const { container } = listRender(regions);
+      const { container } = regionListRender(regions);
 
       expect(container).toBeEmptyDOMElement();
     });
