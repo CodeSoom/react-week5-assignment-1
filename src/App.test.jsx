f6d23ec47fea829ecf338773b2131660e1997d94
Noah <mike1124@naver.com>
2020-10-20 13:16:17 +0900

    ✅ [red] Add test for app

---
+++ src/App.test.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { render } from "@testing-library/react";
+
+import App from "./App";
+
+describe("App", () => {
+  const renderApp = () => render(<App />);
+  it("show categories and regions", () => {
+    const { getByText } = renderApp();
+    const categories = [
+      '서울', '대구', '부산', '대전', '강원도', '광주'
+    ]
+    const regions = [
+      '한식', '중식', '일식', '양삭', '분식'
+    ]
+
+    categories.forEach((category) => {
+      expect(getByText(`${category}`)).not.toBeNull();
+    })
+
+    regions.forEach((category) => {
+      expect(getByText(`${category}`)).not.toBeNull();
+    })
+  })
+})
+
