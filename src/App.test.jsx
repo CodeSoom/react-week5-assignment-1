463488cf127ffd3aca79c6a0eeac12a68d1861c2
wjy5446 <wjy5446@gmail.com>
2020-10-20 20:56:04 +0900

    feat) App 디자인 테스트 코드 작성

---
+++ src/App.test.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+
+  context('지역 리스트가 주어지면,', () => {
+    const locationList = ['서울', '대전', '부산'];
+
+    it('지역 선택 버튼들이 출력됩니다.', () => {
+      const { container } = render(<App />);
+
+      locationList.forEach((location) => {
+        expect(container).toHaveTextContent(location);
+      });
+    });
+  });
+
+  context('음식 종류 리스트가 주어지면,', () => {
+    const foodTypeList = ['한식', '중식', '일식'];
+
+    it('음식 종류 선택 버튼들이 출력됩니다.', () => {
+      const { container } = render(<App />);
+
+      foodTypeList.forEach((foodType) => {
+        expect(container).toHaveTextContent(foodType);
+      });
+    })
+  })
+
+  context('결과 리스트가 주어지면,', () => {
+    const resultList = ['양천주가', '밀면넘어져요'];
+
+    it('결과 리스트가 출력됩니다.', () => {
+      const { container } = render(<App />);
+
+      resultList.forEach((result) => {
+        expect(container).toHaveTextContent(result);
+      });
+    })
+  })
+});
