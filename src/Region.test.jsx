b8e0051dc51ffabb90251b1b025df902f4916b71
saseungmin <dbd02169@naver.com>
2020-10-20 20:20:57 +0900

    Region 컴포넌트에 대한 테스트를 구현한다.
    - 버튼 클릭 시 onClick 핸들러가 실행된다.

---
+++ src/Region.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import Region from './Region';
+
+describe('Region', () => {
+  const handleClick = jest.fn();
+  const regionRender = () => render((
+    <Region onClick={handleClick} />
+  ));
+
+  it('버튼 클릭 시 onClick 핸들러가 실행된다.', () => {
+    const { getByText } = regionRender();
+    const button = getByText(/대전/);
+
+    expect(button).not.toBeNull();
+
+    expect(handleClick).not.toBeCalled();
+
+    fireEvent.click(button);
+
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+});
