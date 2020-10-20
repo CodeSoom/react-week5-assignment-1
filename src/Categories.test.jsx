13047c395915dab435635dfdfbb06c8c3ead3c3e
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-20 15:19:10 +0900

    [red] Add click event test for Categories.

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import categories from '../__fixtures__/categories';
 
 import Categories from './Categories';
 
 test('Categories', () => {
-  const { container } = render((
-    <Categories categories={categories} />
+  const handleClickSelect = jest.fn();
+
+  const { container, getByText } = render((
+    <Categories
+      categories={categories}
+      onClick={handleClickSelect}
+    />
   ));
 
   categories.forEach(({ name }) => {
     expect(container).toHaveTextContent(name);
+    fireEvent.click(getByText(name));
   });
+
+  expect(handleClickSelect).toBeCalledTimes(categories.length);
 });
