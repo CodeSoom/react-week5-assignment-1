d6ddb8a668683a6ac432dc7de0446d4e12038a85
saseungmin <dbd02169@naver.com>
2020-10-20 21:11:30 +0900

    Update categorysFixture

--- fixtures/fixtures.js
+++ fixtures/fixtures.js
@@ -8,4 +8,12 @@ export const regionsFixture = [
   { id: 7, name: '인천' },
 ];
 
-export const temp = [];
+export const categorysFixture = [
+  { id: 1, name: '한식' },
+  { id: 2, name: '중식' },
+  { id: 3, name: '일식' },
+  { id: 4, name: '양식' },
+  { id: 5, name: '분식' },
+  { id: 6, name: '과자' },
+  { id: 7, name: '치킨' },
+];
