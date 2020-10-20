228ccda6b73abd7e2f36b0a4082868d433c991e8
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-20 15:20:04 +0900

    [green] Implement onClick in Categories.

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
-export default function Categories({ categories }) {
+export default function Categories({ categories, onClick }) {
   return (
     <ul>
       {
         categories.map(({ name }) => (
           <li key={name}>
-            <button type="button">
+            <button
+              type="button"
+              onClick={onClick}
+            >
               {name}
             </button>
           </li>
