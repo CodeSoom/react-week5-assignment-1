42e7509036e30545e1f6f57bbeebf93aa994b6d4
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-20 17:57:11 +0900

    [green] Implement onClick in Regions.

--- src/Regions.jsx
+++ src/Regions.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-export default function Regions({ regions }) {
+export default function Regions({ regions, onClick }) {
   return (
     <ul>
       { regions.map(({ name }) => (
         <li key={name}>
           <button
             type="button"
+            onClick={onClick}
           >
             {name}
           </button>
