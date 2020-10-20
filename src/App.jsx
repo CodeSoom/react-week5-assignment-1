83e46aa8f892028dde6ec486ff1a226170c3db43
Noah <mike1124@naver.com>
2020-10-20 22:37:27 +0900

    ✅ Add CategoriesContainer and redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import Categories from './Categories';
 
 import Regions from './Regions';
---
+++ src/CategoriesContainer.test.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { fireEvent, render } from '@testing-library/react';
+
+import Categories from './Categories';
+
+describe('Categories', () => {
+  it('show all categories', () => {
+    const handleClick = jest.fn();
+
+    const initialCategories = [
+      { id: 1, category: '한식' },
+      { id: 2, category: '중식' },
+      { id: 3, category: '일식' },
+      { id: 4, category: '양식' },
+      { id: 5, category: '분식' },
+    ];
+
+    const { getByText } = render(
+      <Categories
+        categories={initialCategories}
+        onClick={handleClick}
+      />,
+    );
+
+    initialCategories.forEach(({ category }) => {
+      expect(getByText(`${category}`)).not.toBeNull();
+
+      fireEvent.click(getByText(`${category}`));
+
+      expect(handleClick).toBeCalled();
+    });
+  });
+});
