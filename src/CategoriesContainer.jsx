2221b11f55e101a83f9a0c15ec696dbe69d77103
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-20 15:34:06 +0900

    [green] Implement dispatch in CategoriesContainer.

--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Categories from './Categories';
 
 export default function CategoriesContainer() {
+  const dispatch = useDispatch();
+
   const { categories } = useSelector((state) => ({
     categories: state.categories,
   }));
 
+  function handleClickSelectCategory() {
+    return dispatch();
+  }
+
   return (
-    <Categories categories={categories} />
+    <Categories
+      categories={categories}
+      onClick={handleClickSelectCategory}
+    />
   );
 }
