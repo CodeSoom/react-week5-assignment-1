f6bc0bcc3f9bc18759e8b74786a72f4dd068cc5d
oceanide <oceanide@gmail.com>
2020-10-21 01:09:16 +0900

    [GREEN] add loadRegions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
 
 import RegionsContainer from './RegionsContainer';
 
+import { setRegions } from './actions';
+
+function loadRegions({ dispatch }) {
+  const regions = [];
+
+  dispatch(setRegions(regions));
+}
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    loadRegions({ dispatch });
+  }, []);
+
   return (
     <RegionsContainer />
   );
