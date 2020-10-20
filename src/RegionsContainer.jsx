ff8ff97934575eb66956223405a32c18974d7083
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-20 18:13:31 +0900

    [green] Implement dispatch in RegionsContainer.

--- src/RegionsContainer.jsx
+++ src/RegionsContainer.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Regions from './Regions';
 
 export default function RegionsContainer() {
+  const dispatch = useDispatch();
+
   const { regions } = useSelector((state) => ({
     regions: state.regions,
   }));
 
+  function handleClickSelectRegion() {
+    return dispatch();
+  }
+
   return (
-    <Regions regions={regions} />
+    <Regions
+      regions={regions}
+      onClick={handleClickSelectRegion}
+    />
   );
 }
