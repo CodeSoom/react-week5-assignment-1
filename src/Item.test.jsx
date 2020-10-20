3ef0a7fe5c9006c507d3629fd31e630618f3fe94
saseungmin <dbd02169@naver.com>
2020-10-20 21:11:09 +0900

    Fixed Item test name

--- src/Item.test.jsx
+++ src/Item.test.jsx
@@ -4,15 +4,15 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Item from './Item';
 
-describe('Category', () => {
+describe('Item', () => {
   const handleClick = jest.fn();
 
-  const categoryRender = ({ name }) => render((
+  const itemRender = ({ name }) => render((
     <Item onClick={handleClick} name={name} />
   ));
 
   it('버튼 클릭 시 onClick 핸들러가 실행된다.', () => {
-    const { getByText } = categoryRender({ name: '한식' });
+    const { getByText } = itemRender({ name: '한식' });
     const button = getByText(/한식/);
 
     expect(button).not.toBeNull();
