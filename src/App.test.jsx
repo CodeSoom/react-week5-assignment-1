9deaa5659d0e2cf116e96ae3fef738997ea4c58e
wjy5446 <wjy5446@gmail.com>
2020-10-20 23:31:44 +0900

    feat) App dispatch 테스트 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,7 +2,31 @@ import React from 'react';
 
 import { fireEvent, render } from '@testing-library/react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 describe('App', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  })
+
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => { dispatch });
+  useSelector.mockImplementation((selector) => selector({
+    locationList: [],
+    foodTypeList: [],
+    resultList: [],
+  }));
+
+  it('컴포넌트가 생성되면 setSelectButtons action이 dispatch 됩니다.', () => {
+    render(<App />);
+
+    expect(dispatch).toBeCalledWith({
+      type: 'setSelectButtons',
+    });
+  });
 });
