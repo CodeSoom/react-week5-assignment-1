67a581a4b3203f959f359354e5a473d75ea7880b
wjy5446 <wjy5446@gmail.com>
2020-10-20 21:17:36 +0900

    feat) App 디자인 코드 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
 export default function App() {
+
   return (
-    <></>
+    <>
+      <ul>
+        <li><button>서울</button></li>
+        <li><button>대전</button></li>
+        <li><button>대구</button></li>
+        <li><button>부산</button></li>
+        <li><button>광주</button></li>
+        <li><button>강원도</button></li>
+        <li><button>인천</button></li>
+      </ul>
+
+      <ul>
+        <li><button>한식</button></li>
+        <li><button>중식</button></li>
+        <li><button>일식</button></li>
+        <li><button>양식</button></li>
+        <li><button>분식</button></li>
+      </ul>
+
+      <ul>
+        <li>양천주가</li>
+        <li>밀면넘어져요</li>
+      </ul>
+    </>
   );
 }
