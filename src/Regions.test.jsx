432a1bf123f82ca0e3b0e98d9a89683d134b2258
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-20 17:54:25 +0900

    [red] Add click event test for Regions.

--- src/Regions.test.jsx
+++ src/Regions.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import regions from '../__fixtures__/regions';
 
@@ -9,11 +9,19 @@ import Regions from './Regions';
 jest.mock('react-redux');
 
 test('Regions', () => {
-  const { container } = render((
-    <Regions regions={regions} />
+  const handleClickSelectRegion = jest.fn();
+
+  const { container, getByText } = render((
+    <Regions
+      regions={regions}
+      onClick={handleClickSelectRegion}
+    />
   ));
 
   regions.forEach(({ name }) => {
     expect(container).toHaveTextContent(name);
+    fireEvent.click(getByText(name));
   });
+
+  expect(handleClickSelectRegion).toBeCalledTimes(regions.length);
 });
