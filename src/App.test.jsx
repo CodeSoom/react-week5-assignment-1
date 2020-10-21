c4b0e065b325de5aef2f4d8a1bea9aafc73b9bf7
davidyang2149 <davidyang2149@gmail.com>
2020-10-21 23:44:36 +0900

    [Red] Add App.test.jsx for TDD
    5강 비동기 - 5주 차 과제 1
    - App.test.jsx 추가.
    - App 테스트 코드 작성(Red).

---
+++ src/App.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+describe('App Component', () => {
+  it('page render', () => {
+    const { getByText } = render((
+      <App />
+    ));
+
+    expect(getByText(/서울/)).not.toBeNull();
+    expect(getByText(/대전/)).not.toBeNull();
+    expect(getByText(/대구/)).not.toBeNull();
+    expect(getByText(/부산/)).not.toBeNull();
+    expect(getByText(/광주/)).not.toBeNull();
+    expect(getByText(/강원도/)).not.toBeNull();
+    expect(getByText(/인천/)).not.toBeNull();
+
+    expect(getByText(/한식/)).not.toBeNull();
+    expect(getByText(/중식/)).not.toBeNull();
+    expect(getByText(/일식/)).not.toBeNull();
+    expect(getByText(/양식/)).not.toBeNull();
+    expect(getByText(/분식/)).not.toBeNull();
+  });
+});
