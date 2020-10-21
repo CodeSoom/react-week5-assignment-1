fc6b124318fb66403072423f8fb7b2cb92a6ee43
ParkDongJo <pdj10211@gmail.com>
2020-10-22 00:56:33 +0900

    [week5-2] App.test.jsx 테스트코드 작성하기
    [week5-2] App test 코드 추가

--- src/components/App.test.jsx
+++ src/components/App.test.jsx
@@ -6,14 +6,20 @@ import { useSelector } from 'react-redux';
 
 import App from './App';
 
+import regions from '../../fixtures/regions';
+
+jest.mock('react-redux');
+
 test('App', () => {
   useSelector.mockImplementation((selector) => selector({
     regionStore: {
-      regions: [],
+      regions,
     },
   }));
 
   const { getByText } = render(<App />);
 
-  expect(getByText());
+  expect(getByText(/서울/)).not.toBeNull();
+  expect(getByText(/한식/)).not.toBeNull();
+  expect(getByText(/김초밥/)).not.toBeNull();
 });
