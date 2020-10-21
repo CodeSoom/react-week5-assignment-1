0842589ae81382518afdbe8e6a07a0c607818d1e
goodzzong <goodzzong@gmail.com>
2020-10-21 23:28:56 +0900

    RestaurantsContainer 에 대한 테스트 코드 추가

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
+jest.mock('react-redux');
+
 test('RestaurantsContainer', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: [
+      { id: 1, name: '홍콩반점' },
+      { id: 2, name: '한국식 초밥' },
+    ],
+  }));
+
   const { getByText } = render((
     <RestaurantsContainer />
   ));
 
   expect(getByText(/홍콩반점/)).not.toBeNull();
+  expect(getByText(/한국식 초밥/)).not.toBeNull();
 });
