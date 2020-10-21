6987dc2fd516905e091b7a8a06bdeccdf86c99e4
davidyang2149 <davidyang2149@gmail.com>
2020-10-21 23:50:38 +0900

    [Green] Update App.jsx for Implement
    5강 비동기 - 5주 차 과제 1
    - App.jsx 업데이트.
    - 테스트 코드 작성 후 UI 구현.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 
 export default function App() {
   return (
-    <></>
+    <div>
+      <ul>
+        <li><button type="button">서울</button></li>
+        <li><button type="button">대전</button></li>
+        <li><button type="button">대구</button></li>
+        <li><button type="button">부산</button></li>
+        <li><button type="button">광주</button></li>
+        <li><button type="button">강원도</button></li>
+        <li><button type="button">인천</button></li>
+      </ul>
+      <ul>
+        <li><button type="button">한식</button></li>
+        <li><button type="button">중식</button></li>
+        <li><button type="button">일식</button></li>
+        <li><button type="button">양식</button></li>
+        <li><button type="button">분식</button></li>
+      </ul>
+    </div>
   );
 }
