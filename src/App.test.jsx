3d876f594f33a184be7c45ea82cc02d212b89823
goodzzong <goodzzong@gmail.com>
2020-10-22 00:04:16 +0900

    CategoriesContainer & Categories 를 구현

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,14 +6,16 @@ import { render } from '@testing-library/react';
 
 import App from './App';
 
+import restaurants from '../fixtures/restaurants';
+
+import categories from '../fixtures/categories';
+
 jest.mock('react-redux');
 
 test('App', () => {
   useSelector.mockImplementation((selector) => selector({
-    restaurants: [
-      { id: 1, name: '홍콩반점' },
-      { id: 2, name: '한국식 초밥' },
-    ],
+    restaurants,
+    categories,
   }));
   const { getByText } = render((
     <App />
---
+++ src/Categories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Categories({ categories }) {
+  return (
+    <>
+      <ul>
+        {categories.map((categorie) => (
+          <li key={categorie.id}>
+            {categorie.name}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
