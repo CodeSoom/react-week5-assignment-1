9b749087f7877aa1e70c02a6d8fa6ed33654c31e
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-21 23:11:14 +0900

    [Red] Add loadCategories test to action

--- src/redux/actions.test.js
+++ src/redux/actions.test.js
@@ -3,11 +3,13 @@ import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
 
 import regions from '../../fixtures/regions';
+import categories from '../../fixtures/categories';
 
 import {
   loadRegions,
   setRegions,
   updateLoading,
+  loadCategories,
 } from './actions';
 
 jest.mock('../services/api');
@@ -15,16 +17,30 @@ jest.mock('../services/api');
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
-test('loadRegions', async () => {
-  const store = mockStore({});
+describe('actions', () => {
+  it('loadRegions', async () => {
+    const store = mockStore({});
 
-  await store.dispatch(loadRegions());
+    await store.dispatch(loadRegions());
 
-  const actions = store.getActions();
+    const actions = store.getActions();
 
-  expect(actions).toEqual([
-    updateLoading(true),
-    setRegions(regions),
-    updateLoading(false),
-  ]);
+    expect(actions).toEqual([
+      updateLoading(true),
+      setRegions(regions),
+      updateLoading(false),
+    ]);
+  });
+
+  it('loadCategories', async () => {
+    const store = mockStore({});
+
+    await store.dispatch(loadCategories());
+
+    const actions = store.getActions();
+
+    expect(actions).toEqual([
+      setCategories(categories),
+    ]);
+  });
 });
