a56f86a4ac8d41091b9fae3429b51863583a32ce
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-21 14:55:23 +0900

    Fix RegionsContainer.test.jsx

--- src/RegionsContainer.test.jsx
+++ src/RegionsContainer.test.jsx
@@ -5,13 +5,17 @@ import { render } from '@testing-library/react';
 import RegionsContainer from './RegionsContainer';
 
 test('RegionsContainer', () => {
-  const Regions = ['서울', '대전', '대구'];
+  const regions = [
+    { id: 1, name: '서울' },
+    { id: 2, name: '대전' },
+    { id: 3, name: '대구' },
+  ];
 
   const { getByText } = render((
     <RegionsContainer />
   ));
 
-  Regions.forEach((region) => {
-    expect(getByText(region)).not.toBeNull();
+  regions.forEach(({ name }) => {
+    expect(getByText(name)).not.toBeNull();
   });
 });
