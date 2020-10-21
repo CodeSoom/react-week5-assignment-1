3926e31353f6ca6c3d5c29354e5166d44ba1d8cc
juunini <juuni.ni.i@gmail.com>
2020-10-21 23:32:27 +0900

    Add FoodLocationList in click event test [Red]

--- src/components/FoodLocationList.test.jsx
+++ src/components/FoodLocationList.test.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import FoodLocationList from './FoodLocationList';
 import locales from '../../fixtures/locales';
 import foodCategories from '../../fixtures/foodCategories';
 
 describe('List', () => {
+  const handleClick = jest.fn();
+  const renderFoodLocationList = ({ items }) => render(
+    <FoodLocationList
+      items={items}
+      onClick={handleClick}
+    />,
+  );
+
   context('with locales', () => {
     it('see "서울"', () => {
-      render(<FoodLocationList items={locales} />);
+      renderFoodLocationList({ items: locales });
 
       expect(screen.getByText('서울')).toBeInTheDocument();
     });
@@ -16,9 +24,19 @@ describe('List', () => {
 
   context('with foodCategories', () => {
     it('see "한식"', () => {
-      render(<FoodLocationList items={foodCategories} />);
+      renderFoodLocationList({ items: foodCategories });
 
       expect(screen.getByText('한식')).toBeInTheDocument();
     });
+  });
+
+  it('click test', () => {
+    renderFoodLocationList({ items: locales });
+
+    expect(handleClick).not.toBeCalled();
+
+    fireEvent.click(screen.getByRole('button', { name: '서울' }));
+
+    expect(handleClick).toBeCalled();
   });
 });
