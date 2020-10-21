4bcf3f3aceaf52cc956a5dd7e329761f4045f4e9
Seungmin Lee <seungmin@uxplus.dev>
2020-10-21 21:11:31 +0900

    Add code
    Add code to render `RegionsContainer`.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import RegionsContainer from './RegionsContainer';
 
 export default function App() {
   return (
-    <></>
+    <>
+      <RegionsContainer />
+    </>
   );
 }
