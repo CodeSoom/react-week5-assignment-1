730b0393912f01cd9d4856ddb913e669f627b14e
goodzzong <goodzzong@gmail.com>
2020-10-21 23:35:00 +0900

    RestaurantsContainer 를 구현

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: [
+      { id: 1, name: '홍콩반점' },
+      { id: 2, name: '한국식 초밥' },
+    ],
+  }));
   const { getByText } = render((
     <App />
   ));
