9d23f5143ef9ac6d448d07fb3282393ec1d586c9
Noah <mike1124@naver.com>
2020-10-21 16:36:13 +0900

    [Red] Add test code about reducer

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -2,6 +2,8 @@ import reducer from './reducer';
 
 import categories from './fixtures/categories';
 
+import regions from './fixtures/categories';
+
 import { checkCategories } from './actions';
 
 describe('reducer', () => {
@@ -11,6 +13,15 @@ describe('reducer', () => {
         checkCategories({ id: 1, isChecked: false }));
 
       expect(state.categories[0].isChecked).toBe(true);
+    });
+  });
+
+  describe('checkRegions', () => {
+    it('changes Regions isChecked property', () => {
+      const state = reducer({ regions },
+        checkCategories({ id: 1, isChecked: false }));
+
+      expect(state.regions[0].isChecked).toBe(true);
     });
   });
 });
