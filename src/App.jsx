f4a4733b2acdefef80617e4f7789c0429765b300
oceanide <oceanide@gmail.com>
2020-10-21 13:43:38 +0900

    [REFACTOR] use dispatch with loadRegions in useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,13 @@ import { useDispatch } from 'react-redux';
 
 import RegionsContainer from './RegionsContainer';
 
-import { fetchRegions } from './services/api';
-
-import { setRegions } from './actions';
-
-async function loadRegions({ dispatch }) {
-  const regions = await fetchRegions();
-
-  dispatch(setRegions(regions));
-}
+import { loadRegions } from './actions';
 
 export default function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    loadRegions({ dispatch });
+    dispatch(loadRegions());
   }, []);
 
   return (
