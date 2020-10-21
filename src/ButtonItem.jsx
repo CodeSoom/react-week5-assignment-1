005c86bd7afedd409334bbbade78b5726da52ec2
hyeonhyejin <dev.hyejin04@gmail.com>
2020-10-21 20:10:32 +0900

    Refactor the Button compnent
    I change the name of Button component to the ButtonItem.
    Accordig to change, I refactor the Button component.

--- src/ButtonItem.jsx
+++ src/ButtonItem.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function Button({ children, onClick }) {
   return (
-    <button type="button" onClick={onClick}>
-      {children}
-    </button>
+    <li>
+      <button type="button" onClick={onClick}>
+        {children}
+      </button>
+    </li>
   );
 }
--- src/ButtonList.jsx
+++ src/ButtonList.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import Button from './Button';
+import Button from './ButtonItem';
 
 export default function ButtonList({ labels, onClick }) {
   return (
     <ul>
       {
         labels.map(({ id, name }) => (
-          <li><Button key={id} onClick={onClick}>{name}</Button></li>
+          <Button key={id} onClick={onClick}>{name}</Button>
         ))
       }
     </ul>
