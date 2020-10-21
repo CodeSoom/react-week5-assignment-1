8330911416a8f9ee6d2e18baef2feb7d7aa8572a
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-21 23:05:27 +0900

    [Refactor] Extract common logic

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,15 @@
-export async function fetchRegions() {
-  const url = 'https://eatgo-customer-api.ahastudio.com/regions';
+async function fetchData(url) {
   const response = await fetch(url);
   const data = response.json();
   return data;
 }
 
+export async function fetchRegions() {
+  const url = 'https://eatgo-customer-api.ahastudio.com/regions';
+  return fetchData(url);
+}
+
 export async function fetchCategories() {
   const url = 'https://eatgo-customer-api.ahastudio.com/categories';
-  const response = await fetch(url);
-  const data = response.json();
-  return data;
+  return fetchData(url);
 }
