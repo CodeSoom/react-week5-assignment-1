c6eeac39b77f08b6bd7d9155b6fe7dc49fdf2210
oceanide <oceanide@gmail.com>
2020-10-22 08:32:58 +0900

    [RED] add testcase for setCategories

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,6 +1,9 @@
 import reducer from './reducer';
 
-import { setRegions } from './actions';
+import {
+  setRegions,
+  setCategories,
+} from './actions';
 
 describe('reducer', () => {
   context('setRegions', () => {
@@ -16,6 +19,22 @@ describe('reducer', () => {
       const state = reducer(initialState, setRegions(regions));
 
       expect(state.regions).toHaveLength(1);
+    });
+  });
+
+  context('setCategories', () => {
+    it('changes categories', () => {
+      const initialState = {
+        categories: [],
+      };
+
+      const categories = [
+        { id: 1, name: '한식' },
+      ];
+
+      const state = reducer(initialState, setCategories(categories));
+
+      expect(state.categories).toHaveLength(1);
     });
   });
 });
