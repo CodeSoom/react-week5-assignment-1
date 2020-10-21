e01faff09da543f0fc7ec015d1377185b59467c1
oceanide <oceanide@gmail.com>
2020-10-21 22:47:20 +0900

    [RED] add Categories.test.jsx

---
+++ src/Categories.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Categories from './Categories';
+
+test('<Categories>', () => {
+  const { getByText } = render(<Categories />);
+
+  expect(getByText('한식')).not.toBeNull();
+});
