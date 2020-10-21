185bf9399ba7857c0c7c020b8d4a805aff3130d4
goodzzong <goodzzong@gmail.com>
2020-10-21 23:38:57 +0900

    Restaurants 에 대한 테스트 코드 추가
    fixtures 폴더 추가

--- src/Restaurants.test.jsx
+++ src/Restaurants.test.jsx
@@ -4,10 +4,15 @@ import { render } from '@testing-library/react';
 
 import Restaurants from './Restaurants';
 
+import restaurants from '../fixtures/restaurants';
+
 test('RestaurantsContainer', () => {
   const { getByText } = render((
-    <Restaurants />
+    <Restaurants
+      restaurants={restaurants}
+    />
   ));
 
   expect(getByText(/홍콩반점/)).not.toBeNull();
+  expect(getByText(/한국식 초밥/)).not.toBeNull();
 });
--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -6,14 +6,13 @@ import { render } from '@testing-library/react';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
+import restaurants from '../fixtures/restaurants';
+
 jest.mock('react-redux');
 
 test('RestaurantsContainer', () => {
   useSelector.mockImplementation((selector) => selector({
-    restaurants: [
-      { id: 1, name: '홍콩반점' },
-      { id: 2, name: '한국식 초밥' },
-    ],
+    restaurants,
   }));
 
   const { getByText } = render((
