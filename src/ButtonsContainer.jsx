82ee450a611507c5ba9bbf350943432554911a54
wjy5446 <wjy5446@gmail.com>
2020-10-21 23:59:57 +0900

    feat) ButtonsContainer 컴포넌트 추가

---
+++ src/ButtonsContainer.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { setSelectInfo } from './actions';
+
+import Buttons from './Buttons';
+
+export default function ButtonsContainer({ type }) {
+
+  const dispatch = useDispatch();
+
+  const { buttonNames, selectedButton } = useSelector((state) => ({
+    buttonNames: state[type + 's'],
+    selectedButton: state.selectInfo[type],
+  }));
+
+  function handleClickSelect(event) {
+    const updateInfo = {
+      type: event.target.name,
+      value: event.target.textContent
+    }
+
+    dispatch(setSelectInfo(updateInfo));
+  }
+
+  return (
+    <Buttons name={type} selectedButton={selectedButton} buttonNames={buttonNames} onClickSelect={handleClickSelect} />
+  );
+}
