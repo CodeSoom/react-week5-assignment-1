952b45ba846ccf7f2bf55072a47b4ba9711dc8f9
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-21 22:13:09 +0900

    [Green] RestaurantRegionContainer 컴포넌트 구현
    작성된 RestaurantRegionContainer 테스트에 따라 컴포넌트 구현.

---
+++ src/RestaurantRegionContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function RestaurantRegionContainer() {
+  return (
+    <div>
+      <h2>Restaurant Regions</h2>
+      <p>음식점 지역 정보를 등록해주세요!</p>
+      <ul>
+        <li>서울</li>
+        <li>경기</li>
+        <li>부산</li>
+      </ul>
+    </div>
+  );
+}
