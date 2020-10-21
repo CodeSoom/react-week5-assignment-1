03900fdb7c240d9caa8910a663582a0fb866ebaa
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-22 00:50:21 +0900

    [Red] RestaurantRegionContainer에 대한 selectRegion 테스트 추가
    If selects a region, check the region.
    *TODO: Refector RestaurantRegionContainer and Regions, App.

--- src/RestaurantRegionContainer.test.jsx
+++ src/RestaurantRegionContainer.test.jsx
@@ -1,15 +1,43 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { selectRegion } from './actions';
 
 import RestaurantRegionContainer from './RestaurantRegionContainer';
 
+jest.mock('react-redux');
+
 describe('RestaurantRegionContainer', () => {
-  it('shows title', () => {
-    const { getByText } = render((
-      <RestaurantRegionContainer />
-    ));
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
+  useSelector.mockImplementation((selector) => selector({
+    regions: [
+      { id: 1, name: '서울' },
+      { id: 2, name: '대전' },
+      { id: 3, name: '대구' },
+    ],
+  }));
+
+  it('shows a title', () => {
+    const { getByText } = render(<RestaurantRegionContainer />);
 
     expect(getByText('Restaurant Regions')).not.toBeNull();
+  });
+
+  describe('selects a region', () => {
+    it('checks selected region', () => {
+      const { getByText } = render(<RestaurantRegionContainer />);
+
+      fireEvent.click(getByText('서울'));
+
+      expect(dispatch).toBeCalledWith(selectRegion(1));
+    });
   });
 });
