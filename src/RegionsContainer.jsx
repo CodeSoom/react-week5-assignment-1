6648ce7cc18363f4791da009209d22c16ebac1c0
goodzzong <goodzzong@gmail.com>
2020-10-22 00:42:05 +0900

    RegionsContainer 를 구현

--- src/RegionsContainer.jsx
+++ src/RegionsContainer.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Regions from './Regions';
 
+import {
+  updateRegion,
+} from './actions';
+
 export default function RestaurantsContainer() {
+  const dispatch = useDispatch();
   const { regions } = useSelector((state) => ({
     regions: state.regions,
   }));
+
+  function handleClick(region) {
+    dispatch(updateRegion(region));
+  }
 
   return (
     <>
       <Regions
         regions={regions}
+        onClick={handleClick}
       />
     </>
   );
---
+++ src/actions.js
@@ -0,0 +1,13 @@
+export function updateRegion(region) {
+  return {
+    type: 'updateRegion',
+    payload: {
+      regionName: region,
+    },
+  };
+}
+
+// TODO : delete this.
+export function xxx() {
+
+}
