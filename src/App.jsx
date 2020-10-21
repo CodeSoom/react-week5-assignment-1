006045b960a906c4d003bccde75132b6024c387f
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-21 15:50:13 +0900

    Refactor App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
 
 import RegionsContainer from './RegionsContainer';
 
+import { loadRegions } from './actions';
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRegions());
+  }, []);
+
   return (
     <div>
       <RegionsContainer />
