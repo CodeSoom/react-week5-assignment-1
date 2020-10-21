e8d394b7fc9e950f459458da6e60168100a0243b
Noah <mike1124@naver.com>
2020-10-21 15:10:20 +0900

    Change Regions to RegionsContainer in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
 import React from 'react';
 
-import { useDispatch, useSelector } from 'react-redux';
-
 import CategoriesContainer from './CategoriesContainer';
 
-import Regions from './Regions';
+import RegionsContainer from './RegionsContainer';
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  const { regions } = useSelector((state) => ({
-
-    regions: state.regions,
-  }));
-
-  function onClick(id, isChecked) {
-    dispatch();
-    // [
-    //   ...regions.map((region) => (
-    //     region.id === id ? { ...region, isChecked: !isChecked } : region
-    //   )),
-    // ]
-  }
-
   return (
     <>
       <CategoriesContainer />
-      <Regions regions={regions} onClick={onClick} />
+      <RegionsContainer />
     </>
   );
 }
