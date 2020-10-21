46dd7f55a38e50cdea059d9ccf1678d8f2cb4c3a
wjy5446 <wjy5446@gmail.com>
2020-10-21 22:37:05 +0900

    feat) 선택된 Buttons에  (V)문구가 추가되는 테스트 코드 추가

--- src/Buttons.test.jsx
+++ src/Buttons.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { useSelector, useDispatch } from 'react-redux';
 import { fireEvent, render } from '@testing-library/react';
 
 import Buttons from './Buttons';
@@ -26,37 +27,21 @@ describe('Buttons', () => {
     expect(handleClickSelect).toBeCalled();
   });
 
-  it('버튼을 클릭하면 문구에 "(V)"가 추가됩니다.', () => {
-    const { getByText } = render((
-      <Buttons
-        buttonNameList={['서울']}
-        onClickSelect={handleClickSelect}
-      />));
+  context('selectButton이 주어지면', () => {
+    const buttonNameList = ['서울', '대전', '대구', '부산', '광주', '강원도', '인천'];
+    const selectButton = '서울';
 
-    const button = getByText('서울');
+    it('해당 버튼 문구에 "(V)"가 추가됩니다.', () => {
 
-    fireEvent.click(button);
-    expect(button).toHaveTextContent('서울(V)');
-  });
+      const { container } = render((
+        <Buttons
+          buttonNameList={buttonNameList}
+          selectButton={selectButton}
+        />));
 
-  it('버튼을 클릭하면 이전 선택 버튼 문구는 원래대로 돌아가고, 선택 버튼에 "(V)"가 추가됩니다.', () => {
-    const { getByText } = render((
-      <Buttons
-        buttonNameList={['서울', '대전']}
-        onClickSelect={handleClickSelect}
-      />));
-
-    const selectedButton = getByText('서울');
-    const nonSelectedButton = getByText('대전');
-
-    fireEvent.click(selectedButton);
-    expect(selectedButton).toHaveTextContent('서울(V)');
-    expect(nonSelectedButton).toHaveTextContent('대전');
-
-    fireEvent.click(selectedButton);
-    expect(selectedButton).toHaveTextContent('서울');
-    expect(selectedButton).toHaveTextContent('대전(V)');
-  });
+      expect(container).toHaveTextContent(selectButton + '(V)');
+    });
+  })
 
   context('ButtonsNameList가 주어지면,', () => {
     const buttonNameList = ['서울', '대전', '대구', '부산', '광주', '강원도', '인천'];
