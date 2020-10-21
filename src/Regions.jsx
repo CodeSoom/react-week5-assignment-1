90bea1a02185154d22e7d87c6051b89ce4e3882b
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-21 14:39:51 +0900

    Implement Regions.jsx

---
+++ src/Regions.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function Regions() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <button type="button">
+            서울
+          </button>
+        </li>
+        <li>
+          <button type="button">
+            대전
+          </button>
+        </li>
+        <li>
+          <button type="button">
+            대구
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
