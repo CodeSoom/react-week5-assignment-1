0f8f95073b6f5ce435ed22eab45f208118d42efa
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-22 00:12:32 +0900

    [Refactor] actions.js 생성
    Action creators를 모아둘 actions.js 생성.

---
+++ src/actions.js
@@ -0,0 +1,14 @@
+// Action creators
+export function selectRegion(id) {
+  return {
+    type: '',
+    payload: {
+      id,
+    },
+  };
+}
+
+// TODO: have to delete!
+export function temp() {
+
+}
