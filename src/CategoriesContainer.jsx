05be3c4f56426a5a1dda66d68e41fb7e7922d244
Seungmin Lee <seungmin@uxplus.dev>
2020-10-21 22:18:21 +0900

    Add code
    Add code to render `Categories`.

--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import Categories from './Categories';
+
 export default function CategoriesContainer() {
+  const categories = [];
+
   return (
-    <></>
+    <Categories categories={categories} />
   );
 }
