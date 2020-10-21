0917f09694a3921a492991ff5f3846b1d9b28508
Noah <mike1124@naver.com>
2020-10-21 15:05:52 +0900

    [Red] Add test code about RegionsContainer

---
+++ src/RegionsContainer.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { render } from '@testing-library/react';
+
+import RegionsContainer from './RegionsContainer';
+
+import regions from './fixtures/regions';
+
+jest.mock('react-redux');
+
+describe('RegionsContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    regions,
+  }));
+
+  it('show categories and regions', () => {
+    const { getByText } = render(<RegionsContainer />);
+
+    expect(getByText('한식')).not.toBeNull();
+  });
+});
