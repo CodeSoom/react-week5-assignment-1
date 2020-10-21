65b9d549fbccd1721617094f3cfc02f394fcd457
goodzzong <goodzzong@gmail.com>
2020-10-22 00:07:39 +0900

    Add regions object

---
+++ fixtures/regions.js
@@ -0,0 +1,20 @@
+const regions = [
+  {
+    id: 1, name: '서울',
+  },
+  {
+    id: 3, name: '대전',
+  },
+  {
+    id: 4, name: '대구',
+  },
+  {
+    id: 5, name: '부산',
+  },
+  {
+    id: 6, name: '광주',
+  },
+
+];
+
+export default regions;
