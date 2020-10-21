fbcea25a92722ad4b4f505cdf5981389c26b5239
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-21 22:36:32 +0900

    Regions에 대한 테스트 작성
    음식점 지역 정보를 리스터업할 Regions 컴포넌트에 대한 테스트 작성.
    1st, with one or more regions.
    2nd, without region.

---
+++ src/Regions.test.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import Regions from './Regions';
+
+describe('Regions', () => {
+  const handleClick = jest.fn();
+
+  function renderRegions(regions) {
+    return render((
+      <Regions
+        regions={regions}
+        onClick={handleClick}
+      />
+    ));
+  }
+
+  context('with one or more regions', () => {
+    const regions = [
+      { id: 1, region: '서울', checked: false },
+      { id: 2, region: '경기', checked: false },
+      { id: 3, region: '부산', checked: false },
+
+    ];
+
+    it('show regions of restaurant', () => {
+      const { getByText } = renderRegions(regions);
+
+      regions.forEach(({ region }) => {
+        expect(getByText(region)).not.toBeNull();
+      });
+    });
+
+    it('click region button then checked', () => {
+      const { getByText } = renderRegions(regions);
+
+      regions.forEach(({ region }) => {
+        const button = getByText(region);
+
+        expect(button).not.toBeNull();
+
+        fireEvent.click(button);
+      });
+
+      expect(handleClick).toBeCalledTimes(regions.length);
+    });
+  });
+
+  context('without region', () => {
+    it('show nothing message', () => {
+      const { getByText } = renderRegions([]);
+
+      expect(getByText(/음식점 지역 정보를 등록해주세요/)).not.toBeNull();
+    });
+  });
+});
