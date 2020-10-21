091ff30a7114c3b6a896698be9a3e53ae39f8f72
wjy5446 <wjy5446@gmail.com>
2020-10-21 21:26:42 +0900

    feat) 버튼 클릭 관련 테스트 코드 추가

--- src/Buttons.test.jsx
+++ src/Buttons.test.jsx
@@ -26,6 +26,38 @@ describe('Buttons', () => {
     expect(handleClickSelect).toBeCalled();
   });
 
+  it('버튼을 클릭하면 문구에 "(V)"가 추가됩니다.', () => {
+    const { getByText } = render((
+      <Buttons
+        buttonNameList={['서울']}
+        onClickSelect={handleClickSelect}
+      />));
+
+    const button = getByText('서울');
+
+    fireEvent.click(button);
+    expect(button).toHaveTextContent('서울(V)');
+  });
+
+  it('버튼을 클릭하면 이전 선택 버튼 문구는 원래대로 돌아가고, 선택 버튼에 "(V)"가 추가됩니다.', () => {
+    const { getByText } = render((
+      <Buttons
+        buttonNameList={['서울', '대전']}
+        onClickSelect={handleClickSelect}
+      />));
+
+    const selectedButton = getByText('서울');
+    const nonSelectedButton = getByText('대전');
+
+    fireEvent.click(selectedButton);
+    expect(selectedButton).toHaveTextContent('서울(V)');
+    expect(nonSelectedButton).toHaveTextContent('대전');
+
+    fireEvent.click(selectedButton);
+    expect(selectedButton).toHaveTextContent('서울');
+    expect(selectedButton).toHaveTextContent('대전(V)');
+  });
+
   context('ButtonsNameList가 주어지면,', () => {
     const buttonNameList = ['서울', '대전', '대구', '부산', '광주', '강원도', '인천'];
 
