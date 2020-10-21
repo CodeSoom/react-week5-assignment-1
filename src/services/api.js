64c63efe4fe6ca72154c43a79dd54596cd72e043
oceanide <oceanide@gmail.com>
2020-10-22 08:57:39 +0900

    [REFACTOR] add fetchCategories

--- src/services/api.js
+++ src/services/api.js
@@ -7,5 +7,11 @@ export async function fetchRegions() {
   return data;
 }
 
-// TODO
-export function fetchCategories() { }
+export async function fetchCategories() {
+  const url = 'https://eatgo-customer-api.ahastudio.com/categories';
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data;
+}
