4a911631c3edb3b99a29a99d520558802f77bad6
hyeonhyejin <dev.hyejin04@gmail.com>
2020-10-21 21:54:39 +0900

    [Red] Add a test case into ButtonList.test.jsx
    To implement what is drawn with (V) on the Button, I add a test case in ButtonList.test.jsx.
    ButtonList will receive 'checkedValue' props for draw differently the component corresponding to the id.

--- src/ButtonList.test.jsx
+++ src/ButtonList.test.jsx
@@ -7,16 +7,25 @@ import ButtonList from './ButtonList';
 import regions from '../fixtures/regions';
 
 describe('ButtonList', () => {
-  it('show buttons', () => {
-    const handleClick = jest.fn();
-    const { getByText } = render((
+  const handleClick = jest.fn();
+  function renderButtonList() {
+    return render((
       <ButtonList
         labels={regions}
         onClick={handleClick}
+        checkedValue={1}
       />
     ));
+  }
 
+  it('show buttons', () => {
+    const { getByText } = renderButtonList();
     expect(getByText('서울')).not.toBeNull();
     expect(getByText('부산')).not.toBeNull();
+  });
+
+  it('show checked value with (V)', () => {
+    const { getByText } = renderButtonList();
+    expect(getByText('서울(V)')).not.toBeNull();
   });
 });
