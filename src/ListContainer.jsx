54799414c80e9e201a0aa76e7c853e51bafecd63
saseungmin <dbd02169@naver.com>
2020-10-21 22:42:03 +0900

    ListContainer를 구현한다.

--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -2,32 +2,45 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { categoriesFixture, regionsFixture } from '../fixtures/fixtures';
-import { loadRestaurantInfo } from './actions';
+import { loadRestaurantInfo, updateRestaurant } from './actions';
 import CategoryList from './CategoryList';
 import RegionList from './RegionList';
 
 const ListContainer = () => {
   const dispatch = useDispatch();
 
-  const { categories, regions } = useSelector((state) => ({
+  const {
+    categories, regions, category, region,
+  } = useSelector((state) => ({
+    category: state.category,
     categories: state.categories,
+    region: state.region,
     regions: state.regions,
   }));
 
   const loadRestaurantDispatch = ({ type, info }) => dispatch(loadRestaurantInfo({ type, info }));
+  const updateRestaurantDispatch = (stateId) => dispatch(updateRestaurant(stateId));
 
   useEffect(() => {
     loadRestaurantDispatch({ type: 'categories', info: categoriesFixture });
     loadRestaurantDispatch({ type: 'regions', info: regionsFixture });
   }, []);
 
+  const handleClickButton = (state) => {
+    updateRestaurantDispatch(state);
+  };
+
   return (
     <>
       <RegionList
         regions={regions}
+        onSelectRegionClick={handleClickButton}
+        regionState={region}
       />
       <CategoryList
         categories={categories}
+        onSelectCategoryClick={handleClickButton}
+        categoryState={category}
       />
     </>
   );
