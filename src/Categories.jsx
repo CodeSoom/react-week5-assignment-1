5beab8c9568e0905022bfc2a825b75a7cf3ff9e6
oceanide <oceanide@gmail.com>
2020-10-21 23:18:01 +0900

    [REFACTOR] add categories props

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-export default function Categories() {
+export default function Categories({ categories }) {
   return (
     <ul>
-      <li>
-        <button type="button">한식</button>
-      </li>
+      {categories.map(({ id, name }) => (
+        <li key={id}>
+          <button type="button">{name}</button>
+        </li>
+      ))}
     </ul>
   );
 }
--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 import Categories from './Categories';
 
 export default function CategoriesContainer() {
+  const categories = [
+    { id: 1, name: '한식' },
+  ];
+
   return (
-    <Categories />
+    <Categories categories={categories} />
   );
 }
