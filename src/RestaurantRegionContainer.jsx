5b310e9589a80c397503dd227e567ee5955386c9
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-22 00:57:45 +0900

    [Green] RestaurantRegionContainer 컴포넌트에 리덕스 적용
    useDispatch, useSelector를 통해 selectRegion action creator를 dispatch에 등록.

--- src/RestaurantRegionContainer.jsx
+++ src/RestaurantRegionContainer.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
+import { selectRegion } from './actions';
+
 import Regions from './Regions';
 
 export default function RestaurantRegionContainer() {
-  const regions = [];
+  const { selectedRegionId, regions } = useSelector((state) => ({
+    selectedRegionId: state.selectedRegionId,
+    regions: state.regions,
+  }));
 
-  function handleClick() {
-    return null;
+  const dispatch = useDispatch();
+
+  function handleClick(id) {
+    dispatch(selectRegion(id));
   }
 
   return (
     <div>
       <h2>Restaurant Regions</h2>
       <Regions
+        selected={selectedRegionId}
         regions={regions}
         onClick={handleClick}
       />
