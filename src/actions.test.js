8578e8aa47f3ff3cf43c0e62a85d178579c75dc8
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-21 12:32:15 +0900

    [green] Add loadCategories test for actions.

--- src/actions.test.js
+++ src/actions.test.js
@@ -3,14 +3,18 @@ import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
 
 import regions from '../__fixtures__/regions';
+import categories from '../__fixtures__/categories';
 
 import {
   fetchRegions,
+  fetchCategories,
 } from './services/api';
 
 import {
   loadRegions,
+  loadCategories,
   setRegions,
+  setCategories,
 } from './actions';
 
 jest.mock('./services/api');
@@ -30,6 +34,20 @@ describe('actions', () => {
       const actions = store.getActions();
 
       expect(actions[0]).toEqual(setRegions(regions));
+    });
+  });
+
+  describe('loadCategories', () => {
+    it('dispatches fetched categories data', async () => {
+      fetchCategories.mockResolvedValue(categories);
+
+      const store = mockStore({});
+
+      await store.dispatch(loadCategories());
+
+      const actions = store.getActions();
+
+      expect(actions[0]).toEqual(setCategories(categories));
     });
   });
 });
