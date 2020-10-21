964eb26bde1afb000f726f056273a3787dffaa67
Seungmin Lee <seungmin@uxplus.dev>
2020-10-21 21:26:19 +0900

    Add code
    Add code to render `RestaurantsContainer`.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import RegionsContainer from './RegionsContainer';
 import CategoriesContainer from './CategoriesContainer';
+import RestaurantsContainer from './RestaurantsContainer';
 
 export default function App() {
   return (
     <>
       <RegionsContainer />
       <CategoriesContainer />
+      <RestaurantsContainer />
     </>
   );
 }
