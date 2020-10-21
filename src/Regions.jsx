4a9eac8e7cb0d3ae50bfda4990114507a15d7abb
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-21 14:51:43 +0900

    Refactor Regions.jsx

--- src/Regions.jsx
+++ src/Regions.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Regions({ regions }) {
   return (
     <ul>
-      {regions.map((region) => (
-        <li>
-          {region}
+      {regions.map(({ id, name }) => (
+        <li key={id}>
+          {name}
         </li>
       ))}
     </ul>
