fb39a3fd4b69ef96843966ab391002cd598890aa
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-22 14:40:24 +0900

    add setCategoreis test in reducer.test.js

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,8 +1,9 @@
 import reducer from './reducer';
 
-import { setRegions } from './actions';
+import { setRegions, setCategories } from './actions';
 
 import regions from '../__fixtures__/regions';
+import categories from '../__fixtures__/categories';
 
 describe('reducer', () => {
   describe('setRegions', () => {
@@ -13,6 +14,17 @@ describe('reducer', () => {
 
       const state = reducer(initialState, setRegions(regions));
       expect(state.regions).toHaveLength(3);
+    });
+  });
+
+  describe('setCategories', () => {
+    it('changes categories', () => {
+      const initialState = {
+        categories: [],
+      };
+
+      const state = reducer(initialState, setCategories(categories));
+      expect(state.categories).toHaveLength(3);
     });
   });
 });
