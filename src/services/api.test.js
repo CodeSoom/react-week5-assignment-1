78f41472ae073d76134485eee6b17ee94ff4a3b1
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-22 10:20:54 +0900

    [green] Add fetchRestaurants test for api.

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -2,10 +2,12 @@ import { enableFetchMocks } from 'jest-fetch-mock';
 import {
   fetchRegions,
   fetchCategories,
+  fetchRestaurants,
 } from './api';
 
 import regions from '../../__fixtures__/regions';
 import categories from '../../__fixtures__/categories';
+import restaurants from '../../__fixtures__/restaurants';
 
 describe('api', () => {
   enableFetchMocks();
@@ -29,6 +31,18 @@ describe('api', () => {
 
       const response = await fetchCategories();
       expect(response.data).toEqual(categories);
+    });
+  });
+
+  describe('fetchRestaurants', () => {
+    it('fetches restaurants data', async () => {
+      const selectedRegion = '서울';
+      const selectedCategory = 1;
+
+      fetch.mockResponse(JSON.stringify({ data: restaurants }));
+
+      const response = await fetchRestaurants({ selectedRegion, selectedCategory });
+      expect(response.data).toEqual(restaurants);
     });
   });
 });
