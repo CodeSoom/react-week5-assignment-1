7546a578bc4bdaae8ed870e8ea2a217005bd47f3
saseungmin <dbd02169@naver.com>
2020-10-23 02:09:39 +0900

    App 컴포넌트를 구현한다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import ListContainer from './ListContainer';
+import RestaurantContainer from './RestaurantContainer';
+
 export default function App() {
   return (
-    <></>
+    <>
+      <ListContainer />
+      <RestaurantContainer />
+    </>
   );
 }
