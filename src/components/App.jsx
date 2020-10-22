0b5d73490ba51563f837d909e80ee1b501974331
ParkDongJo <pdj10211@gmail.com>
2020-10-22 22:12:06 +0900

    [week5-2] App 컴포넌트 1차작성

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
+import { regions, categories, restaurants } from '../../fixtures';
+
 export default function App() {
   return (
-    <></>
+    <>
+      <ul>
+        {regions.map((region) => (
+          <li key={region.id}>
+            <button type="button">{region.name}</button>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {categories.map((category) => (
+          <li key={category.id}>
+            <button type="button">{category.name}</button>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {restaurants.map((restaurant) => (
+          <li key={restaurant.id}>
+            {restaurant.name}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import rootReducer from './reducers';
-import App from './App';
+import App from './components/App';
 
 const store = createStore(rootReducer);
 
