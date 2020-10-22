70af135e4af7dbf45ca533e5db82dd9f037cd4b7
oceanide <oceanide@gmail.com>
2020-10-22 12:32:12 +0900

    [GREEN] add onClick props in Regions

--- src/Regions.jsx
+++ src/Regions.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
-export default function Regions({ regions }) {
+export default function Regions({ regions, onClick }) {
   return (
     <ul>
       {regions.map(({ id, name }) => (
         <li key={id}>
-          <button type="button">{name}</button>
+          <button
+            type="button"
+            onClick={onClick}
+          >
+            {name}
+          </button>
         </li>
       ))}
     </ul>
