1704bc732152215508a80c47c6b91ec77182aada
Seungmin Lee <seungmin@uxplus.dev>
2020-10-22 10:51:54 +0900

    Add test code
    Add test code to see whether `Button` handles click.

--- src/Button.test.jsx
+++ src/Button.test.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Button from './Button';
 
+const { click } = fireEvent;
+
 describe('Button', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   const name = '서울';
+  const handleClick = jest.fn();
+
+  function renderButton() {
+    const { container, getByText } = render(
+      <Button
+        name={name}
+        onClick={handleClick}
+      />,
+    );
+
+    return {
+      container,
+      button: getByText('서울'),
+    };
+  }
 
   it('renders', () => {
-    const { container } = render(<Button name={name} />);
+    const { container } = renderButton();
 
     expect(container).not.toBeNull();
     expect(container).toHaveTextContent('서울');
+  });
+
+  it('handles click', () => {
+    const { button } = renderButton();
+
+    click(button);
+
+    expect(handleClick).toBeCalled();
   });
 });
