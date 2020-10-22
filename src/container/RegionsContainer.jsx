c8bdec1365f1489574736f279ef6a122b1cee54d
juunini <juuni.ni.i@gmail.com>
2020-10-23 00:24:47 +0900

    Add RegionsContainer test [Green]

---
+++ src/container/RegionsContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function RegionsContainer() {
+  return (
+    <ul>
+      <li>
+        <button type="button" name="서울">서울(V)</button>
+        <button type="button" name="대전">대전</button>
+      </li>
+    </ul>
+  );
+}
