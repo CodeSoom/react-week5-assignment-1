934bddcc65b9f300dc8ebfc473ca6476fa2c4230
ParkDongJo <pdj10211@gmail.com>
2020-10-22 22:48:07 +0900

    [week5-2] RegionsContainer 컴포넌트 구현

---
+++ src/containers/RegionsContainer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { useSelector } from 'react-redux';
+
+export default function RegionsContainer() {
+  const { regions } = useSelector((state) => ({
+    regions: state.regionStore.regions,
+  }));
+
+  return (
+    <ul>
+      {regions.map((region) => (
+        <li key={region.id}>
+          <button type="button">{region.name}</button>
+        </li>
+      ))}
+    </ul>
+  );
+}
