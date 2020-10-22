02013561fb52e227c2ac1eb7d004cb034403f3a2
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-22 14:29:43 +0900

    Refactor Categories.jsx

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-export default function Categories() {
+export default function Categories({ categories }) {
   return (
-    <div>
-      <ul>
-        <li><button type="button">한식</button></li>
-        <li><button type="button">중식</button></li>
-        <li><button type="button">일식</button></li>
-      </ul>
-    </div>
+    <ul>
+      {categories.map(({ id, name }) => (
+        <li key={id}>
+          <button type="button">
+            {name}
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 }
