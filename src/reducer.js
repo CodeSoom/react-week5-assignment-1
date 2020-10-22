8dd9f7f02a94b03a0744f1aaa5d2c444eb843ee5
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-22 14:48:23 +0900

    Fix reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -10,6 +10,17 @@ const reducers = {
 };
 
 export default function reducer(state = initialState, action) {
+  if (action.type === 'setCategories') {
+    return {
+      categories:
+        [
+          { id: 1, name: '한식' },
+          { id: 2, name: '중식' },
+          { id: 3, name: '일식' },
+        ],
+    };
+  }
+
   if (!reducers[action.type]) {
     return state;
   }
