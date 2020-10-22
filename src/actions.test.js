29e31be8d14d6ee6fca3dfc2ac1133aaf72e5021
saseungmin <dbd02169@naver.com>
2020-10-23 00:53:17 +0900

    action에 대한 테스트를 구현한다.
    - async "loadRestaurantsName" call test

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,9 +1,11 @@
-import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import configureStore from 'redux-mock-store';
 
-import { loadRestaurants, setRestaurants } from './actions';
-import { categoriesFixture } from '../fixtures/fixtures';
-import { fetchRestaurants } from './services/api';
+import {
+  loadRestaurants, setRestaurants, setRestaurantsName, loadRestaurantsName,
+} from './actions';
+import { categoriesFixture, restaurantFixture } from '../fixtures/fixtures';
+import { fetchRestaurants, fetchRestaurantsName } from './services/api';
 
 jest.mock('./services/api');
 
@@ -11,16 +13,32 @@ const middlewares = [thunk]; // NOTE: thunk 미들웨어 추가
 const mockStore = configureStore(middlewares);
 
 describe('actions', () => {
-  it('async "loadRestaurants" call test', async () => {
-    // NOTE: mockResolvedValue 비동기 테스트에서 비동기 함수를 모의하는 데 유용
-    fetchRestaurants.mockResolvedValue(categoriesFixture);
+  describe('loadRestaurants', () => {
+    it('async "loadRestaurants" call test', async () => {
+      // NOTE: mockResolvedValue 비동기 테스트에서 비동기 함수를 모의하는 데 유용
+      fetchRestaurants.mockResolvedValue(categoriesFixture);
 
-    const store = mockStore({});
+      const store = mockStore({});
 
-    await store.dispatch(loadRestaurants('categories'));
+      await store.dispatch(loadRestaurants('categories'));
 
-    const actions = store.getActions();
+      const actions = store.getActions();
 
-    expect(actions[0]).toEqual(setRestaurants({ type: 'categories', info: categoriesFixture }));
+      expect(actions[0]).toEqual(setRestaurants({ type: 'categories', info: categoriesFixture }));
+    });
+  });
+
+  describe('loadRestaurantsName', () => {
+    it('async "loadRestaurantsName" call test', async () => {
+      fetchRestaurantsName.mockResolvedValue(restaurantFixture);
+
+      const store = mockStore({});
+
+      await store.dispatch(loadRestaurantsName({ region: '서울', category: 1 }));
+
+      const actions = store.getActions();
+
+      expect(actions[0]).toEqual(setRestaurantsName(restaurantFixture));
+    });
   });
 });
