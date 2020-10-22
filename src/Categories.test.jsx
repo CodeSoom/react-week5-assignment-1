ce588d0fdcb208f42da58bc4b3a35f94371ee4fa
goodzzong <goodzzong@gmail.com>
2020-10-22 23:17:55 +0900

    Categories 에 대한 테스트 코드 추가

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import Categories from './Categories';
 
 import categories from '../fixtures/categories';
 
 test('Categories', () => {
+  const handleClick = jest.fn();
+
   const { getByText } = render((
     <Categories
       categories={categories}
+      onClick={handleClick}
     />
   ));
+
+  fireEvent.click(getByText(/한식/));
+
+  expect(handleClick).toBeNull();
 
   expect(getByText(/한식/)).not.toBeNull();
   expect(getByText(/중식/)).not.toBeNull();
