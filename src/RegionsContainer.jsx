961537369ca37c0cfa8b9626a57d695ea2506bd2
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-22 17:25:48 +0900

    Add setSelectedRegionName in RegionsContainer.jsx

--- src/RegionsContainer.jsx
+++ src/RegionsContainer.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Regions from './Regions';
+
+import { setSelectedRegionName } from './actions';
 
 export default function RegionsContainer() {
   const { regions } = useSelector((state) => ({
     regions: state.regions,
   }));
 
+  const dispatch = useDispatch();
+
+  function handleClick(event) {
+    const regionName = event.target.innerText;
+
+    dispatch(setSelectedRegionName(regionName));
+  }
   return (
-    <Regions regions={regions} />
+    <Regions regions={regions} onClick={handleClick} />
   );
 }
