a0c318db09be2fa8444d69c7e2785eade2530d4f
oceanide <oceanide@gmail.com>
2020-10-22 12:31:18 +0900

    [RED] add onclick testcase for Regions

--- src/Regions.test.jsx
+++ src/Regions.test.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Regions from './Regions';
 
 test('Regions', () => {
+  const handleClick = jest.fn();
+
   const regions = [
     { id: 1, name: '서울' },
   ];
 
   const { getByText } = render(
-    <Regions regions={regions} />,
+    <Regions regions={regions} onClick={handleClick} />,
   );
 
   expect(getByText('서울')).not.toBeNull();
+
+  expect(handleClick).not.toBeCalled();
+
+  fireEvent.click(getByText('서울'));
+
+  expect(handleClick).toBeCalled();
 });
