f0be9cf60dfbc6c778e816a1ad67f9a268474294
wjy5446 <wjy5446@gmail.com>
2020-10-22 23:43:12 +0900

    refac) renderButtons 함수 생성

--- src/Buttons.test.jsx
+++ src/Buttons.test.jsx
@@ -11,13 +11,15 @@ describe('Buttons', () => {
 
   const handleClickSelect = jest.fn();
 
-  it('버튼을 클릭하면 handleClickSelect 함수가 출력됩니다.', () => {
-    const { getByText } = render((
-      <Buttons
-        buttonNames={[{ id: 1, name: '서울' }]}
-        onClickSelect={handleClickSelect}
-      />));
+  const renderButtons = (buttonNames = [{ id: 1, name: '서울' }], selectedButton = '') => (
+    render(<Buttons
+      buttonNames={buttonNames}
+      selectedButton={selectedButton}
+      onClickSelect={handleClickSelect}
+    />));
 
+  it('버튼을 클릭하면 handleClickSelect 함수가 출력됩니다.', () => {
+    const { getByText } = renderButtons();
     const button = getByText('서울');
 
     expect(handleClickSelect).not.toBeCalled();
@@ -30,11 +32,7 @@ describe('Buttons', () => {
     const selectedButton = '서울';
 
     it('해당 버튼 문구에 "(V)"가 추가됩니다.', () => {
-      const { container } = render((
-        <Buttons
-          buttonNames={buttonNames}
-          selectedButton={selectedButton}
-        />));
+      const { container } = renderButtons(buttonNames, selectedButton);
 
       expect(container).toHaveTextContent(`${selectedButton}(V)`);
     });
@@ -44,7 +42,7 @@ describe('Buttons', () => {
     const buttonNames = [{ id: 1, name: '서울' }, { id: 2, name: '대전' }];
 
     it('ButtonsNameList의 이름을 가진 버튼들이 출력됩니다.', () => {
-      const { container } = render(<Buttons buttonNames={buttonNames} />);
+      const { container } = renderButtons(buttonNames);
 
       buttonNames.forEach(({ name }) => {
         expect(container).toHaveTextContent(name);
