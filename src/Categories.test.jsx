0751764f90f0f240a0f8890884b24b9242f67915
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-22 16:07:54 +0900

    Add click test in Categories.test.jsx

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Categories from './Categories';
 
 import categories from '../__fixtures__/categories';
 
 test('Categories', () => {
+  const handleClick = jest.fn();
+
   const { getByText } = render((
-    <Categories categories={categories} />
+    <Categories categories={categories} onClick={handleClick} />
   ));
 
   categories.forEach(({ name }) => {
     expect(getByText(name)).not.toBeNull();
   });
+
+  fireEvent.click(getByText(categories[0].name));
+
+  expect(handleClick).toBeCalledWith(1);
 });
