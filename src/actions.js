e9a73e5f50ccedb65ddfeabb62ed3c916ce4e2f3
saseungmin <dbd02169@naver.com>
2020-10-23 00:01:15 +0900

    action을 'loadRestaurantsName' 추가한다.

--- src/actions.js
+++ src/actions.js
@@ -17,3 +17,8 @@ export const loadRestaurants = (path) => async (dispatch) => {
   const response = await fetchRestaurants(path);
   dispatch(setRestaurants({ type: path, info: response }));
 };
+
+export const loadRestaurantsName = (payload) => ({
+  type: 'loadRestaurantsName',
+  payload,
+});
