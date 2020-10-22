295670b39708de6e92cfad72c857e96b4b50fa07
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-22 14:28:55 +0900

    Implement Categories.jsx

---
+++ src/Categories.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Categories() {
+  return (
+    <div>
+      <ul>
+        <li><button type="button">한식</button></li>
+        <li><button type="button">중식</button></li>
+        <li><button type="button">일식</button></li>
+      </ul>
+    </div>
+  );
+}
