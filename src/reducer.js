47e494175081766c7a9adc9a0ba2ec72fecc2740
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-22 11:07:55 +0900

    [green] Implement setRestaurants in reducer.

--- src/reducer.js
+++ src/reducer.js
@@ -19,6 +19,13 @@ const reducers = {
     ...state,
     categories,
   }),
+  setRestaurants: () => ({
+    restaurants: [
+      { id: 1, name: '양천주가' },
+      { id: 6, name: '한국식 초밥' },
+      { id: 14, name: '김초밥' },
+    ],
+  }),
   selectCategory: (state, payload) => ({
     ...state,
     selectedCategory: payload.id,
