88df4ece27defb974afb70a31c5bc7204c98dbb2
Noah <mike1124@naver.com>
2020-10-22 19:21:45 +0900

    [Red] Add test code check mark in Region

--- src/Regions.test.jsx
+++ src/Regions.test.jsx
@@ -1,15 +1,49 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import Regions from './Regions';
 
 describe('Regions', () => {
-  const regions = [];
+  const handleClick = jest.fn();
+
+  const regions = [
+    { id: 1, name: '서울' },
+  ];
+
+  const renderRegions = (name = '') => render(
+    <Regions
+      regions={regions}
+      regionName={name}
+      onClick={handleClick}
+    />,
+  );
 
   it('show all regions', () => {
-    const { queryByText } = render(<Regions regions={regions} />);
+    const { queryByText } = renderRegions();
 
-    expect(queryByText('서울')).toBeNull();
+    expect(queryByText('서울')).not.toBeNull();
+  });
+
+  context('click category', () => {
+    it('calls handleClick function', () => {
+      const { getByText } = renderRegions();
+
+      fireEvent.click(getByText('서울'));
+
+      expect(handleClick).toBeCalled();
+    });
+
+    it('check mark in category', () => {
+      const { getByText } = renderRegions();
+
+      fireEvent.click(getByText('서울'));
+
+      const { container } = renderRegions('서울');
+
+      expect(handleClick).toBeCalled();
+
+      expect(container).toHaveTextContent('서울(V)');
+    });
   });
 });
