b73362732acea4363ded3a818ae68e67bdebddbb
ParkDongJo <pdj10211@gmail.com>
2020-10-23 22:27:34 +0900

    [week-5] RegionCotainer에 Region 컴포넌트 적용하기

--- src/containers/RegionsContainer.jsx
+++ src/containers/RegionsContainer.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 
+import Region from '../components/Region';
+
 export default function RegionsContainer() {
   const { regions } = useSelector((state) => ({
     regions: state.region.regions,
   }));
 
+  function handleClick() {
+    // TODO
+  }
+
   return (
     <ul>
       {regions.map((region) => (
-        <li key={region.id}>
-          <button type="button">{region.name}</button>
-        </li>
+        <Region
+          key={region.id}
+          name={region.name}
+          onClick={handleClick}
+        />
       ))}
     </ul>
   );
