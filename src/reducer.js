38953be58a5f75d92a047c3e466f91f57ec30dee
wjy5446 <wjy5446@gmail.com>
2020-10-24 01:06:41 +0900

    feat) 코드 수정

--- src/reducer.js
+++ src/reducer.js
@@ -10,11 +10,15 @@ export const initialState = {
 
 export default function reducer(state = initialState, action) {
   const router = {
-    intializeSelectButtons: () => ({
-      ...state,
-      regions: [{ id: 1, name: '서울' }, { id: 2, name: '대전' }, { id: 3, name: '대구' }, { id: 4, name: '부산' }, { id: 5, name: '광주' }, { id: 6, name: '강원도' }, { id: 7, name: '인천' }],
-      categories: [{ id: 1, name: '한식' }, { id: 2, name: '중식' }, { id: 3, name: '일식' }, { id: 4, name: '양식' }, { id: 5, name: '분식' }, { id: 6, name: '과자' }, { id: 7, name: '치킨' }],
-    }),
+    intializeSelectButtons: () => {
+      const { regions, categories } = action.payload;
+
+      return {
+        ...state,
+        regions,
+        categories,
+      };
+    },
 
     setSelectedButton: () => {
       const { type, value } = action.payload;
@@ -27,6 +31,11 @@ export default function reducer(state = initialState, action) {
         },
       };
     },
+
+    setRestaurants: () => ({
+      ...state,
+      restaurants: action.payload,
+    }),
   };
 
   return (router[action.type] || (() => state))();
