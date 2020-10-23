3d0011b03e968a8176dfffdd5abab09d5b28b229
ParkDongJo <pdj10211@gmail.com>
2020-10-23 17:06:21 +0900

    [week5-2] reducer에 setRegions 함수 추가하기

--- src/reducers/region.js
+++ src/reducers/region.js
@@ -1,8 +1,16 @@
 const initialState = {
   regions: [],
+  selectedIdx: -1,
 };
 
 const region = (state = initialState, action) => {
+  if (action.type === 'setRegions') {
+    const { regions } = action.payload;
+    return {
+      state,
+      regions,
+    };
+  }
   return state;
 };
 
