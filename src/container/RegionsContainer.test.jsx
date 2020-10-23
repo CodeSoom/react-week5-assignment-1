c1e6fb182008f0cb3c01205c1a3fd93744776a2c
juunini <juuni.ni.i@gmail.com>
2020-10-24 01:23:49 +0900

    Add redux test in container/RegionsContainer

--- src/container/RegionsContainer.test.jsx
+++ src/container/RegionsContainer.test.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { render, screen } from '@testing-library/react';
 
 import RegionsContainer from './RegionsContainer';
+import regions from '../../fixtures/regions';
+
+jest.mock('react-redux');
 
 test('RegionsContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+  useSelector.mockImplementation((selector) => selector({
+    regions,
+  }));
+
   render(<RegionsContainer />);
 
   expect(screen.getByRole('button', { name: /서울/ })).toBeInTheDocument();
