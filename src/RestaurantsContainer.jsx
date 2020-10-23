198742295bf330e0fb4836e9223a2bf95e6a8ad1
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-23 14:43:21 +0900

    Implement RestaurantsContainer.jsx

---
+++ src/RestaurantsContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function CategoriesContainer() {
+  return (
+    <div>
+      <ul>
+        <li>양천주가</li>
+        <li>한국식 초밥</li>
+        <li>김초밥</li>
+      </ul>
+    </div>
+  );
+}
