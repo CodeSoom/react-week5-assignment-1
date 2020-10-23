2a474e79fb687b7830d25e5c1ffea92c6160e1f1
thkim <thkim2017@gmail.com>
2020-10-23 20:15:41 +0900

    [red] Add test for App: expecting other city buttons

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -10,4 +10,10 @@ test('App', () => {
   ));
 
   expect(getByText('서울')).not.toBeNull();
+  expect(getByText('대전')).not.toBeNull();
+  expect(getByText('대구')).not.toBeNull();
+  expect(getByText('부산')).not.toBeNull();
+  expect(getByText('광주')).not.toBeNull();
+  expect(getByText('강원도')).not.toBeNull();
+  expect(getByText('인천')).not.toBeNull();
 });
