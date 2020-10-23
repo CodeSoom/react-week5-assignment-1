085ba182f3542598ce07b2aea070abd9b26a2592
oceanide <oceanide@gmail.com>
2020-10-23 17:20:19 +0900

    [REFACTOR] use Restaurants in RestaurantsContainer

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
+import Restaurants from './Restaurants';
+
 export default function RestaurantsContainer() {
   return (
-    <ul>
-      <li>양천주가</li>
-      <li>한국식 초밥</li>
-      <li>김초밥</li>
-    </ul>
+    <Restaurants />
   );
 }
