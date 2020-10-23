b948f70ae819c8778ccfbc9da9a430a3471abcd0
wjy5446 <wjy5446@gmail.com>
2020-10-23 20:36:02 +0900

    refac) reducer 테스트 코드에 호출하면 문구를 제거

--- src/actions.test.js
+++ src/actions.test.js
@@ -4,14 +4,14 @@ import {
 } from './actions';
 
 describe('intializeSelectButtons', () => {
-  it('호출하면 intializeSelectButtons action이 반환합니다. ', () => {
+  it('intializeSelectButtons action이 반환합니다. ', () => {
     const action = intializeSelectButtons();
     expect(action).toEqual({ type: 'intializeSelectButtons' });
   });
 });
 
 describe('setSelectedButton', () => {
-  it('호출하면 setSelectedButton action이 반환합니다. ', () => {
+  it('setSelectedButton action이 반환합니다. ', () => {
     const selectedButton = {
       type: 'lcation',
       value: '서울',
