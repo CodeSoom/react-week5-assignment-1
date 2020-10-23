a82ca296923ed98b1c3c8d65c5a09097f7d908cd
Seungmin Lee <seungmin@uxplus.dev>
2020-10-23 13:38:43 +0900

    Add test code
    Add test code for context, with action.

--- src/reducer.test.jsx
+++ src/reducer.test.jsx
@@ -1,16 +1,33 @@
 import reducer from './reducer';
 
-const initialState = {
-  filter: {
-    region: '',
-    categoryId: 0,
-  },
-};
+import { updateFilter } from './actions';
 
 describe('reducer', () => {
   context('without state nor action', () => {
+    const initialState = {
+      filter: {
+        region: '',
+        categoryId: 0,
+      },
+    };
     it('returns state', () => {
       expect(reducer()).toEqual(initialState);
+    });
+  });
+
+  context('with action', () => {
+    it('updates filter', () => {
+      const previousState = {
+        filter: {
+          region: '서울',
+          categoryId: 1,
+        },
+      };
+      const state = reducer(
+        previousState,
+        updateFilter({ filter: { region: '서울' } }),
+      );
+      expect(state.filter.region).toBe('서울');
     });
   });
 });
