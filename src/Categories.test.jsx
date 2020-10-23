4a30fdb5d7bea8ffc210efa1a66b00a7d25add23
oceanide <oceanide@gmail.com>
2020-10-23 09:53:43 +0900

    [REFACTOR] use categories fixture

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import categories from '../__fixtures__/categories';
+
 import Categories from './Categories';
 
 test('Categories', () => {
-  const categories = [
-    { id: 1, name: '한식' },
-  ];
-
   const { getByText } = render(<Categories categories={categories} />);
 
   expect(getByText('한식')).not.toBeNull();
