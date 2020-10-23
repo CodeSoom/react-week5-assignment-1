798d1d1bda69c4710f5132f1e79ae63fcbd22841
thkim <thkim2017@gmail.com>
2020-10-23 20:13:20 +0900

    [green] Implement a seoul button in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default function App() {
   return (
-    <></>
+    <div>
+      <button type="button" name="address">
+        서울
+      </button>
+    </div>
   );
 }
