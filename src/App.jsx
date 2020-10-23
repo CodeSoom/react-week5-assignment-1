628408fb72fc504c7d16561dab30dffd370602d2
thkim <thkim2017@gmail.com>
2020-10-23 20:35:18 +0900

    [red] Add test for App: expect category buttons

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,6 @@ export default function App() {
           </button>
         </li>
       </ul>
-
     </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -16,4 +16,10 @@ test('App', () => {
   expect(getByText('광주')).not.toBeNull();
   expect(getByText('강원도')).not.toBeNull();
   expect(getByText('인천')).not.toBeNull();
+
+  expect(getByText('한식')).not.toBeNull();
+  expect(getByText('중식')).not.toBeNull();
+  expect(getByText('일식')).not.toBeNull();
+  expect(getByText('양식')).not.toBeNull();
+  expect(getByText('분식')).not.toBeNull();
 });
