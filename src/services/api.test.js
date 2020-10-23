e0b86338a060a7fd5402ca7540c925e9b966ec30
Noah <mike1124@naver.com>
2020-10-24 01:38:02 +0900

    Add api test fetch error

---
+++ src/services/api.test.js
@@ -0,0 +1,13 @@
+import { fetchRestaurants } from './api';
+
+describe('fail to fetch restaurants data', () => {
+  it('should throw Error', async () => {
+    expect.assertions(1);
+
+    try {
+      await fetchRestaurants();
+    } catch (e) {
+      expect(e).toEqual(e);
+    }
+  });
+});
