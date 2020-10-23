c69bb0bf3d4a25af1db316b0e798fab339226faa
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-23 15:49:34 +0900

    Fix api.js

--- src/services/api.js
+++ src/services/api.js
@@ -15,6 +15,10 @@ export async function fetchCategories() {
 }
 
 export async function fetchRestaurants(categoryId, regionName) {
+  if(!categoryId || !regionName) {
+    return [];
+  }
+  
   const url = `https://eatgo-customer-api.ahastudio.com/restaurants?region=${regionName}&category=${categoryId}`;
   const response = await fetch(url);
   const data = await response.json();
