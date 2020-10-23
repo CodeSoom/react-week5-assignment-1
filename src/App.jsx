3317076778c16b5f34d2b13a6854b2e660b30381
thkim <thkim2017@gmail.com>
2020-10-23 21:10:50 +0900

    [refactor] Create Categories + Apply in the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,13 @@
 import React from 'react';
 
 import Regions from './Regions';
+import Categories from './Categories';
 
 export default function App() {
   return (
     <div>
       <Regions />
-      <ul>
-        <li>
-          <button type="button" name="category">
-            한식
-          </button>
-        </li>
-        <li>
-          <button type="button" name="category">
-            중식
-          </button>
-        </li>
-        <li>
-          <button type="button" name="category">
-            일식
-          </button>
-        </li>
-        <li>
-          <button type="button" name="category">
-            양식
-          </button>
-        </li>
-        <li>
-          <button type="button" name="category">
-            분식
-          </button>
-        </li>
-      </ul>
+      <Categories />
     </div>
   );
 }
---
+++ src/Categories.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default function Categories() {
+  return (
+    <ul>
+      <li>
+        <button type="button" name="category">
+          한식
+        </button>
+      </li>
+      <li>
+        <button type="button" name="category">
+          중식
+        </button>
+      </li>
+      <li>
+        <button type="button" name="category">
+          일식
+        </button>
+      </li>
+      <li>
+        <button type="button" name="category">
+          양식
+        </button>
+      </li>
+      <li>
+        <button type="button" name="category">
+          분식
+        </button>
+      </li>
+    </ul>
+  );
+}
