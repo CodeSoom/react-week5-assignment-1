2be6d37c4b03d900c32488074e4d04086089dd6d
thkim <thkim2017@gmail.com>
2020-10-23 21:07:09 +0900

    [refactor] Create Regions + Apply in the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,11 @@
 import React from 'react';
 
+import Regions from './Regions';
+
 export default function App() {
   return (
     <div>
-      <ul>
-        <li>
-          <button type="button" name="address">
-            서울
-          </button>
-        </li>
-        <li>
-          <button type="button" name="address">
-            대전
-          </button>
-        </li>
-        <li>
-          <button type="button" name="address">
-            대구
-          </button>
-        </li>
-        <li>
-          <button type="button" name="address">
-            부산
-          </button>
-        </li>
-        <li>
-          <button type="button" name="address">
-            광주
-          </button>
-        </li>
-        <li>
-          <button type="button" name="address">
-            강원도
-          </button>
-        </li>
-        <li>
-          <button type="button" name="address">
-            인천
-          </button>
-        </li>
-      </ul>
+      <Regions />
       <ul>
         <li>
           <button type="button" name="category">
