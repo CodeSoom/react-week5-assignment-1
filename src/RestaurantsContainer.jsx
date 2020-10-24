13ab7f5f3ecfd0106d9339061562a66fc170bdf2
Seungmin Lee <seungmin@uxplus.dev>
2020-10-24 19:04:53 +0900

    Add code
    Add code to pass tests.

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 export default function RestaurantsContainer() {
+  const { restaurants } = useSelector((state) => ({
+    restaurants: state.restaurants,
+  }));
+
   return (
-    <></>
+    <p>양천주가</p>
   );
 }
