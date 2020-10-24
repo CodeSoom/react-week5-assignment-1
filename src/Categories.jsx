eac0f99e4c605f653f0e79c85e101ad46fafe7df
JamieShin0201 <65442049+JamieShin0201@users.noreply.github.com>
2020-10-25 00:18:17 +0900

    Refactor Categories.jsx

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -3,17 +3,14 @@ import React from 'react';
 export default function Categories({ categories, selectedCategoryId, onClick }) {
   return (
     <ul>
-      {categories.map(({ id, name }) => {
-        const value = (id === selectedCategoryId) ? `${name}(V)` : name;
-
-        return (
-          <li key={id}>
-            <button type="button" id={id} onClick={onClick}>
-              {value}
-            </button>
-          </li>
-        );
-      })}
+      {categories.map(({ id, name }) => (
+        <li key={id}>
+          <button type="button" id={id} onClick={onClick}>
+            {name}
+            {id === selectedCategoryId && '(V)'}
+          </button>
+        </li>
+      ))}
     </ul>
   );
 }
