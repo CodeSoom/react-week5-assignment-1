0e133a00ba79e5e936536d4a24216e4c28f241bb
juunini <juuni.ni.i@gmail.com>
2020-10-24 23:51:31 +0900

    Remove completed TODO list

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,6 @@ import {
 } from './modules/actions';
 
 export default function App() {
-  // TODO: 지역과 음식분류가 모두 클릭 된 뒤 다른 클릭 이벤트가 1초동안 없으면
-  //       `https://eatgo-customer-api.ahastudio.com/restaurants?region=${region}&category=${category}`
-  //       에서 음식점 리스트를 가져온다.
-  // TODO: 음식점 리스트를 그린다.
   const dispatch = useDispatch();
 
   useEffect(() => {
