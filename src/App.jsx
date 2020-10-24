2781c6ad9380ebbf752215a5fcbbc763ddb07f4b
davidyang2149 <davidyang2149@gmail.com>
2020-10-24 15:30:02 +0900

    [Refactor] Update App.jsx
    5강 비동기 - 5주 차 과제 1
    - App.jsx 리팩토링.
    - Regions 컴포넌트 수신.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 
+import Regions from './Regions';
+
 import Restaurants from './Restaurants';
 
 export default function App() {
   return (
     <div>
-      <ul>
-        <li><button type="button">서울</button></li>
-        <li><button type="button">대전</button></li>
-        <li><button type="button">대구</button></li>
-        <li><button type="button">부산</button></li>
-        <li><button type="button">광주</button></li>
-        <li><button type="button">강원도</button></li>
-        <li><button type="button">인천</button></li>
-      </ul>
+      <Regions />
       <ul>
         <li><button type="button">한식</button></li>
         <li><button type="button">중식</button></li>
