482283d72fc3ad0d73fe3f47eeb2e5ec6622968a
thkim <thkim2017@gmail.com>
2020-10-24 19:15:36 +0900

    [refactor] Apply forEach() to Regions.test.jsx

--- src/Regions.test.jsx
+++ src/Regions.test.jsx
@@ -4,16 +4,14 @@ import { render } from '@testing-library/react';
 
 import Regions from './Regions';
 
+import regions from '../fixtures/regions';
+
 test('Regions', () => {
   const { getByText } = render((
     <Regions />
   ));
 
-  expect(getByText('서울')).not.toBeNull();
-  expect(getByText('대전')).not.toBeNull();
-  expect(getByText('대구')).not.toBeNull();
-  expect(getByText('부산')).not.toBeNull();
-  expect(getByText('광주')).not.toBeNull();
-  expect(getByText('강원도')).not.toBeNull();
-  expect(getByText('인천')).not.toBeNull();
+  regions.forEach(({ name }) => {
+    expect(getByText(name)).not.toBeNull();
+  });
 });
