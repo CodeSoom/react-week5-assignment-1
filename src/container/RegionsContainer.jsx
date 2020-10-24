f28149911389b5161d397d84dd23c64cd9ba1396
juunini <juuni.ni.i@gmail.com>
2020-10-24 18:13:57 +0900

    Add container/RegionsContainer click test [Still Red]

--- src/container/RegionsContainer.jsx
+++ src/container/RegionsContainer.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Regions from '../components/Regions';
+import { updateRegion } from '../modules/actions';
 
 export default function RegionsContainer() {
+  const dispatch = useDispatch();
+
   const { regions } = useSelector((state) => ({
     regions: state.regions,
   }));
+
+  function handleClickRegion(region) {
+    dispatch(updateRegion(region));
+  }
 
   return (
     <Regions
