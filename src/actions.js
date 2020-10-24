359d35cb526a3fdf1ab98f8a8501267ffb0f9893
comejtome508 <youngnoh.goh@gmail.com>
2020-10-24 22:41:55 +0900

    [Green] Add actions file

---
+++ src/actions.js
@@ -0,0 +1,13 @@
+export function setRegions(regions) {
+  return {
+    type: 'setRigions',
+    payload: {
+      regions,
+    },
+  };
+}
+
+// TODO: delete this..
+export function xxx () {
+  //
+}
