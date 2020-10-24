6bcafee6a7116da1a3c903cf6f8ce9ca54391ff5
Dokyun Lim <im.koremp@gmail.com>
2020-10-25 04:00:35 +0900

    App 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 
 export default function App() {
   return (
-    <></>
+    <>
+      <ul>
+        <li>서울</li>
+      </ul>
+      <ul>
+        <li>한식</li>
+      </ul>
+    </>
   );
 }
