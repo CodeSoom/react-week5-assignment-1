a566e4f85a3ce63e91bd26adaf2b10cf66628ec1
Seungmin Lee <seungmin@uxplus.dev>
2020-10-24 18:43:02 +0900

    Add test code
    Add test code to see whether restaurants render.

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -2,12 +2,29 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
 import RestaurantsContainer from './RestaurantsContainer';
 
 describe('RestaurantsContainer', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    useSelector.mockImplementation((selector) => selector({
+      restaurants: [
+        {
+          id: 1,
+          categoryId: 1,
+          name: '양천주가',
+          address: '서울 강남구 123456',
+          information: '양천주가 in 서울 강남구 123456',
+        },
+      ],
+    }));
+  });
   it('renders', () => {
-    const { container } = render(<RestaurantsContainer />);
+    const { container, getByText } = render(<RestaurantsContainer />);
 
     expect(container).not.toBeNull();
+    expect(getByText('양천주가')).not.toBeNull();
   });
 });
