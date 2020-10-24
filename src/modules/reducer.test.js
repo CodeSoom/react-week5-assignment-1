b65bdeb1663f5264c36e418ce08725a981c08791
juunini <juuni.ni.i@gmail.com>
2020-10-24 18:31:34 +0900

    Add reducer updateRegions test [Red]

--- src/modules/reducer.test.js
+++ src/modules/reducer.test.js
@@ -1,5 +1,14 @@
+import reducer from './reducer';
+import { updateRegions } from './actions';
+
 describe('reducer', () => {
   it('updateRegions', () => {
+    const state = reducer({
+      selected: {
+        region: '',
+      },
+    }, updateRegions('서울'));
 
+    expect(state.selected.region).toBe('서울');
   });
 });
