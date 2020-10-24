64703efcdb8157437d4c4d91821a146c2f0e8043
juunini <juuni.ni.i@gmail.com>
2020-10-24 18:12:11 +0900

    Add container/RegionsContainer click test [Red]

--- src/container/RegionsContainer.test.jsx
+++ src/container/RegionsContainer.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import RegionsContainer from './RegionsContainer';
 import regions from '../../fixtures/regions';
@@ -19,4 +19,8 @@ test('RegionsContainer', () => {
 
   expect(screen.getByRole('button', { name: /서울/ })).toBeInTheDocument();
   expect(screen.getByRole('button', { name: /대전/ })).toBeInTheDocument();
+
+  fireEvent.click(screen.getByRole('button', { name: /서울/ }));
+
+  expect(dispatch).toBeCalled();
 });
