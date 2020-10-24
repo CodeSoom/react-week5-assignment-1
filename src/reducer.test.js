9f2a593cd81e88b55e3f7e1d6a90ef52400d2185
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-24 12:05:14 +0900

    [Red] reducer, CategoriesContainer에 대한 action 테스트 작성
    1st, if select a category.
    2nd, changes categories.
    
    *TODO: Make action creator and apply reducers into App.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,50 +1,60 @@
 import reducer from './reducer';
 
 import regions from '../__fixtures__/regions';
+import categories from '../__fixtures__/categories';
 
 import { selectRegion, setRegions } from './actions';
 
 describe('reducer', () => {
-  describe('with initial state', () => {
-    const initialState = {
-      selectedRegionId: 0,
-      regions: [],
-    };
+  const initialState = {
+    selectedRegionId: 0,
+    regions: [],
+    selectedCategoryId: 0,
+    categories: [],
+  };
 
-    it('return state', () => {
-      const state = reducer();
-      expect(state).toStrictEqual(initialState);
-    });
-
-    it('return unchanges state', () => {
-      const state = reducer(initialState, {
-        type: 'unknownAction',
-        payload: {
-          name: 'unknown',
-        },
-      });
-      expect(state).toBe(initialState);
-    });
+  it('return state', () => {
+    const state = reducer();
+    expect(state).toStrictEqual(initialState);
   });
 
-  describe('setRegions', () => {
+  it('return unchanges state', () => {
+    const state = reducer(initialState, {
+      type: 'unknownAction',
+      payload: {
+        name: 'unknown',
+      },
+    });
+    expect(state).toBe(initialState);
+  });
+
+  describe('RegionsContaioner', () => {
     it('selectRegion', () => {
-      const clickedRegionId = 1;
-      const state = reducer(regions, selectRegion(clickedRegionId));
+      const selectedCategoryId = 1;
+      const state = reducer(regions, selectRegion(selectedCategoryId));
 
-      expect(state.selectedRegionId).toBe(clickedRegionId);
+      expect(state.selectedRegionId).toBe(selectedCategoryId);
     });
-  });
 
-  describe('setRegions', () => {
     it('changes regions', () => {
-      const initialState = {
-        regions: [],
-      };
-
       const state = reducer(initialState, setRegions(regions));
 
       expect(state.regions).toHaveLength(regions.length);
+    });
+  });
+
+  describe('CategoriesContaioner', () => {
+    it('selectCategory', () => {
+      const selectedCategoryId = 1;
+      const state = reducer(categories, selectCategory(selectedCategoryId));
+
+      expect(state.selectedCategoryId).toBe(selectedCategoryId);
+    });
+
+    it('changes categories', () => {
+      const state = reducer(initialState, setCategories(categories));
+
+      expect(state.categories).toHaveLength(categories.length);
     });
   });
 });
