62a9c2b23c8b6d1f73e7dbadca4cd4080551f455
wjy5446 <wjy5446@gmail.com>
2020-10-24 17:56:51 +0900

    fix) rselectedButtons가 변경되면 setRestaurantsAsync를 호출하도록 변경

--- src/ListContainer.jsx
+++ src/ListContainer.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
+import { setRestaurantsAsync } from './actions';
 import List from './List';
 
 export default function ListContainer() {
-  const { restaurants } = useSelector((state) => ({
+
+  const dispatch = useDispatch();
+
+  const { restaurants, selectedButtons } = useSelector((state) => ({
     restaurants: state.restaurants,
+    selectedButtons: state.selectedButtons,
   }));
+
+  useEffect(() => {
+    const { region, category } = selectedButtons;
+
+    if (region === '' || category === '') {
+      return;
+    }
+
+    dispatch(setRestaurantsAsync());
+    console.log(restaurants);
+
+  }, [selectedButtons]);
 
   return (
     <List items={restaurants} />
