4fa8e21f2142336718aeac163f4e48b83358b28f
davidyang2149 <davidyang2149@gmail.com>
2020-10-24 15:28:15 +0900

    [Green] Update Regions.jsx for Implement
    5강 비동기 - 5주 차 과제 1
    - Regions.jsx 업데이트.
    - 테스트 코드 작성 후 UI 구현.

---
+++ src/Regions.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const resgions = ['서울', '대전', '대구', '부산', '광주', '강원도', '인천'];
+
+export default function Resgions() {
+  return (
+    <>
+      <ul>
+        {resgions.map((resgion) => (
+          <li>{resgion}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
