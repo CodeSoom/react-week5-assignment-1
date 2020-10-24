99a68bba53dc3aff219800b49c44723a9a289529
Seungmin Lee <seungmin@uxplus.dev>
2020-10-24 18:07:17 +0900

    Add test code
    Add test code for dispatch.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
+import { useDispatch } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import App from './App';
 
+jest.mock('react-redux');
+
 describe('App', () => {
-  const { container } = render(<App />);
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    useDispatch.mockReturnValue(dispatch);
+  });
 
   it('renders', () => {
+    const { container } = render(<App />);
+
     expect(container).not.toBeNull();
   });
 });
