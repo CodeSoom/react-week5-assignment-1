faa6f0b430f164daa099e076fc0fa4f5edc5bc65
thkim <thkim2017@gmail.com>
2020-10-24 21:26:30 +0900

    [refactor] redefine initialState in reducer

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -7,12 +7,12 @@ import {
 import categories from '../fixtures/categories';
 
 describe('reducer', () => {
+  const initialState = {
+    categories: [],
+  };
+
   context('setCategories', () => {
     it('changes categories', () => {
-      const initialState = {
-        categories: [],
-      };
-
       const state = reducer(initialState, setCategories(categories));
 
       expect(state.categories).not.toHaveLength(0);
@@ -20,10 +20,6 @@ describe('reducer', () => {
   });
   context('undefined action', () => {
     it("doesn't work", () => {
-      const initialState = {
-        categories: [],
-      };
-
       const state = reducer(initialState, {});
 
       expect(state.categories).toHaveLength(0);
