8ea0e31b29b79a182986c4b628a179d655df2a1b
koremp <im.koremp@gmail.com>
2020-10-25 03:50:24 +0900

    App 테스트 추가

---
+++ src/App.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+test('App', () => {
+  const { getByText } = render(<App />);
+
+  expect(getByText('서울')).not.toBeNull();
+  expect(getByText('한식')).not.toBeNull();
+});
