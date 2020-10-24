5323ccc47de38b3c05fa00617e299dd406730784
juunini <juuni.ni.i@gmail.com>
2020-10-24 18:10:11 +0900

    Using components/Regions into container/RegionsContainer

--- src/container/RegionsContainer.jsx
+++ src/container/RegionsContainer.jsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import Regions from '../components/Regions';
+
 export default function RegionsContainer() {
   const { regions } = useSelector((state) => ({
     regions: state.regions,
   }));
 
   return (
-    <ul>
-      {
-        regions.map(({ id, name }) => (
-          <li key={id}>
-            <button
-              type="button"
-              name={name}
-            >
-              {name}
-            </button>
-          </li>
-        ))
-      }
-    </ul>
+    <Regions
+      regions={regions}
+    />
   );
 }
