1116d6d73e58f1333fbc348c6d50a15e1f355bca
juunini <juuni.ni.i@gmail.com>
2020-10-24 20:33:30 +0900

    Add container/RestaurantsContainer [Refactoring]

--- src/container/RestaurantsContainer.jsx
+++ src/container/RestaurantsContainer.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 export default function RestaurantsContainer() {
-  return (<>양천주가</>);
+  const restaurants = [{
+    id: 1,
+    categoryId: 1,
+    name: '양천주가',
+    address: '서울 강남구 123456',
+    information: '양천주가 in 서울 강남구 123456',
+  }];
+
+  return (
+    <ul>
+      {
+        restaurants.map(({ id, name }) => (
+          <li key={id}>{name}</li>
+        ))
+      }
+    </ul>
+  );
 }
