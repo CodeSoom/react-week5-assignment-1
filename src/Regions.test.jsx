5730e8c7ad2dd28954c420bda76b5b4ee67ba418
davidyang2149 <davidyang2149@gmail.com>
2020-10-24 23:35:40 +0900

    [Red] Update Regions.test.jsx for TDD
    5강 비동기 - 5주 차 과제 1
    - Regions.test.jsx 수정.
    - Regions 테스트 코드 작성(Red).

--- src/Regions.test.jsx
+++ src/Regions.test.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Regions from './Regions';
 
 describe('Regions Component', () => {
+  const handleClick = jest.fn();
+
   it('Page render', () => {
     const { getByText } = render((
       <Regions />
@@ -17,5 +19,15 @@ describe('Regions Component', () => {
     expect(getByText(/광주/)).not.toBeNull();
     expect(getByText(/강원도/)).not.toBeNull();
     expect(getByText(/인천/)).not.toBeNull();
+  });
+
+  it('Click region', () => {
+    const { screen } = render((
+      <Regions />
+    ));
+
+    expect(handleClick).not.toBeCalled();
+    fireEvent.click(screen.getByRole('button', { name: /서울/ }));
+    expect(handleClick).toBeCalled();
   });
 });
