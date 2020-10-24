26c609d15b3ec74f3099a0716009ea1a28efd7d9
juunini <juuni.ni.i@gmail.com>
2020-10-24 18:04:53 +0900

    Add components/Regions test [Refactoring]

--- src/components/Regions.test.jsx
+++ src/components/Regions.test.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 import Regions from './Regions';
+import regionsFixture from '../../fixtures/regions';
 
 describe('Regions', () => {
   it('render', () => {
-    render(<Regions regions={[]} />);
+    render(<Regions regions={regionsFixture} />);
 
     expect(screen.getByText(/서울/)).toBeInTheDocument();
   });
