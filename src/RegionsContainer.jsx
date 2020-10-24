bb6f9c18af20224c9ec1110d1b7b4198ebde1a31
comejtome508 <youngnoh.goh@gmail.com>
2020-10-24 18:58:30 +0900

    [Green] Make RegionsContainer run

--- src/RegionsContainer.jsx
+++ src/RegionsContainer.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 
+import Regions from './Regions';
+
 export default function Regionscontainer() {
+  const { regions } = useSelector((state) => ({
+    regions: state.regions,
+  }));
+
   return (
+    <Regions regions={regions} />
   );
 }
