6d10bd8041d2808f9dd3c6d18a98eaef7f617f17
davidyang2149 <davidyang2149@gmail.com>
2020-10-24 23:50:49 +0900

    [Green] Update Regions.jsx
    5강 비동기 - 5주 차 과제 1
    - Regions.jsx 수정.
    - 테스트 코드 작성 후 UI 구현(버튼).

--- src/Regions.jsx
+++ src/Regions.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const resgions = ['서울', '대전', '대구', '부산', '광주', '강원도', '인천'];
 
-export default function Resgions() {
+export default function Resgions({ onClick }) {
   return (
     <>
       <ul>
         {resgions.map((resgion) => (
           <li key={resgion}>
-            {resgion}
+            <button type="button" onClick={onClick}>{resgion}</button>
           </li>
         ))}
       </ul>
