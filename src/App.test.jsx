bc02bb931f2c94214ae88947eda9d6761296d565
comejtome508 <youngnoh.goh@gmail.com>
2020-10-24 17:45:52 +0900

    [Red] Add regions data

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -8,6 +8,13 @@ test('App', () => {
   const regions = [
     {
       id: 1, name: '서울',
+      id: 2, name: '대전',
+      id: 3, name: '대구',
+      id: 4, name: '부산',
+      id: 5, name: '광주',
+      id: 6, name: '강원도',
+      id: 7, name: '인천',
+
     },
   ];
   const { getByText } = render((
@@ -15,4 +22,6 @@ test('App', () => {
   ));
 
   expect(getByText('서울')).not.toBeNull();
+  expect(getByText('대구')).not.toBeNull();
+  expect(getByText('강원도')).not.toBeNull();
 });
