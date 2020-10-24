a23e4073b944fc58316304249f6b5f89e2c37c9f
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-24 18:39:46 +0900

    [Green] Restaurants 컴포넌트 구현
    Restaurants 테스트를 기반으로 Restaurants 컴포넌트 구현.
    * TODO: apply redux in RestaurantsContainer!

--- src/Restaurants.jsx
+++ src/Restaurants.jsx
@@ -1,1 +1,21 @@
 import React from 'react';
+
+const isEmpty = (array = []) => array.length === 0;
+
+export default function Restaurants({ restaurants }) {
+  if (isEmpty(restaurants)) {
+    return <p>레스토랑이 없습니다!</p>;
+  }
+
+  return (
+    <ul>
+      {restaurants.map(({ id, name, address }) => (
+        <li key={id}>
+          <span title={address}>
+            {name}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+}
