d594256746b903df915534cfb2328f7ebf5bc7bd
thkim <thkim2017@gmail.com>
2020-10-24 19:20:09 +0900

    [refactor] Get rid of duplication

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -20,16 +20,6 @@ test('App', () => {
   ));
 
   expect(getByText('서울')).not.toBeNull();
-  expect(getByText('대전')).not.toBeNull();
-  expect(getByText('대구')).not.toBeNull();
-  expect(getByText('부산')).not.toBeNull();
-  expect(getByText('광주')).not.toBeNull();
-  expect(getByText('강원도')).not.toBeNull();
-  expect(getByText('인천')).not.toBeNull();
 
   expect(getByText('한식')).not.toBeNull();
-  expect(getByText('중식')).not.toBeNull();
-  expect(getByText('일식')).not.toBeNull();
-  expect(getByText('양식')).not.toBeNull();
-  expect(getByText('분식')).not.toBeNull();
 });
