8c100c0fd739475a6fa747f795775d7e4fbe9d95
thkim <thkim2017@gmail.com>
2020-10-24 18:31:52 +0900

    [red] Add a test for CategoriesContainer : useSelector

--- src/CategoriesContainer.test.jsx
+++ src/CategoriesContainer.test.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import CategoriesContainer from './CategoriesContainer';
 
-describe('CategoriesContainer', () => {
-  it('', () => {
-    const { getByText } = render((
-      <CategoriesContainer />
-    ));
-    expect(getByText('한식')).not.toBeNull();
-  });
+test('CategoriesContainer', () => {
+  useSelector.mockImplementation((selector) => selector({
+    categories,
+  }));
+
+  const { getByText } = render((
+    <CategoriesContainer />
+  ));
+
+  expect(getByText('한식')).not.toBeNull();
 });
