f97c3f3ad7bb970b2dedd8ccbaf0c0006df7a333
davidyang2149 <davidyang2149@gmail.com>
2020-10-24 15:45:27 +0900

    [Refactor] Update App.jsx
    5강 비동기 - 5주 차 과제 1
    - App.jsx 리팩토링.
    - Categories 컴포넌트 수신.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 
 import Regions from './Regions';
-
+import Categories from './Categories';
 import Restaurants from './Restaurants';
 
 export default function App() {
   return (
     <div>
       <Regions />
-      <ul>
-        <li><button type="button">한식</button></li>
-        <li><button type="button">중식</button></li>
-        <li><button type="button">일식</button></li>
-        <li><button type="button">양식</button></li>
-        <li><button type="button">분식</button></li>
-      </ul>
+      <Categories />
       <Restaurants />
     </div>
   );
