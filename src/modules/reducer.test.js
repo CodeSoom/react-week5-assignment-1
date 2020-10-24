3b044541db76882be956cb7e3000caf9010f240c
juunini <juuni.ni.i@gmail.com>
2020-10-24 22:15:45 +0900

    Coverage 100% to reducer.js

--- src/modules/reducer.test.js
+++ src/modules/reducer.test.js
@@ -3,6 +3,9 @@ import {
   updateRegion,
   updateCategory,
 } from './actions';
+import regions from '../../fixtures/regions';
+import categories from '../../fixtures/categories';
+import restaurants from '../../fixtures/restaurants';
 
 describe('reducer', () => {
   it('updateRegions', () => {
@@ -23,5 +26,48 @@ describe('reducer', () => {
     }, updateCategory('한식'));
 
     expect(state.selected.category).toBe('한식');
+  });
+
+  it('setRegions', () => {
+    const state = reducer({
+      regions: [],
+    }, {
+      type: 'setRegions',
+      payload: { regions },
+    });
+
+    expect(state.regions).toBe(regions);
+  });
+
+  it('setCategories', () => {
+    const state = reducer({
+      categories: [],
+    }, {
+      type: 'setCategories',
+      payload: { categories },
+    });
+
+    expect(state.categories).toBe(categories);
+  });
+
+  it('setRestaurants', () => {
+    const state = reducer({
+      restaurants: [],
+    }, {
+      type: 'setRestaurants',
+      payload: { restaurants },
+    });
+
+    expect(state.restaurants).toBe(restaurants);
+  });
+
+  context('without state', () => {
+    it('returns initialState', () => {
+      const state = reducer(undefined, { type: undefined });
+
+      expect(state.regions).toStrictEqual([]);
+      expect(state.categories).toStrictEqual([]);
+      expect(state.restaurants).toStrictEqual([]);
+    });
   });
 });
