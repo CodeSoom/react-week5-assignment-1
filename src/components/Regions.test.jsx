c9fd61fb6c729bf725480b5166d8bd84ac877514
juunini <juuni.ni.i@gmail.com>
2020-10-24 18:07:02 +0900

    Add components/Regions test []

--- src/components/Regions.test.jsx
+++ src/components/Regions.test.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Regions from './Regions';
 import regionsFixture from '../../fixtures/regions';
 
 describe('Regions', () => {
+  const handleClick = jest.fn();
+
   it('render', () => {
     render(<Regions regions={regionsFixture} />);
 
     expect(screen.getByText(/서울/)).toBeInTheDocument();
+  });
+
+  it('click test', () => {
+    render(
+      <Regions
+        regions={regionsFixture}
+        onClick={handleClick}
+      />,
+    );
+
+    expect(handleClick).not.toBeCalled();
+
+    fireEvent.click(screen.getByRole('button', { name: '서울' }));
+
+    expect(handleClick).toBeCalled();
   });
 });
