e03d5d95beea7a713ae301c31318c03cc5809d7f
juunini <juuni.ni.i@gmail.com>
2020-10-24 17:57:40 +0900

    api mocks returns empty array

--- src/services/__mocks__/api.js
+++ src/services/__mocks__/api.js
@@ -1,15 +1,11 @@
-import regions from '../../../fixtures/regions';
-import categories from '../../../fixtures/categories';
-import restaurants from '../../../fixtures/restaurants';
-
 export async function fetchRegions() {
-  return regions;
+  return [];
 }
 
 export async function fetchCategories() {
-  return categories;
+  return [];
 }
 
 export async function fetchRestaurants() {
-  return restaurants;
+  return [];
 }
