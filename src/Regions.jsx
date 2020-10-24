f47b4cd360866a3466d0a0fbc204553373085a09
JamieShin0201 <65442049+JamieShin0201@users.noreply.github.com>
2020-10-25 00:20:15 +0900

    Refactor Regions.jsx

--- src/Regions.jsx
+++ src/Regions.jsx
@@ -3,17 +3,14 @@ import React from 'react';
 export default function Regions({ regions, selectedRegionName, onClick }) {
   return (
     <ul>
-      {regions.map(({ id, name }) => {
-        const value = (name === selectedRegionName) ? `${name}(V)` : name;
-
-        return (
-          <li key={id}>
-            <button type="button" onClick={onClick}>
-              {value}
-            </button>
-          </li>
-        );
-      })}
+      {regions.map(({ id, name }) => (
+        <li key={id}>
+          <button type="button" onClick={onClick}>
+            {name}
+            {name === selectedRegionName && '(V)'}
+          </button>
+        </li>
+      ))}
     </ul>
   );
 }
