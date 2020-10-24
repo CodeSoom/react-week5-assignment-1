5c98decd6b0bc6c85623b67992e452677f3dd7b4
thkim <thkim2017@gmail.com>
2020-10-24 19:09:17 +0900

    [refactor] Apply forEach() to Categories.test.jsx

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -11,11 +11,7 @@ test('Categories', () => {
     <Categories categories={categories} />
   ));
 
-  expect(getByText('한식')).not.toBeNull();
-  expect(getByText('중식')).not.toBeNull();
-  expect(getByText('일식')).not.toBeNull();
-  expect(getByText('양식')).not.toBeNull();
-  expect(getByText('분식')).not.toBeNull();
-  expect(getByText('과자')).not.toBeNull();
-  expect(getByText('치킨')).not.toBeNull();
+  categories.forEach(({ name }) => {
+    expect(getByText(name)).not.toBeNull();
+  });
 });
