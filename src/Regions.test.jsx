936e77246662a1c7f99c2d836268dd497a1d56b8
davidyang2149 <davidyang2149@gmail.com>
2020-10-24 15:23:05 +0900

    [Red] Add Regions.test.jsx for TDD
    5강 비동기 - 5주 차 과제 1
    - Regions.test.jsx 추가.
    - Regions 테스트 코드 작성(Red).

---
+++ src/Regions.test.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Regions from './Regions';
+
+describe('Regions Component', () => {
+  it('Page render', () => {
+    const { getByText } = render((
+      <Regions />
+    ));
+
+    expect(getByText(/서울/)).not.toBeNull();
+    expect(getByText(/대전/)).not.toBeNull();
+    expect(getByText(/대구/)).not.toBeNull();
+    expect(getByText(/부산/)).not.toBeNull();
+    expect(getByText(/광주/)).not.toBeNull();
+    expect(getByText(/강원도/)).not.toBeNull();
+    expect(getByText(/인천/)).not.toBeNull();
+  });
+});
