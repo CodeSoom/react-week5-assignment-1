eec19323b190f30a3f6a2499129d2b7f0215ca77
ParkDongJo <pdj10211@gmail.com>
2020-10-25 18:12:51 +0900

    [week5-1][코드고민] selected categoryId에 대한 테스트코드 고민 주석처리

--- src/containers/CategoriesContainer.test.jsx
+++ src/containers/CategoriesContainer.test.jsx
@@ -18,6 +18,7 @@ test('CategoriesContainer', () => {
   useSelector.mockImplementation((selector) => selector({
     category: {
       categories,
+      selectedCategoryId: 0,
     },
   }));
 
@@ -36,3 +37,62 @@ test('CategoriesContainer', () => {
     },
   });
 });
+
+// [ 고민 중인 부분입니다. ]
+// 우선 주석 처리로 고민의 흔적을 남겨놓습니다.
+
+// describe('CategoriesContainer', () => {
+//   const dispatch = jest.fn();
+
+//   useDispatch.mockImplementation(() => dispatch);
+
+//   beforeEach(() => {
+//     useSelector.mockImplementation((selector) => selector({
+//       category: {
+//         categories,
+//         selectedCategoryId: 0,
+//       },
+//     }));
+//   });
+
+//   function renderContainer() {
+//     return render(
+//       <CategoriesContainer />,
+//     );
+//   }
+
+//   it('not excute dispatch selected categoryId', () => {
+//     const { queryByText } = renderContainer();
+
+//     expect(queryByText(/한식/)).not.toBeNull();
+//     expect(queryByText(/중식/)).not.toBeNull();
+//     expect(queryByText(/일식/)).not.toBeNull();
+//   });
+
+//   it('excute dispatch selected categoryId', () => {
+//     const { queryByText } = renderContainer();
+
+//     fireEvent.click(queryByText(/한식/));
+
+//     expect(dispatch).toBeCalledWith({
+//       type: 'updateSelectedCategoryId',
+//       payload: {
+//         categoryId: 1,
+//       },
+//     });
+//   });
+
+//   it('changed selected categoryId to 1', () => {
+//     useSelector.mockImplementation((selector) => selector({
+//       category: {
+//         categories,
+//         selectedCategoryId: 1,
+//       },
+//     }));
+
+//     const { container, queryByText } = renderContainer();
+
+//     console.log('테스트 - ', container);
+//     expect(queryByText(/한식/)).not.toBeNull();
+//   });
+// });
