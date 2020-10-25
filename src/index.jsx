3304e232b7e36498ebf70b2c7f198d5c258d78c1
thkim <thkim2017@gmail.com>
2020-10-25 15:45:46 +0900

    [refactor] Edit index.jsx to apply store to Provider

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
 
+import store from './store';
+
 import App from './App';
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('app'),
