1ac1e8edcd046925b3aac91baf58b64ff5cba766
Noah <mike1124@naver.com>
2020-10-26 02:50:30 +0900

    [Refactor] code in action test
     mockResolvedValue, payload jest

--- src/action.test.js
+++ src/action.test.js
@@ -2,15 +2,10 @@ import configureStore from 'redux-mock-store';
 
 import thunk from 'redux-thunk';
 
-import { fetchCategories, fetchRegions, fetchRestaurants } from './services/api';
-
 import {
-  setCategories,
   loadCategories,
-  setRegions,
   loadRegions,
   loadRestaurants,
-  setRestaurants,
 } from './actions';
 
 const middleware = [thunk];
@@ -21,56 +16,44 @@ describe('async actions', () => {
   const store = mockStore({});
 
   describe('loadCategories', () => {
-    global.fetch = jest.fn(() => Promise.resolve({
-      json: () => Promise.resolve({ categories: [{ id: 1, name: '한식' }] }),
-    }));
-
     beforeEach(() => {
+      global.fetch = jest.fn().mockResolvedValue({ json: () => ({ categories: [{ id: 1, name: '한식' }] }) });
+
       fetch.mockClear();
     });
 
     it('should fetch categories data', async () => {
       await store.dispatch(loadCategories());
 
-      const categories = await fetchCategories();
-
-      expect(store.getActions()[0]).toEqual(setCategories(categories));
+      expect(store.getActions()[0]).toEqual({ payload: { categories: { categories: [{ id: 1, name: '한식' }] } }, type: 'setCategories' });
     });
   });
 
   describe('loadRegions', () => {
-    global.fetch = jest.fn(() => Promise.resolve({
-      json: () => Promise.resolve({ regions: [{ id: 1, name: '서울' }] }),
-    }));
-
     beforeEach(() => {
+      global.fetch = jest.fn().mockResolvedValue({ json: () => ({ regions: [{ id: 1, name: '서울' }] }) });
+
       fetch.mockClear();
     });
 
     it('should fetch regions data', async () => {
       await store.dispatch(loadRegions());
 
-      const regions = await fetchRegions();
-
-      expect(store.getActions()[1]).toEqual(setRegions(regions));
+      expect(store.getActions()[1]).toEqual({ payload: { regions: { regions: [{ id: 1, name: '서울' }] } }, type: 'setRegions' });
     });
   });
 
   describe('loadRestaurants', () => {
-    global.fetch = jest.fn(() => Promise.resolve({
-      json: () => Promise.resolve({ restaurants: [{ id: 1, name: '새마을 식당' }] }),
-    }));
-
     beforeEach(() => {
+      global.fetch = jest.fn().mockResolvedValue({ json: () => ({ restaurants: [{ id: 1, name: '새마을 식당' }] }) });
+
       fetch.mockClear();
     });
 
     it('should fetch restaurants data', async () => {
       await store.dispatch(loadRestaurants({ categoryId: 1, regionName: '서울', stopFetch: 1 }));
 
-      const restaurants = await fetchRestaurants({ categoryId: 1, regionName: '서울' });
-
-      expect(store.getActions()[2]).toEqual(setRestaurants(restaurants));
+      expect(store.getActions()[2]).toEqual({ payload: { restaurants: { restaurants: [{ id: 1, name: '새마을 식당' }] } }, type: 'setRestaurants' });
     });
 
     it('should not fetch restaurant data if conditions are not met', async () => {
