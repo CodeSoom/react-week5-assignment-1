217855d43fc7ae340e2fedb6b9edea511f0a4aaf
davidyang2149 <davidyang2149@gmail.com>
2020-10-25 16:38:19 +0900

    [Red] Add RegionsContainer.test.jsx for TDD
    5강 비동기 - 5주 차 과제 1
    - RegionsContainer.test.jsx 생성.
    - RegionsCpmaomer 테스트 코드 작성(Red).

---
+++ src/RegionsContainer.test.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import RegionsContainer from './RegionsContainer';
+
+describe('RegionsContainer Component', () => {
+  const handleClick = jest.fn();
+
+  it('Page render', () => {
+    const { getByText } = render((
+      <RegionsContainer
+        onClick={handleClick}
+      />
+    ));
+
+    expect(getByText(/서울/)).not.toBeNull();
+    expect(getByText(/대전/)).not.toBeNull();
+    expect(getByText(/대구/)).not.toBeNull();
+    expect(getByText(/부산/)).not.toBeNull();
+    expect(getByText(/광주/)).not.toBeNull();
+    expect(getByText(/강원도/)).not.toBeNull();
+    expect(getByText(/인천/)).not.toBeNull();
+  });
+
+  it('Click region', () => {
+    const { getByText } = render((
+      <RegionsContainer
+        onClick={handleClick}
+      />
+    ));
+
+    expect(handleClick).not.toBeCalled();
+
+    fireEvent.click(getByText(/서울/));
+    expect(handleClick).toBeCalled();
+  });
+});
