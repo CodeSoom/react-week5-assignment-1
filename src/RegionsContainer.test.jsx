ea2146bd317ea78832829cc252a824f84984a34a
davidyang2149 <davidyang2149@gmail.com>
2020-10-25 19:27:47 +0900

    [Red] Update RegionsContainer.test.jsx
    5강 비동기 - 5주 차 과제 1
    - RegionsContainer.test.jsx 수정.
    - (문제) 구현 순서가 뒤바뀜.
    - Green 먼저 구현 후 Red 실행.

--- src/RegionsContainer.test.jsx
+++ src/RegionsContainer.test.jsx
@@ -2,37 +2,47 @@ import React from 'react';
 
 import { render, fireEvent } from '@testing-library/react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import RegionsContainer from './RegionsContainer';
 
+import regions from '../__fixtures__/regions';
+
+import { selectRegion } from './modules/actions';
+
+jest.mock('react-redux');
+
 describe('RegionsContainer Component', () => {
-  const handleClick = jest.fn();
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    regions,
+    selected: {
+      region: '',
+    },
+  }));
+
+  jest.clearAllMocks();
 
   it('Page render', () => {
-    const { getByText } = render((
-      <RegionsContainer
-        onClick={handleClick}
-      />
+    const { container } = render((
+      <RegionsContainer />
     ));
 
-    expect(getByText(/서울/)).not.toBeNull();
-    expect(getByText(/대전/)).not.toBeNull();
-    expect(getByText(/대구/)).not.toBeNull();
-    expect(getByText(/부산/)).not.toBeNull();
-    expect(getByText(/광주/)).not.toBeNull();
-    expect(getByText(/강원도/)).not.toBeNull();
-    expect(getByText(/인천/)).not.toBeNull();
+    regions.forEach(({ name }) => {
+      expect(container).toHaveTextContent(name);
+    });
   });
 
-  it('Click region', () => {
+  it('Click buttons', () => {
     const { getByText } = render((
-      <RegionsContainer
-        onClick={handleClick}
-      />
+      <RegionsContainer />
     ));
 
-    expect(handleClick).not.toBeCalled();
-
-    fireEvent.click(getByText(/서울/));
-    expect(handleClick).toBeCalled();
+    regions.forEach(({ name }) => {
+      fireEvent.click(getByText(name));
+      expect(dispatch).toBeCalledWith(selectRegion(name));
+    });
   });
 });
