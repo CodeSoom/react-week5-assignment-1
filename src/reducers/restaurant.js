fd7f58bf9e3a7ad7fab20eafb9737f0877e8c335
ParkDongJo <pdj10211@gmail.com>
2020-10-25 21:38:37 +0900

    [week5][리펙토링] reducer restaurant defualt 호출 시 테스트 통과 코드작성

--- src/reducers/restaurant.js
+++ src/reducers/restaurant.js
@@ -3,7 +3,7 @@ const initialState = {
 };
 
 const restaurant = (state = initialState, action) => {
-  if (action.type === 'setRestaurants') {
+  if (action?.type === 'setRestaurants') {
     const { restaurants } = action.payload;
     return {
       ...state,
