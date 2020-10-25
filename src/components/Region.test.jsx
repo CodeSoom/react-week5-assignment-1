0bf1caae85b718a5530fa5e8b0e9453243bca113
ParkDongJo <pdj10211@gmail.com>
2020-10-25 16:56:59 +0900

    [week5-2] Region.test 선택된 regionName 경우의 수에 따른 테스트 코드 분리하기
    * 선택된 region name 이 있고 없고 경우의 수에 따른 테스트 코드 작성하기
    * test() 에서 두가지 경우의 수에 대비한 describe - it 으로 구성하기₩

--- src/components/Region.test.jsx
+++ src/components/Region.test.jsx
@@ -4,21 +4,34 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Region from './Region';
 
-test('Regions', () => {
+describe('Region', () => {
   const clickHandler = jest.fn();
 
-  const { queryByText } = render((
-    <Region
-      name="서울"
-      onClick={clickHandler}
-    />
-  ));
+  function renderRegion({ isSelectedReturnValue }) {
+    return render(
+      <Region
+        isSelected={() => isSelectedReturnValue}
+        name="서울"
+        onClick={clickHandler}
+      />,
+    );
+  }
 
-  expect(queryByText(/서울/)).not.toBeNull();
+  it('is not existed selected region', () => {
+    const { queryByText } = renderRegion({ isSelectedReturnValue: false });
 
-  expect(clickHandler).not.toBeCalled();
+    expect(queryByText(/서울/)).not.toBeNull();
 
-  fireEvent.click(queryByText(/서울/));
+    expect(clickHandler).not.toBeCalled();
 
-  expect(clickHandler).toBeCalled();
+    fireEvent.click(queryByText(/서울/));
+
+    expect(clickHandler).toBeCalled();
+  });
+
+  it('is exist selected region', () => {
+    const { queryByText } = renderRegion({ isSelectedReturnValue: true });
+
+    expect(queryByText(/서울(v)/)).toBeNull();
+  });
 });
