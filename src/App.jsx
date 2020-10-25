6e508a3fe49fe57b1ecf7712cded22d21fb84aba
thkim <thkim2017@gmail.com>
2020-10-25 15:38:32 +0900

    [refactor] Apply serCategories to App

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import Regions from './Regions';
+
 import CategoriesContainer from './CategoriesContainer';
+
+import { setCategories } from './actions';
+
+import categories from '../fixtures/categories';
 
 export default function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch();
+    dispatch(setCategories(categories));
   }, []);
 
   return (
