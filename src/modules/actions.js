3d04346d0be07f2fdbfdfcbb020f3623a15b7f1e
davidyang2149 <davidyang2149@gmail.com>
2020-10-25 18:29:48 +0900

    [Update] update File modules
    5강 비동기 - 5주 차 과제 1
    - Regions 컴포넌트용 redux 구조 수정.
    - 기능 추가.

--- src/modules/actions.js
+++ src/modules/actions.js
@@ -1,7 +1,19 @@
+import {
+  fetchRegions,
+} from '../services/api';
+
 export function setRegions(regions) {
   return {
     type: 'setRegions',
     payload: { regions },
+  };
+}
+
+export function getRegions() {
+  return async (dispatch) => {
+    const regions = await fetchRegions();
+
+    dispatch(setRegions(regions));
   };
 }
 
