7ce4effb8fd16e0aa9707525ee509475279e9de5
davidyang2149 <davidyang2149@gmail.com>
2020-10-25 18:19:01 +0900

    [Refactor] Update Regions.jsx
    5강 비동기 - 5주 차 과제 1
    - Regions.jsx 수정.
    - 기능 및 변수 추가.

--- src/Regions.jsx
+++ src/Regions.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const resgions = ['서울', '대전', '대구', '부산', '광주', '강원도', '인천'];
-
-export default function Resgions({ onClick }) {
+export default function Resgions({ regions, selected, onClick }) {
   return (
     <>
       <ul>
-        {resgions.map((resgion) => (
-          <li key={resgion}>
-            <button type="button" onClick={onClick}>{resgion}</button>
+        {regions.map(({ id, name }) => (
+          <li key={id}>
+            <button type="button" onClick={() => onClick(name)}>
+              {selected === name ? `${name} (V)` : name}
+            </button>
           </li>
         ))}
       </ul>
