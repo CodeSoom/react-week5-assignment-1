5c1c7ce9ca4da30b1fc2e9d376e73c19f7ebf50d
juunini <juuni.ni.i@gmail.com>
2020-10-25 12:02:09 +0900

    Add component Restaurnats [Refactoring]

--- src/components/Restaurants.jsx
+++ src/components/Restaurants.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+const restaurants = [{
+  id: 1,
+  categoryId: 1,
+  name: '양천주가',
+  address: '서울 강남구 123456',
+  information: '양천주가 in 서울 강남구 123456',
+}];
+
 export default function Restaurants() {
   return (
     <ul>
-      <li>양천주가</li>
+      {
+        restaurants.map(({ id, name }) => (
+          <li key={id}>
+            {name}
+          </li>
+        ))
+      }
     </ul>
   );
 }
