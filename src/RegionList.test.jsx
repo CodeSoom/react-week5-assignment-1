a4536ef6e43dbda2615088432de224f1c9907d18
Dokyun Lim <im.koremp@gmail.com>
2020-10-25 12:20:11 +0900

    Update RegionList Test

--- src/RegionList.test.jsx
+++ src/RegionList.test.jsx
@@ -7,11 +7,7 @@ import regions from '../fixtures/regions';
 test('RegionList', () => {
   const { getByText } = render(<RegionList regions={regions} />);
 
-  expect(getByText('서울')).not.toBeNull();
-  expect(getByText('대전')).not.toBeNull();
-  expect(getByText('대구')).not.toBeNull();
-  expect(getByText('부산')).not.toBeNull();
-  expect(getByText('광주')).not.toBeNull();
-  expect(getByText('강원도')).not.toBeNull();
-  expect(getByText('인천')).not.toBeNull();
+  regions.forEach(({ name }) => {
+    expect(getByText(name)).not.toBeNull();
+  });
 });
