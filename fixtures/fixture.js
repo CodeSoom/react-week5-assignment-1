bdc19184cb66a3335177a9928c21448404b385fe
wjy5446 <wjy5446@gmail.com>
2020-10-25 21:41:46 +0900

    fix) selectedButtons fixtures를 수정

--- fixtures/fixture.js
+++ fixtures/fixture.js
@@ -1,4 +1,4 @@
 export const regions = [{ id: 1, name: '서울' }, { id: 2, name: '대구' }];
 export const categories = [{ id: 1, name: '한식' }, { id: 2, name: '일식' }];
 export const restaurants = [{ id: 1, name: '밀면' }];
-export const selectedButtons = { region: '서울', category: '한식' };
+export const selectedButtons = { region: { id: 1, name: '서울' }, category: { id: 1, name: '한식' } };
