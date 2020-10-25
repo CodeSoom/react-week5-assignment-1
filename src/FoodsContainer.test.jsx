259faffc5b15d7dd37cc6945f9168372e7403d2f
comejtome508 <youngnoh.goh@gmail.com>
2020-10-25 11:40:54 +0900

    [Red] Seperate foods container

---
+++ src/FoodsContainer.test.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { useSelector } from 'react-redux';
+
+import { render } from '@testing-library/react';
+
+import FoodsContainer from './FoodsContainer';
+
+jest.mock('react-redux');
+
+test('FoodsContainer', () => {
+  const foods = [
+    {
+      id: 1, name:'한식',
+    },
+  ];
+
+  const { getByText } = render((
+    <FoodsContainer />
+  ));
+
+  expect(getByText('한식')).not.toBeNull();
+});
