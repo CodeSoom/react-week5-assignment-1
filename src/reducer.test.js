3646e207abb484898a4d453795d4af027397c04e
comejtome508 <youngnoh.goh@gmail.com>
2020-10-25 12:43:39 +0900

    [Red] add setFoods in reducer

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -17,4 +17,20 @@ describe('reducer', () => {
       expect(state.regions).not.toHaveLength(0);
     });
   });
+
+  describe('setFoods', () => {
+    it('changes foods', () => {
+      const foods = [
+        {
+          id: 1, name:'한식',
+        },
+      ];
+      const initialState = {
+        foods: [],
+      };
+      const state = reducer(initialState, setFoods(foods));
+
+      expect(state.foods).toHaveLength(1);
+    });
+  });
 });
