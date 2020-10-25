2ca060ae0645dd5499d222e208168b24a8276686
davidyang2149 <davidyang2149@gmail.com>
2020-10-25 20:09:04 +0900

    [Red] Update Categories.test.jsx
    5강 비동기 - 5주 차 과제 1
    - Categories.test.jsx 수정.
    - Categories 테스트 코드추가 작성(Red).
    - CategoriesContainer.jsx 연결.

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -1,19 +1,60 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Categories from './Categories';
 
+import categories from '../__fixtures__/categories';
+
 describe('Categories Component', () => {
-  it('Page render', () => {
-    const { getByText } = render((
-      <Categories />
+  const handleClick = jest.fn();
+
+  it('Page render without selected', () => {
+    const selected = '';
+
+    const { container } = render((
+      <Categories
+        categories={categories}
+        selected={selected}
+        onClick={handleClick}
+      />
     ));
 
-    expect(getByText(/한식/)).not.toBeNull();
-    expect(getByText(/중식/)).not.toBeNull();
-    expect(getByText(/일식/)).not.toBeNull();
-    expect(getByText(/양식/)).not.toBeNull();
-    expect(getByText(/분식/)).not.toBeNull();
+    categories.forEach(({ name }) => {
+      expect(container).toHaveTextContent(name);
+    });
+  });
+
+  it('Page render with selected', () => {
+    const selected = '한식';
+
+    const { getByText } = render((
+      <Categories
+        categories={categories}
+        selected={selected}
+        onClick={handleClick}
+      />
+    ));
+
+    expect(getByText('한식 (V)')).not.toBeNull();
+  });
+
+  it('Click category', () => {
+    const selected = '';
+
+    const { container, getByText } = render((
+      <Categories
+        categories={categories}
+        selected={selected}
+        onClick={handleClick}
+      />
+    ));
+
+    categories.forEach(({ name }) => {
+      expect(container).toHaveTextContent(name);
+      fireEvent.click(getByText(name));
+    });
+
+    expect(handleClick).toBeCalledTimes(categories.length);
   });
 });
