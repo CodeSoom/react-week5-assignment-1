cf93cb74ab7fdc05d0a5add669c3ed20a12bc9f5
juunini <juuni.ni.i@gmail.com>
2020-10-25 12:03:21 +0900

    Add component Restaurnats [Refactoring]

--- src/components/Restaurants.jsx
+++ src/components/Restaurants.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 
-const restaurants = [{
-  id: 1,
-  categoryId: 1,
-  name: '양천주가',
-  address: '서울 강남구 123456',
-  information: '양천주가 in 서울 강남구 123456',
-}];
-
-export default function Restaurants() {
+export default function Restaurants({ restaurants }) {
   return (
     <ul>
       {
--- src/components/Restaurants.test.jsx
+++ src/components/Restaurants.test.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 import Restaurants from './Restaurants';
+import restaurants from '../../fixtures/restaurants';
 
 test('Restaurants', () => {
-  render(<Restaurants />);
+  render(<Restaurants
+    restaurants={restaurants}
+  />);
 
   expect(screen.getByText(/양천주가/)).toBeInTheDocument();
 });
