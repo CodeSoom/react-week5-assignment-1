7602790551f7ec2cb3f01412e8d24f1011f3f5b0
goodzzong <goodzzong@gmail.com>
2020-10-27 00:29:07 +0900

    api 에 대한 테스트 코드 추가

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -1,13 +1,45 @@
 import {
   fetchRegions,
+  fetchCategories,
+  fetchRestaurants,
 } from './api';
 
-jest.mock('./api');
+import regions from '../../fixtures/regions';
+import categories from '../../fixtures/categories';
+import restaurants from '../../fixtures/restaurants';
 
 describe('api', () => {
-  it('fetchRegions', async () => {
-    const regions = await fetchRegions();
+  function mockFetch(data) {
+    global.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve(data),
+    }));
+  }
 
-    expect(regions).toHaveLength(0);
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('fetchRegions', async () => {
+    mockFetch(regions);
+    const data = await fetchRegions();
+
+    expect(data).toEqual(regions);
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+
+  it('fetchCategories', async () => {
+    mockFetch(categories);
+    const data = await fetchCategories();
+
+    expect(data).toEqual(categories);
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+
+  it('fetchRestaurants', async () => {
+    mockFetch(restaurants);
+    const data = await fetchRestaurants();
+
+    expect(data).toEqual(restaurants);
+    expect(fetch).toHaveBeenCalledTimes(1);
   });
 });
