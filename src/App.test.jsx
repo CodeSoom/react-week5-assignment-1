7ed4ce4fbc426acf0d4117e88ff758981471af1a
Dokyun Lim <im.koremp@gmail.com>
2020-10-30 18:51:53 +0900

    App test 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
+import { useDispatch } from 'react-redux';
 import App from './App';
 
 test('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   const { getByText } = render(<App />);
 
-  expect(getByText('서울')).not.toBeNull();
-  expect(getByText('한식')).not.toBeNull();
+  expect(dispatch).toBeCalled();
 });
