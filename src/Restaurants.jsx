77b5a0378d00c15bd3d46f506ff310e23f3552e9
dodoongtak <themighty.tak@gmail.com>
2021-02-22 20:44:15 +0900

    feat: renders message to let user click buttons

--- src/Restaurants.jsx
+++ src/Restaurants.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 export default function Restaurants({ restaurants }) {
+  if (restaurants.length === 0) {
+    return (
+      <p>지역과 카테고리를 클릭해주세요.</p>
+    );
+  }
+
   return (
     <ul>
       {restaurants.map((restaurnat) => (
