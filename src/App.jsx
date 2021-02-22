5f93da7745580906a25c7b787591bdfad9a91ef3
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-22 12:14:00 +0900

    Test(App은 지역들을 보여준다) 통과

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function App() {
   return (
-    <></>
+    <>
+      <div>서울</div>
+      <div>부산</div>
+    </>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import App from './App';
+
 import { render } from '@testing-library/react';
 
 describe('App', () => {
   it('지역들을 보여준다.', () => {
     const { queryByText } = render(<App />);
     expect(queryByText('서울')).toBeInTheDocument();
+    expect(queryByText('부산')).toBeInTheDocument();
   })
 })
