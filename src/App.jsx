78427460c053832cd3144e4471d2169680edd06c
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-22 20:59:54 +0900

    App을 리팩토링하여 분류해당의 코드들을 Categories태그로 대체합니다.

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,14 @@ import React from 'react';
 import Location from './Location';
 
 import locations from '../fixtures/locations';
-
 import categories from '../fixtures/categories';
+import Categories from './Categories';
 
 export default function App() {
   return (
     <div>
       <Location locations={locations} />
-
-      <ul>
-        {categories.map((category) => (
-          <li key={category.id}><button type="button">{category.name}</button></li>
-        ))}
-      </ul>
+      <Categories categories={categories} />
     </div>
   );
 }
