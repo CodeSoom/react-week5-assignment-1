7a6f9874bc7e2f82c5b5f78a428fb2ceb8c01969
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-22 17:18:58 +0900

    reducer test(지역버튼 클릭시 상태변환) 작성

---
+++ src/reducer.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { fireEvent, render } from '@testing-library/react';
+
+import changeSelectedRegion from './actions';
+
+import reducer from './reducer';
+
+describe('reducer', () => {
+  const preveState = {
+    regions: [],
+    selectedRegionID: 0,
+  };
+
+  it('지역버튼이 클릭되면 selectedRegionID가 바뀐다.', () => {
+    const state = reducer(preveState, changeSelectedRegion(2));
+    expect(state.selectedRegionID).toBe(2);
+  });
+});
