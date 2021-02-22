8b75ecd4ea32c37fb404e76eff3ba9996f2b4cf0
Najeong-Kim <najeong@Najeong-Kimui-MacBookPro.local>
2021-02-22 17:38:21 +0900

    red : App에  지역 버튼이 보이는지 테스트 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,3 +3,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import App from './App';
+
+describe('App', () => {
+  it('renders region buttons', () => {
+    const { getByText } = render(<App />)
+
+    expect(getByText('서울')).not.toBeNull();
+    expect(getByText('대전')).not.toBeNull();
+  })
+})
