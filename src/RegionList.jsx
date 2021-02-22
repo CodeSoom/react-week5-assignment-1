0ed36d447a7209cd760fcc397b7b86adb66e07eb
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-22 14:33:42 +0900

    test(지역을 클릭하면 handleClick 함수가 실행된다.) 구현 완료

--- src/RegionList.jsx
+++ src/RegionList.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-export default function RegionList({ regions }) {
+export default function RegionList({ regions, onClick }) {
   return (
     <ul>
-      {regions.map(({ id, name }) => <li key={id}>{name}</li>)}
+      {regions.map(({ id, name }) => (
+        <li key={id}>
+          <button type="button" onClick={onClick} >
+            {name}
+          </button>
+        </li>
+      ))}
     </ul>
   );
 }
