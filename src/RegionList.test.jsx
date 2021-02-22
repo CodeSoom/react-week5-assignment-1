f586da8b89d5a53bf26da25713304ef6d1d87154
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-22 14:30:40 +0900

    test (지역을 클릭하면 handleClick함수가 실행된다) 작성

--- src/RegionList.test.jsx
+++ src/RegionList.test.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import RegionList from './RegionList';
 
 describe('RegionContainer', () => {
+  const handleClick = jest.fn();
+
   const regions = [
     { id: 1, name: '서울' },
     { id: 2, name: '인천' },
   ];
 
   it('지역들을 보여준다.', () => {
-    const { queryByText } = render(<RegionList regions={regions} />);
+    const { queryByText } = render(
+      <RegionList
+        regions={regions}
+        onClick={handleClick}
+      />,
+    );
     expect(queryByText('서울')).toBeInTheDocument();
     expect(queryByText('인천')).toBeInTheDocument();
+  });
+
+  it('지역을 클릭하면 handleClick함수가 실행된다.', () => {
+    const { queryByText } = render(
+      <RegionList
+        regions={regions}
+        onClick={handleClick}
+      />,
+    );
+    fireEvent.click(queryByText('서울'));
+    expect(handleClick).toBeCalled();
   });
 });
