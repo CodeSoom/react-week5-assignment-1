d58ecdf8f7dce2ea0f7adf532546681dfafca0be
dodoongtak <themighty.tak@gmail.com>
2021-02-22 20:34:21 +0900

    feat: renders name of the restaurants via reducers

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
+import Restaurants from './Restaurants';
+
 export default function RestaurantsContainer() {
+  const { restaurants } = useSelector((state) => ({
+    restaurants: state.restaurants,
+  }));
+
   return (
-    <div />
+    <Restaurants
+      restaurants={restaurants}
+    />
   );
 }
---
+++ src/reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+  regions: [],
+  categories: [],
+  restaurants: [],
+};
+
+export default function reducer(state = initialState, action) {
+  return state;
+}
