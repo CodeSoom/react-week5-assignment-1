47a3b60d7feda4aba9cbf2b0d00cd1c014e38a99
최창현 <chchoi_einz@einz.co.kr>
2021-02-22 10:49:44 +0900

    Green: 버튼 표시하기 테스트 통과

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,24 @@ import React from 'react';
 
 export default function App() {
   return (
-    <></>
+    <>
+    <ul>
+      <li>서울</li>
+      <li>대전</li>
+      <li>대구</li>
+      <li>부산</li>
+      <li>광주</li>
+      <li>강원도</li>
+      <li>인천</li>
+    </ul>
+
+    <ul>
+      <li>한식</li>
+      <li>중식</li>
+      <li>일식</li>
+      <li>양식</li>
+      <li>분식</li>
+    </ul>
+    </>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -20,8 +20,8 @@ describe('App', () => {
     expect(queryByText(/부산/)).not.toBeNull();
     expect(queryByText(/광주/)).not.toBeNull();
     expect(queryByText(/강원도/)).not.toBeNull();
-
     expect(queryByText(/인천/)).not.toBeNull();
+
     expect(queryByText(/한식/)).not.toBeNull();
     expect(queryByText(/중식/)).not.toBeNull();
     expect(queryByText(/일식/)).not.toBeNull();
