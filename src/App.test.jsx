33e831ce7a8f54d6bbea04d45dddaeb19ded8828
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-22 22:04:55 +0900

    Red) App.test.jsx : 테스트 케이스 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -8,5 +8,12 @@ test('App', () => {
   const { queryByText } = render((<App />));
 
   expect(queryByText('서울')).not.toBeNull();
+  expect(queryByText('대전')).not.toBeNull();
+  expect(queryByText('대구')).not.toBeNull();
+  expect(queryByText('부산')).not.toBeNull();
+  expect(queryByText('광주')).not.toBeNull();
+
   expect(queryByText('한식')).not.toBeNull();
+  expect(queryByText('중식')).not.toBeNull();
+  expect(queryByText('일식')).not.toBeNull();
 });
