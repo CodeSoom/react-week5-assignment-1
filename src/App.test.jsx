b23b50f899596d5684adb26c39f01b74bb8400a0
Najeong-Kim <najeong@Najeong-Kimui-MacBookPro.local>
2021-02-22 17:50:47 +0900

    red : 카테고리 버튼 테스트 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -16,4 +16,13 @@ describe('App', () => {
     expect(getByText('강원도')).not.toBeNull();
     expect(getByText('인천')).not.toBeNull();
   })
+  it('renders category buttons', () => {
+    const { getByText } = render(<App />)
+
+    expect(getByText('한식')).not.toBeNull();
+    expect(getByText('중식')).not.toBeNull();
+    expect(getByText('일식')).not.toBeNull();
+    expect(getByText('양식')).not.toBeNull();
+    expect(getByText('분식')).not.toBeNull();
+  })
 })
