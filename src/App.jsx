186e64f2cd4694b9a826851ab3f660849bd7e003
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-22 22:28:00 +0900

    Refactoring) Categories.jsx, RegionButtons.jsx : 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 
+import RegionButtons from './RegionButtons';
+import Categories from './Categories';
+
 export default function App() {
   return (
     <div>
-      <button type="button">서울</button>
-      <button type="button">대전</button>
-      <button type="button">대구</button>
-      <button type="button">부산</button>
-      <button type="button">광주</button>
-
-      <button type="button">한식</button>
-      <button type="button">중식</button>
-      <button type="button">일식</button>
+      <RegionButtons />
+      <Categories />
     </div>
   );
 }
---
+++ src/Categories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Categories() {
+  return (
+    <div>
+      <button type="button">한식</button>
+      <button type="button">중식</button>
+      <button type="button">일식</button>
+    </div>
+  );
+}
