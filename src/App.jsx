881bab6459f83d6c67279f3d8ced6863e4955b4f
dodoongtak <themighty.tak@gmail.com>
2021-02-22 19:22:25 +0900

    ♻️ Refactor: create Restaurant Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
+import Restaurants from './Restaurants';
+
 export default function App() {
   return (
     <div>
-      <ul>
-        <li>양천주가</li>
-        <li>한국식 초밥</li>
-        <li>김초밥</li>
-      </ul>
+      <Restaurants />
     </div>
   );
 }
