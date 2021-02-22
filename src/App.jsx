ce350d062a07863423370a8d05a144c27f45c987
Najeong-Kim <najeong@Najeong-Kimui-MacBookPro.local>
2021-02-22 17:45:02 +0900

    green : App에 지역 버튼 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default function App() {
   return (
-    <></>
+    <div>
+      <ul>
+        <li><button>서울</button></li>
+        <li><button>대전</button></li>
+      </ul>
+    </div>
   );
 }
