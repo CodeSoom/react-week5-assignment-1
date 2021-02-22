55b2ab80204a58b1ae25553d3a43ab0faebea4dc
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-22 22:03:09 +0900

    Green) App.jsx: 테스트 케이스 통과

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function App() {
   return (
-    <></>
+    <div>
+      <button type="button">서울</button>
+      <button type="button">한식</button>
+    </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import { queryByText, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import App from './App';
 
 test('App', () => {
   const { queryByText } = render((<App />));
-});
 
-expect(queryByText('서울')).not.toBeNull();
-expect(queryByText('한식')).not.toBeNull();
+  expect(queryByText('서울')).not.toBeNull();
+  expect(queryByText('한식')).not.toBeNull();
+});
