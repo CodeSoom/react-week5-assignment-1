d20e24e80378b0f608e6a713ca7e8ecd8d54317e
dodoongtak <themighty.tak@gmail.com>
2021-02-22 18:55:26 +0900

    feat: renders name of the restaurants

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default function App() {
   return (
-    <></>
+    <div>
+      <ul>
+        <li>양천주가</li>
+        <li>한국식 초밥</li>
+        <li>김초밥</li>
+      </ul>
+    </div>
   );
 }
