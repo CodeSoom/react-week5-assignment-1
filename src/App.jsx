e1d948da7e5818b71af035764197d5a7820f508f
Najeong-Kim <najeong@Najeong-Kimui-MacBookPro.local>
2021-02-22 17:51:44 +0900

    green : App에 카테고리 버튼 구현

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,13 @@ export default function App() {
         <li><button>강원도</button></li>
         <li><button>인천</button></li>
       </ul>
+      <ul>
+        <li><button>한식</button></li>
+        <li><button>중식</button></li>
+        <li><button>일식</button></li>
+        <li><button>양식</button></li>
+        <li><button>분식</button></li>
+      </ul>
     </div>
   );
 }
