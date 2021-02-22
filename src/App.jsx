4c51d1495dca7d6fd2816c08a11b230432394ae4
Najeong-Kim <najeong@Najeong-Kimui-MacBookPro.local>
2021-02-22 17:49:03 +0900

    지역 버튼, 테스트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ export default function App() {
       <ul>
         <li><button>서울</button></li>
         <li><button>대전</button></li>
+        <li><button>대구</button></li>
+        <li><button>부산</button></li>
+        <li><button>광주</button></li>
+        <li><button>강원도</button></li>
+        <li><button>인천</button></li>
       </ul>
     </div>
   );
