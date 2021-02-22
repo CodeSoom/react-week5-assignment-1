ab94695a97c312f8609e9a738374697dc1a6adb0
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-22 14:55:34 +0900

    RegionList.test 테스트 중복 코드 제거

--- src/RegionList.test.jsx
+++ src/RegionList.test.jsx
@@ -14,38 +14,30 @@ describe('RegionContainer', () => {
   ];
   const regionID = 1;
 
-  it('지역들을 보여준다.', () => {
-    const { queryByText } = render(
+  function renderList() {
+    return render(
       <RegionList
         regions={regions}
         selectedRegionID={regionID}
         onClick={handleClick}
       />,
     );
+  }
+
+  it('지역들을 보여준다.', () => {
+    const { queryByText } = renderList();
     expect(queryByText('인천')).toBeInTheDocument();
     expect(queryByText('부산')).toBeInTheDocument();
   });
 
   it('지역을 클릭하면 handleClick함수가 실행된다.', () => {
-    const { queryByText } = render(
-      <RegionList
-        regions={regions}
-        selectedRegionID={regionID}
-        onClick={handleClick}
-      />,
-    );
+    const { queryByText } = renderList();
     fireEvent.click(queryByText('인천'));
     expect(handleClick).toBeCalled();
   });
 
   it('선택된 지역은 (V)를 같이 보여준다.', () => {
-    const { queryByText } = render(
-      <RegionList
-        regions={regions}
-        selectedRegionID={regionID}
-        onClick={handleClick}
-      />,
-    );
+    const { queryByText } = renderList();
     expect(queryByText('서울(V)')).toBeInTheDocument();
   });
 });
