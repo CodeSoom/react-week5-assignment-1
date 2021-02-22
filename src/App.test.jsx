ed176b5ebcf5d2f92b28f7f969a8883ed6625037
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-22 18:58:56 +0900

    App을 테스트하는데, 나머지 지역들을 테스트합니다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,5 +9,11 @@ describe('App', () => {
     const { queryByText } = render((<App />));
 
     expect(queryByText(/서울/)).not.toBeNull();
+    expect(queryByText(/대전/)).not.toBeNull();
+    expect(queryByText(/대구/)).not.toBeNull();
+    expect(queryByText(/부산/)).not.toBeNull();
+    expect(queryByText(/광주/)).not.toBeNull();
+    expect(queryByText(/강원도/)).not.toBeNull();
+    expect(queryByText(/인천/)).not.toBeNull();
   });
 });
