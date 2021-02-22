8e3b739e8aac0a10b9d63cf7864ddcfae329f49a
최창현 <chchoi_einz@einz.co.kr>
2021-02-22 10:42:18 +0900

    Red: App.jsx 버튼 테스트 작성

---
+++ src/App.test.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  function renderApp() {
+    return render((
+      <App />
+    ));
+  }
+
+  it('button들을 보여준다.', () => {
+    const { queryByText } = renderApp();
+
+    expect(queryByText(/서울/)).not.toBeNull();
+    expect(queryByText(/대전/)).not.toBeNull();
+    expect(queryByText(/대구/)).not.toBeNull();
+    expect(queryByText(/부산/)).not.toBeNull();
+    expect(queryByText(/광주/)).not.toBeNull();
+    expect(queryByText(/강원도/)).not.toBeNull();
+
+    expect(queryByText(/인천/)).not.toBeNull();
+    expect(queryByText(/한식/)).not.toBeNull();
+    expect(queryByText(/중식/)).not.toBeNull();
+    expect(queryByText(/일식/)).not.toBeNull();
+    expect(queryByText(/양식/)).not.toBeNull();
+    expect(queryByText(/분식/)).not.toBeNull();
+  });
+});
