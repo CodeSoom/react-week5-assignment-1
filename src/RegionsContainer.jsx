f83c78a4a9c18a4bb1c99ec92dab26acae7873cd
최창현 <chchoi_einz@einz.co.kr>
2021-02-22 13:43:37 +0900

    REFACTOR: useSelector로 변경

--- src/RegionsContainer.jsx
+++ src/RegionsContainer.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import Regions from './Regions';
 
 export default function RegionsContainer() {
-  const regions = [
-    { id: 1, name: '서울' },
-    { id: 2, name: '대전' },
-    { id: 3, name: '대구' },
-    { id: 4, name: '부산' },
-    { id: 5, name: '광주' },
-    { id: 6, name: '강원도' },
-    { id: 7, name: '인천' },
-    { id: 1214, name: '제주' },
-    { id: 1219, name: '전주' },
-    { id: 1230, name: '순천' },
-  ];
+  const { regions } = useSelector((state) => ({
+    regions: state.regions,
+  }));
 
-  return (
-    <Regions regions={regions} />
-  );
+  return <Regions regions={regions} />;
 }
--- src/RegionsContainer.test.jsx
+++ src/RegionsContainer.test.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import RegionsContainer from './RegionsContainer';
 
+jest.mock('react-redux');
+
 describe('RegionsContainer', () => {
-  function renderRegionsContainer({ regions }) {
-    return render((
-      <RegionsContainer regions={regions} />
-    ));
-  }
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    useSelector.mockImplementation((selector) => selector({
+      regions: [
+        { id: 1, name: '서울' },
+        { id: 2, name: '대전' },
+        { id: 3, name: '대구' },
+        { id: 4, name: '부산' },
+        { id: 5, name: '광주' },
+        { id: 6, name: '강원도' },
+        { id: 7, name: '인천' },
+        { id: 1214, name: '제주' },
+        { id: 1219, name: '전주' },
+        { id: 1230, name: '순천' },
+      ],
+    }));
+  });
 
   it('button들을 보여준다.', () => {
     const regions = [
@@ -25,7 +42,7 @@ describe('RegionsContainer', () => {
       { id: 1230, name: '순천' },
     ];
 
-    const { queryByText } = renderRegionsContainer({ regions });
+    const { queryByText } = render(<RegionsContainer />);
 
     regions.forEach((region) => {
       const { name } = region;
