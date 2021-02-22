40ef207e2cd7c86fde54b2b348bca7bfadbc6a31
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-22 20:43:07 +0900

    App을 리팩토링하여 지역에 해당하는 코드들을 Location태그로 변경합니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 
-import locations from '../fixtures/locations';
+import Location from './Location';
+
 import categories from '../fixtures/categories';
 
 export default function App() {
   return (
     <div>
-      <ul>
-        {locations.map((location) => (
-          <li key={location.id}><button type="button">{location.name}</button></li>
-        ))}
-      </ul>
+      <Location />
 
       <ul>
         {categories.map((category) => (
