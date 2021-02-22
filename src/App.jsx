18d4df9acae72ccd595bdabfc3ca30311454c8b1
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-22 19:02:54 +0900

    App test(App시작시 초기값들을 가져온다.) 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
 
 import RegionListContainer from './RegionListContainer';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch();
+  }, [])
+
   return (
     <>
       <RegionListContainer />
