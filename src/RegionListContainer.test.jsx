40905464bced8a2efe123c886a5e83e74d4fb01b
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-22 15:44:39 +0900

    RegionListContainer에 대한 test('지역들 버튼을 누르면 dispatch를 실행한다.') 작성

---
+++ src/RegionListContainer.test.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { fireEvent, render } from '@testing-library/react';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+import RegionListContainer from './RegionListContainer';
+
+jest.mock('react-redux');
+
+describe('RegionListContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      region: [
+        { id: 1, name: '서울' },
+        { id: 2, name: '인천' },
+        { id: 3, name: '부산' },
+      ],
+      selectedRegionID: 3,
+    }));
+  });
+
+  it('지역들 중 버튼을 하나 누르면 dispatch를 실행한다.', () => {
+    const { getByText } = render(<RegionListContainer />);
+    fireEvent.click(getByText('인천'));
+    expect(dispatch).toBeCalled();
+  });
+});
