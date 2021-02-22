7559e8b23fe67c202513f778b119f4f8ce6bdf5c
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-22 19:00:07 +0900

    App을 구현하는데 나머지 지역들을 추가합니다.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ export default function App() {
   return (
     <ul>
       <li><button type="button">서울</button></li>
+      <li><button type="button">대전</button></li>
+      <li><button type="button">대구</button></li>
+      <li><button type="button">부산</button></li>
+      <li><button type="button">광주</button></li>
+      <li><button type="button">강원도</button></li>
+      <li><button type="button">인천</button></li>
     </ul>
   );
 }
