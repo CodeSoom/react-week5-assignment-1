44d896262b06c17f56f1cf398142d7bd5cd51e8e
최창현 <chchoi_einz@einz.co.kr>
2021-02-22 11:09:36 +0900

    REFACTOR: 중복 코드  제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,60 @@
 import React from 'react';
 
 export default function App() {
+  const regions = [
+    { id: 1, name: '서울' },
+    { id: 2, name: '대전' },
+    { id: 3, name: '대구' },
+    { id: 4, name: '부산' },
+    { id: 5, name: '광주' },
+    { id: 6, name: '강원도' },
+    { id: 7, name: '인천' },
+    { id: 1214, name: '제주' },
+    { id: 1219, name: '전주' },
+    { id: 1230, name: '순천' },
+  ];
+
+  const categories = [
+    { id: 1, name: '한식' },
+    { id: 2, name: '중식' },
+    { id: 3, name: '일식' },
+    { id: 4, name: '양식' },
+    { id: 5, name: '분식' },
+    { id: 6, name: '과자' },
+    { id: 7, name: '치킨' },
+    { id: 1215, name: '아시아식' },
+    { id: 1216, name: '중동' },
+    { id: 1217, name: '가정식' },
+    { id: 1218, name: '3분요리' },
+    { id: 1231, name: '냉동' },
+  ];
+
   return (
     <>
-    <ul>
-      <li>서울</li>
-      <li>대전</li>
-      <li>대구</li>
-      <li>부산</li>
-      <li>광주</li>
-      <li>강원도</li>
-      <li>인천</li>
-    </ul>
+      <ul>
+        {regions.map(({
+          id, name,
+        }) => (
+          <li key={id}>
+            <button type="button">
+              {name}
+            </button>
+          </li>
+        ))}
+      </ul>
 
-    <ul>
-      <li>한식</li>
-      <li>중식</li>
-      <li>일식</li>
-      <li>양식</li>
-      <li>분식</li>
-    </ul>
+      <ul>
+        {categories.map(({
+          id, name,
+        }) => (
+          <li key={id}>
+            <button type="button">
+              {name}
+            </button>
+          </li>
+        ))}
+      </ul>
+
     </>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -14,18 +14,44 @@ describe('App', () => {
   it('button들을 보여준다.', () => {
     const { queryByText } = renderApp();
 
-    expect(queryByText(/서울/)).not.toBeNull();
-    expect(queryByText(/대전/)).not.toBeNull();
-    expect(queryByText(/대구/)).not.toBeNull();
-    expect(queryByText(/부산/)).not.toBeNull();
-    expect(queryByText(/광주/)).not.toBeNull();
-    expect(queryByText(/강원도/)).not.toBeNull();
-    expect(queryByText(/인천/)).not.toBeNull();
+    const regions = [
+      { id: 1, name: '서울' },
+      { id: 2, name: '대전' },
+      { id: 3, name: '대구' },
+      { id: 4, name: '부산' },
+      { id: 5, name: '광주' },
+      { id: 6, name: '강원도' },
+      { id: 7, name: '인천' },
+      { id: 1214, name: '제주' },
+      { id: 1219, name: '전주' },
+      { id: 1230, name: '순천' },
+    ];
 
-    expect(queryByText(/한식/)).not.toBeNull();
-    expect(queryByText(/중식/)).not.toBeNull();
-    expect(queryByText(/일식/)).not.toBeNull();
-    expect(queryByText(/양식/)).not.toBeNull();
-    expect(queryByText(/분식/)).not.toBeNull();
+    const categories = [
+      { id: 1, name: '한식' },
+      { id: 2, name: '중식' },
+      { id: 3, name: '일식' },
+      { id: 4, name: '양식' },
+      { id: 5, name: '분식' },
+      { id: 6, name: '과자' },
+      { id: 7, name: '치킨' },
+      { id: 1215, name: '아시아식' },
+      { id: 1216, name: '중동' },
+      { id: 1217, name: '가정식' },
+      { id: 1218, name: '3분요리' },
+      { id: 1231, name: '냉동' },
+    ];
+
+    regions.forEach((region) => {
+      const { name } = region;
+
+      expect(queryByText(name)).not.toBeNull();
+    });
+
+    categories.forEach((category) => {
+      const { name } = category;
+
+      expect(queryByText(name)).not.toBeNull();
+    });
   });
 });
