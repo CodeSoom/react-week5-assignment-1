ef92f2747927772d562d2293c2ed7043886f06b5
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-23 17:53:13 +0900

    :white_check_mark: Red-Test: App.test.jsx 수정
    - mock dispatch 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useDispatch } from 'react-redux';
+
 import App from './App';
+
+jest.mock('react-redux');
 
 describe('App', () => {
   it('should be there regions, categories, restaurants container', () => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementationOnce(() => dispatch);
+
     const { container } = render(<App />);
 
     expect(container.querySelector('.regions-container')).not.toBeNull();
