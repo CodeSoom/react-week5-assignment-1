51d5d3d89114a5cb274a1e4dc7e598194d378cd5
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-23 17:41:22 +0900

    :white_check_mark: Red-Test: reducer test 추가
    - GET_REGIONS_SUCCESS

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,7 +1,15 @@
-// import { GET_REGIONS, GET_REGIONS_SUCCESS, GET_REGIONS_FAILURE } from './actions';
+import { GET_REGIONS, GET_REGIONS_SUCCESS, GET_REGIONS_FAILURE } from './actions';
 import reducer from './reducer';
 
 describe('reducer', () => {
+  const initialState = {
+
+    regions: [],
+    categories: [],
+    restaurants: [],
+    error: { regions: null, categories: null, restaurants: null },
+  };
+
   describe('default state', () => {
     context('if there is no action', () => {
       it('shold have default state', () => {
@@ -30,6 +38,23 @@ describe('reducer', () => {
         expect(error.regions).toBeNull();
         expect(error.categories).toBeNull();
         expect(error.restaurants).toBeNull();
+      });
+    });
+  });
+  describe('GET_REGIONS', () => {
+    context('GET_REGIONS_SUCCESS', () => {
+      it('should update state with regions received from server', () => {
+        const changedState = reducer(initialState, { type: GET_REGIONS_SUCCESS });
+        const mockRegions = ['서울', '인천', '대구', '부산', '울산', '광주'];
+        const { regions } = changedState;
+
+        expect(regions).toHaveLength(6);
+        expect(regions.includes(mockRegions[0])).toBe(true);
+        expect(regions.includes(mockRegions[1])).toBe(true);
+        expect(regions.includes(mockRegions[2])).toBe(true);
+        expect(regions.includes(mockRegions[3])).toBe(true);
+        expect(regions.includes(mockRegions[4])).toBe(true);
+        expect(regions.includes(mockRegions[5])).toBe(true);
       });
     });
   });
