af39d66c89f5e48806a6871474cd33cc68287833
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-23 13:59:52 +0900

    Categories를 테스트하는데, onClick 실행 테스트를 추가합니다.

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Categories from './Categories';
 
 import categories from '../fixtures/categories';
 
 describe('Categories', () => {
+  const onClick = jest.fn();
+
   it('분류를 출력합니다.', () => {
     const { queryByText } = render((<Categories categories={categories} />));
 
     categories.forEach((categorie) => {
       expect(queryByText(categorie.name)).not.toBeNull();
     });
+  });
+
+  it('onClick함수가 실행합니다.', () => {
+    const { getByText } = render((
+      <Categories
+        categories={categories}
+        onClick={onClick}
+      />
+    ));
+
+    expect(onClick).not.toBeCalled();
+
+    fireEvent.click(getByText(/한식/));
+
+    expect(onClick).toBeCalled();
   });
 });
