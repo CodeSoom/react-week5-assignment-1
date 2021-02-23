51b55a20fc393e0b67d147f61aa5b676e4588e78
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-23 22:39:11 +0900

    :white_check_mark: Red-Test: App.test.jsx 수정
    - list 존재여부 확인

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
+
+import { regions } from './fixtures/mockData';
 
 jest.mock('react-redux');
 
@@ -12,11 +14,13 @@ describe('App', () => {
   it('should be there regions, categories, restaurants container', () => {
     const dispatch = jest.fn();
     useDispatch.mockImplementationOnce(() => dispatch);
+    useSelector.mockImplementationOnce((selector) => selector({ regions }));
 
     const { container } = render(<App />);
 
     expect(container.querySelector('.regions-container')).not.toBeNull();
     expect(container.querySelector('.categories-container')).not.toBeNull();
     expect(container.querySelector('.restaurants-container')).not.toBeNull();
+    expect(container.querySelector('.list-container')).not.toBeNull();
   });
 });
