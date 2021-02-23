a5c33ff9dff96a55e2cb687d6ce4136c80b87e43
Yoon <yoonhe@gmail.com>
2021-02-23 19:41:27 +0900

    [red] Regions에 대한 테스트를 추가한다

---
+++ src/Regions.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Regions from './Regions';
+
+describe('Regions', () => {
+  it('renders regions', () => {
+    const regions = [{ id: 1, title: '서울' }];
+
+    const { queryByText } = render(<Regions regions={regions} />);
+
+    expect(queryByText('서울')).not.toBeNull();
+  });
+});
