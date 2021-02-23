556ee3dce1bf9e41d886e47c75c36afb396a25a6
Yoon <yoonhe@gmail.com>
2021-02-23 22:02:14 +0900

    [green] CategoriesContainer를 구현한다

--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Categories from './Categories';
 
 export default function CategoriesContainer() {
+  const dispatch = useDispatch();
+
   const { categories } = useSelector((state) => ({
     categories: state.categories,
   }));
 
+  function handleClick(id) {
+    dispatch({
+      type: 'clickCategory',
+      payload: {
+        id,
+      },
+    });
+  }
+
   return (
-    <Categories categories={categories} />
+    <Categories categories={categories} onClick={handleClick} />
   );
 }
