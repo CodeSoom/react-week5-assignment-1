f791042fa0e9ad0986737fbc5404e9b4235395ea
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-23 15:43:36 +0900

    reducer test(카테고리 버튼 입력과 초기실행 데이터 가져오는 상황) 작성

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,11 +1,18 @@
-import { changeSelectedRegion, setRegions } from './actions';
+import {
+  changeSelectedRegion,
+  changeSelectedCategory,
+  setCategories,
+  setRegions,
+} from './actions';
 
 import reducer from './reducer';
 
 describe('reducer', () => {
   const preventState = {
     regions: [],
+    categories: [],
     selectedRegionID: 0,
+    selectedCategoryID: 0,
   };
 
   it('지역버튼이 클릭되면 selectedRegionID가 바뀐다.', () => {
@@ -21,5 +28,20 @@ describe('reducer', () => {
     const state = reducer(preventState, setRegions(data));
 
     expect(state.regions).toHaveLength(2);
+  });
+
+  it('카테고리 버튼이 클릭되면 selectedCategoryID가 바뀐다.', () => {
+    const state = reducer(preventState, changeSelectedCategory(2));
+    expect(state.selectedCategoryID).toBe(2);
+  });
+
+  it('초기 실행시 categories data를 가져온다.', () => {
+    const data = [
+      { id: 1, name: '한식' },
+      { id: 2, name: '중식' },
+    ];
+    const state = reducer(preventState, setCategories(data));
+
+    expect(state.categories).toHaveLength(2);
   });
 });
