0a8a8132dd8b6f53a362eddb0a61bf7967b631e9
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-23 22:30:33 +0900

    :recycle: Refactor: List
    - prop 을 반영할 수 있도록 수정

--- src/List.jsx
+++ src/List.jsx
@@ -5,19 +5,11 @@ import Item from './Item';
 function List({ list }) {
   return (
     <ul>
-      {[
-        { id: 1, name: '서울' },
-        { id: 7, name: '인천' },
-        { id: 3, name: '대구' },
-        { id: 4, name: '부산' },
-        { id: 8, name: '울산' },
-        { id: 5, name: '광주' },
-      ].map((data) => (
+      {list.map((data) => (
         <li key={data.id}>
           <Item text={data.name} />
         </li>
       ))}
-
     </ul>
   );
 }
