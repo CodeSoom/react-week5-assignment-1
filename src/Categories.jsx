998e14da78efed49af735843112ab7f046b32684
Yoon <yoonhe@gmail.com>
2021-02-23 22:24:11 +0900

    [refactoring] Categories를 리팩토링한다

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-export default function Categories({ categories, onClick }) {
+export default function Categories({ categories, onClick, selectId }) {
   return (
     <ul>
       {categories.map((categorie) => (
         <li key={categorie.id}>
           <button type="button" onClick={() => onClick(categorie.id)}>
             {categorie.name}
+            {selectId === categorie.id && '(V)'}
+            {/* 클릭했을때 (V)가 생기는것을 확인해줄 방법을 잘 모르겠습니다... */}
           </button>
         </li>
       ))}
