248ba4d369a7c4c20b50a834166dfc9297654414
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-23 22:22:17 +0900

    :white_check_mark: Red-Test: RegionsContainer 테스트 추가
    - 서버로 부터 데이터를 받는데 성공했을 때 list 출력확인

--- src/RegionsContainer.test.jsx
+++ src/RegionsContainer.test.jsx
@@ -2,18 +2,32 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import RegionsContainer from './RegionsContainer';
+
+import { regions } from './fixtures/mockData';
 
 jest.mock('react-redux');
 
 describe('RegionsContainer', () => {
-  it('should dispatch when mounted', () => {
-    const dispatch = jest.fn();
+  const dispatch = jest.fn();
+  beforeEach(() => {
+    jest.clearAllMocks();
+    useSelector.mockImplementationOnce((selector) => selector({ regions }));
     useDispatch.mockImplementationOnce(() => dispatch);
+  });
 
+  it('should dispatch when mounted', () => {
     render(<RegionsContainer />);
     expect(dispatch).toHaveBeenCalled();
+  });
+
+  it('should render regions', () => {
+    const { getByRole } = render(<RegionsContainer />);
+
+    regions.forEach((region) => {
+      expect(getByRole('list')).toHaveTextContent(region.name);
+    });
   });
 });
---
+++ src/fixtures/mockData.js
@@ -0,0 +1,24 @@
+export const regions = [
+  { id: 1, name: '서울' },
+  { id: 7, name: '인천' },
+  { id: 3, name: '대구' },
+  { id: 4, name: '부산' },
+  { id: 8, name: '울산' },
+  { id: 5, name: '광주' },
+];
+
+export const categories = [
+  { id: 1, name: '한식' },
+  { id: 2, name: '중식' },
+  { id: 3, name: '일식' },
+];
+
+export const restaurants = [
+  {
+    id: 1, categoryId: 1, name: '양천주가', address: '서울 강남구 123456', information: '양천주가 in 서울 강남구 123456',
+  }, {
+    id: 6, categoryId: 1, name: '한국식 초밥', address: '서울 강남구', information: '한국식 초밥 in 서울 강남구',
+  }, {
+    id: 14, categoryId: 1, name: '김초밥', address: '서울시 강남구 역삼동', information: '김초밥 in 서울시 강남구 역삼동',
+  },
+];
