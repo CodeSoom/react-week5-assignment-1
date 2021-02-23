877bb88d7df08d74a29b0256d4b6c369b34e8452
Yoon <yoonhe@gmail.com>
2021-02-23 22:00:06 +0900

    [red] Categories에 대한 테스트를 추가한다

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Categories from './Categories';
 
 describe('Categories', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  const handleClick = jest.fn();
+
   it('renders Categories', () => {
     const categories = [{ id: 1, name: '한식' }];
 
-    const { queryByText } = render(<Categories categories={categories} />);
+    const { queryByText } = render(<Categories
+      categories={categories}
+      onClick={handleClick}
+    />);
 
     expect(queryByText('한식')).not.toBeNull();
+  });
+
+  it('listens category button click event', () => {
+    const categories = [{ id: 1, name: '한식' }];
+
+    const { queryByText } = render(<Categories
+      categories={categories}
+      onClick={handleClick}
+    />);
+
+    fireEvent.click(queryByText('한식'));
+
+    expect(handleClick).toBeCalledWith(1);
   });
 });
