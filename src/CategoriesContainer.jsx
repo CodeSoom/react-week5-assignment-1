feff476d8f93a064657ddb74d23cd238c069927d
dodoongtak <themighty.tak@gmail.com>
2021-02-23 20:50:23 +0900

    ♻️ Refactor: let ButtonTemplate handle rendering categories buttons

--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -1,20 +1,11 @@
 import React from 'react';
 
+import categories from '../fixtures/categories';
+
+import ButtonTemplate from './ButtonTemplate';
+
 export default function CategoriesContainer() {
   return (
-    <div>
-      <button type="button">한식</button>
-      <button type="button">중식</button>
-      <button type="button">일식</button>
-      <button type="button">양식</button>
-      <button type="button">분식</button>
-      <button type="button">과자</button>
-      <button type="button">치킨</button>
-      <button type="button">아시아식</button>
-      <button type="button">중동</button>
-      <button type="button">가정식</button>
-      <button type="button">3분요리</button>
-      <button type="button">냉동</button>
-    </div>
+    <ButtonTemplate buttons={categories} />
   );
 }
