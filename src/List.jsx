f21dea215ef2655c7516f66b11d365da81ae3303
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-23 22:29:44 +0900

    :construction: Green-Working: List.test.jsx 통과하도록 함

--- src/List.jsx
+++ src/List.jsx
@@ -5,11 +5,19 @@ import Item from './Item';
 function List({ list }) {
   return (
     <ul>
-      {list.map((data) => (
+      {[
+        { id: 1, name: '서울' },
+        { id: 7, name: '인천' },
+        { id: 3, name: '대구' },
+        { id: 4, name: '부산' },
+        { id: 8, name: '울산' },
+        { id: 5, name: '광주' },
+      ].map((data) => (
         <li key={data.id}>
           <Item text={data.name} />
         </li>
       ))}
+
     </ul>
   );
 }
