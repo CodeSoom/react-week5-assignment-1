79cf67563dfb3473a05d68d36052426d82ddcc28
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-23 17:32:20 +0900

    :white_check_mark: Red-Test: reducer test 추가
    - 초기상태를 리턴해야하는 경우

---
+++ src/reducer.test.js
@@ -0,0 +1,36 @@
+// import { GET_REGIONS, GET_REGIONS_SUCCESS, GET_REGIONS_FAILURE } from './actions';
+import reducer from './reducer';
+
+describe('reducer', () => {
+  describe('default state', () => {
+    context('if there is no action', () => {
+      it('shold have default state', () => {
+        const {
+          regions, categories, restaurants, error,
+        } = reducer();
+
+        expect(regions).toHaveLength(0);
+        expect(categories).toHaveLength(0);
+        expect(restaurants).toHaveLength(0);
+        expect(error.regions).toBeNull();
+        expect(error.categories).toBeNull();
+        expect(error.restaurants).toBeNull();
+      });
+    });
+
+    context('if there is wrong action', () => {
+      it('shold have default state', () => {
+        const {
+          regions, categories, restaurants, error,
+        } = reducer(undefined, { type: '@@INIT' });
+
+        expect(regions).toHaveLength(0);
+        expect(categories).toHaveLength(0);
+        expect(restaurants).toHaveLength(0);
+        expect(error.regions).toBeNull();
+        expect(error.categories).toBeNull();
+        expect(error.restaurants).toBeNull();
+      });
+    });
+  });
+});
