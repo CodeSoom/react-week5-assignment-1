7a2eaf98d397ee92ddf0cdea3c724703672699b7
Yoon <yoonhe@gmail.com>
2021-02-23 20:42:10 +0900

    [green] Reducer를 구현한다

--- src/reducer.jsx
+++ src/reducer.jsx
@@ -3,5 +3,13 @@ const initialState = {
 };
 
 export default function reducer(state = initialState, action = { type: '' }) {
+  if (action.type === 'setRegions') {
+    const { regions } = action.payload;
+    return {
+      ...state,
+      regions,
+    };
+  }
+
   return state;
 }
