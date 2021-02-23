2244d68b188588e0ba07ad20c09a8ee34b0740ac
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-23 22:29:04 +0900

    :white_check_mark: Red-Test: List test 수정
    - 수정한 mock data 반영

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import List from './List';
+import { regions } from './fixtures/mockData';
 
 describe('List', () => {
   it('should be there list with received prop', () => {
-    const { getByRole } = render(<List list={[1, 2, 3]} />);
+    const { getByRole } = render(<List list={regions} />);
 
     const list = getByRole('list');
 
-    expect(list).toHaveTextContent(1);
-    expect(list).toHaveTextContent(2);
-    expect(list).toHaveTextContent(3);
+    regions.forEach((region) => {
+      expect(list).toHaveTextContent(region.name);
+    });
   });
 });
