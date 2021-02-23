0996d6d5c7f8e8964e13cfe0facf8dc96515e798
canary4651 <canary4651@gmail.com>
2021-02-23 23:44:53 +0900

    Add region buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
+import regions from './fixtures/regions';
+
 export default function App() {
   return (
     <div>
       <h1>Restaurant List</h1>
       <ul>
-        <li>
-          <button type="button">
-            서울
-          </button>
-        </li>
+        {regions.map((region) => (
+          <li key={region.id}>
+            <button type="button">
+              {region.name}
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
---
+++ src/fixtures/regions.js
@@ -0,0 +1,32 @@
+const regions = [
+  {
+    id: 1,
+    name: '서울',
+  },
+  {
+    id: 2,
+    name: '대전',
+  },
+  {
+    id: 3,
+    name: '대구',
+  },
+  {
+    id: 4,
+    name: '부산',
+  },
+  {
+    id: 5,
+    name: '광주',
+  },
+  {
+    id: 6,
+    name: '강원도',
+  },
+  {
+    id: 7,
+    name: '인천',
+  },
+];
+
+export default regions;
