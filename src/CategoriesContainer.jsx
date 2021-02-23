c5975805ac9a62af64a660f1b6bd4165d6db42ab
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-23 14:54:36 +0900

    CategoriesContainer test 통과

--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -2,19 +2,17 @@ import React from 'react';
 
 import { useSelector, useDispatch } from 'react-redux';
 
-import { changeSelectedRegion } from './actions';
-
 import Buttons from './Buttons';
 
-export default function RegionListContainer() {
+export default function RegionsContainer() {
   const dispatch = useDispatch();
   const { regions, selectedRegionID } = useSelector((state) => ({
-    regions: state.regions,
-    selectedRegionID: state.selectedRegionID,
+    regions: state.categories,
+    selectedCategoryID: state.selectedCategoryID,
   }));
 
   function handleClick(id) {
-    return dispatch(changeSelectedRegion(id));
+    return dispatch(id);
   }
 
   return (
