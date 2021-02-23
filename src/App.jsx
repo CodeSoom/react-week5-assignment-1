3c1b4e45378971acd8014d41e111c4ca1b4390ed
dodoongtak <themighty.tak@gmail.com>
2021-02-23 20:46:20 +0900

    feat:  rendering categories buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import RegionsContainer from './RegionsContainer';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
+import CategoriesContainer from './CategoriesContainer';
+
 export default function App() {
   return (
     <div>
       <RegionsContainer />
+      <CategoriesContainer />
       <RestaurantsContainer />
     </div>
   );
--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 
 export default function CategoriesContainer() {
   return (
-    <div />
+    <div>
+      <button type="button">한식</button>
+      <button type="button">중식</button>
+      <button type="button">일식</button>
+      <button type="button">양식</button>
+      <button type="button">분식</button>
+      <button type="button">과자</button>
+      <button type="button">치킨</button>
+      <button type="button">아시아식</button>
+      <button type="button">중동</button>
+      <button type="button">가정식</button>
+      <button type="button">3분요리</button>
+      <button type="button">냉동</button>
+    </div>
   );
 }
