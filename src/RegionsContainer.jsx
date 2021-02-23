b4f73f48e6cfb497a26a65b32f0f0fe7e405ac1a
dodoongtak <themighty.tak@gmail.com>
2021-02-23 20:29:58 +0900

    ♻️ Refactor: let ButtonTemplate handle rendering region buttons

--- src/RegionsContainer.jsx
+++ src/RegionsContainer.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 
+import regions from '../fixtures/regions';
+
+import ButtonTemplate from './ButtonTemplate';
+
 export default function RegionsContainer() {
   return (
-    <div>
-      <button type="button">서울</button>
-      <button type="button">대전</button>
-      <button type="button">대구</button>
-      <button type="button">부산</button>
-      <button type="button">광주</button>
-      <button type="button">강원도</button>
-      <button type="button">인천</button>
-      <button type="button">제주</button>
-      <button type="button">전주</button>
-      <button type="button">순천</button>
-    </div>
+    <ButtonTemplate buttons={regions} />
   );
 }
