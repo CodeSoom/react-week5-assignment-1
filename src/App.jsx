a3f4f46846f72ca500d3877642b6cfa813f889b2
dodoongtak <themighty.tak@gmail.com>
2021-02-23 20:10:48 +0900

    feat:  add test for rendering regions buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import RegionsContainer from './RegionsContainer';
+
 import RestaurantsContainer from './RestaurantsContainer';
 
 export default function App() {
   return (
     <div>
+      <RegionsContainer />
       <RestaurantsContainer />
     </div>
   );
---
+++ src/RegionsContainer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function RegionsContainer() {
+  return (
+    <div>
+      <button type="button">서울</button>
+      <button type="button">대전</button>
+      <button type="button">대구</button>
+      <button type="button">부산</button>
+      <button type="button">광주</button>
+      <button type="button">강원도</button>
+      <button type="button">인천</button>
+      <button type="button">제주</button>
+      <button type="button">전주</button>
+      <button type="button">순천</button>
+    </div>
+  );
+}
