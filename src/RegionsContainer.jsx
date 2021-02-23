98a0277c18ea74259bbf2737c9c78edf4d4c420c
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-23 16:35:46 +0900

    :construction: Green-Working: RegionsContainer 테스트 통과하도록 설정

--- src/RegionsContainer.jsx
+++ src/RegionsContainer.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+// import { getRegions } from './services/api';
 
 function RegionsContainer() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch({ type: 'GET_REGIONS' });
+  }, []);
   return (
     <div className="regions-container">RegionsContainer</div>
   );
