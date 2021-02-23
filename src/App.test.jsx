8091c18f55c239b5d99c716752a6dd06d4d1a8a9
Yoon <yoonhe@gmail.com>
2021-02-23 19:57:29 +0900

    [refactoring] App을 리팩토링한다

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,8 +3,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import App from './App';
+import { useSelector } from '../__mocks__/react-redux';
 
 describe('App', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      regions: [{ id: 1, title: '서울' }],
+    }));
+  });
+
   it('renders regions', () => {
     const { queryByText } = render(<App />);
     expect(queryByText('서울')).not.toBeNull();
