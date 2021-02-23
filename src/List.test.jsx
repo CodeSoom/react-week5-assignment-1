af22f7b133dc71f40c7fba4532f58ae34845675d
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-23 22:44:11 +0900

    :white_check_mark: Red-Test: List 테스트 추가
    - prop 없을 때 테스트 추가

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -6,13 +6,25 @@ import List from './List';
 import { regions } from './fixtures/mockData';
 
 describe('List', () => {
-  it('should be there list with received prop', () => {
-    const { getByRole } = render(<List list={regions} />);
+  context('if there is a list', () => {
+    it('should be there list with received prop', () => {
+      const { getByRole } = render(<List list={regions} />);
 
-    const list = getByRole('list');
+      const list = getByRole('list');
 
-    regions.forEach((region) => {
-      expect(list).toHaveTextContent(region.name);
+      regions.forEach((region) => {
+        expect(list).toHaveTextContent(region.name);
+      });
+    });
+  });
+
+  context('if there is no list', () => {
+    it('there should be nothing', () => {
+      const { queryByRole } = render(<List />);
+
+      const list = queryByRole('list');
+
+      expect(list).toBeNull();
     });
   });
 });
