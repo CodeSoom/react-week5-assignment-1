599c78c57dbfede6bfb0b23dcb631bbfca2c5c2d
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-24 14:25:41 +0900

    :construction: Green-Working: CategoriesContainer 테스트 통과

--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 function CategoriesContainer() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch();
+  }, []);
   return (
     <div className="categories-container">categories-container</div>
   );
