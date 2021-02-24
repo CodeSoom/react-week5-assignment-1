7f3d0a607228efa8e1b7a12bf94567cc00391111
김민성 <yssj2049@gmail.com>
2021-02-24 23:09:21 +0900

    GREEN : pass button render test

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 
 export default function App() {
   return (
-    <>선PR 후과제</>
+    <div>
+      <ul>
+        <li>서울</li>
+        <li>대전</li>
+      </ul>
+      <ul>
+        <li>한식</li>
+        <li>일식</li>
+      </ul>
+    </div>
   );
 }
