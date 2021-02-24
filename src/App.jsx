73a3a78b5f6500de27aa8e0a7af739da5a032573
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-24 19:50:46 +0900

    App에 CategoriesContainer를 적용합니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import Categories from './Categories';
-
-import categories from '../fixtures/categories';
+import CategoriesContainer from './CategoriesContainer';
 
 import LocationsContainer from './LocationsContainer';
 
 export default function App() {
-  const [state, setState] = useState({
-    selectedcategoryname: '',
-  });
-
-  const { selectedcategoryname } = state;
-
-  function handleClickCategory(event) {
-    setState({
-      ...state,
-      selectedcategoryname: event.target.value,
-    });
-  }
-
   return (
     <div>
       <LocationsContainer />
-      <Categories
-        categories={categories}
-        onClick={handleClickCategory}
-        selectedcategoryname={selectedcategoryname}
-      />
+      <CategoriesContainer />
     </div>
   );
 }
