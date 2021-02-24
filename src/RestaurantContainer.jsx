5fd53e8db92ff0992bcb955ce78acd4af047f16a
Yoon <yoonhe@gmail.com>
2021-02-24 17:28:49 +0900

    [refactoring] RestaurantsContainer를 리팩토링합니다

--- src/RestaurantContainer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function RestaurantContainer() {
-  return (
-    <ul>
-      <li>양천주가</li>
-    </ul>
-  );
-}
--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import RestaurantContainer from './RestaurantContainer';
+import { useSelector } from 'react-redux';
+import RestaurantsContainer from './RestaurantsContainer';
 
-describe('RestaurantContainer', () => {
+jest.mock('react-redux');
+
+describe('RestaurantsContainer', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      restaurants: [{ id: 1, name: '양천주가' }],
+    }));
+  });
+
   it('renders categories', () => {
-    const { queryByText } = render(<RestaurantContainer />);
+    const { queryByText } = render(<RestaurantsContainer />);
 
     expect(queryByText('양천주가')).not.toBeNull();
   });
