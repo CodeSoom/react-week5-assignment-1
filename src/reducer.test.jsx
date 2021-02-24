040ab0b8547bea572994f05a5c435144049e93c9
dodoongtak <themighty.tak@gmail.com>
2021-02-24 22:04:07 +0900

    ✅ test:  add a test for changing regions

--- src/reducer.test.jsx
+++ src/reducer.test.jsx
@@ -1,6 +1,8 @@
 import reducer from './reducer';
 
-import { restaurants } from '../fixtures';
+import { regions, restaurants } from '../fixtures';
+
+import setRegions from './actions';
 
 describe('reducer', () => {
   it('returns default parameter, initial state', () => {
@@ -17,5 +19,13 @@ describe('reducer', () => {
     expect(state.restaurants[0].name).toBe('양천주가');
     expect(state.restaurants[1].name).toBe('한국식 초밥');
     expect(state.restaurants[2].name).toBe('김초밥');
+  });
+
+  it('changes regions', () => {
+    const state = reducer({ regions }, setRegions(regions));
+
+    expect(state.regions[0].name).toBe('서울');
+    expect(state.regions[1].name).toBe('대전');
+    expect(state.regions[2].name).toBe('대구');
   });
 });
