72e9581c0f1d854f20f4ecb8de916df58603345c
canary4651 <canary4651@gmail.com>
2021-02-24 23:24:51 +0900

    ♻️  Refactoring RegionsContanier and add reducer

--- src/RegionsContainer.jsx
+++ src/RegionsContainer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
 import Regions from './Regions';
 
-import regions from './fixtures/regions';
-
 export default function RegionsContianer() {
+  const { regions } = useSelector((state) => ({
+    regions: state.regions,
+  }));
+
   return (
     <div>
       <Regions regions={regions} />
--- src/RegionsContainer.test.jsx
+++ src/RegionsContainer.test.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import RegionsContainer from './RegionsContainer';
 
+jest.mock('react-redux');
+
 describe('RegionsContainer', () => {
+  const useSelector = jest.fn();
+
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      regions: [{ id: 1, name: '서울' }],
+    }));
+  });
+
   it('renders region', () => {
     const { queryByText } = render(<RegionsContainer />);
 
