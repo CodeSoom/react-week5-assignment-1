faf763d1e844acad969c85440fc4037aa08ad41c
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-24 14:25:16 +0900

    :white_check_mark: Red-Test: CategoriesContainer 테스트 추가

---
+++ src/CategoriesContainer.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { useDispatch } from 'react-redux';
+
+import CategoriesContainer from './CategoriesContainer';
+
+jest.mock('react-redux');
+
+describe('CategoriesContainer', () => {
+  const dispatch = jest.fn();
+  beforeEach(() => {
+    jest.clearAllMocks();
+    useDispatch.mockImplementationOnce(() => dispatch);
+  });
+
+  it('should dispatch when mounted', () => {
+    render(<CategoriesContainer />);
+    expect(dispatch).toHaveBeenCalledTimes(1);
+  });
+});
