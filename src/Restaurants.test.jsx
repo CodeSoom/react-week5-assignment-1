e1af44dbecc7f06431657daa5fa52ea634fd32b2
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-24 22:27:01 +0900

    :white_check_mark: Red-Test: Restaurants
    - 레스토랑 목록 출력

--- src/Restaurants.test.jsx
+++ src/Restaurants.test.jsx
@@ -8,7 +8,7 @@ import { restaurants } from './fixtures/mockData';
 describe('Restaurants', () => {
   context('if there are restaurants', () => {
     it('should be there restaurants with received prop', () => {
-      const { getByRole } = render(<Restaurants Restaurants={restaurants} />);
+      const { getByRole } = render(<Restaurants restaurants={restaurants} />);
 
       restaurants.forEach((reataurant) => {
         expect(getByRole('list')).toHaveTextContent(reataurant.name);
