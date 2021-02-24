1945a24f2da875c9e1b0f54c1307a8a0f10f3964
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-24 10:30:05 +0900

    :construction: Working: redux-devtools-extension 적용

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import reducer from './reducer';
 
 const middleWares = [thunk];
 
-const store = createStore(reducer, applyMiddleware(...middleWares));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleWares)));
 
 export default store;
