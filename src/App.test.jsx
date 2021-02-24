331dd7514c5f3e73c37b9b55b9bbdd78bcab7daa
김민성 <yssj2049@gmail.com>
2021-02-24 23:11:55 +0900

    RED : remained button render test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,9 +6,18 @@ import App from './App';
 
 test('App', () => {
   const { queryByText } = render((<App />));
+  
   expect(queryByText('서울')).not.toBeNull();
   expect(queryByText('대전')).not.toBeNull();
+  expect(queryByText('대구')).not.toBeNull();
+  expect(queryByText('부산')).not.toBeNull();
+  expect(queryByText('광주')).not.toBeNull();
+  expect(queryByText('강원도')).not.toBeNull();
+  expect(queryByText('인천')).not.toBeNull();
 
   expect(queryByText('한식')).not.toBeNull();
+  expect(queryByText('중식')).not.toBeNull();
   expect(queryByText('일식')).not.toBeNull();
+  expect(queryByText('양식')).not.toBeNull();
+  expect(queryByText('분식')).not.toBeNull();
 });
