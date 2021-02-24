1d74a56c8631f3f58b532b94ce1a20289216bb74
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-24 22:32:58 +0900

    :recycle: Refactor: Restaurants
    - 레스토랑 목록 없을 때

--- src/Restaurants.jsx
+++ src/Restaurants.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 function Restaurants({ restaurants }) {
-  return <></>;
-  // return (
-  //   <ul className="restaurant-list">
-  //     {restaurants.map((restaurant) => <li key={restaurant.id}>{restaurant.name}</li>)}
-  //   </ul>
-  // );
+  if (!restaurants) {
+    return <></>;
+  }
+  return (
+    <ul className="restaurant-list">
+      {restaurants.map((restaurant) => <li key={restaurant.id}>{restaurant.name}</li>)}
+    </ul>
+  );
 }
 
 export default Restaurants;
