477641302996b01feb717484889066d682910b05
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-24 14:59:34 +0900

    :recycle: Refactor: CategoriesContainer

--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -1,30 +1,20 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { createThunk, GET_CATEGORIES } from './actions';
+import List from './List';
 import { getCategories } from './services/api';
 
 function CategoriesContainer() {
   const dispatch = useDispatch();
+
+  const categories = useSelector((state) => state.categories);
+
   useEffect(() => {
     dispatch(createThunk(GET_CATEGORIES, getCategories));
   }, []);
   return (
-    <div className="categories-container">
-      <ul>
-        {[
-          { id: 1, name: '한식' },
-          { id: 2, name: '중식' },
-          { id: 3, name: '일식' },
-        ].map((data) => (
-          <li key={data.id}>
-            <button type="button">
-              {data.name}
-            </button>
-          </li>
-        ))}
-      </ul>
-
-    </div>
+    <div className="categories-container"><List list={categories} /></div>
   );
 }
 
