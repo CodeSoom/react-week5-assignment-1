7a313e5877a47aefe34d3a1f61a9b9647565d7fc
Yoon <yoonhe@gmail.com>
2021-02-24 18:25:56 +0900

    [red] App에 대한 테스트코드를 추가합니다

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,41 +1,68 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 
 import App from './App';
-import { loadCategories, loadRegions } from './actions';
+// import { loadCategories, loadRegions, loadRestaurants } from './actions';
 
 jest.mock('react-redux');
 jest.mock('./services/api');
-jest.mock('./actions');
+// jest.mock('./actions');
 
 describe('App', () => {
   const dispatch = jest.fn();
-  const asyncFunction = jest.fn();
+  // const asyncFunction = jest.fn();
+
+  function useSelectorMockMaker(store) {
+    useSelector.mockImplementation((selector) => selector(store));
+  }
 
   beforeEach(() => {
     jest.resetAllMocks();
 
-    useSelector.mockImplementation((selector) => selector({
-      regions: [{ id: 1, name: '서울' }],
-      categories: [{ id: 1, name: '한식' }],
-    }));
-
-    loadRegions.mockImplementation(() => asyncFunction);
-    loadCategories.mockImplementation(() => asyncFunction);
+    // loadRegions.mockImplementation(() => asyncFunction);
+    // loadCategories.mockImplementation(() => asyncFunction);
+    // loadCategories.mockImplementation(() => asyncFunction);
 
     useDispatch.mockImplementation(() => dispatch);
   });
 
-  it('renders regions and categories and restaurant', () => {
-    const { queryByText } = render(<App />);
+  context('without selectCategoryId and selectRegionId', () => {
+    it('renders regions and categories', () => {
+      useSelectorMockMaker({
+        regions: [{ id: 1, name: '서울' }],
+        categories: [{ id: 1, name: '한식' }],
+        restaurants: [{ id: 1, name: '양천주가' }],
+      });
 
-    expect(dispatch).toBeCalledTimes(2);
-    expect(dispatch).toHaveBeenNthCalledWith(1, loadRegions());
-    expect(dispatch).toHaveBeenNthCalledWith(2, loadCategories());
+      const { queryByText } = render(<App />);
 
-    expect(queryByText('서울')).not.toBeNull();
-    expect(queryByText('한식')).not.toBeNull();
-    expect(queryByText('양천주가')).not.toBeNull();
+      expect(dispatch).toBeCalledTimes(2);
+
+      expect(queryByText('서울')).not.toBeNull();
+      expect(queryByText('한식')).not.toBeNull();
+      expect(queryByText('양천주가')).not.toBeNull();
+    });
+  });
+
+  context('with selectCategoryId and selectRegionId', () => {
+    it('renders regions and categories and restaurants', async () => {
+      useSelectorMockMaker({
+        regions: [{ id: 1, name: '서울' }],
+        categories: [{ id: 1, name: '한식' }],
+        restaurants: [],
+        selectRegionId: 1,
+        selectCategoryId: 1,
+      });
+
+      const { queryByText, getByText } = render(<App />);
+
+      expect(dispatch).toBeCalledTimes(3);
+
+      expect(queryByText('서울(V)')).not.toBeNull();
+      expect(queryByText('한식(V)')).not.toBeNull();
+
+      await waitFor(() => expect(getByText('양천주가')).toBeInTheDocument());
+    });
   });
 });
