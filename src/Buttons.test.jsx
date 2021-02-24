e4e8b278e0c90adcd7372dbfe60e0994a30c8fa1
dodoongtak <themighty.tak@gmail.com>
2021-02-24 18:50:30 +0900

    ✅ test:  remove meaningless tests

--- src/Buttons.test.jsx
+++ src/Buttons.test.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import Buttons from './Buttons';
 
 import regions from '../fixtures/regions';
-
-import categories from '../fixtures/categories';
 
 describe('Buttons', () => {
   const renderButtons = (buttons) => render((
@@ -14,36 +12,4 @@ describe('Buttons', () => {
       buttons={buttons}
     />
   ));
-
-  it('render regions buttons', () => {
-    const { queryByText } = renderButtons(regions);
-
-    expect(queryByText('서울')).not.toBeNull();
-    expect(queryByText('대전')).not.toBeNull();
-    expect(queryByText('대구')).not.toBeNull();
-    expect(queryByText('부산')).not.toBeNull();
-    expect(queryByText('광주')).not.toBeNull();
-    expect(queryByText('강원도')).not.toBeNull();
-    expect(queryByText('인천')).not.toBeNull();
-    expect(queryByText('제주')).not.toBeNull();
-    expect(queryByText('전주')).not.toBeNull();
-    expect(queryByText('순천')).not.toBeNull();
-  });
-
-  it('renders categories buttons', () => {
-    const { queryByText } = renderButtons(categories);
-
-    expect(queryByText('한식')).not.toBeNull();
-    expect(queryByText('중식')).not.toBeNull();
-    expect(queryByText('일식')).not.toBeNull();
-    expect(queryByText('양식')).not.toBeNull();
-    expect(queryByText('분식')).not.toBeNull();
-    expect(queryByText('과자')).not.toBeNull();
-    expect(queryByText('치킨')).not.toBeNull();
-    expect(queryByText('아시아식')).not.toBeNull();
-    expect(queryByText('중동')).not.toBeNull();
-    expect(queryByText('가정식')).not.toBeNull();
-    expect(queryByText('3분요리')).not.toBeNull();
-    expect(queryByText('냉동')).not.toBeNull();
-  });
 });
