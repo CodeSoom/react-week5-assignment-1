ccd1b614f1f4399ae85a0a8b2eb639f0298e50d7
최창현 <chchoi_einz@einz.co.kr>
2021-02-24 10:29:38 +0900

    Red: 테스트 작성. Buttons 클릭이벤트

--- src/Buttons.test.jsx
+++ src/Buttons.test.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Buttons from './Buttons';
 
 import { regions } from '../fixtures/restaurants';
 
 describe('Buttons', () => {
+  const handleClick = jest.fn();
+  const items = regions;
+
   it('button들을 보여준다.', () => {
     const { queryByText } = render((
-      <Buttons buttons={regions} />
+      <Buttons buttons={items} />
     ));
 
-    regions.forEach((button) => {
+    items.forEach((button) => {
       const { name } = button;
 
       expect(queryByText(name)).not.toBeNull();
     });
+  });
+
+  it('버튼을 클릭하면 handleClick함수가 실행된다.', () => {
+    const { queryByText } = render((
+      <Buttons
+        buttons={items}
+        handleClick={handleClick}
+      />
+    ));
+
+    items.forEach((item) => {
+      fireEvent.click(queryByText(item.name));
+    });
+
+    expect(handleClick).toBeCalledTimes(items.length);
   });
 });
