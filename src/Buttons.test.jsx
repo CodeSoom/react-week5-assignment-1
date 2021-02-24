7aa2d0b878c958c136462023f63863b04a6bb9cf
dodoongtak <themighty.tak@gmail.com>
2021-02-24 18:55:30 +0900

    ✅ test:  add a test for checking number of function call by clicking button

--- src/Buttons.test.jsx
+++ src/Buttons.test.jsx
@@ -7,9 +7,22 @@ import Buttons from './Buttons';
 import regions from '../fixtures/regions';
 
 describe('Buttons', () => {
+  const handleClick = jest.fn();
+
   const renderButtons = (buttons) => render((
     <Buttons
       buttons={buttons}
+      handleClick={handleClick}
     />
   ));
+
+  it('calls handleClick function as many as buttons', () => {
+    const { getByText } = renderButtons(regions);
+
+    regions.forEach(({ name }) => {
+      fireEvent.click(getByText(name));
+    });
+
+    expect(handleClick).toBeCalledTimes(regions.length);
+  });
 });
