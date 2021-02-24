9336e9d01fe7e95f0d510f8f620e646237ab46ff
김민성 <yssj2049@gmail.com>
2021-02-24 23:37:29 +0900

    REFACTOR : sepreate App with Categories & Regions

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,13 @@
 import React from 'react';
 
+import Regions from './Regions';
+import Categories from './Categories';
+
 export default function App() {
   return (
     <div>
-      <ul>
-        <li>
-          <button type="button">서울</button>
-        </li>
-        <li>
-          <button type="button">대구</button>
-        </li>
-        <li>
-          <button type="button">부산</button>
-        </li>
-        <li>
-          <button type="button">광주</button>
-        </li>
-        <li>
-          <button type="button">강원도</button>
-        </li>
-        <li>
-          <button type="button">인천</button>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <button type="button">한식</button>
-        </li>
-        <li>
-          <button type="button">중식</button>
-        </li>
-        <li>
-          <button type="button">일식</button>
-        </li>
-        <li>
-          <button type="button">양식</button>
-        </li>
-        <li>
-          <button type="button">분식</button>
-        </li>
-      </ul>
+      <Regions />
+      <Categories />
     </div>
   );
 }
---
+++ src/Categories.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Categories() {
+  return (
+    <ul>
+      <li>
+        <button type="button">한식</button>
+      </li>
+      <li>
+        <button type="button">중식</button>
+      </li>
+      <li>
+        <button type="button">일식</button>
+      </li>
+      <li>
+        <button type="button">양식</button>
+      </li>
+      <li>
+        <button type="button">분식</button>
+      </li>
+    </ul>
+  );
+}
