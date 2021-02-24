fc207f49aec5fdfac8c7cd3363a21929958e5854
canary4651 <canary4651@gmail.com>
2021-02-25 00:04:55 +0900

    🔴 Create setRegions reducer test

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,5 +1,9 @@
 import reducer from './reducer';
 
+import {
+  setRegions,
+} from './actions';
+
 describe('reducer', () => {
   describe('default state', () => {
     it('returns initialState', () => {
@@ -8,6 +12,23 @@ describe('reducer', () => {
       const { regions } = state;
 
       expect(regions).toHaveLength(0);
+    });
+  });
+
+  describe('setRegions', () => {
+    it('changes regions', () => {
+      const state = reducer({
+        rigions: [],
+      }, setRegions({
+        regions: [
+          {
+            id: 1,
+            name: '서울',
+          },
+        ],
+      }));
+
+      expect(state.regions).not.toHaveLength(0);
     });
   });
 });
