8e12ad2e2c9f4d1016bc0cbd22b3f0f0a599a4e2
Yoon <yoonhe@gmail.com>
2021-02-24 10:22:52 +0900

    [red] Categories에 대한 테스트를 추가한다

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -34,4 +34,32 @@ describe('Categories', () => {
 
     expect(handleClick).toBeCalledWith(1);
   });
+
+  context('when selected', () => {
+    it('renders "category name" + "(V)"', () => {
+      const categories = [{ id: 1, name: '한식' }];
+
+      const { queryByText } = render(<Categories
+        categories={categories}
+        onClick={handleClick}
+        selectId={1}
+      />);
+
+      expect(queryByText('한식(V)')).not.toBeNull();
+    });
+  });
+
+  context('When not selected', () => {
+    it('renders "category name"', () => {
+      const categories = [{ id: 1, name: '한식' }];
+
+      const { queryByText } = render(<Categories
+        categories={categories}
+        onClick={handleClick}
+        selectId={null}
+      />);
+
+      expect(queryByText('한식')).not.toBeNull();
+    });
+  });
 });
