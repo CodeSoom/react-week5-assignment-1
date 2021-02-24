24d7632fafd519f1172a3b111c468fdc4623f18e
김민성 <yssj2049@gmail.com>
2021-02-24 23:13:59 +0900

    GREEN : pass remained button render test

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,19 @@ export default function App() {
       <ul>
         <li>서울</li>
         <li>대전</li>
+        <li>대구</li>
+        <li>부산</li>
+        <li>광주</li>
+        <li>강원도</li>
+        <li>인천</li>
+
       </ul>
       <ul>
         <li>한식</li>
+        <li>중식</li>
         <li>일식</li>
+        <li>양식</li>
+        <li>분식</li>
       </ul>
     </div>
   );
