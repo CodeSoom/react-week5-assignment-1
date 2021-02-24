37eb393ccc167baf2d575bdbfd0690d232f4a663
EL <ickyicky@gmail.com>
2021-02-24 19:31:48 +0900

    Red: 테스트 작성. categories의 데이터를 받아서 상태를 업데이틓나다.

--- src/reducer.test.jsx
+++ src/reducer.test.jsx
@@ -2,9 +2,10 @@ import reducer from './reducer';
 
 import {
   setRegions,
+  setCategories,
 } from './actions';
 
-import { regions } from '../fixtures/restaurants';
+import { regions, categories } from '../fixtures/restaurants';
 
 describe('reducer', () => {
   describe('setRegions', () => {
@@ -16,6 +17,18 @@ describe('reducer', () => {
       const state = reducer(initialState, setRegions(regions));
 
       expect(state.regions).not.toHaveLength(0);
+    });
+  });
+
+  describe('setCategories', () => {
+    it('categories의 데이터를 받아서 상태를 업데이트한다.', () => {
+      const initialState = {
+        categories: [],
+      };
+
+      const state = reducer(initialState, setCategories(categories));
+
+      expect(state.categories).not.toHaveLength(0);
     });
   });
 });
