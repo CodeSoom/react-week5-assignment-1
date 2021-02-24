9a68481d8f23945cbef8a4bc83804f95e2b8fd6f
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-24 21:56:59 +0900

    :construction: Green-Working: RestaurantsContainer
    - 레스토랑 목록 가져오기

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function RestaurantsContainer() {
+  const dispatch = useDispatch();
+
+  const { regionName, categoryId } = useSelector((state) => state.watching);
+
+  useEffect(() => {
+    dispatch();
+  }, [regionName && categoryId]);
   return (
     <div className="restaurants-container">restaurants-container</div>
   );
 }
+/*
+TODO
 
+1 > 지역 선택하고 종류 선택하면 dispatch 된다.
+
+2 > 서버로부터 받아왔으면 출력한다.
+
+*/
 export default RestaurantsContainer;
