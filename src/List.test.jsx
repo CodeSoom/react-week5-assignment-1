e603600f6c5fd7b323c4afbbddfda4a9e7925259
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-24 15:14:33 +0900

    :white_check_mark: Red-Test: List

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import List from './List';
 import { regions } from './fixtures/mockData';
@@ -8,13 +8,17 @@ import { regions } from './fixtures/mockData';
 describe('List', () => {
   context('if there is a list', () => {
     it('should be there list with received prop', () => {
-      const { getByRole } = render(<List list={regions} />);
+      const onClick = jest.fn();
+      const { getByText, getByRole } = render(<List list={regions} onClick={onClick} />);
 
       const list = getByRole('list');
 
       regions.forEach((region) => {
         expect(list).toHaveTextContent(region.name);
       });
+
+      fireEvent.click(getByText(regions[0].name));
+      expect(onClick).toHaveBeenCalledWith(regions[0]);
     });
   });
 
