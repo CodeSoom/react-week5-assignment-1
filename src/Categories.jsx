568f80e74f4ddec758dd605f271cc5df3ff526c7
Yoon <yoonhe@gmail.com>
2021-02-24 10:24:07 +0900

    Categories를 구현한다

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -6,9 +6,9 @@ export default function Categories({ categories, onClick, selectId }) {
       {categories.map((categorie) => (
         <li key={categorie.id}>
           <button type="button" onClick={() => onClick(categorie.id)}>
-            {categorie.name}
-            {selectId === categorie.id && '(V)'}
-            {/* 클릭했을때 (V)가 생기는것을 확인해줄 방법을 잘 모르겠습니다... */}
+            {selectId === categorie.id
+              ? `${categorie.name}(V)`
+              : categorie.name}
           </button>
         </li>
       ))}
