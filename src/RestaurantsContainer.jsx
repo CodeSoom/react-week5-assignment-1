511e1585924d71c42aa1972bd2ef0fcc9ceaa84a
dodoongtak <themighty.tak@gmail.com>
2021-02-25 20:26:09 +0900

    feat:  renders a message for user to click category button

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -5,9 +5,16 @@ import { useSelector } from 'react-redux';
 import Restaurants from './Restaurants';
 
 export default function RestaurantsContainer() {
-  const { restaurants } = useSelector((state) => ({
+  const { restaurants, clicked } = useSelector((state) => ({
     restaurants: state.restaurants,
+    clicked: state.clicked,
   }));
+
+  if (clicked.region && !clicked.category) {
+    return (
+      <p>카테고리 버튼도 클릭 해주세요</p>
+    );
+  }
 
   return (
     <Restaurants
