a5a30a0fe5d2519e1faf361bcf71f4ccdd40c149
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-25 14:05:49 +0900

    :recycle: Refactor: api
    - getRegions

--- src/services/api.js
+++ src/services/api.js
@@ -3,5 +3,6 @@ const baseUrl = 'https://eatgo-customer-api.ahastudio.com';
 export async function getRegions() {
   const url = `${baseUrl}/regions`;
   const response = await fetch(url);
+  const data = await response.json();
   return await response.json();
 }
