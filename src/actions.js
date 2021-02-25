13ed9a9879f65b02be763cf5e596871fc73b47b6
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-25 11:39:36 +0900

    :recycle: Refactor: actions
    - getRegionsThunk

--- src/actions.js
+++ src/actions.js
@@ -12,17 +12,8 @@ export function getRegionsThunk() {
   return async (dispatch) => {
     dispatch({ type: 'getRegions' });
     try {
-      dispatch({
-        type: 'getRegionsSuccess',
-        payload: [
-          { id: 1, name: '서울' },
-          { id: 7, name: '인천' },
-          { id: 3, name: '대구' },
-          { id: 4, name: '부산' },
-          { id: 8, name: '울산' },
-          { id: 5, name: '광주' },
-        ],
-      });
+      const data = await getRegions();
+      dispatch({ type: 'getRegionsSuccess', payload: data });
     } catch (error) {
       dispatch({ type: 'getRegionsFailure', payload: error });
     }
