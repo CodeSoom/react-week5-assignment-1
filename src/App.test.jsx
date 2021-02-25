542d8690323934bdb14e9a93916d01e026920695
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-25 15:24:01 +0900

    App테스트를 리팩토링하여 react-redux를 import하는 불필요과정 삭제

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useDispatch, useSelector } from 'react-redux';
 import App from './App';
 
 import locations from '../fixtures/locations';
 import categories from '../fixtures/categories';
-import { useDispatch, useSelector } from '../__mocks__/react-redux';
 
 jest.mock('react-redux');
 jest.mock('./services/api');
