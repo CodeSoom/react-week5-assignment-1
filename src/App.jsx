15a4c65bf8411a696e8470175797b1e933cf0a5f
datalater <the7mincheol@gmail.com>
2021-02-25 11:15:04 +0900

    App에 레스토랑 지역 버튼이 나오도록 구현하라

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default function App() {
   return (
-    <></>
+    <ul>
+      <li>
+        <button type="button">서울</button>
+      </li>
+    </ul>
   );
 }
