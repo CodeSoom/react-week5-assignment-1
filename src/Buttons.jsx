bf4bfd4996aac6dcf58b1a942c63fc9525299f61
dodoongtak <themighty.tak@gmail.com>
2021-02-25 12:17:54 +0900

    ♻️ Refactor: add attributes such as key using uuid and name

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import uuid from 'react-uuid';
+
 export default function Buttons({ buttons, handleClick }) {
   return (
     <ul>
       {buttons.map(({ id, name }) => (
         <li key={id}>
           <button
+            key={uuid()}
+            name={name}
             type="button"
             onClick={handleClick}
           >
