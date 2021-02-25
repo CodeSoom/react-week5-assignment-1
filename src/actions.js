6e37adc778adc00d8aa6a7fc205f78545b5aac5a
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-25 11:51:11 +0900

    :construction: Green-Working: actions
    - getRestaurantsThunk

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,4 @@
-import { getCategories, getRegions } from './services/api';
+import { getCategories, getRegions, getRestaurants } from './services/api';
 
 export function selectRegion(regionName) {
   return { type: 'selectRegion', payload: regionName };
@@ -28,6 +28,29 @@ export function getCategoriesThunk() {
       dispatch({ type: 'getCategoriesSuccess', payload: data });
     } catch (error) {
       dispatch({ type: 'getCategoriesFailure', payload: error });
+    }
+  };
+}
+
+export function getRestaurantsThunk() {
+  return async (dispatch) => {
+    dispatch({ type: 'getRestaurants' });
+    try {
+      const data = await getRestaurants();
+      dispatch({
+        type: 'getRestaurantsSuccess',
+        payload: [
+          {
+            id: 1, categoryId: 1, name: '양천주가', address: '서울 강남구 123456', information: '양천주가 in 서울 강남구 123456',
+          }, {
+            id: 6, categoryId: 1, name: '한국식 초밥', address: '서울 강남구', information: '한국식 초밥 in 서울 강남구',
+          }, {
+            id: 14, categoryId: 1, name: '김초밥', address: '서울시 강남구 역삼동', information: '김초밥 in 서울시 강남구 역삼동',
+          },
+        ],
+      });
+    } catch (error) {
+      dispatch({ type: 'getRestaurantsFailure', payload: error });
     }
   };
 }
