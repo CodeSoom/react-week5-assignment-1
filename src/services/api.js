e00b10f251f56585bd8097c3127ed131d72bbb8e
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-25 14:09:33 +0900

    :construction: Green-Working: api
    - getCategories

--- src/services/api.js
+++ src/services/api.js
@@ -4,5 +4,11 @@ export async function getRegions() {
   const url = `${baseUrl}/regions`;
   const response = await fetch(url);
   const data = await response.json();
+  return data;
+}
+
+export async function getCategories() {
+  const url = `${baseUrl}/categories`;
+  const response = await fetch(url);
   return await response.json();
 }
