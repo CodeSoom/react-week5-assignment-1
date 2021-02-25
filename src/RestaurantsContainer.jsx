65b7c92a1a6f44e2aa9b2d842254555b3b256670
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-26 02:43:56 +0900

    RestaurantsContainer를 구현하여 state의 식당 정보를 다룹니다.

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -1,25 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { useDispatch, useSelector } from 'react-redux';
-
-import { loadRestaurants } from './actions';
+import { useSelector } from 'react-redux';
 
 import Restaurants from './Restaurants';
 
 export default function RestaurantsContainer() {
-  const dispatch = useDispatch();
-
-  const { locationName, categoryName, restaurants } = useSelector((state) => ({
-    locationName: state.selectedLocationName,
-    categoryName: state.selectedCategoryName,
+  const { restaurants } = useSelector((state) => ({
     restaurants: state.restaurants,
   }));
-
-  useEffect(() => {
-    if (locationName && categoryName) {
-      dispatch(loadRestaurants(locationName, categoryName));
-    }
-  }, [locationName, categoryName]);
 
   return (
     <Restaurants restaurants={restaurants} />
