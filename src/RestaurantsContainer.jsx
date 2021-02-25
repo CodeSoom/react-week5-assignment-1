ee47b9e0545012460320ef2e043e3f3e0cc8d7cc
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-25 14:35:30 +0900

    :recycle: Refactor: RestaurantsContainer
    - 식당목록 출력

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -1,33 +1,27 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+
 import { getRestaurantsThunk } from './actions';
+import Restaurants from './Restaurants';
 
 function RestaurantsContainer() {
   const dispatch = useDispatch();
 
-  const { regionName, categoryId } = useSelector((state) => state.watching);
+  const { watching, restaurants } = useSelector((state) => ({
+    restaurants: state.restaurants, watching: state.watching,
+  }));
+
+  const { regionName, categoryId } = watching;
 
   useEffect(() => {
     if (regionName && categoryId) {
       dispatch(getRestaurantsThunk());
     }
   }, [regionName && categoryId]);
+
   return (
     <div>
-      <ul>
-        {[
-          {
-            id: 1, categoryId: 1, name: '양천주가', address: '서울 강남구 123456', information: '양천주가 in 서울 강남구 123456',
-          },
-          {
-            id: 6, categoryId: 1, name: '한국식 초밥', address: '서울 강남구', information: '한국식 초밥 in 서울 강남구',
-          },
-          {
-            id: 14, categoryId: 1, name: '김초밥', address: '서울시 강남구 역삼동', information: '김초밥 in 서울시 강남구 역삼동',
-          },
-        ].map((data) => <li key={data.id}>{data.name}</li>)}
-      </ul>
-
+      <Restaurants restaurants={restaurants} />
     </div>
   );
 }
