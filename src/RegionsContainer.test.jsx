923dd78c82cc5d9580d9b8f8350e4d88f8eb800b
dodoongtak <themighty.tak@gmail.com>
2021-02-25 14:25:18 +0900

    ✅ test:  add a test for appending new region

--- src/RegionsContainer.test.jsx
+++ src/RegionsContainer.test.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { regions } from '../fixtures';
+
+import {
+  setClicked,
+} from './actions';
 
 import RegionsContainer from './RegionsContainer';
 
 describe('RegionsContainer', () => {
+  const dispatch = jest.fn();
+
   beforeEach(() => {
     useSelector.mockImplementation((selector) => selector({
       regions,
     }));
+
+    useDispatch.mockImplementation(() => dispatch);
   });
 
   it('renders region buttons', () => {
@@ -21,5 +29,13 @@ describe('RegionsContainer', () => {
     expect(queryByText('서울')).not.toBeNull();
     expect(queryByText('대전')).not.toBeNull();
     expect(queryByText('대구')).not.toBeNull();
+  });
+
+  it('appends a new region into Clicked upon clicking region', () => {
+    const { queryByText } = render(<RegionsContainer />);
+
+    fireEvent.click(queryByText('서울'));
+
+    expect(dispatch).toBeCalledWith(setClicked({ region: '서울' }));
   });
 });
