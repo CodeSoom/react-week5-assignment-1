dd65df564d2f74b557ced3b4b3c9af553fa0f857
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-25 14:18:46 +0900

    :recycle: Refactor: mockData
    - 주석삭제

--- src/fixtures/mockData.js
+++ src/fixtures/mockData.js
@@ -12,10 +12,7 @@ export const categories = [
   { id: 2, name: '중식' },
   { id: 3, name: '일식' },
 ];
-/*
 
-TODO : 줄바꿈 할 것
-*/
 export const restaurants = [
   {
     id: 1, categoryId: 1, name: '양천주가', address: '서울 강남구 123456', information: '양천주가 in 서울 강남구 123456',
