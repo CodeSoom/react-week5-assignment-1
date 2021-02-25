c19cddc37910cea7441ed79aa40b8785e322ad98
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-25 22:53:48 +0900

    :recycle: Refactor:  Restaurants
    - 가독성 위한 줄바꿈

--- src/Restaurants.jsx
+++ src/Restaurants.jsx
@@ -6,7 +6,11 @@ function Restaurants({ restaurants }) {
   }
   return (
     <ul>
-      {restaurants.map((restaurant) => <li key={restaurant.id}>{restaurant.name}</li>)}
+      {restaurants.map((restaurant) => (
+        <li key={restaurant.id}>
+          {restaurant.name}
+        </li>
+      ))}
     </ul>
   );
 }
