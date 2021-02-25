1114862443d6d467dccd347a05309a12c3fb6ba0
jjkim01 <jaen6563@gmail.com>
2021-02-26 02:25:06 +0900

    test: 서울, 한식 버튼이 눌러졌는지 테스트 추가

--- src/Items.test.jsx
+++ src/Items.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Items from './Items';
 
@@ -8,13 +8,22 @@ import region from '../fixtures/region';
 import category from '../fixtures/category';
 
 test('Items', () => {
-  const { queryByText } = render((
+  const handleClick = jest.fn();
+
+  const { getByText, queryByText } = render((
     <Items
       region={region}
       category={category}
+      onClick={handleClick}
     />
   ));
 
   expect(queryByText(/서울/)).not.toBeNull();
   expect(queryByText(/한식/)).not.toBeNull();
+
+  fireEvent.click(getByText(/서울/));
+  expect(handleClick).toBeCalled();
+
+  fireEvent.click(getByText(/한식/));
+  expect(handleClick).toBeCalled();
 });
