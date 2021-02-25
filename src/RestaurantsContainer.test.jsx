0d76c29ccbe7f29fa191abdfbd5858f753e4ef8a
datalater <the7mincheol@gmail.com>
2021-02-25 17:10:26 +0900

    레스토랑 목록이 있을 경우 컨테이너에서 잘 나오는지 테스트하라

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -2,7 +2,26 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import RestaurantsContainer from './RestaurantsContainer';
+
+jest.mock('react-redux');
+
+const dispatch = jest.fn();
+
+beforeEach(() => {
+  jest.clearAllMocks();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: [
+      '홍콩반점',
+      '포방터 돈까스',
+    ],
+  }));
+});
 
 describe('RestaurantsContainer', () => {
   context('without restaurants', () => {
@@ -10,6 +29,15 @@ describe('RestaurantsContainer', () => {
       const { queryByText } = render(<RestaurantsContainer />);
 
       expect(queryByText(/레스토랑 목록이 없습니다/)).not.toBeNull();
+    });
+  });
+
+  context('with restaurants', () => {
+    it('renders restaurants', () => {
+      const { getByText } = render(<RestaurantsContainer />);
+
+      expect(getByText('홍콩반점')).not.toBeNull();
+      expect(getByText('포방터 돈까스')).not.toBeNull();
     });
   });
 });
