4bc639948e6e9e13a8ddf0d833a7182dd09ab002
Yoon <yoonhe@gmail.com>
2021-02-25 19:33:28 +0900

    [refactoring] Buttons를 리팩토링합니다.

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -6,9 +6,11 @@ export default function Buttons({ buttons, onClick, selectId }) {
       {buttons.map((button) => (
         <li key={button.id}>
           <button type="button" onClick={() => onClick(button.id)}>
-            {selectId === button.id
-              ? `${button.name}(V)`
-              : button.name}
+            {
+              `${button.name}${
+                selectId === button.id ? '(V)' : ''
+              }`
+            }
           </button>
         </li>
       ))}
