5bd024e6ed7d4369721fcf784cdd206055009833
dodoongtak <themighty.tak@gmail.com>
2021-02-26 00:22:55 +0900

    ✅ test: add a test for rendering (V) on the selected item

--- src/RegionsContainer.test.jsx
+++ src/RegionsContainer.test.jsx
@@ -4,20 +4,25 @@ import { render, fireEvent } from '@testing-library/react';
 
 import { useSelector, useDispatch } from 'react-redux';
 
+import given from 'given2';
+
 import { regions } from '../fixtures';
 
-import {
-  setClicked,
-} from './actions';
+import { setClicked } from './actions';
 
 import RegionsContainer from './RegionsContainer';
 
 describe('RegionsContainer', () => {
+  given('clicked', () => ({
+    region: '',
+  }));
+
   const dispatch = jest.fn();
 
   beforeEach(() => {
     useSelector.mockImplementation((selector) => selector({
       regions,
+      clicked: given.clicked,
     }));
 
     useDispatch.mockImplementation(() => dispatch);
@@ -29,6 +34,16 @@ describe('RegionsContainer', () => {
     expect(queryByText('서울')).not.toBeNull();
     expect(queryByText('대전')).not.toBeNull();
     expect(queryByText('대구')).not.toBeNull();
+  });
+
+  it('renders (V) along the selected button', () => {
+    given('clicked', () => ({
+      region: '서울',
+    }));
+
+    const { queryByText } = render(<RegionsContainer />);
+
+    expect(queryByText('서울(V)')).not.toBeNull();
   });
 
   it('appends a new region into Clicked upon clicking region', () => {
