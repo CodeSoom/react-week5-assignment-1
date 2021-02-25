191cd2b3bf26ec08e1c1030d80568988cc78b464
dodoongtak <themighty.tak@gmail.com>
2021-02-25 13:45:00 +0900

    fixup! ✅ test:  verification on clicked array

--- src/reducer.test.jsx
+++ src/reducer.test.jsx
@@ -11,7 +11,7 @@ describe('reducer', () => {
     expect(state.regions).toHaveLength(0);
     expect(state.categories).toHaveLength(0);
     expect(state.restaurants).toHaveLength(0);
-    expect(state.clicked).toHaveLength(0);
+    expect(state.clicked instanceof Object).toBe(true);
   });
 
   it('returns state', () => {
