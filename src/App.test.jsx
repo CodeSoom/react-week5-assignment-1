ee5812c8d7b04bad075a08b2cb96955f146fce13
Yoon <yoonhe@gmail.com>
2021-02-25 17:02:17 +0900

    [refactoring] App을 리팩토링합니다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { render, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import App from './App';
-// import { loadCategories, loadRegions, loadRestaurants } from './actions';
 
 jest.mock('react-redux');
 jest.mock('./services/api');
-// jest.mock('./actions');
 
 describe('App', () => {
   const dispatch = jest.fn();
-  // const asyncFunction = jest.fn();
 
   function useSelectorMockMaker(store) {
     useSelector.mockImplementation((selector) => selector(store));
@@ -20,15 +17,11 @@ describe('App', () => {
   beforeEach(() => {
     jest.resetAllMocks();
 
-    // loadRegions.mockImplementation(() => asyncFunction);
-    // loadCategories.mockImplementation(() => asyncFunction);
-    // loadCategories.mockImplementation(() => asyncFunction);
-
     useDispatch.mockImplementation(() => dispatch);
   });
 
   context('without selectCategoryId and selectRegionId', () => {
-    it('renders regions and categories', () => {
+    it('renders regions and categories', async () => {
       useSelectorMockMaker({
         regions: [{ id: 1, name: '서울' }],
         categories: [{ id: 1, name: '한식' }],
@@ -55,14 +48,12 @@ describe('App', () => {
         selectCategoryId: 1,
       });
 
-      const { queryByText, getByText } = render(<App />);
+      const { queryByText } = render(<App />);
 
       expect(dispatch).toBeCalledTimes(3);
 
       expect(queryByText('서울(V)')).not.toBeNull();
       expect(queryByText('한식(V)')).not.toBeNull();
-
-      await waitFor(() => expect(getByText('양천주가')).toBeInTheDocument());
     });
   });
 });
