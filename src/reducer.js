ec5546233b5ba1e96162230b45cb9e344d502d57
jjkim01 <jaen6563@gmail.com>
2021-02-26 04:07:47 +0900

    웹페이지에서 클릭하면 항목 옆에 (V) 추가하는 기능 구현

--- src/reducer.js
+++ src/reducer.js
@@ -17,10 +17,21 @@ export default function reducer(state = initalState, action = { type: '' }) {
   }
 
   if (action.type === 'getRestaurant') {
+    const { region, category } = state;
     const { item } = action.payload;
+    // item이 (V) 포함이면 (V) 지워버림
+    // item이 region에 있는 애면 거기에 (V)함
+    // item이 category에 있는 애면 거기에 (V)함
+    if (item.indexOf('(V)') === -1 && region.indexOf(item) > -1) {
+      region.splice(region.indexOf(item), 1, item.concat('(V)'));
+    }
+
+    if (item.indexOf('(V)') === -1 && category.indexOf(item) > -1) {
+      category.splice(category.indexOf(item), 1, item.concat('(V)'));
+    }
+
     return {
       ...state,
-      item,
     };
   }
   return state;
