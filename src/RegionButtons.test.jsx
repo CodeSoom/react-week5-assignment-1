776b9e98e25d658e45c442fc2ccfe4a3050e6fae
datalater <the7mincheol@gmail.com>
2021-02-25 11:32:27 +0900

    레스토랑 지역 버튼을 클릭하면 마킹되는지 테스트하라

--- src/RegionButtons.test.jsx
+++ src/RegionButtons.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import RegionButtons from './RegionButtons';
 
@@ -9,5 +9,13 @@ describe('RegionButtons', () => {
     const { getByText } = render(<RegionButtons />);
 
     expect(getByText('서울')).not.toBeNull();
+  });
+
+  it('marks the clicked button', () => {
+    const { getByText } = render(<RegionButtons />);
+
+    fireEvent.click(getByText('서울'));
+
+    expect(getByText('서울V')).not.toBeNull();
   });
 });
