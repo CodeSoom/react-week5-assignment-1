d6697aa68ee4103e858bf04babf50276cdc994f9
dodoongtak <themighty.tak@gmail.com>
2021-02-26 00:27:31 +0900

    ✅ test: add a test for rendering (V) on the selected item

--- src/CategoriesContainer.test.jsx
+++ src/CategoriesContainer.test.jsx
@@ -4,20 +4,25 @@ import { render, fireEvent } from '@testing-library/react';
 
 import { useSelector, useDispatch } from 'react-redux';
 
+import given from 'given2';
+
 import { categories } from '../fixtures';
 
-import {
-  setClicked,
-} from './actions';
+import { setClicked } from './actions';
 
 import CategoriesContainer from './CategoriesContainer';
 
 describe('CategoriesContainer', () => {
+  given('clicked', () => ({
+    category: '',
+  }));
+
   const dispatch = jest.fn();
 
   beforeEach(() => {
     useSelector.mockImplementation((selector) => selector({
       categories,
+      clicked: given.clicked,
     }));
 
     useDispatch.mockImplementation(() => dispatch);
@@ -29,6 +34,16 @@ describe('CategoriesContainer', () => {
     expect(queryByText('한식')).not.toBeNull();
     expect(queryByText('중식')).not.toBeNull();
     expect(queryByText('일식')).not.toBeNull();
+  });
+
+  it('renders (V) along the selected button', () => {
+    given('clicked', () => ({
+      category: '한식',
+    }));
+
+    const { queryByText } = render(<CategoriesContainer />);
+
+    expect(queryByText('한식(V)')).not.toBeNull();
   });
 
   it('appends a new category into Clicked upon clicking region', () => {
