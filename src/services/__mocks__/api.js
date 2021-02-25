6931c7b0e14b9367bfc5b9f7eed5e0191bb180d6
EL <ickyicky@gmail.com>
2021-02-25 22:11:16 +0900

    TEST: API Mock 추가

--- src/services/__mocks__/api.js
+++ src/services/__mocks__/api.js
@@ -2,7 +2,6 @@ export async function fetchRegions() {
   return [];
 }
 
-export function name() {
-  // TODO: Lint 에러방지용 함수. 삭제하기
-  return '';
+export function fetchCategories() {
+  return [];
 }
