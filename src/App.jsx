d0646430e927a4f7354b97a4b3f601dbf7cf563f
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-26 02:31:35 +0900

    App을 구현하여 api로부터 식당목록을 불러옵니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import CategoriesContainer from './CategoriesContainer';
 
@@ -9,15 +9,27 @@ import LocationsContainer from './LocationsContainer';
 import {
   loadLocations,
   loadCategories,
+  loadRestaurants,
 } from './actions';
 
 export default function App() {
   const dispatch = useDispatch();
 
+  const { locationName, categoryName } = useSelector((state) => ({
+    locationName: state.selectedLocationName,
+    categoryName: state.selectedCategoryName,
+  }));
+
   useEffect(() => {
     dispatch(loadLocations());
     dispatch(loadCategories());
   }, []);
+
+  useEffect(() => {
+    if (locationName && categoryName) {
+      dispatch(loadRestaurants(locationName, categoryName));
+    }
+  }, [locationName, categoryName]);
 
   return (
     <div>
