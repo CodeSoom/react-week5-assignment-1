d1bc999daf73c586ab338c56b3de9e07979aa563
datalater <the7mincheol@gmail.com>
2021-02-25 15:24:51 +0900

    등록되지 않은 액션이 오면 상태를 변화하지 않는지 테스트하라

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -5,26 +5,42 @@ import {
 } from './actions';
 
 describe('reducer', () => {
-  describe('setRegion', () => {
-    context('without previous state', () => {
-      const initialState = undefined;
-
-      it('set region using initial state', () => {
-        const state = reducer(initialState, setRegion('서울'));
-
-        expect(state.currentRegion).toBe('서울');
-      });
-    });
-
-    context('with previous state', () => {
+  context('without registered action', () => {
+    it('does not change the state', () => {
       const initialState = {
         currentRegion: '',
       };
 
-      it('set region', () => {
-        const state = reducer(initialState, setRegion('서울'));
+      const state = reducer(initialState, {
+        type: undefined,
+      });
 
-        expect(state.currentRegion).toBe('서울');
+      expect(state).toBe(initialState);
+    });
+  });
+
+  context('with registered action', () => {
+    describe('setRegion', () => {
+      context('without previous state', () => {
+        const initialState = undefined;
+
+        it('set region using initial state', () => {
+          const state = reducer(initialState, setRegion('서울'));
+
+          expect(state.currentRegion).toBe('서울');
+        });
+      });
+
+      context('with previous state', () => {
+        const initialState = {
+          currentRegion: '',
+        };
+
+        it('set region', () => {
+          const state = reducer(initialState, setRegion('서울'));
+
+          expect(state.currentRegion).toBe('서울');
+        });
       });
     });
   });
