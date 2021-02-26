a2f10210e580e83365a3a66adffb52691572b447
Najeong-Kim <najeong@Najeong-Kimui-MacBookPro.local>
2021-02-26 17:18:16 +0900

    red : Region onClick 테스트 작성

--- src/Regions.test.jsx
+++ src/Regions.test.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Regions from './Regions';
 
 import regions from '../fixtures/regions';
 
 describe('Regions', () => {
+  const handleClick = jest.fn();
+
+  const renderRegions = () => render((
+    <Regions 
+      regions={regions} 
+      onClickCheck={handleClick} 
+    />
+  ))
+
   it('renders regions', () => {
-    const { queryByText } = render((
-      <Regions regions={regions} />
-    ))
+    const { queryByText } = renderRegions();
     
     regions.forEach((region) => {
       expect(queryByText(region.name)).not.toBeNull();
     })
   })
+
+  it('clicks region buttons', () => {
+    const { queryByText } = renderRegions();
+
+    categories.forEach((region) => {
+      fireEvent.click(queryByText(region.name));
+    })  
+
+    expect(handleClick).toBeCalledTimes(regions.length);
+  })  
 })
