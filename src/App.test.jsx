c23599e0308dfbc7a6c4f4196ced6408472b895d
Yoon <yoonhe@gmail.com>
2021-02-26 20:21:59 +0900

    [refactoring] App을 리팩토링합니다

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { render } from '@testing-library/react';
 
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
 import given from 'given2';
 import App from './App';
+
+export const mockStore = configureMockStore([thunk]);
 
 jest.mock('react-redux');
 jest.mock('./services/api');
@@ -25,19 +30,15 @@ describe('App', () => {
   });
 
   context('without selectedCategory and selectedRegion', () => {
-    it('renders regions and categories', async () => {
-      const { queryByText } = render(<App />);
+    it('loads regions and categories', async () => {
+      render(<App />);
 
       expect(dispatch).toBeCalledTimes(2);
-
-      expect(queryByText('서울')).not.toBeNull();
-      expect(queryByText('한식')).not.toBeNull();
-      expect(queryByText('양천주가')).not.toBeNull();
     });
   });
 
   context('with selectedCategory and selectedRegion', () => {
-    it('renders regions and categories and restaurants', async () => {
+    it('loads regions and categories and restaurants', async () => {
       given('selectedRegion', () => (
         { id: 1, name: '서울' }
       ));
@@ -46,12 +47,9 @@ describe('App', () => {
         { id: 1, name: '한식' }
       ));
 
-      const { queryByText } = render(<App />);
+      render(<App />);
 
       expect(dispatch).toBeCalledTimes(3);
-
-      expect(queryByText('서울(V)')).not.toBeNull();
-      expect(queryByText('한식(V)')).not.toBeNull();
     });
   });
 });
