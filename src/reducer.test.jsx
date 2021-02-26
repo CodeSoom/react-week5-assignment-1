91dacbccfdfc7cd057672f3a970ea16ba2cf8e2d
EL <ickyicky@gmail.com>
2021-02-27 00:19:23 +0900

    Red: selectRegion 테스트 작성

--- src/reducer.test.jsx
+++ src/reducer.test.jsx
@@ -3,17 +3,19 @@ import reducer from './reducer';
 import {
   setRegions,
   setCategories,
+  selectRegion,
 } from './actions';
 
 import { regions, categories } from '../fixtures/restaurants';
 
 describe('reducer', () => {
-  it('파라미터의 값이 없을 때는 initialState로 상태를 업데이트한다.', () => {
-    const initialState = {
-      regions: [],
-      categories: [],
-    };
+  const initialState = {
+    regions: [],
+    categories: [],
+    selectedRegion: null,
+  };
 
+  it('파라미터의 값이 없을 때는 initialState로 상태를 업데이트한다.', () => {
     const state = reducer();
 
     expect(state).toEqual(initialState);
@@ -21,10 +23,6 @@ describe('reducer', () => {
 
   describe('setRegions', () => {
     it('regions의 데이터를 받아서 상태를 업데이트한다.', () => {
-      const initialState = {
-        regions: [],
-      };
-
       const state = reducer(initialState, setRegions(regions));
 
       expect(state.regions).not.toHaveLength(0);
@@ -33,13 +31,20 @@ describe('reducer', () => {
 
   describe('setCategories', () => {
     it('categories의 데이터를 받아서 상태를 업데이트한다.', () => {
-      const initialState = {
-        categories: [],
-      };
-
       const state = reducer(initialState, setCategories(categories));
 
       expect(state.categories).not.toHaveLength(0);
+    });
+  });
+
+  describe('selectRegion', () => {
+    it('클릭된 지역으로 selectedRegion 상태를 업데이트한다.', () => {
+      const state = reducer(
+        initialState,
+        selectRegion({id: 1, name: '서울' }),
+      );
+
+      expect(state.selectedRegion).not.toBeNull();
     });
   });
 });
