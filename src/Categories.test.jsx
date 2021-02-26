923e6814f1652c7e566de2343ee4d6bce8682705
Najeong-Kim <najeong@Najeong-Kimui-MacBookPro.local>
2021-02-26 14:36:53 +0900

    red : Categories onClick 테스트 추가

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Categories from './Categories';
 
 import categories from '../fixtures/categories';
 
 describe('Categories', () => {
+  const handleClick = jest.fn();
+
+  const { queryByText } = render((
+    <Categories categories={categories} onClick={handleClick} />
+  ))
+
   it('renders categories', () => {
-    const { queryByText } = render((
-      <Categories categories={categories} />
-    ))
-    
     categories.forEach((category) => {
       expect(queryByText(category.name)).not.toBeNull();
     })
   })
+
+  it('clicks categories', () => {
+    categories.forEach((category) => {
+      expect(handleClick).not.toBeCalledWith(category.id);
+      fireEvent.click(queryByText(category.id));
+      expect(handleClick).toBeCalledWith(category.id);
+    })
+  })  
 })
