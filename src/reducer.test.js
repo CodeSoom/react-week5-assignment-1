dc60ead4677dac4ea96f56d7119f4ef52f76393d
Najeong-Kim <najeong@Najeong-Kimui-MacBookPro.local>
2021-02-27 18:21:05 +0900

    reducer setSelected 테스트 작성

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -5,21 +5,10 @@ import regions from '../fixtures/regions'
 import categories from '../fixtures/categories';
 
 import {
-  chooseRegions, setRegions, setCategories,
+  setSelected, setRegions, setCategories,
 } from './actions';
 
 describe('reducer', () => {
-  // describe('chooseRegions', () => {
-  //   it('chooses region buttons', () => {
-  //     const initialState = {
-  //       regions: [],
-  //     }
-  //     const state = reducer(initialState, chooseRegions(regions))
-
-  //     expect(state.regions).not.toHaveLength(0);
-  //   })
-  // })
-
   describe('setRegions', () => {
     it('renders region buttons', () => {
       const initialState = {
@@ -39,6 +28,36 @@ describe('reducer', () => {
       const state = reducer(initialState, setCategories(categories));
 
       expect(state.categories).not.toHaveLength(0);
+    })
+  })
+
+  describe('setSelected', () => {
+    it('changes selected state', () => {
+      const initialState = {
+        selected: {
+          region: {
+            id: '',
+            name: '',
+          },
+          category: {
+            id: '',
+            name: '',
+          },
+        }
+      }
+      const state = reducer(initialState, setSelected({
+        region: {
+          id: 1,
+          name: '서울',
+        },
+        category: {
+          id: 1,
+          name: '한식',
+        }
+      }))
+
+      expect(state.selected.region.name).toBe('서울');
+      expect(state.selected.category.name).toBe('한식');
     })
   })
 })
