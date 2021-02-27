f9818e53869dfdf2ba6f4c08c2bc4cd3f4228bb1
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-27 21:05:25 +0900

    Api를 테스트하여 fetchRestaurants를 검사합니다.

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -1,6 +1,8 @@
-import { fetchLocations } from './api';
+import { fetchCategories, fetchLocations, fetchRestaurants } from './api';
 
 import locations from '../../fixtures/locations';
+import categories from '../../fixtures/categories';
+import restaurants from '../../fixtures/restaurants';
 
 function dataMock(data) {
   global.fetch = jest.fn(async () => Promise.resolve({
@@ -9,11 +11,31 @@ function dataMock(data) {
 }
 
 describe('api', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('fetchLoactions', async () => {
     dataMock(locations);
 
     const data = await fetchLocations();
 
     expect(data).toBe(locations);
+  });
+
+  it('fetchCategories', async () => {
+    dataMock(categories);
+
+    const data = await fetchCategories();
+
+    expect(data).toBe(categories);
+  });
+
+  it('fetchRestaurants', async () => {
+    dataMock(restaurants);
+
+    const data = await fetchRestaurants();
+
+    expect(data).toBe(restaurants);
   });
 });
