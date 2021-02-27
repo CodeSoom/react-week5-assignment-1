216b5d7fc625f2660c3ff3809d01e77bbe17e3e5
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-27 17:48:46 +0900

    Actions를 테스트하여 loadCategories를 테스트합니다.

--- src/actions.test.js
+++ src/actions.test.js
@@ -3,13 +3,17 @@ import thunk from 'redux-thunk';
 import configureStore from 'redux-mock-store';
 
 import locations from '../fixtures/locations';
+import categories from '../fixtures/categories';
 
 import {
   loadLocations,
+  setCategories,
+  loadCategories,
   setLocations,
 } from './actions';
 
 import {
+  fetchCategories,
   fetchLocations,
 } from './services/api';
 
@@ -17,7 +21,6 @@ const middleWeares = [thunk];
 const mockStore = configureStore(middleWeares);
 
 jest.mock('./services/api');
-// const fetchLocationsMock = require('./services/api');
 
 describe('actions test', () => {
   it('loadLocations test', async () => {
@@ -30,7 +33,17 @@ describe('actions test', () => {
     const actions = store.getActions();
 
     expect(actions[0]).toEqual(setLocations(locations));
+  });
 
-    expect(fetchLocations).toBeCalledWith('https://eatgo-customer-api.ahastudio.com/regions');
+  it('loadCategories test', async () => {
+    const store = mockStore({});
+
+    fetchCategories.mockResolvedValue(categories);
+
+    await store.dispatch(loadCategories());
+
+    const actions = store.getActions();
+
+    expect(actions[0]).toEqual(setCategories(categories));
   });
 });
