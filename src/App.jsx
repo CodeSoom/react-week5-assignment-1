ac0685792bd016b51f3ba584512621c383a6894e
ErickRyu <rkggmdii@gmail.com>
2021-03-02 17:22:07 +0900

    Can see 서울 in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function App() {
   return (
-    <></>
+    <>
+      <p>서울</p>
+    </>
   );
 }
---
+++ src/App.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+test('App', () => {
+  const { container } = render(
+    <App />,
+  );
+  expect(container).toHaveTextContent('서울');
+});
