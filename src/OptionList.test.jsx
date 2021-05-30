710965e90cf32601de1b02bf1c13bb5d52b448e9
yujonglee <yujonglee@g.skku.edu>
2021-05-31 00:23:13 +0900

    버튼을 클릭했을 때 버튼 이름이 변하는 것을 테스트하도록 OptionList테스트를 수정하라

--- src/OptionList.test.jsx
+++ src/OptionList.test.jsx
@@ -1,13 +1,17 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import OptionList from './OptionList';
 
 describe('OptionList', () => {
-  it('renders options', () => {
+  it('renders option that listens to click event', () => {
     const options = ['a', 'b', 'c', 'd', 'e'];
 
-    const { container } = render(<OptionList options={options} />);
-    options.forEach((option) => {
-      expect(container).toHaveTextContent(option);
-    });
+    const { getByRole, queryByRole } = render(<OptionList options={options} />);
+
+    fireEvent.click(getByRole('button', { name: 'a' }));
+    expect(getByRole('button', { name: 'a(V)' })).toBeInTheDocument();
+
+    fireEvent.click(getByRole('button', { name: 'b' }));
+    expect(queryByRole('button', { name: 'a(V)' })).not.toBeInTheDocument();
+    expect(getByRole('button', { name: 'b(V)' })).toBeInTheDocument();
   });
 });
