09834144c072dfa295703de0d922cd731e698ea8
yujonglee <yujonglee@g.skku.edu>
2021-05-30 23:55:15 +0900

    텍스트가 아니라 버튼을 렌더하는 것으로 App테스트를 수정하라

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,17 +5,17 @@ describe('App', () => {
   it('renders categories', () => {
     const categories = ['한식', '중식', '일식', '양식', '분식'];
 
-    const { container } = render(<App />);
+    const { getByRole } = render(<App />);
     categories.forEach((category) => {
-      expect(container).toHaveTextContent(category);
+      expect(getByRole('button', { name: category }));
     });
   });
   it('renders regions', () => {
     const regions = ['서울', '대전', '대구', '부산', '광주', '강원도', '인천'];
 
-    const { container } = render(<App />);
+    const { getByRole } = render(<App />);
     regions.forEach((region) => {
-      expect(container).toHaveTextContent(region);
+      expect(getByRole('button', { name: region }));
     });
   });
 });
