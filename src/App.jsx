d7e27f7cabe4c4f82f8db5249c9f15c95a7c2217
yujonglee <yujonglee@g.skku.edu>
2021-05-30 23:45:02 +0900

    App에서 지역들을 렌더하게 하라

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 export default function App() {
   const categories = ['한식', '중식', '일식', '양식', '분식'];
+  const regions = ['서울', '대전', '대구', '부산', '광주', '강원도', '인천'];
   return (
-    <ul>
-      {categories.map((category, i) => (
-        <li key={i}>
-          {category}
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {categories.map((category, i) => (
+          <li key={i}>
+            {category}
+          </li>
+        ))}
+      </ul>
+      ,
+
+      <ul>
+        {regions.map((region, i) => (
+          <li key={i}>
+            {region}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
