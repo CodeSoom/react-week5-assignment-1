5e91aa8ca8f6b5cb67a1588578129b392096e5ed
yujonglee <yujonglee@g.skku.edu>
2021-05-30 23:43:09 +0900

    App에서 카테고리들을 렌더하게 하라

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 export default function App() {
+  const categories = ['한식', '중식', '일식', '양식', '분식'];
   return (
-    <></>
+    <ul>
+      {categories.map((category, i) => (
+        <li key={i}>
+          {category}
+        </li>
+      ))}
+    </ul>
   );
 }
