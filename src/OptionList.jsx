e8cf06b4d884be072f2adaeecf47efa1653262cf
yujonglee <yujonglee@g.skku.edu>
2021-05-31 00:34:42 +0900

    index as key를 제거하라

--- src/OptionList.jsx
+++ src/OptionList.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react';
 
 export default function OptionList({ options }) {
   const [selected, setSelected] = useState('');
+
   return (
     <ul>
-      {options.map((option, i) => {
+      {options.map((option) => {
         const name = (selected === option) ? `${option}(V)` : option;
         const handleClick = () => setSelected(name);
 
         return (
-          <li key={i}>
+          <li key={option}>
             <button type="button" onClick={handleClick}>
               {name}
             </button>
