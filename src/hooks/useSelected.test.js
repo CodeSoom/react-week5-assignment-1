0f29e929ffc4b2f479db4704a137f990d0c35c7f
yujonglee <yujonglee@g.skku.edu>
2021-05-31 22:14:55 +0900

    useSelected훅 테스트에서 beforeEach로 중복을 제거하라

--- src/hooks/useSelected.test.js
+++ src/hooks/useSelected.test.js
@@ -10,28 +10,30 @@ import {
 jest.mock('react-redux');
 
 describe('useSelected', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
   it('returns selected', () => {
     useSelector.mockImplementation((selector) => selector({ category: '한식' }));
     const { result } = renderHook(() => useSelected('category'));
+
     expect(result.current.selected).toBe('한식');
   });
 
   it('returns setSelected for category', () => {
-    const dispatch = jest.fn();
-    useDispatch.mockImplementation(() => dispatch);
-    useSelector.mockImplementation((selector) => selector({ category: '한식' }));
-
     const { result } = renderHook(() => useSelected('category'));
+
     result.current.setSelected('양식');
     expect(dispatch).toBeCalledWith(updateSelectedCategory('양식'));
   });
 
   it('returns setSelected for region', () => {
-    const dispatch = jest.fn();
-    useDispatch.mockImplementation(() => dispatch);
-    useSelector.mockImplementation((selector) => selector({ category: '서울' }));
-
     const { result } = renderHook(() => useSelected('region'));
+
     result.current.setSelected('부산');
     expect(dispatch).toBeCalledWith(updateSelectedRegion('부산'));
   });
