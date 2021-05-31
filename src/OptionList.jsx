c47ca883c2b174f197675c6cc3db0d5419fc4dd4
yujonglee <yujonglee@g.skku.edu>
2021-05-31 22:09:17 +0900

    OptionList에서 useSelcted훅을 사용하라

--- src/OptionList.jsx
+++ src/OptionList.jsx
@@ -1,25 +1,16 @@
-import { useDispatch, useSelector } from 'react-redux';
-
-import {
-  updateSelectedCategory,
-  updateSelectedRegion,
-} from './selectedSlice';
+import useSelected from './hooks/useSelected';
 
 export default function OptionList({ options, mode }) {
-  const dispatch = useDispatch();
-  const selected = useSelector((state) => state[mode]);
+  const { selected, setSelected } = useSelected(mode);
 
   return (
     <ul>
       {options.map((option) => {
         const name = (selected === option) ? `${option}(V)` : option;
-        const handleClick = (mode === 'region')
-          ? () => dispatch(updateSelectedRegion(name))
-          : () => dispatch(updateSelectedCategory(name));
 
         return (
           <li key={option}>
-            <button type="button" onClick={handleClick}>
+            <button type="button" onClick={() => setSelected(name)}>
               {name}
             </button>
           </li>
