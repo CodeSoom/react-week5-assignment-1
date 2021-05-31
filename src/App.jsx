c111e6d32d79d2704f69026f4a8829b0221cdcf0
yujonglee <yujonglee@g.skku.edu>
2021-05-31 22:28:16 +0900

    App에서 OptionList에 mode를 넘기도록 하라

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,18 @@ import OptionList from './OptionList';
 export default function App() {
   const categories = ['한식', '중식', '일식', '양식', '분식'];
   const regions = ['서울', '대전', '대구', '부산', '광주', '강원도', '인천'];
+
   return (
     <>
-      <OptionList options={categories} />
-      <OptionList options={regions} />
+      <OptionList
+        options={categories}
+        mode="category"
+      />
+
+      <OptionList
+        options={regions}
+        mode="regions"
+      />
     </>
   );
 }
