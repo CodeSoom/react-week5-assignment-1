41d3e0490ba7c46f5701113c9f6dc49eb2ef78a5
hanvit7 <hanvit7@gmail.com>
2021-06-02 00:09:49 +0900

    테스트 - App에 redux mock 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,14 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 test('App', () => {
+  useSelector.mockImplementation((selector) => selector({}));
+
   const { container } = render(<App />);
 
   expect(container).toHaveTextContent('서울');
