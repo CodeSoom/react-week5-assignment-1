bff0782112c22296630f2060a190664454cb5756
hanvit7 <hanvit7@gmail.com>
2021-06-02 00:09:10 +0900

    테스트 - RegionContainer 테스트 추가
    - redux에서 regions 상태 조회 테스트 추가

--- src/RegionContainer.test.jsx
+++ src/RegionContainer.test.jsx
@@ -1,10 +1,20 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import RegionContainer from './RegionContainer';
 
+jest.mock('react-redux');
+
 test('RegionContainer', () => {
-  const { getByText, getByRole } = render(<RegionContainer />);
+  const regions = ['서울', '대전', '대구'];
+
+  useSelector.mockImplementation((selector) => selector({
+    regions,
+  }));
+
+  const { getByText, getByRole } = render(<RegionContainer regions={regions} />);
 
   expect(getByRole('listitem')).not.toBeNull();
-  expect(getByText('서울')).not.toBeNull();
+  expect(getByText(regions[1])).not.toBeNull();
 });
