244d3bb7869d73ca00a6b9c872c70c50fa0c5e29
dngwoodo <dngwoodo@gmail.com>
2021-06-01 21:13:58 +0900

    feat: Request regions, categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { categories } from '../fixtures/categories';
-import { regions } from '../fixtures/regions';
+import { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  loadRegionNames,
+  loadCategories,
+} from './actions';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRegionNames());
+    dispatch(loadCategories());
+  }, []);
+
+  const { regionNames, categories } = useSelector((state) => ({
+    regionNames: state.regionNames,
+    categories: state.categories,
+  }));
+
   return (
     <div>
       <ul>
-        {regions.map(({ name, id }) => (
+        {regionNames.map(({ name, id }) => (
           <li key={id}>
             <button type="button">{name}</button>
           </li>
