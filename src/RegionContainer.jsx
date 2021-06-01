7c5678af2f6113a60925631324128ce385434a98
hanvit7 <hanvit7@gmail.com>
2021-06-01 23:27:01 +0900

    구현 - RigionContainer에 redux 추가
    - useSelector로 regions 상태 조회

--- src/RegionContainer.jsx
+++ src/RegionContainer.jsx
@@ -1,5 +1,11 @@
+import { useSelector } from 'react-redux';
+
 import Region from './Region';
 
 export default function RigionContainer() {
-  return <Region />;
+  const { regions } = useSelector((state) => ({
+    regions: state.regions,
+  }));
+
+  return <Region regions={regions} />;
 }
