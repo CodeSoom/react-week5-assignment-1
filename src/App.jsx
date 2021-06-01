e5f88e043116aece05a81a534b80c26a9db0182b
dngwoodo <dngwoodo@gmail.com>
2021-06-01 16:12:18 +0900

    feat: Show regions, categories button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { categories } from '../fixtures/categories';
+import { regions } from '../fixtures/regions';
+
 export default function App() {
   return (
-    <></>
+    <div>
+      <ul>
+        {regions.map((region) => (
+          <li key={region}>
+            <button type="button">{region}</button>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {categories.map((category) => (
+          <li key={category}>
+            <button type="button">{category}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
