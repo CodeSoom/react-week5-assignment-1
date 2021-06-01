6ccf3d1168d25f35cad9e7e59a34fc556c6cbb96
seong-dodo <dyrnxm52@hanmail.net>
2021-06-01 17:07:04 +0900

    레스토랑 지역 목록 API 가져오기 위한 코드 추가
    - App컴포넌트에 레스토랑 지역 목록 가져오기 위해 코드 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
+
 import RegionsContainer from './RegionsContainer';
 
+import {
+  setRegions,
+} from './actions';
+
+function loadRegions({ dispatch }) {
+  const regions = [];
+
+  dispatch(setRegions(regions));
+}
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    loadRegions({ dispatch });
+  }, []);
+
   return (
     <div>
       <RegionsContainer />
