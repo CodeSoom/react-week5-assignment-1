22d55aee038a1c185159a84a8645ab89d9845677
hanvit7 <hanvit7@gmail.com>
2021-06-01 23:11:13 +0900

    테스트 - App에 region 이름 테스트 추가

---
+++ src/App.test.jsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+test('App', () => {
+  const { container } = render(<App />);
+
+  expect(container).toHaveTextContent('서울');
+});
