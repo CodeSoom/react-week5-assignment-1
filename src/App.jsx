b61032ca5f712c99c4e32360297f1a4996cc47c7
yujonglee <yujonglee@g.skku.edu>
2021-06-02 15:37:13 +0900

    App에서 dispatch하도록 useEffect코드를 추가하라

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import OptionList from './OptionList';
+import { setCategories, setRegions } from './redux_module/groupsSlice';
 
 export default function App() {
   const categories = useSelector((state) => state.groups.categories);
   const regions = useSelector((state) => state.groups.regions);
+
+  useEffect(() => {
+    const dispatch = useDispatch();
+    dispatch(setCategories(categories));
+    dispatch(setRegions(regions));
+  });
 
   return (
     <>
