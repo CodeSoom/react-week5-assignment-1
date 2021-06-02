af633a0742b0d5b05013935e6f9e76684e9a799e
yujonglee <yujonglee@g.skku.edu>
2021-06-02 19:25:48 +0900

    ButtonList 테스트에 기능테스트를 추가하라

--- src/components/ButtonList.test.jsx
+++ src/components/ButtonList.test.jsx
@@ -3,17 +3,41 @@ import { render } from '@testing-library/react';
 import ButtonList from './ButtonList';
 
 describe('buttonList', () => {
-  it('renders buttons', () => {
-    const names = [1, 2, 3, 4];
-    const handleClick = jest.fn();
+  context('when nothing is selected', () => {
+    it('renders buttons with name', () => {
+      const names = [1, 2, 3, 4];
+      const handleClick = jest.fn();
+      const selected = null;
 
-    const { getAllByRole } = render(
-      <ButtonList
-        names={names}
-        onClick={handleClick}
-      />,
-    );
+      const { getAllByRole, queryByText } = render(
+        <ButtonList
+          names={names}
+          onClick={handleClick}
+          selected={selected}
+        />,
+      );
 
-    expect(getAllByRole('button')).toHaveLength(names.length);
+      expect(getAllByRole('button')).toHaveLength(names.length);
+      expect(queryByText(/(V)/)).not.toBeInTheDocument();
+    });
+  });
+
+  context('when something is selected', () => {
+    it('renders buttons and adds (V) to selected one', () => {
+      const names = [1, 2, 3, 4];
+      const handleClick = jest.fn();
+      const selected = 1;
+
+      const { getAllByRole, getByText } = render(
+        <ButtonList
+          names={names}
+          onClick={handleClick}
+          selected={selected}
+        />,
+      );
+
+      expect(getAllByRole('button')).toHaveLength(names.length);
+      expect(getByText(/(V)/)).toBeInTheDocument();
+    });
   });
 });
