a84cd45197286749cea842a3e26eff4cf290267a
yujonglee <yujonglee@g.skku.edu>
2021-06-02 11:41:10 +0900

    불필요한 액션생성함수 테스트를 제거하라

--- src/redux_module/selectSlice.test.js
+++ src/redux_module/selectSlice.test.js
@@ -4,22 +4,6 @@ import reducer, {
 } from './selectedSlice';
 
 describe('selectSlice', () => {
-  describe('selectCategory', () => {
-    it('changes selected category', () => {
-      expect(selectCategory('한식')).toEqual(
-        { type: 'selected/selectCategory', payload: '한식' },
-      );
-    });
-  });
-
-  describe('selectRegion', () => {
-    it('changes selected region', () => {
-      expect(selectRegion('서울')).toEqual(
-        { type: 'selected/selectRegion', payload: '서울' },
-      );
-    });
-  });
-
   describe('reducer', () => {
     it('updates selected category', () => {
       const oldState = { category: '', region: '' };
