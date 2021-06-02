7f457f3d91ae95d70d9a8a01899cd3fb3c5bd6ed
yujonglee <yujonglee@g.skku.edu>
2021-06-02 18:15:02 +0900

    결과 restaurants 목록을 업데이트하는 리듀서 테스트를 작성하라

--- src/redux_module/selectSlice.test.js
+++ src/redux_module/selectSlice.test.js
@@ -1,6 +1,7 @@
 import reducer, {
   selectCategory,
   selectRegion,
+  updateRestaurants,
 } from './selectedSlice';
 
 describe('selectSlice', () => {
@@ -22,6 +23,36 @@ describe('selectSlice', () => {
       expect(reducer(
         oldState,
         selectRegion('서울'),
+      )).toEqual(newState);
+    });
+
+    it('updates selected region', () => {
+      const oldState = { category: '', region: '' };
+      const newState = { category: '', region: '서울' };
+
+      expect(reducer(
+        oldState,
+        selectRegion('서울'),
+      )).toEqual(newState);
+    });
+
+    it('updates matched restaurant', () => {
+      const restaurantInfos = [{
+        id: 5,
+        categoryId: 3,
+        name: '원초밥',
+        address: '서울 강남구',
+        information: '원초밥 in 서울 강남구',
+      }];
+
+      const oldState = { restaurants: [] };
+      const newState = {
+        restaurants: restaurantInfos,
+      };
+
+      expect(reducer(
+        oldState,
+        updateRestaurants(restaurantInfos),
       )).toEqual(newState);
     });
   });
