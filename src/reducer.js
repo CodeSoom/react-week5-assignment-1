eedb4e1f68d651efa10d9e556933fbf7f03146f1
seong-dodo <dyrnxm52@hanmail.net>
2021-06-03 01:02:08 +0900

    reducer if문 제거
    - reducer if문 제거, object literals 사용

--- src/reducer.js
+++ src/reducer.js
@@ -4,23 +4,25 @@ const initialState = {
   categories: [],
 };
 
+const actionTable = (state, action) => ({
+  setRegions: {
+    ...state,
+    regions: action.payload.regions,
+  },
+  setCategories: {
+    ...state,
+    categories: action.payload.categories,
+  },
+  Default: state,
+}[action.type] || 'Default');
+
 export default function reducer(state = initialState, action) {
   if (action === undefined) {
     return state;
   }
-
-  if (action.type === 'setRegions') {
-    return {
-      ...state,
-      regions: action.payload.regions,
-    };
-  }
-  if (action.type === 'setCategories') {
-    return {
-      ...state,
-      categories: action.payload.categories,
-    };
+  if (action.payload === undefined) {
+    return state;
   }
 
-  return state;
+  return actionTable(state, action);
 }
