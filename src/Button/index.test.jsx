b373a6bb6f205a8ce73f03d9c04029d6d6e4992c
dngwoodo <dngwoodo@gmail.com>
2021-06-02 23:18:24 +0900

    test: Update Button test

--- src/Button/index.test.jsx
+++ src/Button/index.test.jsx
@@ -3,29 +3,41 @@ import { fireEvent, render } from '@testing-library/react';
 import Button from '.';
 
 describe('Button', () => {
+  const name = '부산';
+  const search = 'search';
+
   const handleClick = jest.fn();
-
-  it('renders button', () => {
-    const { getByRole } = render(
-      <Button
-        content="부산"
-        onClick={handleClick}
-      />,
-    );
-
-    expect(getByRole('button', { name: '부산' })).toBeInTheDocument();
-  });
 
   it('listens click event', () => {
     const { getByRole } = render(
       <Button
-        content="부산"
+        name={name}
         onClick={handleClick}
+        search={search}
       />,
     );
 
-    fireEvent.click(getByRole('button', { name: '부산' }));
+    fireEvent.click(getByRole('button', { name }));
 
-    expect(handleClick).toBeCalledWith('부산');
+    expect(handleClick).toBeCalledWith({
+      search,
+      value: name,
+    });
+  });
+
+  context('when selected', () => {
+    it('renders button with (V)', () => {
+      const { getByRole } = render(<Button name={name} search={search} selected={name} />);
+
+      expect(getByRole('button', { name: `${name}(V)` })).toBeInTheDocument();
+    });
+  });
+
+  context('when not selected', () => {
+    it('renders button without (V)', () => {
+      const { getByRole } = render(<Button name={name} search={search} selected="서울" />);
+
+      expect(getByRole('button', { name })).toBeInTheDocument();
+    });
   });
 });
