28c18b1573bcdbbafaf32782eeba2f40cd3f3c49
seong-dodo <dyrnxm52@hanmail.net>
2021-06-02 20:52:47 +0900

    1. App 테스트 코드 추가
    레스토랑 분류 목록을 그린다에 대한 테스트 코드 추가하기

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -18,4 +18,16 @@ describe('App', () => {
 
     expect(getByText(/서울/)).not.toBeNull();
   });
+
+  it('레스토랑 분류 목록을 그린다', () => {
+    useSelector.mockImplementation((selector) => selector({
+      categories: [
+        { id: 1, name: '한식' },
+      ],
+    }));
+
+    const { getByText } = render(<App />);
+
+    expect(getByText(/한식/)).not.toBeNull();
+  });
 });
