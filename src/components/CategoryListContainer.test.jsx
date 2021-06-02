1727d5cc210a370cad63cb80d0f1fcd7f5bb707c
yujonglee <yujonglee@g.skku.edu>
2021-06-02 19:43:16 +0900

    categoryContainer에 테스트를 추가하라

--- src/components/CategoryListContainer.test.jsx
+++ src/components/CategoryListContainer.test.jsx
@@ -1,12 +1,13 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { useDispatch, useSelector } from 'react-redux';
+import { selectCategory } from '../redux_module/selectedSlice';
 
 import CategoryListContainer from './CategoryListContainer';
 
+jest.mock('react-redux');
+
 describe('CategoryListContainer', () => {
   it('', () => {
-    const dispatch = jest.mock();
-    useDispatch.mockImplementation(() => dispatch);
     useSelector.mockImplementation((selector) => selector({
       selected: {
         category: '한식',
@@ -22,5 +23,26 @@ describe('CategoryListContainer', () => {
     expect(queryByRole('button', { name: '한식' })).not.toBeInTheDocument();
 
     expect(getByRole('button', { name: '중식' })).toBeInTheDocument();
+  });
+
+  it('', () => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      selected: {
+        category: '한식',
+      },
+      groups: {
+        categories: ['한식', '중식'],
+      },
+    }));
+
+    const { getByRole } = render(<CategoryListContainer />);
+
+    fireEvent.click(getByRole('button', { name: '한식(V)' }));
+    expect(dispatch).toBeCalledWith(selectCategory('한식'));
+
+    fireEvent.click(getByRole('button', { name: '중식' }));
+    expect(dispatch).toBeCalledWith(selectCategory('중식'));
   });
 });
