d5a392fb18fc642c4f5c58b289c924bc7e75eb2f
seong-dodo <dyrnxm52@hanmail.net>
2021-06-03 22:43:10 +0900

    RegionsContainer 테스트: 피드백반영
    - beforeEach 사용
    - 테스트 시나리오 변경
    - getByText -> queryByText로 수정

--- src/RegionsContainer.test.jsx
+++ src/RegionsContainer.test.jsx
@@ -7,15 +7,17 @@ import RegionsContainer from './RegionsContainer';
 jest.mock('react-redux');
 
 describe('RegionsContainer', () => {
-  it('레스토랑 지역 목록이 그려진다', () => {
+  beforeEach(() => {
     useSelector.mockImplementation((selector) => selector({
       regions: [
         { id: 1, name: '서울' },
       ],
     }));
+  });
 
-    const { getByText } = render(<RegionsContainer />);
+  it('renders regions list', () => {
+    const { queryByText } = render(<RegionsContainer />);
 
-    expect(getByText(/서울/)).not.toBeNull();
+    expect(queryByText('서울')).not.toBeNull();
   });
 });
