fd248c1d70905d75c58521b5723c396bab72f49e
dngwoodo <dngwoodo@gmail.com>
2021-06-04 05:52:46 +0900

    feat: Add CategoryButtonsContainer component

---
+++ src/CategoryButtonsContainer/index.jsx
@@ -0,0 +1,30 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { changeSearch } from '../actions';
+
+import Buttons from '../Buttons';
+
+export default function CategoryButtonsContainer() {
+  const dispatch = useDispatch();
+
+  const { categories } = useSelector((state) => ({
+    categories: state.categories,
+  }));
+
+  const selected = useSelector((state) => state.search.category);
+
+  function handleClickChangeSearch({ value }) {
+    dispatch(changeSearch({
+      search: 'category',
+      value,
+    }));
+  }
+
+  return (
+    <Buttons
+      values={categories}
+      selected={selected}
+      onClickChangeSearch={handleClickChangeSearch}
+    />
+  );
+}
