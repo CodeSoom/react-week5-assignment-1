14a02f9e0fcc8172131d9240ae94dd2780c9257f
yujonglee <yujonglee@g.skku.edu>
2021-06-03 14:44:44 +0900

    비동기 테스트를 프로미스를 반환하는 것에서 await를 이용하는 것으로 바꿔라

--- src/redux_module/asyncActions.test.js
+++ src/redux_module/asyncActions.test.js
@@ -11,42 +11,39 @@ describe('asyncActions', () => {
   const middlewares = [thunk];
   const mockStore = configureStore(middlewares);
 
-  it('fetches categories when being dispatched', () => {
+  it('fetches categories when being dispatched', async () => {
     const store = mockStore({});
-    return store.dispatch(loadCategories())
-      .then(() => {
-        const actions = store.getActions();
-        expect(actions[0]).toEqual(setCategories([
-          { id: 1, name: '한식' },
-          { id: 2, name: '중식' },
-          { id: 3, name: '일식' },
-          { id: 4, name: '양식' },
-          { id: 5, name: '분식' },
-        ]));
-      });
+    await store.dispatch(loadCategories());
+
+    const actions = store.getActions();
+    expect(actions[0]).toEqual(setCategories([
+      { id: 1, name: '한식' },
+      { id: 2, name: '중식' },
+      { id: 3, name: '일식' },
+      { id: 4, name: '양식' },
+      { id: 5, name: '분식' },
+    ]));
   });
 
-  it('fetches regions when being dispatched', () => {
+  it('fetches regions when being dispatched', async () => {
     const store = mockStore({});
-    return store.dispatch(loadRegions())
-      .then(() => {
-        const actions = store.getActions();
-        expect(actions[0]).toEqual(setRegions([
-          '서울', '대전', '대구', '부산', '광주', '강원도',
-        ]));
-      });
+    await store.dispatch(loadRegions());
+
+    const actions = store.getActions();
+    expect(actions[0]).toEqual(setRegions([
+      '서울', '대전', '대구', '부산', '광주', '강원도',
+    ]));
   });
 
-  it('fetches restuarants when being dispatched', () => {
+  it('fetches restuarants when being dispatched', async () => {
     const store = mockStore({});
-    return store.dispatch(loadRestaurants('서울', 1))
-      .then(() => {
-        const actions = store.getActions();
-        expect(actions[0]).toEqual(updateRestaurants([
-          '양천주가',
-          '한국식 초밥',
-          '김초밥',
-        ]));
-      });
+    await store.dispatch(loadRestaurants('서울', 1));
+
+    const actions = store.getActions();
+    expect(actions[0]).toEqual(updateRestaurants([
+      '양천주가',
+      '한국식 초밥',
+      '김초밥',
+    ]));
   });
 });
