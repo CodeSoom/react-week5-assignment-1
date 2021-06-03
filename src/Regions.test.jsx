a494b9246b6f5ebd4c3dd17c1a6bfb0b5ef1c5d1
seong-dodo <dyrnxm52@hanmail.net>
2021-06-03 21:36:36 +0900

    1단계: reducer 테스트 코드 추가
    - default action type에 대한 테스트 코드 추가

--- src/Regions.test.jsx
+++ src/Regions.test.jsx
@@ -2,8 +2,6 @@ import { render } from '@testing-library/react';
 
 import Regions from './Regions';
 
-jest.mock('react-redux');
-
 test('Regions', () => {
   const regions = [
     { id: 1, name: '서울' },
--- src/reducer.test.js
+++ src/reducer.test.js
@@ -6,31 +6,43 @@ import {
 } from './actions';
 
 describe('reducer', () => {
-  it('Regions이 보여진다', () => {
-    const regions = [
-      { id: 1, name: '서울' },
-    ];
+  describe('setRegions', () => {
+    it('get regions obeject and changes regions', () => {
+      const regions = [
+        { id: 1, name: '서울' },
+      ];
 
-    const initialState = {
-      regions: [],
-    };
+      const initialState = {
+        regions: [],
+      };
 
-    const state = reducer(initialState, setRegions(regions));
+      const state = reducer(initialState, setRegions(regions));
 
-    expect(state.regions).toHaveLength(1);
+      expect(state.regions).toHaveLength(1);
+    });
   });
 
-  it('Categories이 보여진다', () => {
-    const categories = [
-      { id: 1, name: '한식' },
-    ];
+  describe('setCategories', () => {
+    it('get categories obeject and changes categories', () => {
+      const categories = [
+        { id: 1, name: '한식' },
+      ];
 
-    const initialState = {
-      categories: [],
-    };
+      const initialState = {
+        categories: [],
+      };
 
-    const state = reducer(initialState, setCategories(categories));
+      const state = reducer(initialState, setCategories(categories));
 
-    expect(state.categories).toHaveLength(1);
+      expect(state.categories).toHaveLength(1);
+    });
+  });
+
+  describe('Default action', () => {
+    it('return state', () => {
+      const state = reducer();
+
+      expect(state.regions).toHaveLength(0);
+    });
   });
 });
