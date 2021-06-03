98a62bcc18d8006113581b7ae0a0a5a0178f0762
seong-dodo <dyrnxm52@hanmail.net>
2021-06-03 13:52:21 +0900

    1. Categories 테스트 코드 수정
    - Categories 테스트 코드 수정

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -2,17 +2,17 @@ import { fireEvent, render } from '@testing-library/react';
 
 import Categories from './Categories';
 
-jest.mock('react-redux');
-
 test('Categories', () => {
   const categories = [
     { id: 1, name: '한식' },
   ];
+
+  const handleClick = jest.fn();
 
   const { getByText } = render(<Categories categories={categories} />);
   const button = getByText('한식');
 
   fireEvent.click(button);
 
-  expect(button).toBeCalled();
+  expect(handleClick).toBeCalled();
 });
