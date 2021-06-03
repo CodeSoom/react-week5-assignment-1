fe72e48e89963fb4194899995a64deaebf5e3224
yujonglee <yujonglee@g.skku.edu>
2021-06-03 09:33:00 +0900

    loadRestuarants를 작성하라

--- src/redux_module/asyncActions.js
+++ src/redux_module/asyncActions.js
@@ -1,5 +1,6 @@
 import { setCategories, setRegions } from './groupsSlice';
-import { fetchRegions, fetchCategories } from '../servies/api';
+import { fetchRegions, fetchCategories, fetchRestuarants } from '../servies/api';
+import { updateRestaurants } from './selectedSlice';
 
 export function loadCategories() {
   return async (dispatch) => {
@@ -12,5 +13,12 @@ export function loadRegions() {
   return async (dispatch) => {
     const regions = await fetchRegions();
     dispatch(setRegions(regions));
+  };
+}
+
+export function loadRestuarants() {
+  return async (dispatch) => {
+    const restaurants = await fetchRestuarants();
+    dispatch(updateRestaurants(restaurants));
   };
 }
