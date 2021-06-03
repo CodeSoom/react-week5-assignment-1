504b5053e825880b01401c7b326ba5a7b0d79a93
seong-dodo <dyrnxm52@hanmail.net>
2021-06-03 22:25:09 +0900

    2단계: Regions 기능구현
    - Regions 컴포넌트에 버튼 태그 만들기

--- src/Regions.jsx
+++ src/Regions.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
 export default function Regions({ regions }) {
+  function handleClick() {
+    return {
+    };
+  }
+
   return (
     <ul>
       {regions.map((region) => (
         <li key={region.id}>
-          {region.name}
+          <button
+            type="button"
+            onClick={handleClick}
+          >
+            {region.name}
+          </button>
         </li>
       ))}
     </ul>
