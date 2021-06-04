07b86a510c533da8e903045e555b0208aebdef05
dngwoodo <dngwoodo@gmail.com>
2021-06-04 09:39:02 +0900

    feat: Add condition
    region과 categoryId가 없을 때는 fetch 못하도록 조건 추가

--- src/container/RestaurantsContainer/RestaurantsContainer.jsx
+++ src/container/RestaurantsContainer/RestaurantsContainer.jsx
@@ -1,21 +1,32 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { loadRestaurants } from '../../actions';
+
 export default function RestaurantsContainer() {
   const dispatch = useDispatch();
 
-  const { restaurants, region, categoryId } = useSelector((state) => ({
+  const {
+    restaurants,
+    region, categoryId,
+  } = useSelector((state) => ({
     restaurants: state.restaurants,
     region: state.search.region,
     categoryId: state.search.categoryId,
   }));
 
   useEffect(() => {
-    dispatch({
-      region,
-      categoryId,
-    });
-  }, []);
+    if (!region) {
+      return;
+    }
+
+    // NOTE: id가 0이 있을 수도 있으므로 ''로 지정
+    if (categoryId === '') {
+      return;
+    }
+
+    dispatch(loadRestaurants({ region, categoryId }));
+  }, [region, categoryId]);
 
   return (
     <ul>
