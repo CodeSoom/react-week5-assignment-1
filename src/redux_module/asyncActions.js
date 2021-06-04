723c654537b7418752ee80298454747eeaae8a65
yujonglee <yujonglee@g.skku.edu>
2021-06-04 14:12:53 +0900

    화살표함수를 이용해 고차함수를 더 간결하게 표현하라

--- src/redux_module/asyncActions.js
+++ src/redux_module/asyncActions.js
@@ -1,26 +1,17 @@
 import { setCategories, setRegions, setSelectedRestaurants } from './RestaurantSlice';
 import { fetchRegions, fetchCategories, fetchRestaurants } from '../servies/api';
 
-export function loadCategories() {
-  return async (dispatch) => {
-    const categories = await fetchCategories();
+export const loadCategories = () => async (dispatch) => {
+  const categories = await fetchCategories();
+  return dispatch(setCategories(categories));
+};
 
-    return dispatch(setCategories(categories));
-  };
-}
+export const loadRegions = () => async (dispatch) => {
+  const regions = await fetchRegions();
+  return dispatch(setRegions(regions));
+};
 
-export function loadRegions() {
-  return async (dispatch) => {
-    const regions = await fetchRegions();
-
-    return dispatch(setRegions(regions));
-  };
-}
-
-export function loadRestaurants(regionName, categoryId) {
-  return async (dispatch) => {
-    const restaurants = await fetchRestaurants(regionName, categoryId);
-
-    return dispatch(setSelectedRestaurants(restaurants));
-  };
-}
+export const loadRestaurants = (regionName, categoryId) => async (dispatch) => {
+  const restaurants = await fetchRestaurants(regionName, categoryId);
+  return dispatch(setSelectedRestaurants(restaurants));
+};
