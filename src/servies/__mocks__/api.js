5e25b42bf0934347a8894bcaef5697a83f470885
yujonglee <yujonglee@g.skku.edu>
2021-06-04 20:10:43 +0900

    api mock함수를 jest.fn으로 감싸라

--- src/servies/__mocks__/api.js
+++ src/servies/__mocks__/api.js
@@ -1,15 +1,21 @@
-export const fetchRegions = async () => ['서울', '대전', '대구', '부산', '광주', '강원도'];
+export const fetchRegions = jest.fn(
+  async () => ['서울', '대전', '대구', '부산', '광주', '강원도'],
+);
 
-export const fetchCategories = async () => [
-  { id: 1, name: '한식' },
-  { id: 2, name: '중식' },
-  { id: 3, name: '일식' },
-  { id: 4, name: '양식' },
-  { id: 5, name: '분식' }];
+export const fetchCategories = jest.fn(
+  async () => [
+    { id: 1, name: '한식' },
+    { id: 2, name: '중식' },
+    { id: 3, name: '일식' },
+    { id: 4, name: '양식' },
+    { id: 5, name: '분식' }],
+);
 
-export const fetchRestaurants = async (regionName, categoryId) => {
-  if ([regionName, categoryId].includes(null)) {
-    return [];
-  }
-  return ['양천주가', '한국식 초밥', '김초밥'];
-};
+export const fetchRestaurants = jest.fn(
+  async (regionName, categoryId) => {
+    if ([regionName, categoryId].includes(null)) {
+      return [];
+    }
+    return ['양천주가', '한국식 초밥', '김초밥'];
+  },
+);
