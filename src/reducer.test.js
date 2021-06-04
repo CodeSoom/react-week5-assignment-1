50738208fd270abe3bb5571e9d3403de5d98e9a3
seong-dodo <dyrnxm52@hanmail.net>
2021-06-04 20:30:03 +0900

    1단계 reducer selecteCategory 테스트 코드 추가
    - 리듀서 테스트 코드 추가: gets selecteCategory object

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -4,11 +4,12 @@ import {
   setRegions,
   setCategories,
   selecteRegion,
+  selecteCategory,
 } from './actions';
 
 describe('reducer', () => {
   describe('setRegions', () => {
-    it('get regions object and changes regions', () => {
+    it('gets regions object and changes regions', () => {
       const regions = [
         { id: 1, name: '서울' },
       ];
@@ -24,7 +25,7 @@ describe('reducer', () => {
   });
 
   describe('setCategories', () => {
-    it('get categories object and changes categories', () => {
+    it('gets categories object and changes categories', () => {
       const categories = [
         { id: 1, name: '한식' },
       ];
@@ -40,7 +41,7 @@ describe('reducer', () => {
   });
 
   describe('selecteRegion', () => {
-    it('get selecteRegion object', () => {
+    it('gets selecteRegion object', () => {
       const selectedRegion = { id: 1, name: '서울' };
 
       const initialState = {
@@ -54,8 +55,23 @@ describe('reducer', () => {
     });
   });
 
+  describe('selecteCategory', () => {
+    it('gets selecteCategory object', () => {
+      const selectedCategory = { id: 1, name: '한식' };
+
+      const initialState = {
+        selectedCategory: {},
+      };
+
+      const state = reducer(initialState, selecteCategory(selectedCategory));
+
+      expect(state.selectedCategory.id).toBe(1);
+      expect(state.selectedCategory.name).toBe('한식');
+    });
+  });
+
   describe('Default action', () => {
-    it('return state', () => {
+    it('returns state', () => {
       const state = reducer();
 
       expect(state.regions).toHaveLength(0);
