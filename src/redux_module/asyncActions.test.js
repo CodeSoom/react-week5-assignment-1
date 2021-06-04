1c9837723f4f3af58af54a177b2f3cd9ca700727
yujonglee <yujonglee@g.skku.edu>
2021-06-04 20:11:03 +0900

    asyncActions 테스트 설명을 개선하라

--- src/redux_module/asyncActions.test.js
+++ src/redux_module/asyncActions.test.js
@@ -9,42 +9,48 @@ jest.mock('../servies/api');
 describe('asyncActions', () => {
   const mockStore = configureStore(getDefaultMiddleware());
 
-  it('fetches categories when being dispatched', async () => {
-    const store = mockStore({});
-    await store.dispatch(loadCategories());
+  describe('loadCategories', () => {
+    it('fetches categories when being dispatched', async () => {
+      const store = mockStore({});
+      await store.dispatch(loadCategories());
 
-    const actions = store.getActions();
+      const actions = store.getActions();
 
-    expect(actions[0]).toEqual(setCategories([
-      { id: 1, name: '한식' },
-      { id: 2, name: '중식' },
-      { id: 3, name: '일식' },
-      { id: 4, name: '양식' },
-      { id: 5, name: '분식' },
-    ]));
+      expect(actions[0]).toEqual(setCategories([
+        { id: 1, name: '한식' },
+        { id: 2, name: '중식' },
+        { id: 3, name: '일식' },
+        { id: 4, name: '양식' },
+        { id: 5, name: '분식' },
+      ]));
+    });
   });
 
-  it('fetches regions when being dispatched', async () => {
-    const store = mockStore({});
-    await store.dispatch(loadRegions());
+  describe('loadRegions', () => {
+    it('fetches regions when being dispatched', async () => {
+      const store = mockStore({});
+      await store.dispatch(loadRegions());
 
-    const actions = store.getActions();
+      const actions = store.getActions();
 
-    expect(actions[0]).toEqual(setRegions([
-      '서울', '대전', '대구', '부산', '광주', '강원도',
-    ]));
+      expect(actions[0]).toEqual(setRegions([
+        '서울', '대전', '대구', '부산', '광주', '강원도',
+      ]));
+    });
   });
 
-  it('fetches restuarants when being dispatched', async () => {
-    const store = mockStore({});
-    await store.dispatch(loadRestaurants('서울', 1));
+  describe('loadRestaurants', () => {
+    it('fetches restuarants when being dispatched', async () => {
+      const store = mockStore({});
+      await store.dispatch(loadRestaurants('서울', 1));
 
-    const actions = store.getActions();
+      const actions = store.getActions();
 
-    expect(actions[0]).toEqual(setSelectedRestaurants([
-      '양천주가',
-      '한국식 초밥',
-      '김초밥',
-    ]));
+      expect(actions[0]).toEqual(setSelectedRestaurants([
+        '양천주가',
+        '한국식 초밥',
+        '김초밥',
+      ]));
+    });
   });
 });
