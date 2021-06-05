245e6bcd677bb02bf6c46a3eecb7fbc6cb523b1c
hanvit7 <hanvit7@gmail.com>
2021-06-05 23:23:07 +0900

    테스트 - setRegions 테스트 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,3 +1,23 @@
 import reducer from './reducer';
 
-test('reducer', () => {});
+import {
+  setRegions,
+} from './actions';
+
+describe('reducer', () => {
+  describe('setRegions', () => {
+    it('changes regions', () => {
+      const initialState = {
+        regions: [],
+      };
+
+      const regions = [
+        { id: 1, name: '서울' },
+      ];
+
+      const state = reducer(initialState, setRegions(regions));
+
+      expect(state.regions).toHaveLength(1);
+    });
+  });
+});
