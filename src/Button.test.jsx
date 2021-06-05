49f8a8befb341480a21491f77d307bcf81467d6f
seong-dodo <dyrnxm52@hanmail.net>
2021-06-05 14:15:41 +0900

    1단계 : Button 테스트 코드 추가
    - 선택된 버튼의 이름이 변경되는 테스트 코드 추가

--- src/Button.test.jsx
+++ src/Button.test.jsx
@@ -2,13 +2,40 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Button from './Button';
 
-test('Button', () => {
-  const item = { id: 1, name: '서울' };
+describe('Button', () => {
+  it('renders button', () => {
+    const item = { id: 1, name: '서울' };
 
-  const { queryByText } = render(<Button item={item} />);
-  const button = queryByText('서울');
+    const handleClick = jest.fn();
 
-  fireEvent.click(queryByText('서울'));
+    const { queryByText } = render(
+      <Button
+        item={item}
+        onclick={handleClick}
+      />,
+    );
+    const button = queryByText('서울');
 
-  expect(button).not.toBeNull();
+    fireEvent.click(queryByText('서울'));
+
+    expect(button).not.toBeNull();
+  });
+
+  it('clicks button and changes button name', () => {
+    const selectedItem = { id: 1, name: '서울' };
+
+    const handleClick = jest.fn();
+
+    const { queryByText } = render(
+      <Button
+        item={selectedItem}
+        onclick={handleClick}
+      />,
+    );
+
+    fireEvent.click(queryByText('서울'));
+
+    expect(handleClick).toBeCalledWith({ id: 1, name: '서울' });
+    expect(queryByText('서울(V)')).not.toBeNull();
+  });
 });
