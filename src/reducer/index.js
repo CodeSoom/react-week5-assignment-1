0d3bc3c92019409b418d4b5f76d24a26123414af
dngwoodo <dngwoodo@gmail.com>
2021-06-05 23:47:57 +0900

    refactor: Refactor reducer for readability

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -8,58 +8,59 @@ const initialState = {
   },
 };
 
+function setRegions(state, action) {
+  const { regions } = action.payload;
+
+  return {
+    ...state,
+    regions,
+  };
+}
+
+function setCategories(state, action) {
+  const { categories } = action.payload;
+
+  return {
+    ...state,
+    categories,
+  };
+}
+
+function setRestaurants(state, action) {
+  const { restaurants } = action.payload;
+
+  return {
+    ...state,
+    restaurants,
+  };
+}
+
+function changeSearch(state, action) {
+  const { search, value } = action.payload;
+
+  return {
+    ...state,
+    search: {
+      ...state.search,
+      [search]: value,
+    },
+  };
+}
+
+function defaultAction(state) {
+  return state;
+}
+
+const actionType = {
+  setRegions,
+  setCategories,
+  setRestaurants,
+  changeSearch,
+  defaultAction,
+};
+
 export default function reducer(state = initialState, action) {
   const { type } = action;
 
-  function setRegions() {
-    const { regions } = action.payload;
-
-    return {
-      ...state,
-      regions,
-    };
-  }
-
-  function setCategories() {
-    const { categories } = action.payload;
-
-    return {
-      ...state,
-      categories,
-    };
-  }
-
-  function setRestaurants() {
-    const { restaurants } = action.payload;
-
-    return {
-      ...state,
-      restaurants,
-    };
-  }
-
-  function changeSearch() {
-    const { search, value } = action.payload;
-
-    return {
-      ...state,
-      search: {
-        ...state.search,
-        [search]: value,
-      },
-    };
-  }
-
-  const actionType = {
-    setRegions,
-    setCategories,
-    setRestaurants,
-    changeSearch,
-  };
-
-  if (!actionType[type]) {
-    return state;
-  }
-
-  return actionType[type]();
+  return (actionType[type] || defaultAction)(state, action);
 }
