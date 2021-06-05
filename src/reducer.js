04b456e6a42b5bb1012d6c7dcf4db0db0ae05c9b
hanvit7 <hanvit7@gmail.com>
2021-06-06 00:11:11 +0900

    구현 - defaultReducer 구현
    - TypeError 오류를 없애기 위해 defaultReducer 추가

--- src/reducer.js
+++ src/reducer.js
@@ -18,6 +18,10 @@ const reducers = {
   },
 };
 
+function defaultReducer(state) {
+  return state;
+}
+
 export default function reducer(state = initialState, action) {
-  return reducers[action.type](state, action);
+  return (reducers[action.type] || defaultReducer)(state, action);
 }
