317d374f4a64d4ec97febedd26c4c8d3912bd402
최한울 <hanul.choi95@nts-corp.com>
2021-06-05 15:39:21 +0900

    카테고리명 클릭 dispatch 함수에 대한 테스트를 작성한다

--- src/RestaurantCategoriesContainer.test.jsx
+++ src/RestaurantCategoriesContainer.test.jsx
@@ -2,24 +2,37 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantCategoriesContainer from './RestaurantCategoriesContainer';
 
 jest.mock('react-redux');
 
 describe('RestaurantCategoriesContainer', () => {
-  useSelector.mockImplementation((selector) => selector({
-    categories: [
-      { id: 1, name: '한식' },
-    ],
-  }));
-
   it('render', () => {
+    const dispatch = jest.fn();
+
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      categories: [
+        { id: 1, name: '한식' },
+      ],
+    }));
+
     const { getByText } = render((
       <RestaurantCategoriesContainer />
     ));
 
     expect(getByText('한식')).not.toBeNull();
+
+    fireEvent.click(getByText('한식'));
+
+    expect(dispatch).toBeCalledWith({
+      type: 'checkCategory',
+      payload: {
+        id: 1,
+      },
+    });
   });
 });
