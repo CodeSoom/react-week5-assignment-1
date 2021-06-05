955f26adcf5140a55335ef6048349ae44e4d6173
yujonglee <yujonglee@g.skku.edu>
2021-06-05 23:48:12 +0900

    asyncAction에서 return문들을 삭제하라

--- src/redux_module/asyncActions.js
+++ src/redux_module/asyncActions.js
@@ -3,15 +3,15 @@ import { fetchRegions, fetchCategories, fetchRestaurants } from '../servies/api'
 
 export const loadCategories = () => async (dispatch) => {
   const categories = await fetchCategories();
-  return dispatch(setCategories(categories));
+  dispatch(setCategories(categories));
 };
 
 export const loadRegions = () => async (dispatch) => {
   const regions = await fetchRegions();
-  return dispatch(setRegions(regions));
+  dispatch(setRegions(regions));
 };
 
 export const loadRestaurants = (regionName, categoryId) => async (dispatch) => {
   const restaurants = await fetchRestaurants(regionName, categoryId);
-  return dispatch(setSelectedRestaurants(restaurants));
+  dispatch(setSelectedRestaurants(restaurants));
 };
