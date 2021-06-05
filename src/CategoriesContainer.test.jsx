24c994fa4caf26e2ba15e816d6ddaceb93f0c4b1
seong-dodo <dyrnxm52@hanmail.net>
2021-06-05 22:52:20 +0900

    CategoriesContainer 테스트 코드 추가(코드구현 후 반영)
    - handleClick에 대한 테스트 코드 추가

--- src/CategoriesContainer.test.jsx
+++ src/CategoriesContainer.test.jsx
@@ -1,23 +1,39 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import CategoriesContainer from './CategoriesContainer';
 
 jest.mock('react-redux');
 
 describe('CategoriesContainer', () => {
+  const dispatch = useDispatch();
+
   beforeEach(() => {
     useSelector.mockImplementation(() => ({
       categories: [
         { id: 1, name: '한식' },
       ],
+      selectedCategory: { category: { id: 1, name: '한식' } },
     }));
+
+    useDispatch.mockImplementation(() => dispatch);
   });
 
   it('renders categories list', () => {
     const { queryByText } = render(<CategoriesContainer />);
 
     expect(queryByText(/한식/)).not.toBeNull();
+  });
+
+  it('handleClick event and dispatches selecteCategory', () => {
+    const { queryByText } = render(<CategoriesContainer />);
+
+    fireEvent.click(queryByText('한식'));
+
+    expect(dispatch).toBeCalledWith({
+      type: 'selecteCategory',
+      payload: { category: { id: 1, name: '한식' } },
+    });
   });
 });
