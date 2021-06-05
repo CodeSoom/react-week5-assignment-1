eb45b9828a26453af06315dabec776b627094bda
최한울 <hanul.choi95@nts-corp.com>
2021-06-05 13:59:12 +0900

    지역명 클릭에 대한 테스트를 작성한다

--- src/RestaurantRegions.test.jsx
+++ src/RestaurantRegions.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import RestaurantRegions from './RestaurantRegions';
 
@@ -9,11 +9,20 @@ describe('RestaurantRegions', () => {
     { id: 1, name: '서울' },
   ];
 
+  const handleClickRegion = jest.fn();
+
   it('render', () => {
     const { getByText } = render((
-      <RestaurantRegions regions={regions} />
+      <RestaurantRegions
+        regions={regions}
+        onClickRegion={handleClickRegion}
+      />
     ));
 
     expect(getByText('서울')).not.toBeNull();
+
+    fireEvent.click(getByText('서울'));
+
+    expect(handleClickRegion).toBeCalled();
   });
 });
