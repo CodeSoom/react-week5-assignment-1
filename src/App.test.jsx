e750ec02af8e97b874607c5d5aa15236b507c383
hanvit7 <hanvit7@gmail.com>
2021-06-05 23:32:01 +0900

    테스트 - app에 dispatch 호출 테스트 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
 
@@ -9,11 +9,16 @@ import regions from '../fixtures/regions';
 jest.mock('react-redux');
 
 test('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
   useSelector.mockImplementation((selector) => selector({
     regions,
   }));
 
   const { container } = render(<App />);
+
+  expect(dispatch).toBeCalled();
 
   expect(container).toHaveTextContent('서울');
 });
