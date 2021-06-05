6a8ce606a6b579c52020a25fb89687c43183e8d4
최한울 <hanul.choi95@nts-corp.com>
2021-06-05 22:19:11 +0900

    (V) 값 있는 지역명 클릭 시 (V) 값이 없어지는 테스트를 작성한다

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -6,14 +6,26 @@ import {
 } from './actions';
 
 describe('reducer', () => {
-  it('checkRegion', () => {
-    const state = reducer({
-      regions: [
-        { id: 1, name: '서울' },
-      ],
-    }, checkRegion(1));
+  describe('checkRegion', () => {
+    context('with V sign', () => {
+      const state = reducer({
+        regions: [
+          { id: 1, name: '서울' },
+        ],
+      }, checkRegion(1));
 
-    expect(state.regions[0].name).toBe('서울(V)');
+      expect(state.regions[0].name).toBe('서울');
+    });
+
+    context('without V sign', () => {
+      const state = reducer({
+        regions: [
+          { id: 1, name: '서울' },
+        ],
+      }, checkRegion(1));
+
+      expect(state.regions[0].name).toBe('서울(V)');
+    });
   });
 
   it('checkCategory', () => {
