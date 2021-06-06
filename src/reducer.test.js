e88a73a70366447a70756ffb4d457c7ee85e9cd4
hanvit7 <hanvit7@gmail.com>
2021-06-07 00:53:06 +0900

    테스트 - setRestaurants 테스트 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -3,6 +3,7 @@ import reducer from './reducer';
 import {
   setRegions,
   setCategories,
+  setRestaurants,
   selectRegion,
   selectCategory,
 } from './actions';
@@ -38,35 +39,55 @@ describe('reducer', () => {
     });
   });
 
+  describe('setRestaurants', () => {
+    it('changes Restaurants', () => {
+      const initialState = {
+        restaurants: [],
+      };
+
+      const restaurants = [
+        { id: 1, name: '마법사주방' },
+      ];
+
+      const state = reducer(initialState, setRestaurants(restaurants));
+
+      expect(state.restaurants).toHaveLength(1);
+    });
+  });
+
   describe('selectRegion', () => {
-    const initialState = {
-      regions: [
-        { id: 1, name: '서울' },
-      ],
-      selectedRegion: null,
-    };
+    it('changes selected region', () => {
+      const initialState = {
+        regions: [
+          { id: 1, name: '서울' },
+        ],
+        selectedRegion: null,
+      };
 
-    const state = reducer(initialState, selectRegion(1));
+      const state = reducer(initialState, selectRegion(1));
 
-    expect(state.selectedRegion).toEqual({
-      id: 1,
-      name: '서울',
+      expect(state.selectedRegion).toEqual({
+        id: 1,
+        name: '서울',
+      });
     });
   });
 
   describe('selectCategory', () => {
-    const initialState = {
-      categories: [
-        { id: 1, name: '한식' },
-      ],
-      selectedCategory: null,
-    };
+    it('changes selected category', () => {
+      const initialState = {
+        categories: [
+          { id: 1, name: '한식' },
+        ],
+        selectedCategory: null,
+      };
 
-    const state = reducer(initialState, selectCategory(1));
+      const state = reducer(initialState, selectCategory(1));
 
-    expect(state.selectedCategory).toEqual({
-      id: 1,
-      name: '한식',
+      expect(state.selectedCategory).toEqual({
+        id: 1,
+        name: '한식',
+      });
     });
   });
 });
