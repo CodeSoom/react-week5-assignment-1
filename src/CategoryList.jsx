a3c7672dbdb0019837c7f44247a1b40841f2039a
passwd10 <parkinseo94@naver.com>
2021-09-08 02:12:12 +0900

    refactor: Item을 이용하여 리팩토링한다

--- src/CategoryList.jsx
+++ src/CategoryList.jsx
@@ -1,18 +1,18 @@
+import Item from './Item';
+
 export default function CategoryList({ categories, onClick, selectedCategory }) {
   return (
     <ul>
       {categories.map(({ id, name }) => (
-        <li key={id}>
-          <button
-            type="button"
-            onClick={() => onClick({
-              field: 'selectedCategory',
-              value: { id, name },
-            })}
-          >
-            {id === selectedCategory.id ? `${name}(V)` : name}
-          </button>
-        </li>
+        <Item
+          key={id}
+          onClick={() => onClick({
+            field: 'selectedCategory',
+            value: { id, name },
+          })}
+        >
+          {id === selectedCategory.id ? `${name}(V)` : name}
+        </Item>
       ))}
     </ul>
   );
--- src/RegionList.jsx
+++ src/RegionList.jsx
@@ -1,18 +1,18 @@
+import Item from './Item';
+
 export default function RegionList({ regions, onClick, selectedRegion }) {
   return (
     <ul>
       {regions.map(({ id, name }) => (
-        <li key={id}>
-          <button
-            type="button"
-            onClick={() => onClick({
-              field: 'selectedRegion',
-              value: { id, name },
-            })}
-          >
-            {id === selectedRegion.id ? `${name}(V)` : name}
-          </button>
-        </li>
+        <Item
+          key={id}
+          onClick={() => onClick({
+            field: 'selectedRegion',
+            value: { id, name },
+          })}
+        >
+          {id === selectedRegion.id ? `${name}(V)` : name}
+        </Item>
       ))}
     </ul>
   );
