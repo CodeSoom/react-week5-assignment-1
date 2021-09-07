c28ce3415af87ea7706ead4bc1d801a51410c988
passwd10 <parkinseo94@naver.com>
2021-09-07 19:06:41 +0900

    test: App 테스트를 추가한다

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 
 import App from './App';
@@ -6,19 +6,47 @@ import App from './App';
 jest.mock('./services/api');
 
 describe('App', () => {
-  it('renders categories', async () => {
-    await act(async () => render(<App />));
+  context('without click item', () => {
+    it('renders categories', async () => {
+      await act(async () => render(<App />));
 
-    expect(screen.getByText('한식')).toBeInTheDocument();
-    expect(screen.getByText('중식')).toBeInTheDocument();
-    expect(screen.getByText('일식')).toBeInTheDocument();
+      expect(screen.getByText('한식')).toBeInTheDocument();
+      expect(screen.getByText('중식')).toBeInTheDocument();
+      expect(screen.getByText('일식')).toBeInTheDocument();
+    });
+
+    it('renders regions', async () => {
+      await act(async () => render(<App />));
+
+      expect(screen.getByText('서울')).toBeInTheDocument();
+      expect(screen.getByText('대전')).toBeInTheDocument();
+      expect(screen.getByText('대구')).toBeInTheDocument();
+    });
   });
 
-  it('renders regions', async () => {
-    await act(async () => render(<App />));
+  context('with click item', () => {
+    it('renders item with (v)', async () => {
+      await act(async () => render(<App />));
 
-    expect(screen.getByText('서울')).toBeInTheDocument();
-    expect(screen.getByText('대전')).toBeInTheDocument();
-    expect(screen.getByText('대구')).toBeInTheDocument();
+      fireEvent.click(screen.getByText('한식'));
+
+      expect(screen.getByText('한식(V)')).toBeInTheDocument();
+      expect(screen.getByText('중식')).toBeInTheDocument();
+
+      fireEvent.click(screen.getByText('중식'));
+
+      expect(screen.getByText('한식')).toBeInTheDocument();
+      expect(screen.getByText('중식(V)')).toBeInTheDocument();
+
+      fireEvent.click(screen.getByText('서울'));
+
+      expect(screen.getByText('서울(V)')).toBeInTheDocument();
+      expect(screen.getByText('대전')).toBeInTheDocument();
+
+      fireEvent.click(screen.getByText('대전'));
+
+      expect(screen.getByText('서울')).toBeInTheDocument();
+      expect(screen.getByText('대전(V)')).toBeInTheDocument();
+    });
   });
 });
