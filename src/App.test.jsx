6e3ff2453d8ac1d34c8d9db22d085ca016161551
kooku <koomg9599@gmail.com>
2021-09-07 17:42:28 +0900

    App test 작성

---
+++ src/App.test.jsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+test('App', () => {
+  const { getByText } = render(<App />);
+
+  expect(getByText('한식'));
+  expect(getByText('서울'));
+});
