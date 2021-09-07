220b45f4255d1bbb917a14133f8ca445e414da44
kooku <koomg9599@gmail.com>
2021-09-07 18:11:06 +0900

    App test 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,22 @@
 import { render } from '@testing-library/react';
 
+import { useDispatch, useSelector } from '../__mocks/react-redux';
+
 import App from './App';
 
 test('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+  useSelector.mockImplementation((selector) => selector({
+    categories: [
+      { id: 1, category: '한식' },
+    ],
+    regions: [
+      { id: 1, region: '서울' },
+    ],
+  }));
+
   const { getByText } = render(<App />);
 
   expect(getByText('한식'));
