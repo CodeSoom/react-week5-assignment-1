866b1f56f4b3fcea89bab414d2e4f1b432f0a20b
kooku <koomg9599@gmail.com>
2021-09-08 22:11:04 +0900

    App 기본 마크업 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 export default function App() {
   return (
     <>
+      <ul>
+        <li>
+          <button type="button">서울</button>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <button type="button">한식</button>
+        </li>
+      </ul>
     </>
   );
 }
