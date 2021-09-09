6c4c88f7c4ba9df80f6378cbdd084ec804f60962
passwd10 <parkinseo94@naver.com>
2021-09-09 20:05:12 +0900

    test: App 테스트를 수정한다

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -8,65 +8,61 @@ import App from './App';
 jest.mock('./services/api');
 
 describe('App', () => {
-  context('without click item', () => {
-    it('renders categories', async () => {
-      await act(async () => render(<App />));
+  it('renders categories', async () => {
+    await act(async () => render(<App />));
 
-      expect(screen.getByText('한식')).toBeInTheDocument();
+    expect(screen.getByText('한식')).toBeInTheDocument();
+    expect(screen.getByText('중식')).toBeInTheDocument();
+    expect(screen.getByText('일식')).toBeInTheDocument();
+  });
+
+  it('renders regions', async () => {
+    await act(async () => render(<App />));
+
+    expect(screen.getByText('서울')).toBeInTheDocument();
+    expect(screen.getByText('대전')).toBeInTheDocument();
+    expect(screen.getByText('대구')).toBeInTheDocument();
+  });
+
+  it('renders item with (v)', async () => {
+    await act(async () => render(<App />));
+
+    fireEvent.click(screen.getByText('한식'));
+
+    await waitFor(() => {
+      expect(screen.getByText('한식(V)')).toBeInTheDocument();
       expect(screen.getByText('중식')).toBeInTheDocument();
-      expect(screen.getByText('일식')).toBeInTheDocument();
     });
 
-    it('renders regions', async () => {
-      await act(async () => render(<App />));
+    fireEvent.click(screen.getByText('중식'));
 
-      expect(screen.getByText('서울')).toBeInTheDocument();
+    await waitFor(() => {
+      expect(screen.getByText('한식')).toBeInTheDocument();
+      expect(screen.getByText('중식(V)')).toBeInTheDocument();
+    });
+
+    fireEvent.click(screen.getByText('서울'));
+
+    await waitFor(() => {
+      expect(screen.getByText('서울(V)')).toBeInTheDocument();
       expect(screen.getByText('대전')).toBeInTheDocument();
-      expect(screen.getByText('대구')).toBeInTheDocument();
+    });
+
+    fireEvent.click(screen.getByText('대전'));
+
+    await waitFor(() => {
+      expect(screen.getByText('서울')).toBeInTheDocument();
+      expect(screen.getByText('대전(V)')).toBeInTheDocument();
     });
   });
 
-  context('with click item', () => {
-    it('renders item with (v)', async () => {
-      await act(async () => render(<App />));
+  it('renders result restaurants', async () => {
+    await act(async () => render(<App />));
 
-      fireEvent.click(screen.getByText('한식'));
+    fireEvent.click(screen.getByText('서울'));
+    fireEvent.click(screen.getByText('한식'));
 
-      await waitFor(() => {
-        expect(screen.getByText('한식(V)')).toBeInTheDocument();
-        expect(screen.getByText('중식')).toBeInTheDocument();
-      });
-
-      fireEvent.click(screen.getByText('중식'));
-
-      await waitFor(() => {
-        expect(screen.getByText('한식')).toBeInTheDocument();
-        expect(screen.getByText('중식(V)')).toBeInTheDocument();
-      });
-
-      fireEvent.click(screen.getByText('서울'));
-
-      await waitFor(() => {
-        expect(screen.getByText('서울(V)')).toBeInTheDocument();
-        expect(screen.getByText('대전')).toBeInTheDocument();
-      });
-
-      fireEvent.click(screen.getByText('대전'));
-
-      await waitFor(() => {
-        expect(screen.getByText('서울')).toBeInTheDocument();
-        expect(screen.getByText('대전(V)')).toBeInTheDocument();
-      });
-    });
-
-    it('renders result restaurants', async () => {
-      await act(async () => render(<App />));
-
-      fireEvent.click(screen.getByText('서울'));
-      fireEvent.click(screen.getByText('한식'));
-
-      await waitFor(() => expect(screen.getByText('양천주가')).toBeInTheDocument());
-      await waitFor(() => expect(screen.getByText('한국식초밥')).toBeInTheDocument());
-    });
+    await waitFor(() => expect(screen.getByText('양천주가')).toBeInTheDocument());
+    await waitFor(() => expect(screen.getByText('한국식초밥')).toBeInTheDocument());
   });
 });
