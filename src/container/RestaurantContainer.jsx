b75b6d0df54d7cfd87497b7ef16b9992b23b4d7a
passwd10 <parkinseo94@naver.com>
2021-09-10 00:20:47 +0900

    feat: RestaurantContainer를 구현한다

---
+++ src/container/RestaurantContainer.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { loadRestaurants } from '../store/actions';
+
+import RestaurantList from '../presentational/RestaurantList';
+
+export default function RestaurantContainer() {
+  const dispatch = useDispatch();
+
+  const {
+    selectedRegion,
+    selectedCategory,
+    restaurants,
+  } = useSelector((state) => state);
+
+  const isEmpty = (param) => Object.keys(param).length === 0;
+
+  useEffect(() => {
+    if (isEmpty(selectedRegion) || isEmpty(selectedCategory)) {
+      return;
+    }
+
+    dispatch(loadRestaurants({
+      regionName: selectedRegion.name,
+      categoryId: selectedCategory.id,
+    }));
+  }, [selectedRegion, selectedCategory]);
+
+  return (
+    <RestaurantList
+      restaurants={restaurants}
+    />
+  );
+}
