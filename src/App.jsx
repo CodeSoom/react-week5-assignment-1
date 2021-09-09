69b94535dcf1757019e8c9dc0ca0ea87c20a06f2
passwd10 <parkinseo94@naver.com>
2021-09-10 00:21:04 +0900

    refactor: App을 리팩토링한다

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,13 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import { loadRestaurants } from './store/actions';
-
 import RegionContainer from './container/RegionContainer';
 import CategoryContainer from './container/CategoryContainer';
-import RestaurantList from './presentational/RestaurantList';
+import RestaurantContainer from './container/RestaurantContainer';
 
 export default function App() {
-  const dispatch = useDispatch();
-  const {
-    selectedRegion,
-    selectedCategory,
-    restaurants,
-  } = useSelector((state) => state);
-
-  const isEmpty = (param) => Object.keys(param).length === 0;
-
-  useEffect(() => {
-    if (isEmpty(selectedRegion) || isEmpty(selectedCategory)) {
-      return;
-    }
-
-    dispatch(loadRestaurants({
-      regionName: selectedRegion.name,
-      categoryId: selectedCategory.id,
-    }));
-  }, [selectedRegion, selectedCategory]);
-
   return (
     <>
       <RegionContainer />
       <CategoryContainer />
-      <RestaurantList
-        restaurants={restaurants}
-      />
+      <RestaurantContainer />
     </>
   );
 }
