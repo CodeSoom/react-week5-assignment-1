48af30aa0cdd5d22eb945400f34d12b32caff63b
lazy-sky <1cielo@naver.com>
2021-09-10 20:09:29 +0900

    red: Testing App
    - it renders regions

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,4 +9,11 @@ describe('App', () => {
     expect(getByText('한식')).not.toBeNull();
     expect(getByText('중식')).not.toBeNull();
   });
+
+  it('renders regions', () => {
+    const { getByText } = render(<App />);
+
+    expect(getByText('서울')).not.toBeNull();
+    expect(getByText('부산')).not.toBeNull();
+  });
 });
