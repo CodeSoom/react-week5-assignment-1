c026f9b48be6555279b69e553d72b0af302202f2
psy082 <zhappy114@gmail.com>
2021-09-10 20:16:05 +0900

    test: App test 재작성
    - tdd를 위한 기본적인 내용으로 테스트 내용 변경

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,36 +1,18 @@
 import { render, fireEvent } from '@testing-library/react';
 
-import { useDispatch, useSelector } from 'react-redux';
-
 import App from './App';
 
 jest.mock('react-redux');
 
 describe('App', () => {
 
-  const dispatch = jest.fn();
-
-  useDispatch.mockImplementation(() => dispatch);
-
-  useSelector.mockImplementation((selector) => selector({
-    regions: ['서울', '대전', '대구']
-  }))
-
   it('shows regions as buttons', () => {
     const { getByText } = render((
       <App />
     ));
 
-    expect(dispatch).toBeCalledWith({
-      type: 'getRegions'
-    });
-
     expect(getByText('서울')).not.toBeNull();
     expect(getByText('대전')).not.toBeNull();
     expect(getByText('대구')).not.toBeNull();
-
-    fireEvent.click(getByText('서울'));
-    expect(getByText('서울')).toBeNull();
-    expect(getByText('서울(V)')).not.toBeNull();
-  })
+  });
 })
