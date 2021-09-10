1aa5b7b0903418b4a2385e93aee6ab1d6db4b6f9
kokoball <kbp4154@naver.com>
2021-09-10 10:39:26 +0900

    Categories.test 수정 (button 추가)

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-export default function Categories({ categories }) {
+export default function Categories({ categories, handleInputText }) {
   return (
     <ul>
       {categories.map((category) => (
         <li key={category.id}>
-          {category.name}
+          <button key={category.id} type="button" onClick={handleInputText}>
+            {category.name}
+          </button>
         </li>
       ))}
     </ul>
--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Categories from './Categories';
 
 import categories from '../fixtures/categories';
 
 test('Categories', () => {
+  const handleInputText = jest.jn();
+
   const { getByText } = render((
-    <Categories categories={categories} />
+    <Categories categories={categories} onClick={handleInputText} />
   ));
 
   expect(getByText('한식')).not.toBeNull();
+  fireEvent.click(getByText(/등록/));
 });
