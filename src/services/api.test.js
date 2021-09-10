cdb63c1d1c45f332ad49732ae6676f1aa8321fbf
Aiden <rheech22@gmail.com>
2021-09-10 10:11:01 +0900

    Test: Add API test

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -1,19 +1,90 @@
-const url = 'https://eatgo-customer-api.ahastudio.com/';
+import {
+  fetchCategories,
+  fetchRegions,
+  fetchRestaurants,
+} from './api';
 
-export async function fetchCategories() {
-  const response = await fetch(`${url}categories`);
-  const data = await response.json();
-  return data;
-}
+describe('api', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
 
-export async function fetchRegions() {
-  const response = await fetch(`${url}regions`);
-  const data = await response.json();
-  return data;
-}
+    global.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve([]),
+    }));
+  });
 
-export async function fetchRestaurants(regionName, categoryId) {
-  const response = await fetch(`${url}restaurants?region=${regionName}&category=${categoryId}`);
-  const data = await response.json();
-  return data;
-}
+  it('지역 목록을 가져온다', async () => {
+    fetch.mockImplementationOnce(() => Promise.resolve({
+      json: () => Promise.resolve([
+        { id: 1, name: '서울' },
+        { id: 2, name: '대구' },
+        { id: 3, name: '부산' },
+      ]),
+    }));
+    const data = await fetchRegions();
+    expect(data).toEqual([
+      { id: 1, name: '서울' },
+      { id: 2, name: '대구' },
+      { id: 3, name: '부산' },
+    ]);
+    expect(fetch).toHaveBeenCalledWith('https://eatgo-customer-api.ahastudio.com/regions');
+  });
+
+  it('카테고리 목록을 가져온다', async () => {
+    fetch.mockImplementationOnce(() => Promise.resolve({
+      json: () => Promise.resolve([
+        { id: 1, name: '한식' },
+        { id: 2, name: '중식' },
+        { id: 3, name: '일식' },
+      ]),
+    }));
+
+    const data = await fetchCategories();
+    expect(data).toEqual([
+      { id: 1, name: '한식' },
+      { id: 2, name: '중식' },
+      { id: 3, name: '일식' },
+    ]);
+    expect(fetch).toHaveBeenCalledWith('https://eatgo-customer-api.ahastudio.com/categories');
+  });
+
+  it('지역 목록을 가져온다', async () => {
+    fetch.mockImplementationOnce(() => Promise.resolve({
+      json: () => Promise.resolve([
+        {
+          id: 9,
+          categoryId: 2,
+          name: '호신각',
+          address: '서울 강남구',
+          information: '호신각 in 서울 강남구',
+        },
+        {
+          id: 10,
+          categoryId: 2,
+          name: '홍콩반점',
+          address: '서울시 서대문구',
+          information: '홍콩반점 in 서울시 서대문구',
+        },
+      ]),
+    }));
+
+    const data = await fetchRestaurants();
+    expect(data).toEqual([
+      {
+        id: 9,
+        categoryId: 2,
+        name: '호신각',
+        address: '서울 강남구',
+        information: '호신각 in 서울 강남구',
+      },
+      {
+        id: 10,
+        categoryId: 2,
+        name: '홍콩반점',
+        address: '서울시 서대문구',
+        information: '홍콩반점 in 서울시 서대문구',
+      },
+    ]);
+    expect(fetch).toHaveBeenCalledWith('https://eatgo-customer-api.ahastudio.com/restaurants?region=undefined&category=undefined');
+  });
+});
