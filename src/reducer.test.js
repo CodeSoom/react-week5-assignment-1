b6100292ac851f1832429954123ab925fcb0c6a4
kokoball <kbp4154@naver.com>
2021-09-10 15:58:30 +0900

    reducer Regions test 작성

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -2,9 +2,11 @@ import reducer from './reducer';
 
 import {
   setCategories,
+  setRegions,
 } from './actions';
 
 import categories from '../fixtures/categories';
+import regions from '../fixtures/regions';
 
 describe('reducer', () => {
   describe('setCategories', () => {
@@ -16,6 +18,17 @@ describe('reducer', () => {
       const state = reducer(initailState, setCategories(categories));
 
       expect(state.categories).toHaveLength(1);
+    });
+  });
+  describe('setRegions', () => {
+    it('changes Regions', () => {
+      const initailState = {
+        regions: [],
+      };
+
+      const state = reducer(initailState, setRegions(regions));
+
+      expect(state.regions).toHaveLength(1);
     });
   });
 });
