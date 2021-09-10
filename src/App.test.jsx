3c7101e9f87d2adf7cf4be3e6d50b533044cf9b6
lazy-sky <1cielo@naver.com>
2021-09-10 20:41:28 +0900

    red: Testing to listen click event

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,10 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
 describe('App', () => {
+  const handleClick = jest.fn();
+
   it('renders cateogries', () => {
     const { getByText } = render(<App />);
 
@@ -15,5 +17,16 @@ describe('App', () => {
 
     expect(getByText('서울')).not.toBeNull();
     expect(getByText('부산')).not.toBeNull();
+  });
+
+  it('listens click event', () => {
+    const { getByText } = render(<App />);
+
+    expect(getByText('한식')).not.toBeNull();
+
+    fireEvent.click(getByText('한식'));
+
+    expect(handleClick).toBeCalled();
+    expect(getByText('한식(V)')).not.toBeNull();
   });
 });
