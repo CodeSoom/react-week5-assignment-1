6843296c6bd87ff426c8c0a2433029f39f9028eb
passwd10 <parkinseo94@naver.com>
2021-09-10 17:23:47 +0900

    test: action 테스트를 추가한다

--- src/store/reducer.test.js
+++ src/store/reducer.test.js
@@ -1,6 +1,15 @@
-import reducer from './reducer';
+import configureMockStore from 'redux-mock-store';
 
-import { updateField } from './actions';
+import thunk from 'redux-thunk';
+
+import reducer, {
+  updateField, loadCategories, loadRegions, loadRestaurants,
+} from './reducer';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+jest.mock('../services/api');
 
 describe('reducer', () => {
   describe('updateField', () => {
@@ -23,6 +32,99 @@ describe('reducer', () => {
       );
 
       expect(nextState.categories).toBe(categoriesData);
+    });
+  });
+
+  describe('loadCategories', () => {
+    it('dispatches updateField action', async () => {
+      const expectedAction = [updateField({
+        field: 'categories',
+        value: [
+          { id: 1, name: '한식' },
+          { id: 2, name: '중식' },
+          { id: 3, name: '일식' },
+        ],
+      })];
+
+      const store = mockStore({
+        categories: [
+          { id: 1, name: '한식' },
+          { id: 2, name: '중식' },
+          { id: 3, name: '일식' },
+        ],
+      });
+
+      await store.dispatch(loadCategories());
+
+      expect(store.getActions()).toEqual(expectedAction);
+    });
+  });
+
+  describe('loadRegions', () => {
+    it('dispatches updateField action', async () => {
+      const expectedAction = [updateField({
+        field: 'regions',
+        value: [
+          { id: 1, name: '서울' },
+          { id: 2, name: '대전' },
+          { id: 3, name: '대구' },
+        ],
+      })];
+
+      const store = mockStore({
+        regions: [
+          { id: 1, name: '서울' },
+          { id: 2, name: '대전' },
+          { id: 3, name: '대구' },
+        ],
+      });
+
+      await store.dispatch(loadRegions());
+
+      expect(store.getActions()).toEqual(expectedAction);
+    });
+  });
+
+  describe('loadRestaurants', () => {
+    it('dispatches updateField action', async () => {
+      const expectedAction = [updateField({
+        field: 'restaurants',
+        value: [{
+          id: 1,
+          categoryId: 1,
+          name: '양천주가',
+          address: '서울 강남구 123456',
+          information: '양천주가 in 서울 강남구 123456',
+        },
+        {
+          id: 6,
+          categoryId: 1,
+          name: '한국식초밥',
+          address: '서울 강남구',
+          information: '한국식 초밥 in 서울 강남구',
+        }],
+      })];
+
+      const store = mockStore({
+        restaurants: [{
+          id: 1,
+          categoryId: 1,
+          name: '양천주가',
+          address: '서울 강남구 123456',
+          information: '양천주가 in 서울 강남구 123456',
+        },
+        {
+          id: 6,
+          categoryId: 1,
+          name: '한국식초밥',
+          address: '서울 강남구',
+          information: '한국식 초밥 in 서울 강남구',
+        }],
+      });
+
+      await store.dispatch(loadRestaurants({ regionName: '서울', categoryId: 1 }));
+
+      expect(store.getActions()).toEqual(expectedAction);
     });
   });
 });
