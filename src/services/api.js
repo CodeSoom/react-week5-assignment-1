91626b1764283e2f6d2e01b5167232fe89e3c9f8
Aiden <rheech22@gmail.com>
2021-09-12 00:37:23 +0900

    Test: API more

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,31 @@
 const url = 'https://eatgo-customer-api.ahastudio.com/';
 
 export async function fetchRegions() {
-  const response = await fetch(`${url}regions`);
-  const regions = await response.json();
-  return regions;
+  try {
+    const response = await fetch(`${url}regions`);
+    const regions = await response.json();
+    return regions;
+  } catch (error) {
+    return [];
+  }
 }
 
 export async function fetchCategories() {
-  const response = await fetch(`${url}categories`);
-  const categories = await response.json();
-  return categories;
+  try {
+    const response = await fetch(`${url}categories`);
+    const categories = await response.json();
+    return categories;
+  } catch (error) {
+    return [];
+  }
 }
 
 export async function fetchRestaurants(regionName, categoryId) {
-  const response = await fetch(`${url}restaurants?region=${regionName}&category=${categoryId}`);
-  const restaurants = await response.json();
-  return restaurants;
+  try {
+    const response = await fetch(`${url}restaurants?region=${regionName}&category=${categoryId}`);
+    const restaurants = await response.json();
+    return restaurants;
+  } catch (error) {
+    return [];
+  }
 }
--- src/services/api.test.js
+++ src/services/api.test.js
@@ -5,6 +5,8 @@ import {
 } from './api';
 
 describe('api', () => {
+  const url = 'https://eatgo-customer-api.ahastudio.com/';
+
   beforeEach(() => {
     jest.clearAllMocks();
 
@@ -13,78 +15,89 @@ describe('api', () => {
     }));
   });
 
-  it('지역 목록을 가져온다', async () => {
-    fetch.mockImplementationOnce(() => Promise.resolve({
-      json: () => Promise.resolve([
-        { id: 1, name: '서울' },
-        { id: 2, name: '대구' },
-        { id: 3, name: '부산' },
-      ]),
-    }));
-    const data = await fetchRegions();
-    expect(data).toEqual([
-      { id: 1, name: '서울' },
-      { id: 2, name: '대구' },
-      { id: 3, name: '부산' },
-    ]);
-    expect(fetch).toHaveBeenCalledWith('https://eatgo-customer-api.ahastudio.com/regions');
+  describe('fetchRegions', () => {
+    context('서버 응답이 있다면', () => {
+      it('지역 목록을 가져온다', async () => {
+        fetch.mockImplementationOnce(() => Promise.resolve({
+          json: () => Promise.resolve([
+            { id: 1, name: '서울' },
+          ]),
+        }));
+        const regions = await fetchRegions();
+        expect(regions).toEqual([
+          { id: 1, name: '서울' },
+        ]);
+        expect(fetch).toHaveBeenCalledWith(`${url}regions`);
+      });
+    });
+
+    context('서버 응답이 없다면', () => {
+      it('빈 목록을 반환한다', async () => {
+        fetch.mockImplementationOnce(() => Promise.reject());
+        const regions = await fetchRegions();
+        expect(regions).toEqual([]);
+        expect(fetch).toHaveBeenCalledWith(`${url}regions`);
+      });
+    });
   });
 
-  it('카테고리 목록을 가져온다', async () => {
-    fetch.mockImplementationOnce(() => Promise.resolve({
-      json: () => Promise.resolve([
-        { id: 1, name: '한식' },
-        { id: 2, name: '중식' },
-        { id: 3, name: '일식' },
-      ]),
-    }));
+  describe('fetchCategories', () => {
+    context('서버 응답이 있다면', () => {
+      it('카테고리 목록을 가져온다', async () => {
+        fetch.mockImplementationOnce(() => Promise.resolve({
+          json: () => Promise.resolve([
+            { id: 1, name: '한식' },
+          ]),
+        }));
 
-    const data = await fetchCategories();
-    expect(data).toEqual([
-      { id: 1, name: '한식' },
-      { id: 2, name: '중식' },
-      { id: 3, name: '일식' },
-    ]);
-    expect(fetch).toHaveBeenCalledWith('https://eatgo-customer-api.ahastudio.com/categories');
+        const categories = await fetchCategories();
+        expect(categories).toEqual([
+          { id: 1, name: '한식' },
+        ]);
+        expect(fetch).toHaveBeenCalledWith(`${url}categories`);
+      });
+    });
+
+    context('서버 응답이 없다면', () => {
+      it('빈 목록을 반환한다', async () => {
+        fetch.mockImplementationOnce(() => Promise.reject());
+        const categories = await fetchCategories();
+        expect(categories).toEqual([]);
+        expect(fetch).toHaveBeenCalledWith(`${url}categories`);
+      });
+    });
   });
 
-  it('지역 목록을 가져온다', async () => {
-    fetch.mockImplementationOnce(() => Promise.resolve({
-      json: () => Promise.resolve([
-        {
-          id: 9,
-          categoryId: 2,
-          name: '호신각',
-          address: '서울 강남구',
-          information: '호신각 in 서울 강남구',
-        },
-        {
-          id: 10,
-          categoryId: 2,
-          name: '홍콩반점',
-          address: '서울시 서대문구',
-          information: '홍콩반점 in 서울시 서대문구',
-        },
-      ]),
-    }));
+  describe('fetchCategories', () => {
+    context('서버 응답이 있다면', () => {
+      it('식당 목록을 가져온다', async () => {
+        fetch.mockImplementationOnce(() => Promise.resolve({
+          json: () => Promise.resolve([
+            {
+              id: 9,
+              name: '호신각',
+            },
+          ]),
+        }));
 
-    const data = await fetchRestaurants();
-    expect(data).toEqual([
-      {
-        id: 9,
-        categoryId: 2,
-        name: '호신각',
-        address: '서울 강남구',
-        information: '호신각 in 서울 강남구',
-      },
-      {
-        id: 10,
-        categoryId: 2,
-        name: '홍콩반점',
-        address: '서울시 서대문구',
-        information: '홍콩반점 in 서울시 서대문구',
-      },
-    ]);
-    expect(fetch).toHaveBeenCalledWith('https://eatgo-customer-api.ahastudio.com/restaurants?region=undefined&category=undefined');
+        const restaurants = await fetchRestaurants();
+        expect(restaurants).toEqual([
+          {
+            id: 9,
+            name: '호신각',
+          },
+        ]);
+        expect(fetch).toHaveBeenCalledWith(`${url}restaurants?region=undefined&category=undefined`);
+      });
+    });
+
+    context('서버 응답이 없다면', () => {
+      it('빈 목록을 반환한다', async () => {
+        fetch.mockImplementationOnce(() => Promise.reject());
+        const restaurants = await fetchRestaurants();
+        expect(restaurants).toEqual([]);
+        expect(fetch).toHaveBeenCalledWith(`${url}restaurants?region=undefined&category=undefined`);
+      });
+    });
   });
 });
