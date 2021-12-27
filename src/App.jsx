857dc680bde52aad5194821d57f2ecce00815a7d
emewjin <emewjin@gmail.com>
2021-12-27 21:56:22 +0900

    new: App 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
-import Categories from './Categories';
+import { useDispatch } from 'react-redux';
+
+import { useEffect } from 'react';
+import CategoriesContainer from './CategoriesContainer';
+
+import { setCategories } from './actions';
+import { CATEGORIES } from './fixtures';
 
 export default function App() {
-  return <Categories />;
+  const dispatch = useDispatch();
+
+  const loadCategories = () => {
+    dispatch(setCategories(CATEGORIES));
+  };
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  return <CategoriesContainer />;
 }
