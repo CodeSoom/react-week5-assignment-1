fe9bcc429aa8fadc10378c6b0152c1863df276a5
emewjin <emewjin@gmail.com>
2021-12-27 21:17:17 +0900

    new: CategoriesContainer 테스트 추가

---
+++ src/CategoriesContainer.test.jsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+
+import CategoriesContainer from './CategoriesContainer';
+
+describe('CategoriesContainer', () => {
+  it('render', () => {
+    render(<CategoriesContainer />);
+  });
+});
