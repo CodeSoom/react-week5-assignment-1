28d49ceecd3a94fcc31a553d2fd5da513118220b
emewjin <emewjin@gmail.com>
2021-12-27 21:15:45 +0900

    new: Categories 테스트 추가

---
+++ src/Categories.test.jsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+
+import Categories from './Categories';
+
+describe('Categories', () => {
+  it('render', () => {
+    render(<Categories />);
+  });
+});
