2116509c4db696d9bcdc681c8453cb284c789778
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-27 23:40:07 +0900

    [refactor] 로딩시 categori 설정은 App 처리로 인한 코드 변경

--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -1,17 +1,7 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import {
-  setCategories,
-} from './actions';
+import { useSelector } from 'react-redux';
 
 export default function CategoriesContainer() {
-  const dispatch = useDispatch();
   const { categories } = useSelector((state) => state);
-
-  useEffect(() => {
-    dispatch(setCategories());
-  }, []);
 
   return (
     <ul>
