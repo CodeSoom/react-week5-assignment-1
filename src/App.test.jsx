b6113997f285db16ee40cfe1ca73216cbf4560bd
emewjin <emewjin@gmail.com>
2021-12-27 21:56:05 +0900

    up: App 테스트에 리덕스 요소 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,21 @@
 import { render } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
+import { CATEGORIES } from './fixtures';
+
+jest.mock('react-redux');
 
 describe('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+  useSelector.mockImplementation((selector) =>
+    selector({
+      categories: CATEGORIES,
+    })
+  );
+
   it('render', () => {
     render(<App />);
   });
