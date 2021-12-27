690fa1be391b82185fd8fe8bcde05d65feb835f7
emewjin <emewjin@gmail.com>
2021-12-27 20:44:20 +0900

    new: 초기세팅

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,7 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -15,9 +12,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,4 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app'),
-);
+ReactDOM.render(<App />, document.getElementById('app'));
