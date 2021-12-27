26a4d4b457fa678003c90bbd0054236fb673dabd
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-27 23:22:21 +0900

    [test] Categories 테스트 코드 작성 - react-reudx mocking - categories 테스트 값 선언

---
+++ src/CategoriesContainer.test.jsx
@@ -0,0 +1,38 @@
+import { render } from '@testing-library/react';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+import CategoriesContainer from './CategoriesContainer';
+
+import { categories } from '../__fixture__/categories';
+
+jest.mock('react-redux');
+
+describe('CategoriesContainer', () => {
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+  useSelector.mockImplementation((selector) => selector({
+    categories,
+  }));
+
+  it('get categories', () => {
+    const { getByRole } = render(
+      <CategoriesContainer />,
+    );
+
+    categories.forEach((category) => {
+      getByRole('button', { name: category.name });
+    });
+  });
+
+  it('calls dispatch after loading', () => {
+    render(
+      <CategoriesContainer />,
+    );
+
+    expect(dispatch).toBeCalled();
+    expect(dispatch).toBeCalledWith({
+      type: 'setCategories',
+    });
+  });
+});
