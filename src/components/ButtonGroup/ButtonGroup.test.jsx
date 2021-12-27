b9fc1a4de25904b5f8e4d8dc6de6e1199538a140
padawanR0k <padawanR0k@gmail.com>
2021-12-28 00:15:13 +0900

    up: Button Group 테스트 코드 추가

--- src/components/ButtonGroup/ButtonGroup.test.jsx
+++ src/components/ButtonGroup/ButtonGroup.test.jsx
@@ -1,8 +1,8 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import ButtonGroup from './ButtonGroup';
 
 describe('ButtonGroup', () => {
-  test('전달한 목록과 렌더링 방식에 따라 내용을 출력한다.', () => {
+  it('전달한 목록과 렌더링 방식에 따라 내용을 출력한다.', () => {
     const { getByText } = render(<ButtonGroup
       items={[
         { id: 1, text: '버튼1' },
@@ -12,5 +12,21 @@ describe('ButtonGroup', () => {
     />);
 
     expect(getByText('버튼1')).not.toBeNull();
+  });
+
+  it('클릭시 전달받은 onClick을 실행한다.', () => {
+    const onClick = jest.fn();
+    const { getByText } = render(<ButtonGroup
+      items={[
+        { id: 1, text: '버튼1' },
+      ]}
+      render={(item) => item.text}
+      keyOfItem="id"
+      onClick={onClick}
+    />);
+
+    fireEvent.click(getByText('버튼1'));
+
+    expect(onClick).toBeCalledWith({ id: 1, text: '버튼1' });
   });
 });
