a96b64e47f7bacde5cee0137afbb6169c8511cb9
emewjin <emewjin@gmail.com>
2021-12-27 21:38:19 +0900

    fix: 카테고리 컨테이너 렌더링 테스트 수정

--- src/CategoriesContainer.test.jsx
+++ src/CategoriesContainer.test.jsx
@@ -1,13 +1,21 @@
 import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
 
 import CategoriesContainer from './CategoriesContainer';
+
 import { CATEGORIES } from './fixtures';
 
+jest.mock('react-redux');
+
 describe('CategoriesContainer', () => {
+  useSelector.mockImplementation((selector) =>
+    selector({
+      categories: CATEGORIES,
+    })
+  );
+
   it('render', () => {
-    const { container } = render(
-      <CategoriesContainer categories={CATEGORIES} />
-    );
+    const { container } = render(<CategoriesContainer />);
 
     expect(container).toHaveTextContent('한식');
   });
