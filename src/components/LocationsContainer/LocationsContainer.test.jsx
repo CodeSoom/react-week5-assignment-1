3443fccf5f29753781d48b6b693def5d6d60209c
padawanR0k <padawanR0k@gmail.com>
2021-12-28 19:52:24 +0900

    up: LocationsContainer test redux 모킹 추가

--- src/components/LocationsContainer/LocationsContainer.test.jsx
+++ src/components/LocationsContainer/LocationsContainer.test.jsx
@@ -1,20 +1,38 @@
 import { fireEvent, render } from '@testing-library/react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
 import LocationsContainer from './LocationsContainer';
 
 jest.mock('react-redux');
-jest.mock('../../api/services');
+jest.mock('../../services/api');
 
 describe('LocationsContainer', () => {
-  it('지역명들을 fetch한 후 노출한다.', () => {
-    const { getByText } = render(<LocationsContainer />);
+  const dispatch = jest.fn();
 
-    expect(getByText('서울')).not.toBeNull();
+  beforeEach(() => {
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
+  afterEach(() => {
+    dispatch.mockRestore();
+  });
+
+  it('컴포넌트 mount시, 지역명들을 fetch한다.', async () => {
+    useSelector.mockImplementation((selector) => selector({
+      locations: [],
+    }));
+
+    render(<LocationsContainer />);
+    expect(dispatch).toBeCalled();
   });
 
   it('지역을 클릭하면 dispatch를 실행한다.', () => {
-    const dispatch = jest.fn();
-    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      locations: [
+        { id: 1, name: '서울' },
+      ],
+    }));
+
     const { getByText } = render(<LocationsContainer />);
 
     fireEvent.click(getByText('서울'));
