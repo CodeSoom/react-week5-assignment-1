c1c9d266539a26db5eddc1e41e6ced0a72c4126d
emewjin <emewjin@gmail.com>
2021-12-28 17:44:10 +0900

    loadRegions action 테스트를 추가한다

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,11 +1,11 @@
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 
-import { loadCategories } from './actions';
+import { loadCategories, loadRegions } from './actions';
 
 import { fetchCategories } from './services/api';
 
-import { CATEGORIES } from './fixtures';
+import { CATEGORIES, REGIONS } from './fixtures';
 
 jest.mock('./services/api');
 
@@ -13,15 +13,24 @@ const mockStore = configureStore([thunk]);
 fetchCategories.mockResolvedValue(CATEGORIES);
 
 describe('actions', () => {
+  const store = mockStore({});
   describe('loadCategories', () => {
     it('setCategories를 호출한다 ', async () => {
-      const store = mockStore({});
-
       await store.dispatch(loadCategories());
 
       const actions = store.getActions();
 
       expect(actions[0].payload.categories).toEqual(CATEGORIES);
+    });
+  });
+
+  describe('loadRegions', () => {
+    it('setRegions를 호출한다', async () => {
+      await store.dispatch(loadRegions());
+
+      const actions = store.getActions();
+
+      expect(actions[0].payload.regions).toEqual(REGIONS);
     });
   });
 });
