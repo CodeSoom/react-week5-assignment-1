1763dfa57e0943f3ec2d47b2b7d4c0d4d10e0ad6
emewjin <emewjin@gmail.com>
2021-12-28 19:15:54 +0900

    식당 목록을 보여주는 테스트를 작성한다

---
+++ src/Restaurants.test.jsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+
+import { RESTAURANTS } from './fixtures';
+
+import Restaurants from './Restaurants';
+
+describe('Restaurants', () => {
+  const renderComponent = (restaurants) =>
+    render(<Restaurants restaurants={restaurants} />);
+
+  context('식당 목록이 있으면', () => {
+    it('목록을 보여준다 ', () => {
+      renderComponent(RESTAURANTS);
+      expect(screen.getByText('도산분식')).not.toBeNull();
+    });
+  });
+
+  context('식당 목록이 없으면', () => {
+    it('헬퍼메세지를 보여준다 ', () => {
+      renderComponent([]);
+      expect(screen.getByText('조건에 맞는 식당이 없어요')).not.toBeNull();
+    });
+  });
+});
