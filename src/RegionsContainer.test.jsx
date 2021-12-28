a1d83396b77cccaf25211183e15907ed7a7d610f
AutowiniFront <autowinifront@naver.com>
2021-12-28 23:37:06 +0900

    RegionsContainer 테스트 수정

--- src/RegionsContainer.test.jsx
+++ src/RegionsContainer.test.jsx
@@ -8,11 +8,13 @@ jest.mock('react-redux');
 describe('RegionsContainer', () => {
   it('전역 상태 regions 렌더링', () => {
     useSelector.mockImplementation((selector) => selector({
-      regions: [],
+      regions: [{
+        id: 100,
+        name: '테스트',
+      }],
     }));
 
     const { getByText } = render(<RegionsContainer />);
-
-    expect(getByText('서울')).toBeNull();
+    expect(getByText('테스트')).not.toBeNull();
   });
 });
