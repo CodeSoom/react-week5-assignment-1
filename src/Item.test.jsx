c36e9672b34adb0a29dea77ed7b8dd94f21e9b26
PAPICO <ocipap0531@gmail.com>
2021-12-28 21:09:03 +0900

    Item selectedId 테스트 작성

--- src/Item.test.jsx
+++ src/Item.test.jsx
@@ -4,9 +4,12 @@ import Item from './Item';
 describe('Item', () => {
   const handleClick = jest.fn();
 
-  const renderComponent = () => render(<Item
-    item={{ name: 'test', id: 1 }}
-    selectedId={1}
+  const renderComponent = ({
+    item = { name: 'test', id: 1 },
+    selectedId = 1,
+  } = {}) => render(<Item
+    item={item}
+    selectedId={selectedId}
     onClick={handleClick}
   />);
 
@@ -18,6 +21,12 @@ describe('Item', () => {
     const { container } = renderComponent();
 
     expect(container).not.toBeNull();
+  });
+
+  it('selectedId 와 item 의 id 가 동일하면 "(V)" 가 표시된다.', () => {
+    const { container } = renderComponent({ item: { id: 1, name: 'test' }, selectedId: 1 });
+
+    expect(container).toHaveTextContent(/(V)/);
   });
 
   it('버튼 클릭 시, handleClick 이 실행된다.', () => {
