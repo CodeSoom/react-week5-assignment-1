a2c50ce67cd606c0c20ffec084b29d474db9efd2
PAPICO <ocipap0531@gmail.com>
2021-12-28 18:40:59 +0900

    카테고리 목록 출력 테스트 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,16 +2,25 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
+  const renderApp = () => render(<App />);
+
   it('App 렌더링', () => {
-    const { container } = render(<App />);
+    const { container } = renderApp();
 
     expect(container).not.toBeNull();
   });
 
   it('렌더링 시, 지역 리스트가 노출 된다.', () => {
-    const { container } = render(<App />);
+    const { container } = renderApp();
 
     expect(container).toHaveTextContent('서울');
     expect(container).toHaveTextContent('대전');
+  });
+
+  it('렌더링 시, 카테고리 리스트가 노출된다.', () => {
+    const { container } = renderApp();
+
+    expect(container).toHaveTextContent('한식');
+    expect(container).toHaveTextContent('중식');
   });
 });
