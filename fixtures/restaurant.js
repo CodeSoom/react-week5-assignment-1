0fc3fa608a4249bfe2a21a1631cdc5039dce1205
wallababa <ansrlansrl@naver.com>
2021-12-28 23:30:15 +0900

    fixture 업데이트

--- fixtures/restaurant.js
+++ fixtures/restaurant.js
@@ -1,6 +1,6 @@
-export const regions = [{ id: 1, name: 'foo' }];
+export const regions = [{ id: 1, name: 'foo' }, { id: 2, name: 'bar' }];
 
-export const categories = [{ id: 1, name: 'foo' }];
+export const categories = [{ id: 1, name: 'foo' }, { id: 2, name: 'bar' }];
 
 export const restaurants = [{
   id: 1, categoryId: 1, name: 'foo', address: 'bar', information: 'baz',
