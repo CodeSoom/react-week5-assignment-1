3deeae8859a0c2fb222d881196860547d514a5a7
Ravi <ravi@inflearn.com>
2021-12-28 22:47:13 +0900

    category options 렌더링 테스트 코드

--- src/components/SearchFilterContainer/index.test.jsx
+++ src/components/SearchFilterContainer/index.test.jsx
@@ -7,10 +7,18 @@ jest.mock('react-redux');
 
 describe('SearchFilterContainer', () => {
   it('renders region options', () => {
-    useSelector.mockImplementation((selector) => selector({ regions: ['서울'] }));
+    useSelector.mockImplementation((selector) => selector({ regions: ['서울'], categories: [] }));
     const { getByLabelText, getByRole } = render(<SearchFilterContainer />);
 
     expect(getByLabelText('지역')).toBeInTheDocument();
     expect(getByRole('option', { value: '서울' })).toBeInTheDocument();
+  });
+
+  it('renders category options', () => {
+    useSelector.mockImplementation((selector) => selector({ regions: [], categories: ['한식'] }));
+    const { getByLabelText, getByRole } = render(<SearchFilterContainer />);
+
+    expect(getByLabelText('분류')).toBeInTheDocument();
+    expect(getByRole('option', { value: '한식' })).toBeInTheDocument();
   });
 });
