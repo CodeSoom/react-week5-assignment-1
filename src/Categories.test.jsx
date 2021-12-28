cb2c0dcdf2dffa1630d2d31351e47be2bd9928e2
emewjin <emewjin@gmail.com>
2021-12-28 17:09:12 +0900

    카테고리가 있을 때 없을 때 테스트 문맥 추가

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -7,23 +7,28 @@ import { CATEGORIES } from './fixtures';
 describe('Categories', () => {
   const renderComponent = (categories) =>
     render(<Categories categories={categories} />);
+
   it('render', () => {
     const { container } = renderComponent(CATEGORIES);
 
     expect(container).toHaveTextContent('한식');
   });
 
-  it('카테고리를 클릭하면 V표시된다', () => {
-    const { container, getByRole } = renderComponent(CATEGORIES);
+  context('카테고리가 있을 때', () => {
+    it('카테고리를 클릭하면 V표시된다', () => {
+      const { container, getByRole } = renderComponent(CATEGORIES);
 
-    const button = getByRole('button', { name: '한식' });
-    fireEvent.click(button);
-    expect(container).toHaveTextContent('한식(V)');
+      const button = getByRole('button', { name: '한식' });
+      fireEvent.click(button);
+      expect(container).toHaveTextContent('한식(V)');
+    });
   });
 
-  it('카테고리가 없으면 헬퍼메세지가 출력된다', () => {
-    const { container } = renderComponent([]);
+  context('카테고리가 없을 때', () => {
+    it('헬퍼메세지가 출력된다', () => {
+      const { container } = renderComponent([]);
 
-    expect(container).toHaveTextContent('카테고리가 존재하지 않습니다');
+      expect(container).toHaveTextContent('카테고리가 존재하지 않습니다');
+    });
   });
 });
