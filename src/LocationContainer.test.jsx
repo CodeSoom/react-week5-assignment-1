03fec3003e690809606fa3d8fb0d615eef694685
jeongseop.lee <sjl0614@naver.com>
2021-12-28 21:12:51 +0900

    LocationContainer.test 리덕스테스트로 수정

--- src/LocationContainer.test.jsx
+++ src/LocationContainer.test.jsx
@@ -1,8 +1,18 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import LocationContainer from './LocationContainer';
 
+import locations from '../fixtures/locations';
+
+jest.mock('react-redux');
+
 describe('LocationContainer', () => {
+  useSelector.mockImplementation((selector) => selector({
+    locations,
+  }));
+
   it('renders location list', () => {
     const { getByText } = render((
       <LocationContainer />
