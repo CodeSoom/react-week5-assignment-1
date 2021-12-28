ab649584404817cd63e8f848adb474b70221073d
Ravi <ravi@inflearn.com>
2021-12-28 23:41:04 +0900

    options 초기화 테스트 코드

--- src/components/SearchFilterContainer/index.test.jsx
+++ src/components/SearchFilterContainer/index.test.jsx
@@ -1,13 +1,20 @@
 import { render } from '@testing-library/react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import SearchFilterContainer from '.';
 
 jest.mock('react-redux');
 
 describe('SearchFilterContainer', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
   it('renders region options', () => {
-    useSelector.mockImplementation((selector) => selector({ regions: ['서울'], categories: [] }));
+    useSelector.mockImplementation((selector) => selector({ regions: [{ id: 1, name: '서울' }], categories: [] }));
     const { getByLabelText, getByRole } = render(<SearchFilterContainer />);
 
     expect(getByLabelText('지역')).toBeInTheDocument();
@@ -15,10 +22,17 @@ describe('SearchFilterContainer', () => {
   });
 
   it('renders category options', () => {
-    useSelector.mockImplementation((selector) => selector({ regions: [], categories: ['한식'] }));
+    useSelector.mockImplementation((selector) => selector({ regions: [], categories: [{ id: 1, name: '한식' }] }));
     const { getByLabelText, getByRole } = render(<SearchFilterContainer />);
 
     expect(getByLabelText('분류')).toBeInTheDocument();
     expect(getByRole('option', { value: '한식' })).toBeInTheDocument();
+  });
+
+  context('when the component did mounted', () => {
+    it('initialize options', () => {
+      render(<SearchFilterContainer />);
+      expect(dispatch).toBeCalled();
+    });
   });
 });
