be7d61bf5a8939092a6bf1f4ffcdce051d5a8c6a
emewjin <emewjin@gmail.com>
2021-12-28 16:58:57 +0900

    카테고리가 없을 때 테스트를 추가한다

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -5,18 +5,25 @@ import Categories from './Categories';
 import { CATEGORIES } from './fixtures';
 
 describe('Categories', () => {
-  const renderComponent = () => render(<Categories categories={CATEGORIES} />);
+  const renderComponent = (categories) =>
+    render(<Categories categories={categories} />);
   it('render', () => {
-    const { container } = renderComponent();
+    const { container } = renderComponent(CATEGORIES);
 
     expect(container).toHaveTextContent('한식');
   });
 
   it('카테고리를 클릭하면 V표시된다', () => {
-    const { container, getByRole } = renderComponent();
+    const { container, getByRole } = renderComponent(CATEGORIES);
 
     const button = getByRole('button', { name: '한식' });
     fireEvent.click(button);
     expect(container).toHaveTextContent('한식(V)');
+  });
+
+  it('카테고리가 없으면 헬퍼메세지가 출력된다', () => {
+    const { container } = renderComponent([]);
+
+    expect(container).toHaveTextContent('카테고리가 존재하지 않습니다');
   });
 });
