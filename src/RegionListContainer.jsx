e9d16d85b106b51620b0b6efaffb13ed98f21967
PAPICO <ocipap0531@gmail.com>
2021-12-28 20:02:39 +0900

    지역 렌더링 중앙 상태로 변경 구현

--- src/RegionListContainer.jsx
+++ src/RegionListContainer.jsx
@@ -1,17 +1,7 @@
+import { useSelector } from 'react-redux';
+
 export default function RegionListContainer() {
-  const regions = [
-    {
-      id: 'eaebe552-ae57-4ac8-8c3c-1cb3d9dd9e54',
-      name: '서울',
-    },
-    {
-      id: '2ae6620b-e902-4fc4-ac6d-f4a733acf2db',
-      name: '대전',
-    },
-    {
-      id: 'dbe6172f-efb9-4e00-8cbc-21cbb5f2eaab',
-      name: '대구',
-    }];
+  const { regions } = useSelector((state) => ({ regions: state.regions }));
 
   return (
     <ul>
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,4 +1,54 @@
-function reducer(state, action) {
+const initialState = {
+  regions:
+    [
+      {
+        id: 1,
+        name: '서울',
+      },
+      {
+        id: 2,
+        name: '대전',
+      },
+      {
+        id: 3,
+        name: '대구',
+      },
+      {
+        id: 4,
+        name: '부산',
+      },
+      {
+        id: 5,
+        name: '광주',
+      },
+      {
+        id: 6,
+        name: '강원도',
+      },
+      {
+        id: 7,
+        name: '인천',
+      },
+      {
+        id: 8,
+        name: '제주',
+      },
+      {
+        id: 9,
+        name: '전주',
+      },
+      {
+        id: 10,
+        name: '순천',
+      },
+      {
+        id: 11,
+        name: '독도',
+      },
+    ],
+};
+
+function reducer(state = initialState, action) {
   return state;
 }
 
