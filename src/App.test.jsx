444b3371687a9f481201545ffcdac77b52c1aff5
PAPICO <ocipap0531@gmail.com>
2021-12-28 20:05:02 +0900

    카테고리 리스트 중앙 상태로 변경 테스트 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,6 +9,8 @@ describe('App', () => {
   const renderApp = () => render(<App />);
 
   beforeEach(() => {
+    jest.clearAllMocks();
+
     useSelector.mockImplementation((selector) => selector({
       regions: [
         {
@@ -54,6 +56,48 @@ describe('App', () => {
         {
           id: 11,
           name: '독도',
+        },
+      ],
+      categories: [
+        {
+          id: 1,
+          name: '한식',
+        },
+        {
+          id: 2,
+          name: '중식',
+        },
+        {
+          id: 3,
+          name: '일식',
+        },
+        {
+          id: 4,
+          name: '양식',
+        },
+        {
+          id: 5,
+          name: '분식',
+        },
+        {
+          id: 6,
+          name: '과자',
+        },
+        {
+          id: 7,
+          name: '치킨',
+        },
+        {
+          id: 1003,
+          name: '테스트',
+        },
+        {
+          id: 1034,
+          name: '음료',
+        },
+        {
+          id: 1036,
+          name: '사탕',
         },
       ],
     }));
--- src/CategoryListContainer.test.jsx
+++ src/CategoryListContainer.test.jsx
@@ -1,8 +1,61 @@
 import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
+
 import CategoryListContainer from './CategoryListContainer';
+
+jest.mock('react-redux');
 
 describe('CategoryListContainer', () => {
   const renderComponent = () => render(<CategoryListContainer />);
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    useSelector.mockImplementation((selector) => selector({
+      categories: [
+        {
+          id: 1,
+          name: '한식',
+        },
+        {
+          id: 2,
+          name: '중식',
+        },
+        {
+          id: 3,
+          name: '일식',
+        },
+        {
+          id: 4,
+          name: '양식',
+        },
+        {
+          id: 5,
+          name: '분식',
+        },
+        {
+          id: 6,
+          name: '과자',
+        },
+        {
+          id: 7,
+          name: '치킨',
+        },
+        {
+          id: 1003,
+          name: '테스트',
+        },
+        {
+          id: 1034,
+          name: '음료',
+        },
+        {
+          id: 1036,
+          name: '사탕',
+        },
+      ],
+    }));
+  });
 
   it('CategoryListContainer 렌더링', () => {
     const { container } = renderComponent();
