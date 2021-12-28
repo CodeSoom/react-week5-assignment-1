102c8da768e1a2484ee9c4e6531cab4150ed7850
emewjin <emewjin@gmail.com>
2021-12-28 18:59:21 +0900

    리듀서 테스트 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,6 +1,6 @@
 import reducer from './reducer';
 
-import { setCategories, setRegions } from './actions';
+import { setCategories, setRegions, setRegion, setCategory } from './actions';
 import { CATEGORIES, REGIONS } from './fixtures';
 
 describe('reducer', () => {
@@ -25,6 +25,30 @@ describe('reducer', () => {
       const { regions } = reducer(previousSate, setRegions(REGIONS));
 
       expect(regions.length).toBe(2);
+    });
+  });
+
+  describe('setCategory', () => {
+    const previousSate = {
+      category: '',
+    };
+
+    it('선택된 카테고리를 저장한다', () => {
+      const { category } = reducer(previousSate, setCategory('양식'));
+
+      expect(category).toBe('양식');
+    });
+  });
+
+  describe('setRegion', () => {
+    const previousSate = {
+      region: '',
+    };
+
+    it('선택된 지역을 저장한다', () => {
+      const { region } = reducer(previousSate, setRegion('서울'));
+
+      expect(region).toBe('서울');
     });
   });
 });
