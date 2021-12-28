ef47bdeddf9d193e8a710cf479eaa1bee3d50411
wallababa <ansrlansrl@naver.com>
2021-12-28 23:19:48 +0900

    ItemButtons items가 있는 경우 랜더링 테스트 작성

--- src/components/ItemButtons.test.jsx
+++ src/components/ItemButtons.test.jsx
@@ -1,10 +1,20 @@
 import { render } from '@testing-library/react';
 import ItemButtons from './ItemButtons';
+import { regions } from '../../fixtures/restaurant';
 
 describe('ItemButtons', () => {
   it('items를 전달하지 않으면 비어있습니다.', () => {
     const { queryAllByRole } = render(<ItemButtons />);
 
     expect(queryAllByRole('listitem')).toHaveLength(0);
+  });
+
+  it('items를 전달하면 버튼 리스트를 보여줍니다.', () => {
+    const { queryAllByRole, queryByRole } = render(<ItemButtons items={regions} />);
+
+    expect(queryAllByRole('listitem')).toHaveLength(regions.length);
+    regions.forEach(({ name }) => {
+      expect(queryByRole('button', { name })).not.toBeNull();
+    });
   });
 });
