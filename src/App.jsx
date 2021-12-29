5fc476b282e1e2cef7b2044579e593fdda4de3f5
jeongseop.lee <sjl0614@naver.com>
2021-12-29 20:55:16 +0900

    App locationsq 불러오기 테스트 통과

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
 import LocationContainer from './LocationContainer';
 
+import { setLocation } from './action';
+
+function loadLocations({ dispatch }) {
+  const locations = [];
+
+  dispatch(setLocation(locations));
+}
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    loadLocations({ dispatch });
+  }, []);
+
   return (
     <div>
       <h2>지역</h2>
