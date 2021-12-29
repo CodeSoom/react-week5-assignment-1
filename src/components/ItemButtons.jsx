4346e0f22825ed55e72a648254e873945917bade
wallababa <ansrlansrl@naver.com>
2021-12-29 21:32:41 +0900

    React를 import 하도록 수정

--- src/components/ItemButtons.jsx
+++ src/components/ItemButtons.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function ItemButtons({ items = [], onClick, selected }) {
   return (
     <ul>
--- src/pages/RestaurantPage.jsx
+++ src/pages/RestaurantPage.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import RestaurantCategoriesContainer from '../components/RestaurantCategoriesContainer';
 import RestaurantRegionsContainer from '../components/RestaurantRegionsContainer';
 
