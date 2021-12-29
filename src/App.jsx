8578bfe0f136fcccff9232afdd460864a2423b5b
leeok <milkteatea0628@gmail.com>
2021-12-30 01:58:37 +0900

    강의기반 비동기 학습

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Lists from './Lists';
+
+import {
+  loadRegions,
+  loadCategories,
+} from './actions';
+
 export default function App() {
+  const dispatch = useDispatch();
+  const { regions, categories } = useSelector((state) => ({
+    regions: state.regions,
+    categories: state.categories,
+  }));
+
+  useEffect(() => {
+    dispatch(loadRegions());
+    dispatch(loadCategories());
+  }, []);
+
   return (
     <div>
       <h1>Restaurants</h1>
-      <ul>
-        <li>
-          서울
-        </li>
-        <li>
-          <input type='button' value='대전' />
-        </li>
-        <li>
-          부산
-        </li>
-      </ul>
-      <ul>
-        <li>
-          한식
-        </li>
-        <li>
-          양식
-        </li>
-        <li>
-          분식
-        </li>
-      </ul>
+      <Lists items={regions} />
+      <Lists items={categories} />
     </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,9 +2,10 @@ import { render } from "@testing-library/react";
 
 import App from './App';
 
+jest.mock('react-redux');
+jest.mock('./services/api');
+
 describe('App', () => {
-  // 1. 지역목록을 보여준다!
-  // 2. 지역목록을 누를 수 있는 버튼으로 보여준다!
   test('show region button list', () => {
     const { getByText, getByDisplayValue } = render((
       <App />
@@ -13,5 +14,4 @@ describe('App', () => {
     expect(getByText(/대전/)).not.toBeNull();
     expect(getByDisplayValue('대전')).not.toBeNull();
   });
-  // 카테고리도 똑같으니까 나중에.
 });
