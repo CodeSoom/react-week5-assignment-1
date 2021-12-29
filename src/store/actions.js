312ab198863cb16b03284debc53f3e8244075eb9
PAPICO <ocipap0531@gmail.com>
2021-12-29 19:04:37 +0900

    reducer store 리스트 정보 업데이트 테스트 작성

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,7 @@
 export const CHANGE_REGION = 'changeRegion';
 export const CHANGE_CATEGORY = 'changeCategory';
+export const SET_REGIONS = 'setRegions';
+export const SET_CATEGORIES = 'setCategories';
 
 export const changeRegion = (regionName) => ({
   type: CHANGE_REGION,
@@ -12,5 +14,19 @@ export const changeCategory = (categoryId) => ({
   type: CHANGE_CATEGORY,
   payload: {
     categoryId,
+  },
+});
+
+export const setRegions = (regions) => ({
+  type: SET_REGIONS,
+  payload: {
+    regions,
+  },
+});
+
+export const setCategories = (categories) => ({
+  type: SET_CATEGORIES,
+  payload: {
+    categories,
   },
 });
--- src/store/reducer.test.js
+++ src/store/reducer.test.js
@@ -1,9 +1,11 @@
-import { changeCategory, changeRegion } from './actions';
+import {
+  changeCategory, changeRegion, setCategories, setRegions,
+} from './actions';
 import reducer from './reducer';
 
 describe('reducer', () => {
   describe('changeRegion', () => {
-    it('넘겨받은 regionName 으로 변경후, 새로운 state 를 반환한다. ', () => {
+    it('새로운 regionName 으로 변경후, 새로운 state 를 반환한다. ', () => {
       const prevState = {
         selected: {
           regionName: null,
@@ -23,7 +25,7 @@ describe('reducer', () => {
   });
 
   describe('changeCategory', () => {
-    it('넘겨받은 categoryId 으로 변경후, 새로운 state 를 반환한다. ', () => {
+    it('새로운 categoryId 로 변경후, 새로운 state 를 반환한다. ', () => {
       const prevState = {
         selected: {
           categoryId: null,
@@ -38,6 +40,42 @@ describe('reducer', () => {
         selected: {
           categoryId: 1,
         },
+      });
+    });
+  });
+
+  describe('setCategories', () => {
+    it('새로운 카테고리 리스트로 변경 후, 새로운 state 를 반환하다.', () => {
+      const prevState = {
+        categories: [],
+      };
+
+      const action = setCategories([{ name: '한식', id: 1 }]);
+
+      const state = reducer(prevState, action);
+
+      expect(state).toEqual({
+        categories: [
+          { name: '한식', id: 1 },
+        ],
+      });
+    });
+  });
+
+  describe('setRegions', () => {
+    it('새로운 지역 리스트로 변경 후, 새로운 state 를 반환한다.', () => {
+      const prevState = {
+        regions: [],
+      };
+
+      const action = setRegions([{ name: '서울', id: 1 }]);
+
+      const state = reducer(prevState, action);
+
+      expect(state).toEqual({
+        regions: [
+          { name: '서울', id: 1 },
+        ],
       });
     });
   });
