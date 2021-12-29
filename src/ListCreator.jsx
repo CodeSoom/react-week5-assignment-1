48566c14630da84b9e35bd9b78520773bc88d4a6
Superduper-India <sunyoung1542@gmail.com>
2021-12-29 23:30:24 +0900

    Update: ListCreator/ 클릭이벤트 핸들러 구현

--- src/ListCreator.jsx
+++ src/ListCreator.jsx
@@ -1,8 +1,11 @@
 // Presentational Components: 레스토랑 목록을 추가해준다.
-export default function ListCreator() {
+export default function ListCreator({ onClick }) {
   return (
     <div>
-      <button type="button">
+      <button
+        type="button"
+        onClick={onClick}
+      >
         등록
       </button>
     </div>
