104c48902c1256133468114c34b546bb5161f17d
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-29 19:26:25 +0900

    [test] Item 컴포넌트 테스트 추가

--- src/Item.test.jsx
+++ src/Item.test.jsx
@@ -1,67 +1,39 @@
 import { fireEvent, render } from '@testing-library/react';
-import { useDispatch } from 'react-redux';
 
 import Item from './Item';
 
-jest.mock('react-redux');
-
 describe('Item', () => {
-  const dispatch = jest.fn();
-  useDispatch.mockImplementation(() => dispatch);
+  const handleClick = jest.fn();
 
-  context('with region value', () => {
-    it('render "region" button', () => {
-      const region = {
-        id: 1,
-        name: '서울',
-      };
+  const renderComponent = ({ name, selected }) => render(
+    <Item
+      name={name}
+      selected={selected}
+      onClick={handleClick}
+    />,
+  );
 
-      const { getByRole } = render(
-        <Item value={region} />,
-      );
-
-      expect(getByRole('button', { name: region.name })).toBeInTheDocument();
-    });
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
-  context('with category value', () => {
-    it('render "category" button', () => {
-      const category = {
-        id: 1,
-        name: '한식',
-      };
+  it('renders button', () => {
+    const { getByRole } = renderComponent({ name: '서울', selected: false });
 
-      const { getByRole } = render(
-        <Item value={category} />,
-      );
-
-      expect(getByRole('button', { name: category.name })).toBeInTheDocument();
-    });
+    expect(getByRole('button', { name: '서울' })).toBeInTheDocument();
   });
 
-  context('when click "region" button', () => {
-    const value = {
-      id: 1,
-      name: '서울',
-    };
+  it('clicks button, calls handleClick hanlder', () => {
+    const { getByRole } = renderComponent({ name: '서울', selected: false });
 
-    // TODO: delete 주석
-    // Item 은 렌더링과 버튼을 클릭하는 역할이 있음
-    // => 렌더링은 name 렌더 및 selected 면 V 를 추가 해서 렌더
-    // => 클릭은 selectedRegion 을 변경함.
-    it('set selectedRegion', () => {
-      const { getByRole } = render(
-        <Item value={value} />,
-      );
+    fireEvent.click(getByRole('button', { name: '서울' }));
 
-      fireEvent.click(getByRole('button', { name: value.name }));
+    expect(handleClick).toBeCalledWith('서울');
+  });
 
-      expect(dispatch).toBeCalledWith({
-        type: 'setSelectedRegion',
-        payload: {
-          name: value.name,
-        },
-      });
-    });
+  it('selected button, render button with name and V', () => {
+    const { getByRole } = renderComponent({ name: '서울', selected: true });
+
+    expect(getByRole('button', { name: '서울 V' })).toBeInTheDocument();
   });
 });
