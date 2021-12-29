01c3b6849cb697e4c182f80826539209136ac966
wallababa <ansrlansrl@naver.com>
2021-12-29 21:57:04 +0900

    RestaurantsContainer 리스트 업데이트 테스트 작성 및 구현

--- src/components/RestaurantsContainer.jsx
+++ src/components/RestaurantsContainer.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadRestaurants } from '../actions/restaurant';
 import RestaurantList from './RestaurantList';
 
 export default function RestaurantContainer() {
-  const { restaurants } = useSelector((state) => state.restaurant);
+  const dispatch = useDispatch();
+  const {
+    restaurants,
+    selected: { regionName, categoryId },
+  } = useSelector((state) => state.restaurant);
+
+  useEffect(() => {
+    if (regionName && categoryId) {
+      dispatch(loadRestaurants({ regionName, categoryId }));
+    }
+  }, [regionName, categoryId]);
+
   return (
     <>
       <RestaurantList restaurants={restaurants} />
--- src/components/RestaurantsContainer.test.jsx
+++ src/components/RestaurantsContainer.test.jsx
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import RestaurantContainer from './RestaurantsContainer';
 import { restaurants } from '../../fixtures/restaurant';
 
@@ -7,10 +7,41 @@ jest.mock('react-redux');
 
 describe('RestaurantContainer', () => {
   it('restaurants가 존재한다면 레스토랑 리스트를 보여줍니다.', () => {
-    useSelector.mockImplementation(() => ({ restaurants }));
+    useSelector.mockImplementation(() => ({ restaurants, selected: {} }));
 
     const { queryAllByRole } = render(<RestaurantContainer />);
 
     expect(queryAllByRole('listitem')).toHaveLength(restaurants.length);
+  });
+
+  describe('선택한 지역과 분류에 따라 레스토랑 리스트를 업데이트 합니다.', () => {
+    it('지역과 분류가 모두 선택되었다면 레스토랑 리스트를 업데이트 합니다.', () => {
+      const mockDispatch = jest.fn();
+      useDispatch.mockImplementation(() => mockDispatch);
+      useSelector.mockImplementation(() => ({
+        selected: {
+          regionName: 'foo',
+          categoryId: 1,
+        },
+      }));
+
+      render(<RestaurantContainer />);
+
+      expect(mockDispatch).toBeCalledTimes(1);
+    });
+
+    it('지역, 분류 둘 중 하나만 선택되었다면 리스트를 업데이트 하지 않습니다.', () => {
+      const mockDispatch = jest.fn();
+      useDispatch.mockImplementation(() => mockDispatch);
+      useSelector.mockImplementation(() => ({
+        selected: {
+          regionName: 'foo',
+        },
+      }));
+
+      render(<RestaurantContainer />);
+
+      expect(mockDispatch).toBeCalledTimes(0);
+    });
   });
 });
