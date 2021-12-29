0236e01655d44e1e2d2eab76ae1fad3995b1f463
Superduper-India <sunyoung1542@gmail.com>
2021-12-29 16:53:51 +0900

    Update: App test/ 모킹테스트 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,18 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import App from './App';
+
+import restaurants from '../fixtures/fixture';
 
 jest.mock('react-redux');
 
 describe('App', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurants,
+  }));
+
   const renderApp = () => render((<App />));
 
   it('sets restaurants', () => {
