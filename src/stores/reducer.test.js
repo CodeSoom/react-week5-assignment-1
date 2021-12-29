383a97e3d4adb13dfff0cba605c957dd5dfff61d
emewjin <emewjin@gmail.com>
2021-12-29 19:27:17 +0900

    defaultReducer 테스트 추가

--- src/stores/reducer.test.js
+++ src/stores/reducer.test.js
@@ -1,7 +1,13 @@
 import reducer from './reducer';
 
-import { setCategories, setRegions, setRegion, setCategory } from './actions';
-import { CATEGORIES, REGIONS } from '../lib/fixtures';
+import {
+  setCategories,
+  setRegions,
+  setRegion,
+  setCategory,
+  setRestaurants,
+} from './actions';
+import { CATEGORIES, REGIONS, RESTAURANTS } from '../lib/fixtures';
 
 describe('reducer', () => {
   describe('setCategories', () => {
@@ -25,6 +31,21 @@ describe('reducer', () => {
       const { regions } = reducer(previousSate, setRegions(REGIONS));
 
       expect(regions.length).toBe(2);
+    });
+  });
+
+  describe('setRestaurants', () => {
+    const previousSate = {
+      restaurants: [],
+    };
+
+    it('식당 목록을 저장한다', () => {
+      const { restaurants } = reducer(
+        previousSate,
+        setRestaurants(RESTAURANTS)
+      );
+
+      expect(restaurants.length).toBe(1);
     });
   });
 
@@ -55,6 +76,36 @@ describe('reducer', () => {
       const { region } = reducer(previousSate, setRegion(REGIONS[0]));
 
       expect(region).toEqual(REGIONS[0]);
+    });
+  });
+
+  describe('defaultReducer', () => {
+    const initialState = {
+      categories: [],
+      regions: [],
+      restaurants: [],
+      category: {
+        id: 0,
+        name: '',
+      },
+      region: {
+        id: 0,
+        name: '',
+      },
+    };
+
+    context('state가 존재함', () => {
+      it('state를 반환한다', () => {
+        const { regions } = reducer({ regions: REGIONS }, { type: 'noAction' });
+        expect(regions).toEqual(REGIONS);
+      });
+    });
+
+    context('state가 없음', () => {
+      it('초기 state를 반환한다', () => {
+        const state = reducer(undefined, { type: 'noAction' });
+        expect(state).toEqual(initialState);
+      });
     });
   });
 });
