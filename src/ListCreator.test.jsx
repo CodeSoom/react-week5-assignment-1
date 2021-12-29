1981a8321118852d0985ef11449a8d27fedbda89
Superduper-India <sunyoung1542@gmail.com>
2021-12-30 00:53:03 +0900

    Update: ListCreator test/ 레스토랑 목록이 dom에 있다는 예상 테스트

--- src/ListCreator.test.jsx
+++ src/ListCreator.test.jsx
@@ -1,5 +1,4 @@
-// Presentational Components:
-// 레스토랑 목록을 추가해주는 것에 대한 테스트
+// Presentational Components: 레스토랑 목록을 추가해주는 것에 대한 테스트
 import { render, fireEvent } from '@testing-library/react';
 
 import ListCreator from './ListCreator';
@@ -7,10 +6,16 @@ import ListCreator from './ListCreator';
 jest.mock('react-redux');
 
 describe('ListCreator', () => {
+  const restaurant = {
+    name: '모토쿠라시',
+    category: '일식',
+    place: '서울시 송파구',
+  };
   const handleClick = jest.fn();
 
   const renderListCreator = () => render((
     <ListCreator
+      restaurant={restaurant}
       onClick={handleClick}
     />
   ));
@@ -23,5 +28,13 @@ describe('ListCreator', () => {
     fireEvent.click(getByText(/등록/));
 
     expect(handleClick).toBeCalled();
+  });
+
+  it('changes to call onChange handler with createRestaurant', () => {
+    const { getByDisplayValue } = renderListCreator();
+
+    expect(getByDisplayValue('모토쿠라시')).toBeInTheDocument();
+    expect(getByDisplayValue('일식')).toBeInTheDocument();
+    expect(getByDisplayValue('서울시 송파구')).toBeInTheDocument();
   });
 });
