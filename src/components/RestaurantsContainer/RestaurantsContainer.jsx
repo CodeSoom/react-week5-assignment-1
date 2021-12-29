5fd3b32b6a0a69cd5892fa2f9a398ebc11663043
padawanR0k <padawanR0k@gmail.com>
2021-12-30 01:23:10 +0900

    new: 레스토랑 목록 컨테이너 컴포넌트 구현

---
+++ src/components/RestaurantsContainer/RestaurantsContainer.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Restaurants from '../Restaurants/Restaurants';
+import { fetchRestaurants } from '../../reducer/actions';
+
+export default function RestaurantsContainer() {
+  const { selected, restaurants } = useSelector((store) => ({
+    selected: store.selected,
+    restaurants: store.restaurants,
+  }));
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (selected.location && selected.category) {
+      dispatch(fetchRestaurants(selected));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (selected.location && selected.category) {
+      dispatch(fetchRestaurants(selected));
+    }
+  }, [selected.location, selected.category]);
+
+  if (!(selected.location && selected.category)) {
+    return '지역과 카테고리를 선택해주세요';
+  }
+
+  if (!restaurants?.length) {
+    return '레스토랑이 존재하지 않습니다.';
+  }
+
+  return (<Restaurants restaurants={restaurants} />);
+}
