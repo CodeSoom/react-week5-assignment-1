cf3dc021a648730b85df71d6cb07c0048ab2afa5
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-29 17:01:18 +0900

    [refactor] Regions 컨테이너 Regions 컴포넌트 렌더하도록 수정

--- src/RegionsContainer.jsx
+++ src/RegionsContainer.jsx
@@ -1,15 +1,13 @@
 import { useSelector } from 'react-redux';
 
+import Regions from './Regions';
+
 export default function RegionsContainer() {
   const { regions } = useSelector((state) => state);
 
   return (
     <>
-      {regions.map((region) => (
-        <button type="button" key={region.id}>
-          {region.name}
-        </button>
-      ))}
+      <Regions regions={regions} />
     </>
   );
 }
