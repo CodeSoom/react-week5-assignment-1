a6803ca6fe3f4b4257903788a954e52eac5cf7c2
wallababa <ansrlansrl@naver.com>
2021-12-29 21:26:10 +0900

    RestaurantList 컴포넌트 테스트 작성

--- fixtures/restaurant.js
+++ fixtures/restaurant.js
@@ -2,6 +2,11 @@ export const regions = [{ id: 1, name: 'foo' }, { id: 2, name: 'bar' }];
 
 export const categories = [{ id: 1, name: 'foo' }, { id: 2, name: 'bar' }];
 
-export const restaurants = [{
-  id: 1, categoryId: 1, name: 'foo', address: 'bar', information: 'baz',
-}];
+export const restaurants = [
+  {
+    id: 1, categoryId: 1, name: 'foo', address: 'bar', information: 'baz',
+  },
+  {
+    id: 2, categoryId: 2, name: 'bar', address: 'baz', information: 'foo',
+  },
+];
---
+++ src/components/RestaurantList.test.jsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+import RestaurantList from './RestaurantList';
+import { restaurants } from '../../fixtures/restaurant';
+
+describe('RestaurantList', () => {
+  it('restaurants를 전달하지 않으면 비어있습니다.', () => {
+    const { queryAllByRole } = render(<RestaurantList />);
+
+    expect(queryAllByRole('listitem')).toHaveLength(0);
+  });
+
+  it('restaurants를 전달하면 레스토랑 리스트를 보여줍니다.', () => {
+    const { queryAllByRole, queryByRole } = render(<RestaurantList restaurants={restaurants} />);
+
+    expect(queryAllByRole('listitem')).toHaveLength(restaurants.length);
+    restaurants.forEach(({ name }) => {
+      expect(queryByRole('listitem', { name })).not.toBeNull();
+    });
+  });
+});
