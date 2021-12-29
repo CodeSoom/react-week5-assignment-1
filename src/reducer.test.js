5b85ff26806e269265c2485ae71ce988ee1c5f9d
leeok <milkteatea0628@gmail.com>
2021-12-30 01:58:53 +0900

    앞으로 테스트 코드 작성하기

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,3 +1,31 @@
+import reducer from './reducer';
+
+import {
+  setRegions,
+} from './actions';
+
 describe('reducer', () => {
-  describe('')
+  const initialRestaurant = {
+    name: '',
+    category: '',
+    address: '',
+  }
+
+  const initialState = {
+    newId: 100,
+    restaurants: [],
+    restaurant: initialRestaurant,
+    categories: [],
+    regions: [],
+  }
+
+  // 초기값으로 지역을 모두 셋팅한다.
+  describe('setRegions', () => {
+    it('set the all regions', () => {
+      const state = reducer(initialState, setRegions());
+      const length = state.regions.length;
+
+      expect(length).toBe(11);
+    });
+  });
 });
