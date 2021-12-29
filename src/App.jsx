dae69f7c5c09bd9264c2ab188bd812c454303542
yeji <lerry597@gmail.com>
2021-12-29 17:09:24 +0900

    [test] App 컴포넌트에서는 지역버튼들을 보여줄 수 있다 테스트 통과

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 export default function App() {
   return (
-    <></>
+    <>
+      <button type="button">서울</button>
+      <button type="button">대전</button>
+      <button type="button">대구</button>
+    </>
   );
 }
