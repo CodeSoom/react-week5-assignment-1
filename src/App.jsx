6a961d86644b4564693b758ff22e4be0c812bc64
Superduper-India <sunyoung1542@gmail.com>
2021-12-29 15:57:07 +0900

    Update: Restaurants/ 텍스트 리스트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
+import Restaurants from './Restaurants';
+
 export default function App() {
   return (
     <div>
       <h1>Restaurants</h1>
-      <ul>
-        <li>
-          레스토랑
-        </li>
-      </ul>
+      <Restaurants />
     </div>
   );
 }
---
+++ src/Restaurants.jsx
@@ -0,0 +1,9 @@
+export default function Restaurants() {
+  return (
+    <ul>
+      <li>
+        레스토랑
+      </li>
+    </ul>
+  );
+}
