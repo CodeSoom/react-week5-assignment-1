c7052c2f35a36e81f6a1703c7f5914163a1bc854
Superduper-India <sunyoung1542@gmail.com>
2021-12-29 16:01:13 +0900

    Update: Restaurants test/ restaurant를 컴포넌트에 넘겨주는 것 테스트

--- src/Restaurants.test.jsx
+++ src/Restaurants.test.jsx
@@ -5,7 +5,20 @@ import Restaurants from './Restaurants';
 jest.mock('react-redux');
 
 describe('Restaurants', () => {
-  const renderRestaurants = () => render((<Restaurants />));
+  const restaurant = [
+    {
+      id: 1,
+      name: '모토쿠라시',
+      category: '일식',
+      place: '서울시 송파구',
+    },
+  ];
+
+  const renderRestaurants = () => render((
+    <Restaurants
+      restaurant={restaurant}
+    />
+  ));
 
   it('sets restaurants', () => {
     const { container } = renderRestaurants();
