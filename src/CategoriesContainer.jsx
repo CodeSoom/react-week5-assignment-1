8acd20ac58ad5b2bade141454cf02caf97f1491a
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-29 17:02:34 +0900

    [refactor] Categories 컨테이너 Categories 컴포넌트 렌더하도록 수정

--- src/CategoriesContainer.jsx
+++ src/CategoriesContainer.jsx
@@ -1,17 +1,11 @@
 import { useSelector } from 'react-redux';
 
+import Categories from './Categories';
+
 export default function CategoriesContainer() {
   const { categories } = useSelector((state) => state);
 
   return (
-    <ul>
-      {categories.map((category) => (
-        <li key={category.id}>
-          <button type="button">
-            {category.name}
-          </button>
-        </li>
-      ))}
-    </ul>
+    <Categories categories={categories} />
   );
 }
