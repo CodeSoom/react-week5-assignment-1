5558d80a025eb85fdf0a0d16b59debf6ecbf440c
yeji <lerry597@gmail.com>
2021-12-29 17:40:39 +0900

    [test] LocalList에서는  지역명들을  보여준다. 테스트 작성

---
+++ src/LocalList.test.jsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+
+import LocalList from './LocalList';
+
+describe('LocalList', () => {
+  it('지역명(버튼)들을 보여준다.', () => {
+    const locals = [
+      { id: 1, name: '서울' },
+      { id: 2, name: '대전' },
+      { id: 2, name: '대구' },
+    ];
+
+    const { container } = render(
+      <LocalList locals={locals} />,
+    );
+
+    expect(container).toHaveTextContent('서울');
+    expect(container).toHaveTextContent('대전');
+    expect(container).toHaveTextContent('대구');
+  });
+});
