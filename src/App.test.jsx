6c4124338df46a858cc7705ce3cce687e2dbf308
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-29 17:22:21 +0900

    [test] App 컴포넌트 렌더 테스트 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,6 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
 
+import { categories } from '../__fixtures__/categories';
+import { regions } from '../__fixtures__/regions';
+
 jest.mock('react-redux');
 jest.mock('./services/api');
 
@@ -10,15 +13,30 @@ describe('App', () => {
   const dispatch = jest.fn();
   useDispatch.mockImplementation(() => dispatch);
   useSelector.mockImplementation((selector) => selector({
-    categories: [],
+    regions,
+    categories,
   }));
 
   context('when load', () => {
     it('set regions and categories', () => {
       render(<App />);
 
-      // expect(dispatch).toBeCalled();
+      // TODO: changes test, redux-thunk 테스트 형태로 변경 예정 with redux-mock-store
       expect(dispatch).toBeCalledTimes(2);
     });
+
+    it('render regions and categories', () => {
+      const { getByRole } = render(<App />);
+
+      regions.forEach((region) => {
+        expect(getByRole('button', { name: region.name })).toBeInTheDocument();
+      });
+
+      categories.forEach((category) => {
+        expect(getByRole('button', { name: category.name })).toBeInTheDocument();
+      });
+    });
+
+    // TODO: render restaurants
   });
 });
