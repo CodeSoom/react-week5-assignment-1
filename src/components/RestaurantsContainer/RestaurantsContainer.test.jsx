3aad788a11cb41a538ba5c45d773468aa8698430
padawanR0k <padawanR0k@gmail.com>
2021-12-30 01:23:45 +0900

    up: 레스토랑 목록 컨테이너 테스트 코드 개선

--- src/components/RestaurantsContainer/RestaurantsContainer.test.jsx
+++ src/components/RestaurantsContainer/RestaurantsContainer.test.jsx
@@ -1,8 +1,7 @@
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantsContainer from './RestaurantsContainer';
-import { setRestaurants } from '../../reducer/actions';
 
 jest.mock('react-redux');
 jest.mock('../../services/api');
@@ -25,7 +24,7 @@ describe('RestaurantsContainer', () => {
       }));
 
       const { getByText } = render(<RestaurantsContainer />);
-      expect(getByText('지역과 카테고리를 선택해주세요')).not.toBeNull();
+      expect(getByText(/지역과 카테고리를 선택해주세요/)).not.toBeNull();
     });
 
     it('선택되어있는 경우, 레스토랑 목록을 불러오기위해 dispatch한다.', () => {
@@ -40,10 +39,40 @@ describe('RestaurantsContainer', () => {
 
       render(<RestaurantsContainer />);
 
-      expect(dispatch).toBeCalledWith(setRestaurants({ location, category }));
+      expect(dispatch).toBeCalled();
     });
 
     it('변경된 경우, 레스토랑 목록을 불러오기위해 dispatch한다.', () => {
+      const location = { id: 1, name: '서울' };
+      const category = { id: 1, name: '한식' };
+      useSelector.mockImplementation((selector) => selector({
+        restaurants: [
+          {
+            id: 1, categoryId: 1, name: '양천주가', address: '서울 강남구 123456', information: '양천주가 in 서울 강남구 123456',
+          },
+          {
+            id: 6, categoryId: 1, name: '한국식 초밥', address: '서울 강남구', information: '한국식 초밥 in 서울 강남구',
+          },
+        ],
+        locations: [
+          { id: 1, name: '서울' },
+          { id: 2, name: '대전' },
+        ],
+        selected: {
+          location,
+          category,
+        },
+      }));
+
+      render(<RestaurantsContainer />);
+      // TODO: 선택된 항목을 변경햇을때, dispatch가 작동하는지 확인하는 테스트 추가
+
+      expect(dispatch).toBeCalled();
+    });
+  });
+
+  context('레스토랑 목록이', () => {
+    it('존재하지 않는 경우, "레스토랑이 존재하지 않습니다."를 노출한다.', async () => {
       const location = { id: 1, name: '서울' };
       const category = { id: 1, name: '한식' };
       useSelector.mockImplementation((selector) => selector({
@@ -54,10 +83,7 @@ describe('RestaurantsContainer', () => {
       }));
 
       const { getByText } = render(<RestaurantsContainer />);
-
-      fireEvent.click(getByText('대전'));
-
-      expect(dispatch).toBeCalledWith(setRestaurants({ id: 2, name: '대전' }, category));
+      expect(getByText(/레스토랑이 존재하지 않습니다./)).not.toBeNull();
     });
   });
 });
