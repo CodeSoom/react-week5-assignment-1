c4e9e4c7ec450a3756e47815a8cba6d839387e7b
PAPICO <ocipap0531@gmail.com>
2021-12-29 20:33:04 +0900

    RestaurantListContainer restaurant 가 비어있는 경우 예외처리

--- src/RestaurantListContainer.jsx
+++ src/RestaurantListContainer.jsx
@@ -4,6 +4,11 @@ export default function RestaurantListContainer() {
   const { restaurants } = useSelector((state) => ({
     restaurants: state.restaurants,
   }));
+
+  if (restaurants.length === 0) {
+    return <></>;
+  }
+
   return (
     <ul>
       {restaurants.map((restaurant) => <li key={restaurant.id}>{restaurant.name}</li>)}
