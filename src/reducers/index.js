34d9e66db87ae55b30957c93e627b1184067af5b
CheolWoong Choi <bluesky4381@naver.com>
2021-12-29 21:04:37 +0900

    reducer 지역, 카테고리, 식당 set~ 액션에 대한 테스트 코드 & 구현

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,36 @@
 const initialState = {
+  regions: [],
+  categories: [],
+  restaurants: [],
 };
 
 export default function reducer(state = initialState, action) {
+  if (action.type === 'setRegions') {
+    const { regions } = action.payload;
+
+    return {
+      ...state,
+      regions,
+    };
+  }
+
+  if (action.type === 'setCategories') {
+    const { categories } = action.payload;
+
+    return {
+      ...state,
+      categories,
+    };
+  }
+
+  if (action.type === 'setRestaurants') {
+    const { restaurants } = action.payload;
+
+    return {
+      ...state,
+      restaurants,
+    };
+  }
+
   return state;
 }
