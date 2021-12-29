288fd5429f5c6b47b13d4179347dc1d38bfd68e7
Superduper-India <sunyoung1542@gmail.com>
2021-12-30 00:57:44 +0900

    Update: ListCreator test/ 레스토랑 목록이 dom에 있다는 예상 구현

--- src/ListCreator.jsx
+++ src/ListCreator.jsx
@@ -1,7 +1,25 @@
 // Presentational Components: 레스토랑 목록을 추가해준다.
-export default function ListCreator({ onClick }) {
+export default function ListCreator({ restaurant, onClick }) {
   return (
     <div>
+      <input
+        value={restaurant.name}
+        name="name"
+        type="text"
+        placeholder="이름"
+      />
+      <input
+        value={restaurant.category}
+        name="category"
+        type="text"
+        placeholder="분류"
+      />
+      <input
+        value={restaurant.place}
+        name="place"
+        type="text"
+        placeholder="주소"
+      />
       <button type="button" onClick={onClick}>
         등록
       </button>
