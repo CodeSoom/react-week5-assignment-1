9038ccc72ed608b41a58630966de48427965cd73
jeongseop.lee <sjl0614@naver.com>
2021-12-29 20:31:49 +0900

    App 지역 초기값 불러오는 테스트 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,15 @@
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
 
 jest.mock('react-redux');
 
 describe('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
   useSelector.mockImplementation((selector) => selector({
     locations: [],
   }));
@@ -15,6 +18,13 @@ describe('App', () => {
     const { queryByText } = render((
       <App />
     ));
+
+    expect(dispatch).toBeCalledWith({
+      type: 'setLocation',
+      payload: {
+        location: [],
+      },
+    });
 
     expect(queryByText('지역')).not.toBeNull();
   });
