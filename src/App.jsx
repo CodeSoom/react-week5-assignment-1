10045118436a5ad748cf7e6731dc6803276a362b
AutowiniFront <autowinifront@naver.com>
2021-12-29 17:58:54 +0900

    App 구현: 레스토랑 지역 목록을 setRegions 액션을 통해 전역으로 저장
    dispatch로 setRegions 액션을 전달한다

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import regions from '../fixtures/regions';
+import { setRegions } from './actions';
+
 import RegionsContainer from './RegionsContainer';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setRegions({ regions }));
+  }, []);
+
   return (
     <>
       <RegionsContainer />
