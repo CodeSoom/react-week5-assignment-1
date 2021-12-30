a6246a91df00d4123d5b3cebf555cb28284cfdb2
padawanR0k <padawanR0k@gmail.com>
2021-12-30 19:11:50 +0900

    up: App 컴포넌트에 식당목록 컨테이너 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import CategoriesContainer from './components/CategoriesContainer/CategoriesContainer';
 import LocationsContainer from './components/LocationsContainer/LocationsContainer';
+import RestaurantsContainer from './components/RestaurantsContainer/RestaurantsContainer';
 
 export default function App() {
   return (
     <main>
       <LocationsContainer />
       <CategoriesContainer />
+      <RestaurantsContainer />
     </main>
   );
 }
