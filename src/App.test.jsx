79deae289653e2a9068a67074aede4f0aab5c550
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-30 15:43:34 +0900

    [테스트] App -1 - render 점검

---
+++ src/App.test.jsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+test('App', () => {
+  const { getByText } = render(<App />);
+
+  expect(getByText('서울')).not.toBeNull();
+  expect(getByText('한식')).not.toBeNull();
+  expect(getByText('양천주가')).not.toBeNull();
+});
