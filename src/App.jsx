a8423dae9a367089d620d317777276cb2129f964
CheolWoong Choi <bluesky4381@naver.com>
2021-12-31 00:05:12 +0900

    loadRegions 액션 추가 & App 컴포넌트에 Regions Container 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useEffect } from 'react';
-// import { useSelector } from 'react-redux';
-import { fetchCategories, fetchRegions, fetchRestaurants } from './services/api';
+import RegionsContainer from './containers/RegionsContainer';
+import CategoriesContainer from './containers/CategoriesContainer';
+import RestaurantsContainer from './containers/RestaurantsContainer';
 
 export default function App() {
-  useEffect(() => {
-    fetchCategories();
-    fetchRegions();
-    fetchRestaurants('서울', 1);
-  }, []);
-
   return (
     <div>
-      지역과 카테고리를 선택하세요.
+      <RegionsContainer />
+      {/* <CategoriesContainer /> */}
+      {/* <RestaurantsContainer /> */}
     </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,32 @@
 import { render } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
 
+jest.mock('react-redux');
+
 describe('App', () => {
+  const dispatch = jest.fn();
+
   const renderApp = () => render(<App />);
 
-  it('아무 정보가 없으면, 안내 메시지를 보여줘야 한다.', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      regions: {
+        regions: [],
+        activeId: -1,
+      },
+      categories: {
+        categories: [],
+        activeId: -1,
+      },
+    }));
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
+  it('지역 정보가 없으면, 사용자에게 안내 메시지를 보여준다.', () => {
     const { getByText } = renderApp();
 
-    expect(getByText(/지역과 카테고리를 선택하세요./)).toBeInTheDocument();
+    expect(getByText(/지역 정보를 가져오세요./)).toBeInTheDocument();
   });
 });
