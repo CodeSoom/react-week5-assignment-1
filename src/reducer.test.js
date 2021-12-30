769df1712ae7f9239c05906787cb2098049c80f5
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-30 18:14:38 +0900

    [테스트] reducer-2 (버튼 선택기능)

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,11 +1,17 @@
-import { loadCategories, loadRegions } from './actions';
+import {
+  checkCategory, checkRegion, loadCategories, loadRegions,
+} from './actions';
 import reducer from './reducer';
 
 test('reducer', () => {
   const state = {
     categories: [],
     regions: [],
+    checkedCategory: undefined,
+    checkedRegion: undefined,
   };
   expect(reducer(state, loadCategories()).categories).toHaveLength(1);
   expect(reducer(state, loadRegions()).regions).toHaveLength(1);
+  expect(reducer(state, checkCategory(1)).checkedCategory).toBe(1);
+  expect(reducer(state, checkRegion(1)).checkedRegion).toBe(1);
 });
