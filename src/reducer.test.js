cde3fd2d198e312eff4c169993be6dec836c6065
Superduper-India <sunyoung1542@gmail.com>
2021-12-30 13:45:48 +0900

    Update: reducer test/ 필드에 입력하면 name, value값이 바뀌는 테스트

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -8,7 +8,6 @@ import {
   setRestaurants,
   addRestaurant,
   putRestaurantField,
-
 } from './action';
 
 describe('reducer', () => {
@@ -21,7 +20,7 @@ describe('reducer', () => {
     restaurant: {},
   }));
 
-  context('with setRestaurants', () => { // 테스트이름을 액션명으로 해도 되는지?
+  context('with setRestaurants action', () => { // 테스트이름을 액션명으로 해도 되는지?
     it('updates state with restaurants', () => {
       const initialState = {
         restaurants: [],
@@ -33,7 +32,7 @@ describe('reducer', () => {
     });
   });
 
-  context('without setRestaurants', () => {
+  context('without setRestaurants action', () => {
     it('changes nothing', () => {
       const initialState = {
         restaurants: [],
@@ -45,8 +44,8 @@ describe('reducer', () => {
     });
   });
 
-  context('with addRestaurant', () => {
-    it('appends restaurant into restaurants and clears', () => {
+  context('with addRestaurant action', () => {
+    it('appends restaurant into restaurants and clears input field', () => {
       const initialState = {
         newId: 101,
         restaurants: [],
@@ -62,6 +61,26 @@ describe('reducer', () => {
       expect(state.restaurants).toHaveLength(1);
       expect(state.restaurant.name).toBe('');
       expect(state.restaurants[0].id).toBe(101);
+    });
+  });
+
+  context('with putRestaurantField action', () => {
+    it('changes', () => {
+      const initialState = {
+        restaurants: [],
+        restaurant: {
+          name: '초기값',
+          category: '초기값2',
+          place: '초기값3',
+        },
+      };
+
+      const state = reducer(initialState, putRestaurantField({
+        name: 'name',
+        value: '왈랄라',
+      }));
+
+      expect(state.restaurant.name).toBe('왈랄라');
     });
   });
 });
