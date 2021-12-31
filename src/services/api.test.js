b74d99c6664d2957170c494e61e792b7161b4a8c
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-31 17:46:26 +0900

    [테스트] api 해당 링크가 제대로 되어있는지 점검 각 fetch함수 return 성공 여부

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -1,7 +1,37 @@
-import { fetchCategories, fetchRegions, fetchRestaurant } from './api';
+import { fetchRegions, fetchCategories, fetchRestaurant } from './api';
 
-test('', () => {
-  // expect(fetchCategories()).not.toBeNull()
-  // expect(fetchRegions()).not.toBeNull()
-  // expect(fetchRestaurant(1, 1)).not.toBeNull()
+describe('api', () => {
+  const regionUrl = 'https://eatgo-customer-api.ahastudio.com/regions';
+  const categoriesUrl = 'https://eatgo-customer-api.ahastudio.com/categories';
+  const restaurantuUrl = 'https://eatgo-customer-api.ahastudio.com/restaurants?region=1&category=1';
+
+  const fetch = global.fetch = jest.fn().mockResolvedValue({
+    async json() {
+      return true;
+    },
+  });
+
+  describe('fetchRegions', () => {
+    it('returns regions', async () => {
+      const result = await fetchRegions();
+      expect(result).toBe(true);
+      expect(fetch).toBeCalledWith(regionUrl);
+    });
+  });
+
+  describe('fetchCategories', () => {
+    it('returns regions', async () => {
+      const result = await fetchCategories();
+      expect(result).toBe(true);
+      expect(fetch).toBeCalledWith(categoriesUrl);
+    });
+  });
+
+  describe('fetchRestaurant', () => {
+    it('returns regions', async () => {
+      const result = await fetchRestaurant(1, 1);
+      expect(result).toBe(true);
+      expect(fetch).toBeCalledWith(restaurantuUrl);
+    });
+  });
 });
