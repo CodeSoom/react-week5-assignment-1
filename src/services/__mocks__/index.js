b051bf49838ec36dfbc8b3f3267a454c68234120
PAPICO <ocipap0531@gmail.com>
2022-01-01 22:37:55 +0900

    redux-mock-store 적용

--- src/services/__mocks__/index.js
+++
@@ -1,9 +0,0 @@
-export const fetchRegions = async () => [];
-
-export const fetchCategories = async () => [];
-
-export const fetchRestaurants = async ({ regionName, categoryId }) => {
-  const response = await fetch(`https://eatgo-customer-api.ahastudio.com/restaurants?region=${regionName}&category=${categoryId}`);
-
-  return response.json();
-};
--- src/store/asyncActions.test.js
+++ src/store/asyncActions.test.js
@@ -1,23 +1,61 @@
-import { loadRegions } from './asyncActions';
-import { setRegions } from './actions';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import { loadCategories, loadRegions, loadRestaurants } from './asyncActions';
+import { fetchCategories, fetchRegions, fetchRestaurants } from '../services';
+
+import { CATEGORIES, REGIONS, RESTAURANTS } from '../fixture';
+import { setCategories, setRegions, setRestaurants } from './actions';
+
+const mockStore = configureStore([thunk]);
 
 jest.mock('../services/index.js');
 
 describe('asyncActions', () => {
-  const dispatch = jest.fn();
-
+  let store;
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  describe('loadRegions', () => {
-    it('loadRegions 는 api 통신 후 setRegions 를 dispatch 한다.', () => {
-      const callback = loadRegions();
+  beforeEach(() => {
+    fetchRegions.mockResolvedValue(REGIONS);
+    fetchCategories.mockResolvedValue(CATEGORIES);
+    fetchRestaurants.mockResolvedValue(RESTAURANTS);
+  });
 
-      callback.call(this, dispatch);
-      expect(dispatch).toBeCalled();
-      // TODO test 실패
-      expect(dispatch).toBeCalledWith(setRegions([])); // 테스트 실패 ㅠ
+  describe('loadRegions', () => {
+    it('loadRegions 는 api 통신 후 setRegions 를 dispatch 한다.', async () => {
+      store = mockStore({});
+
+      await store.dispatch(loadRegions());
+
+      const actions = store.getActions();
+
+      expect(actions[0]).toEqual(setRegions(REGIONS));
+    });
+  });
+
+  describe('loadCategories', () => {
+    it('loadCategories 는 api 통신 후 setCategories 를 dispatch 한다.', async () => {
+      store = mockStore({});
+
+      await store.dispatch(loadCategories());
+
+      const actions = store.getActions();
+
+      expect(actions[0]).toEqual(setCategories(CATEGORIES));
+    });
+  });
+
+  describe('loadRestaurants', () => {
+    it('loadRestaurants 는 api 통신 후 setRegions 를 dispatch 한다.', async () => {
+      store = mockStore({ selected: { regionName: '서울', categoryId: 1 } });
+
+      await store.dispatch(loadRestaurants());
+
+      const actions = store.getActions();
+
+      expect(actions[0]).toEqual(setRestaurants(RESTAURANTS));
     });
   });
 });
