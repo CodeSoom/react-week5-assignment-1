f99662752dc22e6437f6eb6c366a4d129f1bb43b
Lerry <lerry@Lerryui-MacBookPro.local>
2022-01-02 02:03:46 +0900

    [fix] 피드백 반영

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,9 +4,9 @@ import App from './App';
 
 describe('App', () => {
   it('지역명(버튼)들을 보여준다.', () => {
-    const { container } = render(
-      <App />,
-    );
+    const { container } = render((
+      <App />
+    ));
 
     expect(container).toHaveTextContent('서울');
     expect(container).toHaveTextContent('대전');
