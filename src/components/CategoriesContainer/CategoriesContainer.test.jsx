78e7ef52d991d22f02495b25938a619f0da669b7
padawanR0k <padawanR0k@gmail.com>
2022-01-01 16:37:53 +0900

    up: given2를 활용하여 중복된 테스트 코드 제거

--- src/components/CategoriesContainer/CategoriesContainer.test.jsx
+++ src/components/CategoriesContainer/CategoriesContainer.test.jsx
@@ -1,6 +1,7 @@
 import { fireEvent, render } from '@testing-library/react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import given from 'given2';
 import CategoriesContainer from './CategoriesContainer';
 import { setCategory } from '../../reducer/actions';
 import { categories } from '../../fixtures';
@@ -11,29 +12,35 @@ jest.mock('../../services/api');
 describe('CategoriesContainer', () => {
   const dispatch = jest.fn();
 
+  given('categories', () => []);
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   beforeEach(() => {
     dispatch.mockRestore();
     useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      categories: given.categories,
+    }));
   });
 
   it('컴포넌트 mount시, 음식종류들을 불러온다.', async () => {
-    useSelector.mockImplementation((selector) => selector({
-      categories: [],
-    }));
-
     render(<CategoriesContainer />);
+
     expect(dispatch).toBeCalled();
   });
 
-  it('음식 종류를 클릭하면 콜백함수에 클릭한 음식종류를 전달하여 실행한다.', () => {
-    useSelector.mockImplementation((selector) => selector({
-      categories,
-    }));
+  context('음식 카테고리 목록이 있을 때', () => {
+    given('categories', () => categories);
 
-    const { getByText } = render(<CategoriesContainer />);
+    it('카테고리를 클릭하면 콜백함수가 실행된다.', () => {
+      const { getByText } = render(<CategoriesContainer />);
 
-    fireEvent.click(getByText('한식'));
+      fireEvent.click(getByText('한식'));
 
-    expect(dispatch).toBeCalledWith(setCategory({ id: 1, name: '한식', selected: false }));
+      expect(dispatch).toBeCalledWith(setCategory({ id: 1, name: '한식', selected: false }));
+    });
   });
 });
