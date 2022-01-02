b16aeb027c387471fe63410e210e2734c404b614
AutowiniFront <autowinifront@naver.com>
2022-01-02 22:24:20 +0900

    레스토랑 목록 API 호출하여 초기값 세팅 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { loadCategories, loadRegions } from './actions';
+import { loadCategories, loadRegions, loadRestaurants } from './actions';
 
 import RegionsContainer from './RegionsContainer';
 import CategoriesContainer from './CategoriesContainer';
@@ -9,11 +9,15 @@ import RestaurantsContainer from './RestaurantsContainer';
 
 export default function App() {
   const dispatch = useDispatch();
+  const { region, category } = useSelector((state) => state);
 
   useEffect(() => {
     dispatch(loadRegions());
     dispatch(loadCategories());
-  }, []);
+    if (region !== '' && category !== '') {
+      dispatch(loadRestaurants({ region, category }));
+    }
+  }, [region, category]);
 
   return (
     <>
