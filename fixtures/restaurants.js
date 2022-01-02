0edde3de4f101336fe95937c1e644376b9376128
AutowiniFront <autowinifront@naver.com>
2022-01-02 20:41:50 +0900

    RestaurantsContainer가 store에서 restaurants를 받아오는 것에 대한 테스트 추가

---
+++ fixtures/restaurants.js
@@ -0,0 +1,11 @@
+const restaurants = [
+  {
+    id: 6,
+    categoryId: 1,
+    name: '한국식 초밥',
+    address: '서울 강남구',
+    information: '한국식 초밥 in 서울 강남구',
+  },
+];
+
+export default restaurants;
