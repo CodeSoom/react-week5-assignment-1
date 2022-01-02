1fb913c78f91cbb1900471f3330e1f4e0d6b1e91
AutowiniFront <autowinifront@naver.com>
2022-01-02 21:39:21 +0900

    지역 버튼 클릭 시 region 값 변경 구현

--- src/Regions.jsx
+++ src/Regions.jsx
@@ -1,10 +1,21 @@
+import { useDispatch } from 'react-redux';
+import { setRegion } from './actions';
+
 export default function Regions({ regions }) {
+  const dispatch = useDispatch();
+
+  const onClick = (e) => {
+    const region = e.target.value;
+
+    dispatch(setRegion({ region }));
+  };
+
   return (
     <ul>
       {
         regions.map(({ id, name }) => (
           <li key={id}>
-            <button type="button">{name}</button>
+            <button type="button" onClick={onClick} value={name}>{name}</button>
           </li>
         ))
       }
