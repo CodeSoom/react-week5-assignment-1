dff764c3cb161a1ea8bdb22e3312524c1e089cff
AutowiniFront <autowinifront@naver.com>
2022-01-02 19:30:48 +0900

    카테고리 목록 데이터를 변화시키는 action에 대한 테스트 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,18 +1,29 @@
-import reducer from './reducer';
-import { setRegions } from './actions';
+import reducer, { initialState } from './reducer';
+import { setRegions, setCategories } from './actions';
 
 import regions from '../fixtures/regions';
 
 describe('reducer', () => {
   describe('setRegions', () => {
     it('changes regions', () => {
-      const initialState = {
-        regions: [],
-      };
-
       const state = reducer(initialState, setRegions({ regions }));
 
       expect(state.regions).toHaveLength(11);
+    });
+  });
+
+  describe('setCategories', () => {
+    it('changes categories', () => {
+      const state = reducer(initialState, setCategories({
+        categories: [
+          {
+            id: 1,
+            name: '한식',
+          },
+        ],
+      }));
+
+      expect(state.categories).toHaveLength(1);
     });
   });
 });
