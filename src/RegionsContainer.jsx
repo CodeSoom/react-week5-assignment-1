8b258f2f08b1b4a36d5474dc1911d88e5a564633
Lerry <lerry@Lerryui-MacBookPro.local>
2022-01-03 00:13:59 +0900

    [feat] 지역 불러오고 선택하기 test - 구현

--- src/RegionsContainer.jsx
+++ src/RegionsContainer.jsx
@@ -10,7 +10,8 @@ import {
 
 export default function RegionsContainer() {
   const dispatch = useDispatch();
-  const regions = useSelector((state) => state.regions);
+
+  const { regions, selectedRegion } = useSelector((state) => state);
 
   function handleClick(regionId) {
     dispatch(selectRegion(regionId));
@@ -25,6 +26,7 @@ export default function RegionsContainer() {
             onClick={() => handleClick(region.id)}
           >
             {region.name}
+            {selectedRegion && region.id === selectedRegion.id ? '(V)' : null}
           </button>
         </li>
       ))}
