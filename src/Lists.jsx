55d7acb0b78975db347d8e21a72d66a70a9be6af
leeok <milkteatea0628@gmail.com>
2022-01-02 19:19:01 +0900

    Lists 예외처리?

--- src/Lists.jsx
+++ src/Lists.jsx
@@ -1,15 +1,27 @@
 import Button from './Button'
 
 export default function Lists({ items, onClick }) {
+  if ( items !== undefined && onClick !== undefined ) {
+    return (
+      <ul>
+      {items.map((item) => (
+        <li key={item.id}>
+          <Button id={item.id} onClick={onClick}>
+            {item.name}
+          </Button>
+        </li>
+      ))}
+      </ul>
+    );
+  }
+
   return (
     <ul>
     { items !== undefined ? items.map((item) => (
       <li key={item.id}>
-        <Button id={item.id} onClick={onClick}>
-          {item.name}
-        </Button>
+        {item.name}
       </li>
-    )) : ''}
+    )) : <li>Api 정보가 없습니다.</li> }
     </ul>
   );
 }
