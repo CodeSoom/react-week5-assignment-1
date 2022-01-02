d34bfb6a00b2e12215c97a744b57d54b068f3ebd
AutowiniFront <autowinifront@naver.com>
2022-01-02 16:43:54 +0900

    App 테스트: fetchRegions mocking

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,22 +3,20 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import App from './App';
 
-import regions from '../fixtures/regions';
-import { setRegions } from './actions';
-
 jest.mock('react-redux');
+jest.mock('./services/api');
 
 describe('App', () => {
   it('레스토랑 지역 목록 렌더링', () => {
     const dispatch = jest.fn();
 
-    useSelector.mockImplementation((selector) => selector({
-      regions,
-    }));
     useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      regions: [],
+    }));
 
     render(<App />);
 
-    expect(dispatch).toBeCalledWith(setRegions({ regions }));
+    expect(dispatch).toBeCalled();
   });
 });
