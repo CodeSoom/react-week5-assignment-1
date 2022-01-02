73adc72a5745f45ee4f5922473970579294fdbfa
leeok <milkteatea0628@gmail.com>
2022-01-02 19:18:49 +0900

    App 에서 기능 추출 후 두 컴포넌트를 SelectLists 로 통합

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,10 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-
-import Regions from './Regions';
-import Categories from './Categories';
-
-import {
-  loadRegions,
-  loadCategories,
-} from './actions';
+import SelectLists from "./SelectLists";
 
 export default function App() {
-  const dispatch = useDispatch();
-  const { regions, categories } = useSelector((state) => ({
-    regions: state.regions,
-    categories: state.categories,
-  }));
-
-  useEffect(() => {
-    dispatch(loadRegions());
-    dispatch(loadCategories());
-  }, []);
-
   return (
     <div>
       <h1>Restaurants</h1>
-      <Regions items={regions} />
-      <Categories items={categories} />
+      <SelectLists />
     </div>
   );
 }
---
+++ src/SelectLists.jsx
@@ -0,0 +1,68 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Lists from './Lists'
+
+import {
+  loadCategories,
+  loadRegions,
+  loadRestaurants,
+} from './actions';
+
+export default function SelectLists() {
+  const dispatch = useDispatch();
+  const { categories, regions, restaurants } = useSelector((state) => ({
+    regions: state.regions,
+    categories: state.categories,
+    restaurants: state.restaurants,
+  }));
+
+  function setSelect(regionName = '미선택', categoryId = 0) {
+    return {
+      regionName,
+      categoryId,
+    }
+  }
+
+  useEffect(() => {
+    dispatch(loadCategories());
+    dispatch(loadRegions());
+    dispatch(loadRestaurants(setSelect()));
+  }, []);
+
+  function regionsClick(event) {
+    const buttons = document.querySelectorAll('ul')[0].querySelectorAll('button');
+    buttons.forEach((button) => {
+      button.className = '';
+    });
+    event.target.className = 'regionCheck';
+  
+    const categoryChceck = document.querySelector('.categoryCheck');
+    return dispatch(loadRestaurants(setSelect(
+      event.target.innerText,
+      categoryChceck !== null ? categoryChceck.id : undefined,
+    )));
+  }
+
+  function categoriesClick(event) {
+    const buttons = document.querySelectorAll('ul')[1].querySelectorAll('button');
+    buttons.forEach((button) => {
+      button.className = '';
+    });
+    event.target.className = 'categoryCheck';
+
+    const regionCheck = document.querySelector('.regionCheck');
+    return dispatch(loadRestaurants(setSelect(
+      regionCheck !== null ? regionCheck.innerText : undefined,
+      event.target.id,
+    )));
+  }
+
+  return (
+    <div>
+      <Lists items={regions} onClick={regionsClick} />
+      <Lists items={categories} onClick={categoriesClick} />
+      <Lists items={restaurants} />
+    </div>
+  );
+}
