7b6ef0c22cf522fade6856b92f94e283021fa367
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-04-18 19:59:41 +0900

    Make actions
    Make actions.

---
+++ src/actions.js
@@ -0,0 +1,12 @@
+export function setLocations(locations) {
+  return {
+    type: 'setLocations',
+    payload: {
+      locations,
+    },
+  };
+}
+
+// TODO: Delete this.
+export function a() {
+}
