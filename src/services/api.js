06fd2e63712e0b498331a889992a3c8f3181219c
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-04-18 22:12:17 +0900

    Add api
    Add api.

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+export async function fetchLocations() {
+ //TODO: fetch data
+ return [];
+}
+
+export async function fetchCategories() {
+ //TODO: fetch data
+ return [];
+}
+
+export async function fetchRestaurants() {
+  //TODO: fetch data
+  return [];
+ }
