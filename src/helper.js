060caa48517ba2f0050e008a461a5b6d022bf129
kjoobin <imkj1148@gmail.com>
2022-04-18 17:23:32 +0900

    name functions 분리 리팩토링

---
+++ src/helper.js
@@ -0,0 +1,7 @@
+export const nameFunctions = {
+  [true]: (name) => `${name} (V)`,
+  [false]: (name) => name,
+};
+
+// TODO: remove
+export const xxx = {};
---
+++ src/helper.test.js
@@ -0,0 +1,15 @@
+import {
+  nameFunctions,
+} from './helper';
+
+describe('helper', () => {
+  describe('nameFunctions', () => {
+    it('returns with (V)', () => {
+      expect(nameFunctions.true('한식')).toBe('한식 (V)');
+    });
+
+    it('returns name', () => {
+      expect(nameFunctions.false('한식')).toBe('한식');
+    });
+  });
+});
