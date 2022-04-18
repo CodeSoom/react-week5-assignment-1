8da874298daa2dd59fe148c881c7f9a53dca0730
kjoobin <imkj1148@gmail.com>
2022-04-18 16:56:04 +0900

    주소 목록 렌더링 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import CategoriesContainer from './CategoriesContainer';
 
 export default function App() {
+  const addresses = [
+    { id: 1, name: '서울' },
+  ];
   return (
     <div>
+      <ul>
+        {addresses.map((address) => (
+          <li key={address.id}>
+            {address.name}
+          </li>
+        ))}
+      </ul>
       <CategoriesContainer />
     </div>
   );
