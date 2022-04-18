47a687993fd9912c67376bad77189b10aa61bcb3
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-04-18 19:06:30 +0900

    Make test run
    Make test run.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 export default function App() {
   return (
-    <></>
+    <>
+      <h1>Restaurants</h1>
+      <h2>Location</h2>
+      <ul>
+        <li>서울</li>
+        <li>인천</li>
+      </ul>
+      <h2>Category</h2>
+      <ul>
+        <li>한식</li>
+        <li>분식</li>
+      </ul>
+    </>
   );
 }
