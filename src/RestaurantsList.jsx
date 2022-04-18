780b6c27b35ac642a2bc381ebdc190e59bb6ca7e
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-04-18 20:32:30 +0900

    Make test run
    Make test run.

---
+++ src/RestaurantsList.jsx
@@ -0,0 +1,11 @@
+export default function RestaurantsList({ restaurants }) {
+  return (
+    <ul>
+      {restaurants.map((restaurant) => (
+        <li key={restaurant.id}>
+          {restaurant.name}
+        </li>
+      ))}
+    </ul>
+  );
+}
