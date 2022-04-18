bed6d3d9aa030d868aef02e50f773a7fb892e349
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-04-18 21:13:06 +0900

    Write test for RestaurantsListContainer
    Write test for RestaurantsListContainer.

---
+++ src/RestaurantsListContainer.test.jsx
@@ -0,0 +1,21 @@
+import { useSelector } from 'react-redux';
+
+import { render } from '@testing-library/react';
+
+import RestaurantsListContainer from './RestaurantsListContainer';
+
+import restaurants from '../fixtures/restaurants';
+
+jest.mock('react-redux');
+
+test('RestaurantsListContainer', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurants,
+  }));
+
+  const { queryByText } = render((
+    <RestaurantsListContainer />
+  ));
+
+  expect(queryByText('양천주가')).not.toBeNull();
+});
