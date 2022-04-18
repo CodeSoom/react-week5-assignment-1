5fdad22f973a465a4f3be7f63fbc4f8e7d4ba4a9
kjoobin <imkj1148@gmail.com>
2022-04-18 16:03:21 +0900

    카테고리 클릭 이벤트 테스트

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -1,17 +1,32 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import Categories from './Categories';
 
 describe('Categories', () => {
-  it('renders categories', () => {
-    const categories = [
-      { id: 1, name: '한식' },
-    ];
+  const categories = [
+    { id: 1, name: '한식' },
+  ];
 
+  it('renders categories', () => {
     const { container } = render((
       <Categories categories={categories} />
     ));
 
     expect(container).toHaveTextContent('한식');
+  });
+
+  it('listens for click event on select category', () => {
+    const onSelect = jest.fn();
+
+    const { getByText } = render((
+      <Categories
+        categories={categories}
+        onSelect={onSelect}
+      />
+    ));
+
+    fireEvent.click(getByText('한식'));
+
+    expect(onSelect).toBeCalled();
   });
 });
