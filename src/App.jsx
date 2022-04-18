0ec8f571343b18e2c9e797a9120dd5796f46b922
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-04-18 19:09:34 +0900

    Make Locations component
    Make Locations component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import Locations from './Locations';
+
 export default function App() {
   return (
     <>
       <h1>Restaurants</h1>
       <h2>Location</h2>
-      <ul>
-        <li>서울</li>
-        <li>인천</li>
-      </ul>
+      <Locations />
       <h2>Category</h2>
       <ul>
         <li>한식</li>
---
+++ src/Locations.jsx
@@ -0,0 +1,8 @@
+export default function Locations() {
+  return (
+    <ul>
+      <li>서울</li>
+      <li>인천</li>
+    </ul>
+  );
+}
