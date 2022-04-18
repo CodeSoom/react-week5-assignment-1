f6959ba11307f32ca49ed7383f985330a006e5ec
kjoobin <imkj1148@gmail.com>
2022-04-18 16:14:38 +0900

    카테고리 클릭 이벤트 테스트

--- src/CategoriesContainer.test.jsx
+++ src/CategoriesContainer.test.jsx
@@ -1,9 +1,17 @@
-import { render } from '@testing-library/react';
-import { useSelector } from 'react-redux';
+import { fireEvent, render } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import CategoriesContainer from './CategoriesContainer';
 
 describe('CategoriesContainer', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
     categories: [
       { id: 1, name: '한식' },
@@ -16,5 +24,15 @@ describe('CategoriesContainer', () => {
     ));
 
     expect(container).toHaveTextContent('한식');
+  });
+
+  it('listens for click event on select category', () => {
+    const { getByText } = render((
+      <CategoriesContainer />
+    ));
+
+    fireEvent.click(getByText('getByText'));
+
+    expect(dispatch).toBeCalled();
   });
 });
