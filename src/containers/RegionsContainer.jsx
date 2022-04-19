b607dfbf237184dcc8b0e91741db60d6cf54818b
이찬호 <0407chan@naver.com>
2022-04-19 23:47:17 +0900

    refactor. RegionsContainer 컴포넌트 추가

---
+++ src/containers/RegionsContainer.jsx
@@ -0,0 +1,13 @@
+import { useSelector } from 'react-redux';
+
+import Regions from '../components/Regions';
+
+export default function RegionsContainer() {
+  const { regions } = useSelector((state) => ({
+    regions: state.regions,
+  }));
+
+  return (
+    <Regions regions={regions} />
+  );
+}
