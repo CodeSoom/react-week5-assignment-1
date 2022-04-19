2cc33e243e40345e01d90f1ec94a37ea6cbae06c
HJUN <guswns823@gmail.com>
2022-04-20 00:46:29 +0900

    지역 목록 테스트 코드 수정 - 목록이 없을 때 테스트 코드 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import Regions from './Regions';
 
 export default function App() {
+  const regions = [
+    { id: 1, name: '서울' },
+    { id: 2, name: '대전' },
+    { id: 3, name: '대구' },
+    { id: 4, name: '부산' },
+    { id: 5, name: '광주' },
+    { id: 6, name: '강원도' },
+    { id: 7, name: '인천' },
+    { id: 8, name: '제주' },
+    { id: 9, name: '전주' },
+    { id: 10, name: '순천' },
+    { id: 11, name: '독도' },
+  ];
+
   return (
     <>
       <h1>레스토랑</h1>
-      <Regions />
+      <Regions regions={regions} />
 
       <h2>카테고리</h2>
       <ul>
