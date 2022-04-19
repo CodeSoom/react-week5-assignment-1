425a2500fc552c5f1d76f624d411fe1573dc6320
HJUN <guswns823@gmail.com>
2022-04-19 23:56:43 +0900

    App 기본 레이아웃 구성 및 테스트 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
 export default function App() {
   return (
-    <></>
+    <>
+      <h1>레스토랑</h1>
+      <h2>지역</h2>
+      <ul>
+        <li>서울</li>
+        <li>대전</li>
+        <li>대구</li>
+        <li>부산</li>
+        <li>광주</li>
+        <li>강원도</li>
+        <li>인천</li>
+      </ul>
+
+      <h2>카테고리</h2>
+      <ul>
+        <li>한식</li>
+        <li>중식</li>
+        <li>일식</li>
+        <li>양식</li>
+        <li>분식</li>
+      </ul>
+    </>
   );
 }
---
+++ src/App.test.jsx
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+test('App', () => {
+  const { queryByText } = render((
+    <App />
+  ));
+
+  expect(queryByText(/서울/)).not.toBeNull();
+  expect(queryByText(/인천/)).not.toBeNull();
+
+  expect(queryByText(/한식/)).not.toBeNull();
+  expect(queryByText(/분식/)).not.toBeNull();
+});
