82295df00232cfd7dd92661074169e6810e85323
free-ko <duddnr654@naver.com>
2022-04-19 22:10:04 +0900

    Refactor: use UseSelector to set categories in CategoriesContainer.test.jsx

--- src/CategoriesContainer.test.jsx
+++ src/CategoriesContainer.test.jsx
@@ -1,9 +1,17 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import CategoriesContainer from './CategoriesContainer';
 
 describe('CategoriesContainer', () => {
   it('renders categories', () => {
+    useSelector.mockImplementation((selector) => selector({
+      categories: [
+        { id: 1, name: '한식' },
+      ],
+    }));
+
     const { queryByText } = render((
       <CategoriesContainer />
     ));
