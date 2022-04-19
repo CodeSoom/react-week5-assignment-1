fc9317f40d52e0dbc6133a7c1a47fc3564b2a57a
0hu <dladudgn123@naver.com>
2022-04-19 20:51:43 +0900

    Implement - app 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 export default function App() {
   return (
-    <></>
+    <div>
+      <p>서울</p>
+      <p>대전</p>
+      <p>한식</p>
+      <p>중식</p>
+    </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 
-import App from './.App';
+import App from './App';
 
 test('App', () => {
   const { queryByText } = render(
