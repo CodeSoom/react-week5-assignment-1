da7e0e4563d705a3ac3a1d40cca5975a9a343332
이찬호 <0407chan@naver.com>
2022-04-19 23:42:51 +0900

    refactor. fetchRegions api추가

--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,12 @@ export async function fetchCategories() {
   const url = 'https://eatgo-customer-api.ahastudio.com/categories';
   const response = await fetch(url);
   const data = await response.json();
-  // TODO: getch Get /categories
-  // REST - CRUD => Read - collection / member, element
   return data;
 }
 
-export function hello() {
-
+export async function fetchRegions() {
+  const url = 'https://eatgo-customer-api.ahastudio.com/regions';
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
 }
