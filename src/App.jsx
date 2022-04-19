9eaec6bf15b3c059af010b63f3e7b121c4a68fc2
HJUN <guswns823@gmail.com>
2022-04-20 00:01:49 +0900

    지역, 카테고리 리스트 버튼 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,46 @@ export default function App() {
       <h1>레스토랑</h1>
       <h2>지역</h2>
       <ul>
-        <li>서울</li>
-        <li>대전</li>
-        <li>대구</li>
-        <li>부산</li>
-        <li>광주</li>
-        <li>강원도</li>
-        <li>인천</li>
+        <li>
+          <button type="button">서울</button>
+        </li>
+        <li>
+          <button type="button">대전</button>
+        </li>
+        <li>
+          <button type="button">대구</button>
+        </li>
+        <li>
+          <button type="button">부산</button>
+        </li>
+        <li>
+          <button type="button">광주</button>
+        </li>
+        <li>
+          <button type="button">강원도</button>
+        </li>
+        <li>
+          <button type="button">인천</button>
+        </li>
       </ul>
 
       <h2>카테고리</h2>
       <ul>
-        <li>한식</li>
-        <li>중식</li>
-        <li>일식</li>
-        <li>양식</li>
-        <li>분식</li>
+        <li>
+          <button type="button">한식</button>
+        </li>
+        <li>
+          <button type="button">중식</button>
+        </li>
+        <li>
+          <button type="button">일식</button>
+        </li>
+        <li>
+          <button type="button">양식</button>
+        </li>
+        <li>
+          <button type="button">분식</button>
+        </li>
       </ul>
     </>
   );
