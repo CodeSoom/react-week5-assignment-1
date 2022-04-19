fcf78c73c4907a22731e6960a6564e6022435f9a
free-ko <duddnr654@naver.com>
2022-04-19 21:55:57 +0900

    Test: use useSelector mockImplementation in App.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,15 @@
 import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
 
 import App from './App';
 
 describe('App', () => {
+  useSelector.mockImplementation((selector) => selector({
+    categories: [
+      { id: 1, name: '한식' },
+    ],
+  }));
+
   it('renders categories', () => {
     const { queryByText } = render((
       <App />
