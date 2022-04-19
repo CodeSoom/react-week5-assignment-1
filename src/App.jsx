1dbca638e94eacc3bcc75b577c81e8451bd618df
free-ko <duddnr654@naver.com>
2022-04-19 21:23:11 +0900

    Feat: add categories in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 export default function App() {
+  const categories = [
+    { id: 1, name: '한식' },
+  ];
+
   return (
-    <></>
+    <div>
+      <ul>
+        {categories.map((category) => (
+          <li key={category.id}>
+            {category.name}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
