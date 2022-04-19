6adf2206bacccbc83101ba24afd3474fcb7cb5a5
이찬호 <0407chan@naver.com>
2022-04-19 23:58:25 +0900

    refactor. handleRegionClick 함수 추가

--- src/containers/RegionsContainer.jsx
+++ src/containers/RegionsContainer.jsx
@@ -1,13 +1,26 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Regions from '../components/Regions';
 
+import { updateRegionName } from '../redux/actions';
+
 export default function RegionsContainer() {
-  const { regions } = useSelector((state) => ({
+  const dispatch = useDispatch();
+
+  const { regions, regionName } = useSelector((state) => ({
     regions: state.regions,
+    regionName: state.regionName,
   }));
 
+  function handleRegionClick({ value }) {
+    dispatch(updateRegionName({ regionName: value }));
+  }
+
   return (
-    <Regions regions={regions} />
+    <Regions
+      regions={regions}
+      regionName={regionName}
+      onRegionClick={handleRegionClick}
+    />
   );
 }
