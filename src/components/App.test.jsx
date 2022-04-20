c0af3c39c4c4eddc8cce7c5eb523f98fc3245992
이찬호 <0407chan@naver.com>
2022-04-20 22:00:56 +0900

    refactor. App 테스트에 지역, 카테고리, 식당 출력 테스트 추가

--- src/components/App.test.jsx
+++ src/components/App.test.jsx
@@ -7,11 +7,25 @@ import App from './App';
 jest.mock('react-redux');
 jest.mock('../services/api');
 
-test('App', () => {
+describe('App', () => {
   const dispatch = jest.fn();
   useDispatch.mockImplementation(() => dispatch);
 
   useSelector.mockImplementation((selector) => selector({
+    regions: [{
+      id: 1,
+      name: '서울',
+    }, {
+      id: 2,
+      name: '대전',
+    }],
+    categories: [{
+      id: 1,
+      name: '한식',
+    }, {
+      id: 2,
+      name: '일식',
+    }],
     restaurants: [{
       id: 1,
       name: '두향',
@@ -23,10 +37,31 @@ test('App', () => {
     ],
   }));
 
-  const { queryByText } = render((
-    <App />
-  ));
+  function renderApp() {
+    return render(<App />);
+  }
+  it('fetchs regions', () => {
 
-  expect(queryByText(/두향/)).not.toBeNull();
-  expect(queryByText(/맥도날드/)).not.toBeNull();
+  });
+
+  it('renders regions', () => {
+    const { queryByText } = renderApp();
+
+    expect(queryByText(/서울/)).not.toBeNull();
+    expect(queryByText(/대전/)).not.toBeNull();
+  });
+
+  it('renders categories', () => {
+    const { queryByText } = renderApp();
+
+    expect(queryByText(/한식/)).not.toBeNull();
+    expect(queryByText(/일식/)).not.toBeNull();
+  });
+
+  it('renders restautants', () => {
+    const { queryByText } = renderApp();
+
+    expect(queryByText(/두향/)).not.toBeNull();
+    expect(queryByText(/맥도날드/)).not.toBeNull();
+  });
 });
