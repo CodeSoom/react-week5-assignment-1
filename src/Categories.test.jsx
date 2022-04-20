888ae1fba410a60e26b708c6f85fa90abfe3bb4c
free-ko <duddnr654@naver.com>
2022-04-20 12:08:09 +0900

    Refactor: renders categories test code by given2

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -1,17 +1,26 @@
 import { render } from '@testing-library/react';
 
+import 'given2/setup';
+import given from 'given2';
+
 import Categories from './Categories';
 
+const renderCategories = (categories) => render((
+  <Categories
+    categories={categories}
+  />
+));
+
 describe('Categories', () => {
-  it('renders categories', () => {
-    const categories = [
+  context('with categories', () => {
+    given('categories', () => [
       { id: 1, name: '한식' },
-    ];
+    ]);
 
-    const { queryByText } = render((
-      <Categories categories={categories} />
-    ));
+    it('renders categories', () => {
+      const { queryByText } = renderCategories(given.categories);
 
-    expect(queryByText(/한식/)).not.toBeNull();
+      expect(queryByText(/한식/)).not.toBeNull();
+    });
   });
 });
