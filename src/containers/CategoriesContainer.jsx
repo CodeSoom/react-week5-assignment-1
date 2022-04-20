1b89679e97fa4b0207e73adbef1cf94532484fa5
이찬호 <0407chan@naver.com>
2022-04-20 20:42:42 +0900

    refactor. handleCategoryClick 함수 추가, 현재 선택된 category 확인을 위해 categoryId 전달

--- src/containers/CategoriesContainer.jsx
+++ src/containers/CategoriesContainer.jsx
@@ -1,15 +1,26 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Categories from '../components/Categories';
 
+import { updateCategoryId } from '../redux/actions';
+
 export default function CategoriesContainer() {
-  const { categories } = useSelector((state) => ({
+  const dispatch = useDispatch();
+
+  const { categories, categoryId } = useSelector((state) => ({
     categories: state.categories,
+    categoryId: state.categoryId,
   }));
+
+  function handleCategoryClick({ value }) {
+    dispatch(updateCategoryId({ categoryId: value }));
+  }
 
   return (
     <Categories
+      categoryId={categoryId}
       categories={categories}
+      onCategoryClick={handleCategoryClick}
     />
   );
 }
