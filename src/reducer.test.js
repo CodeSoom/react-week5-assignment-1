1c61214505180d2fd2882445c831cba8a8353382
free-ko <duddnr654@naver.com>
2022-04-20 21:00:06 +0900

    Test: add loadCategories actions test code

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,16 +1,18 @@
 import reducer from './reducer';
 
 import {
+  loadCategories,
   setCategories,
 } from './actions';
+
+import categories from '../fixtures/categories';
+
+jest.mock('react-redux');
+jest.mock('./services/api');
 
 describe('reducer', () => {
   describe('setCategories', () => {
     it('sets categories', () => {
-      const categories = [
-        { id: 1, name: '한식' },
-      ];
-
       const initialState = {
         categories: [],
       };
@@ -18,6 +20,22 @@ describe('reducer', () => {
       const state = reducer(initialState, setCategories(categories));
 
       expect(state.categories).toHaveLength(1);
+    });
+  });
+
+  describe('loadCategories', () => {
+    it('called with categories data for renders categories', async () => {
+      const dispatch = jest.fn();
+
+      await loadCategories()(dispatch);
+
+      // 흠... 왜 categories에 빈 배열[]이 나오지???
+      expect(dispatch).toBeCalledWith({
+        type: 'setCategories',
+        payload: {
+          categories,
+        },
+      });
     });
   });
 });
