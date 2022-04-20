a1a0da59ccd0be3c302452749f77019b3e9ff112
이찬호 <0407chan@naver.com>
2022-04-20 20:43:07 +0900

    refactor. Categories에 클릭 이벤트 추가, 현재 선택된 카테고리 표기 (V) 추가

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,4 +1,4 @@
-export default function Categories({ categories }) {
+export default function Categories({ categories, categoryId, onCategoryClick }) {
   if (!categories || categories.length === 0) {
     return <>카테고리가 없어요!</>;
   }
@@ -7,8 +7,9 @@ export default function Categories({ categories }) {
     <ul>
       {categories.map((category) => (
         <li key={category.id}>
-          <button type="button">
+          <button type="button" onClick={() => onCategoryClick({ value: category.id })}>
             {category.name}
+            {categoryId === category.id ? '(V)' : null}
           </button>
         </li>
       ))}
