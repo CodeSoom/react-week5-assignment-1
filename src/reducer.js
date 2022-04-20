9f02d86252f7de9edabc9f535e12b7649f44dcc8
free-ko <duddnr654@naver.com>
2022-04-20 19:59:24 +0900

    Feat: add setCategories from actions and reducer

--- src/reducer.js
+++ src/reducer.js
@@ -3,5 +3,14 @@ const initialState = {
 };
 
 export default function reducer(state = initialState, action) {
+  if (action.type === 'setCategories') {
+    const { categories } = action.payload;
+
+    return {
+      ...state,
+      categories,
+    };
+  }
+
   return state;
 }
