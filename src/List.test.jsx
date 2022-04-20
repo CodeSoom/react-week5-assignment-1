dc5dbef4be901aa68d9cebafd36ddb22fcac6044
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-04-20 19:24:43 +0900

    Write and refactor test
    Write and refactor test.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import List from './List';
 
@@ -7,19 +7,31 @@ import locations from '../fixtures/locations';
 describe('List', () => {
   const handleClick = jest.fn();
 
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it('renders a list of items', () => {
-    const { queryByText } = render((
+  function renderList() {
+    return render((
       <List
         items={locations}
         selectedItemId=""
         onClick={handleClick}
       />
     ));
+  }
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders a list of items', () => {
+    const { queryByText } = renderList();
 
     expect(queryByText('서울')).not.toBeNull();
+  });
+
+  it('calls handleClick', () => {
+    const { getByText } = renderList();
+
+    fireEvent.click(getByText('서울'));
+
+    expect(handleClick).toBeCalled();
   });
 });
