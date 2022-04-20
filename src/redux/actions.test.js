c52de302ccbe7fa34cf1fafb42084ea5846deaf1
이찬호 <0407chan@naver.com>
2022-04-20 22:37:53 +0900

    add actions async funtions test

--- src/redux/actions.test.js
+++ src/redux/actions.test.js
@@ -6,50 +6,55 @@ import { loadRegions, loadCategories, loadRestaurants } from './actions';
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
+jest.mock('../services/api');
+
 describe('async actions', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   describe('loadRegions', () => {
-    it('dispatches setRegions after fetchRegions', () => {
+    it('dispatches setRegions after fetchRegions', async () => {
       const store = mockStore({});
-      store.dispatch(loadRegions());
 
-      expect(store.getActions()).toEqual({
+      await store.dispatch(loadRegions());
+
+      expect(store.getActions()).toEqual([{
         type: 'setRegions',
         payload: {
           regions: [],
         },
-      });
+      }]);
     });
   });
 
   describe('loadCategories', () => {
-    it('dispatches setCategories after fetchCategories', () => {
+    it('dispatches setCategories after fetchCategories', async () => {
       const store = mockStore({});
-      store.dispatch(loadCategories());
 
-      expect(store.getActions()).toEqual({
+      await store.dispatch(loadCategories());
+
+      expect(store.getActions()).toEqual([{
         type: 'setCategories',
         payload: {
-          regions: [],
+          categories: [],
         },
-      });
+      }]);
     });
   });
 
   describe('loadRestaurants', () => {
-    it('dispatches setRestaurants after fetchRestaurants', () => {
+    it('dispatches setRestaurants after fetchRestaurants', async () => {
       const store = mockStore({});
-      store.dispatch(loadRestaurants({ categoryId: 1, regionName: '서울' }));
 
-      expect(store.getActions()).toEqual({
+      await store.dispatch(loadRestaurants({ categoryId: 1, regionName: '서울' }));
+
+      expect(store.getActions()).toEqual([{
         type: 'setRestaurants',
         payload: {
-          regions: [],
+          restaurants: [],
         },
-      });
+      }]);
     });
   });
 });
