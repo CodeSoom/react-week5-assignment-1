2ad010ab0ecb0466fa3ee8b5275e96ce4269b614
HJUN <guswns823@gmail.com>
2022-04-21 02:00:50 +0900

    RegionsContainer 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import Regions from './Regions';
+import RegionsContainer from './RegionsContainer';
 import Categories from './Categories';
 
 export default function App() {
-  const regions = [
-    { id: 1, name: '서울' },
-    { id: 2, name: '대전' },
-    { id: 3, name: '대구' },
-    { id: 4, name: '부산' },
-    { id: 5, name: '광주' },
-    { id: 6, name: '강원도' },
-    { id: 7, name: '인천' },
-    { id: 8, name: '제주' },
-    { id: 9, name: '전주' },
-    { id: 10, name: '순천' },
-    { id: 11, name: '독도' },
-  ];
-
   const categories = [
     { id: 1, name: '한식' },
     { id: 2, name: '중식' },
@@ -27,9 +13,8 @@ export default function App() {
   return (
     <>
       <h1>레스토랑</h1>
-
       <h2>지역</h2>
-      <Regions regions={regions} />
+      <RegionsContainer />
       <h2>카테고리</h2>
       <Categories categories={categories} />
       <h2>조회된 레스토랑</h2>
--- src/Regions.jsx
+++ src/Regions.jsx
@@ -1,4 +1,4 @@
-export default function Regions({ regions }) {
+export default function Regions({ regions, onClick }) {
   if (regions.length === 0) {
     return (
       <p>지역이 없습니다.</p>
@@ -9,7 +9,13 @@ export default function Regions({ regions }) {
     <ul>
       {regions.map((region) => (
         <li key={region.id}>
-          <button type="button">{region.name}</button>
+          <button
+            type="button"
+            id={region.id}
+            onClick={onClick}
+          >
+            {region.name}
+          </button>
         </li>
       ))}
     </ul>
