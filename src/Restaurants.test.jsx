60d62112a510891b49bd80082801bfe424bcf19c
kjoobin <imkj1148@gmail.com>
2022-04-21 17:48:16 +0900

    region 을 선택하기 전 테스트

--- src/Restaurants.test.jsx
+++ src/Restaurants.test.jsx
@@ -1,17 +1,32 @@
 import { render } from '@testing-library/react';
 
+import given from 'given2';
 import Restaurants from './Restaurants';
 
 import restaurants from '../fixture/restaurants';
 
 describe('Restaurants', () => {
+  given('selectRegion', () => true);
+
+  const renderRestaurants = () => render((
+    <Restaurants
+      restaurants={restaurants}
+      isSelectRegion={given.selectRegion}
+    />
+  ));
   it('renders Restaurants', () => {
-    const { container } = render((
-      <Restaurants
-        restaurants={restaurants}
-      />
-    ));
+    const { container } = renderRestaurants();
 
     expect(container).toHaveTextContent('서울식당');
+  });
+
+  context('wihout select region', () => {
+    given('selectRegion', () => false);
+
+    it('renders "지역을 선택해주세요!"', () => {
+      const { container } = renderRestaurants();
+
+      expect(container).toHaveTextContent('지역을 선택해주세요!');
+    });
   });
 });
