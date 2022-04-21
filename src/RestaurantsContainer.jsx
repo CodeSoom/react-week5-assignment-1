f47cbf3af2e834193b727eaf4751ca40e08396be
kjoobin <imkj1148@gmail.com>
2022-04-21 18:02:25 +0900

    RestaurantsContainer 분류를 선택하지 않았을때 구현

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -3,15 +3,17 @@ import { useSelector } from 'react-redux';
 import Restaurants from './Restaurants';
 
 export default function RestaurantsContainer() {
-  const { restaurants, selectRegionId } = useSelector((selector) => ({
+  const { restaurants, selectRegionId, selectCategoryId } = useSelector((selector) => ({
     restaurants: selector.restaurants,
     selectRegionId: selector.selectRegionId,
+    selectCategoryId: selector.selectCategoryId,
   }));
 
   return (
     <Restaurants
       restaurants={restaurants}
       isSelectRegion={!!selectRegionId}
+      isSelectCategory={!!selectCategoryId}
     />
   );
 }
