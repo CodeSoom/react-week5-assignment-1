c991917ea8c9fa37e5135854800335f965f1c8ef
0hu <dladudgn123@naver.com>
2022-04-21 15:13:09 +0900

    Test - Reducer

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,19 +1,34 @@
 import reducer from './reducer';
 
-import { setRegionList } from './action';
+import { selectRegion, setRegionList } from './action';
 
-test('setRegionList', () => {
-  const regionList = [
-    {
-      id: 1,
-      name: '서울',
-    },
-  ];
-  const initialState = {
-    regionList: [],
-  };
-  const state = reducer(
-    initialState, setRegionList(regionList),
-  );
-  expect(state.regionList).toHaveLength(1);
+describe('Reducer', () => {
+  it('setRegionList', () => {
+    const initialState = {
+      regionList: [],
+    };
+
+    const regionList = [
+      {
+        id: 1,
+        name: '서울',
+      },
+    ];
+
+    const state = reducer(
+      initialState, setRegionList(regionList),
+    );
+    expect(state.regionList).toHaveLength(1);
+  });
+
+  it('selects Region', () => {
+    const initialState = {
+      id: '',
+    };
+
+    const state = reducer(
+      initialState, selectRegion(1),
+    );
+    expect(state.id).toBe(1);
+  });
 });
