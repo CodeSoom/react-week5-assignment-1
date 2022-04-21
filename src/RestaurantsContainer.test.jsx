1d9fae79a5d3392f434d9194b630ce0ff5e8aa19
kjoobin <imkj1148@gmail.com>
2022-04-21 18:43:31 +0900

    RestaurantsContainer 테스트

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -24,15 +24,17 @@ describe('RestaurantsContainer', () => {
 
   useSelector.mockImplementation((selector) => selector(given.selector));
 
+  const renderRestaurantsContainer = () => render((
+    <RestaurantsContainer />
+  ));
+
   context('without select region id', () => {
     given('selector', () => ({
       restaurants: [],
     }));
 
     it('renders "지역을 선택해주세요!"', () => {
-      const { container } = render((
-        <RestaurantsContainer />
-      ));
+      const { container } = renderRestaurantsContainer();
 
       expect(container).toHaveTextContent('지역을 선택해주세요!');
     });
@@ -45,19 +47,45 @@ describe('RestaurantsContainer', () => {
     }));
 
     it('renders "분류를 선택해주세요!"', () => {
-      const { container } = render((
-        <RestaurantsContainer />
-      ));
+      const { container } = renderRestaurantsContainer();
 
       expect(container).toHaveTextContent('분류를 선택해주세요!');
     });
   });
 
+  context('when loading', () => {
+    given('selector', () => ({
+      restaurants: [],
+      selectRegionId: 1,
+      selectCategoryId: 1,
+      loading: true,
+    }));
+
+    it('renders "레스토랑 목록을 불러오고 있습니다."', () => {
+      const { container } = renderRestaurantsContainer();
+
+      expect(container).toHaveTextContent('레스토랑 목록을 불러오고 있습니다.');
+    });
+  });
+
+  context('without restaurants', () => {
+    given('selector', () => ({
+      restaurants: [],
+      selectRegionId: 1,
+      selectCategoryId: 1,
+      loading: false,
+    }));
+
+    it('renders "조건에 맞는 레스토랑이 없습니다.', () => {
+      const { container } = renderRestaurantsContainer();
+
+      expect(container).toHaveTextContent('조건에 맞는 레스토랑이 없습니다.');
+    });
+  });
+
   context('with restaurants', () => {
     it('renders restaurants', () => {
-      const { container } = render((
-        <RestaurantsContainer />
-      ));
+      const { container } = renderRestaurantsContainer();
 
       expect(container).toHaveTextContent('서울식당');
     });
