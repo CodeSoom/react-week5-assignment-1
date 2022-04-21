3ee042b2c7f51dfc5415e7392ad123b197781daf
kjoobin <imkj1148@gmail.com>
2022-04-21 17:36:44 +0900

    테스트 설명문 수정
    - 과거형 보다는 현재형을 사용

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -15,19 +15,19 @@ describe('api', () => {
 
   global.fetch = fetch;
 
-  it('called with regions url', async () => {
+  it('calls with regions url', async () => {
     await fetchRegions();
 
     expect(fetch).toBeCalledWith('https://eatgo-customer-api.ahastudio.com/regions');
   });
 
-  it('called with categories url', async () => {
+  it('calls with categories url', async () => {
     await fetchCategories();
 
     expect(fetch).toBeCalledWith('https://eatgo-customer-api.ahastudio.com/categories');
   });
 
-  it('called with restaurants url', async () => {
+  it('calls with restaurants url', async () => {
     await fetchRestaurants({ regionName: '서울', categoryId: 1 });
 
     expect(fetch).toBeCalledWith('https://eatgo-customer-api.ahastudio.com/restaurants?region=서울&category=1');
