d2b5de03c0943581979600461eb05bb1d8f2d20e
rukeni <rukeni@naver.com>
2022-05-30 17:55:27 +0900

    Refactor: useState에 연동

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,62 @@
+import { useState } from 'react';
+
 export default function App() {
+  const initialState = {
+    regions: [
+      {
+        id: 1,
+        name: '서울',
+      },
+      {
+        id: 2,
+        name: '대전',
+      },
+      {
+        id: 3,
+        name: '대구',
+      },
+      {
+        id: 4,
+        name: '부산',
+      },
+      {
+        id: 5,
+        name: '광주',
+      },
+      {
+        id: 6,
+        name: '강원도',
+      },
+      {
+        id: 7,
+        name: '인천',
+      },
+      {
+        id: 8,
+        name: '제주',
+      },
+      {
+        id: 9,
+        name: '전주',
+      },
+      {
+        id: 10,
+        name: '순천',
+      },
+      {
+        id: 11,
+        name: '독도',
+      },
+    ],
+  };
+  const [state, setState] = useState(initialState);
+
   return (
     <div>
       <ul>
-        <li><button type="button">서울</button></li>
-        <li><button type="button">대전</button></li>
-        <li><button type="button">대구</button></li>
-        <li><button type="button">부산</button></li>
-        <li><button type="button">광주</button></li>
-        <li><button type="button">강원도</button></li>
-        <li><button type="button">인천</button></li>
+        {state.regions.map((region) => (
+          <li key={region.id}><button type="button">{region.name}</button></li>
+        ))}
       </ul>
     </div>
   );
