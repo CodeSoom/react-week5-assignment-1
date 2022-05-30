545369f66fd69d776c023e04c6b83f97f32eb36b
rukeni <rukeni@naver.com>
2022-05-30 17:33:48 +0900

    Add: 지역 리스트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 export default function App() {
   return (
-    <></>
+    <div>
+      <ul>
+        <li><button type="button">서울</button></li>
+        <li><button type="button">대전</button></li>
+        <li><button type="button">대구</button></li>
+        <li><button type="button">부산</button></li>
+        <li><button type="button">광주</button></li>
+        <li><button type="button">강원도</button></li>
+        <li><button type="button">인천</button></li>
+      </ul>
+    </div>
   );
 }
