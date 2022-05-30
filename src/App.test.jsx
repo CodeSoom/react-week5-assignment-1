87c5872fd1a48067243e65624d60b53dd32377bc
rukeni <rukeni@naver.com>
2022-05-30 18:26:50 +0900

    Test: region버튼을 클릭하면 V표시가 나와야 한다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import state from '../fixtures/state';
 import App from './App';
@@ -9,6 +10,16 @@ describe('App', () => {
 
     state.regions.forEach((region) => {
       expect(screen.getByText(region.name)).toHaveValue(String(region.id));
+    });
+  });
+  context('when the user clicks a region', () => {
+    it('display region name with V sign in current region', async () => {
+      render(<App />);
+      const user = userEvent.setup();
+      const seoul = screen.getByText('서울');
+      await user.click(seoul);
+
+      expect(seoul).toHaveTextContent('서울(V)');
     });
   });
 });
