535b3e1443689b32d74958ab91415b8712b2545f
rukeni <rukeni@naver.com>
2022-05-31 16:34:24 +0900

    Add: Restaurnats목록 추가

--- src/App.jsx
+++ src/App.jsx
@@ -75,6 +75,29 @@ export default function App() {
         name: '분식',
       },
     ],
+    restaurants: [
+      {
+        id: 1,
+        categoryId: 1,
+        name: '양천주가',
+        address: '서울 강남구 123456',
+        information: '양천주가 in 서울 강남구 123456',
+      },
+      {
+        id: 6,
+        categoryId: 1,
+        name: '한국식 초밥',
+        address: '서울 강남구',
+        information: '한국식 초밥 in 서울 강남구',
+      },
+      {
+        id: 14,
+        categoryId: 1,
+        name: '김초밥',
+        address: '서울시 강남구 역삼동',
+        information: '김초밥 in 서울시 강남구 역삼동',
+      },
+    ],
   };
 
   const [state, setState] = useState(initialState);
@@ -91,6 +114,15 @@ export default function App() {
     <div>
       <Regions state={state} onClick={handleClick} />
       <Categories state={state} onClick={handleClick} />
+      {(state.currentCategoryId && state.currentRegionId) && (
+        <ul>
+          {state.restaurants.map((restaurant) => (
+            <li key={restaurant.id}>
+              {restaurant.name}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
