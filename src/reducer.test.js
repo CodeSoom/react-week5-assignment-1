156e5fd96e816a378b18b8eab9994e9c8db9a594
jaeyooniverse <jaeyoon4819@naver.com>
2022-05-31 14:52:57 +0900

    Refactor #98 - reducer.test.js
    BDD에서 Describe는 테스트 대상이기 때문에 reducer를 테스트 대상으로 정하면
    it은 테스트의 행동이다. 그러므로 기존의 코드에서 액션 타입을 describe로
    정했던 것을 제거하고, reducer에서 존재하지 않는 액션 타입은 특별한 예외,
    심지어 많이 쓰이지도 않기 때문에 indent도 줄이고자 다른 테스트 대상으로 규정
    
    해결: close #98

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -9,93 +9,81 @@ import {
 } from './actions';
 
 describe('reducer', () => {
-  context('존재하는 액션 타입이면', () => {
-    describe('setRegions', () => {
-      it('sets regions with new regions', () => {
-        const regions = [{ id: 1, name: '수원' }];
+  it('sets regions with new regions', () => {
+    const regions = [{ id: 1, name: '수원' }];
 
-        const initialState = {
-          regions: [],
-        };
+    const initialState = {
+      regions: [],
+    };
 
-        const state = reducer(initialState, setRegions(regions));
+    const state = reducer(initialState, setRegions(regions));
 
-        expect(state.regions).toHaveLength(1);
-      });
-    });
-
-    describe('setCategories', () => {
-      it('sets categories with new categories', () => {
-        const categories = [{ id: 1, name: '패스트푸드' }];
-
-        const initialState = {
-          categories: [],
-        };
-
-        const state = reducer(initialState, setCategories(categories));
-
-        expect(state.categories).toHaveLength(1);
-      });
-    });
-
-    describe('setRestaurants', () => {
-      it('sets restaurants with new restaurants', () => {
-        const restaurants = [
-          { id: 1, name: '버거킹' },
-          { id: 2, name: '입주집' },
-        ];
-
-        const initialState = {
-          restaurants: [],
-        };
-
-        const state = reducer(initialState, setRestaurants(restaurants));
-
-        expect(state.restaurants).toHaveLength(2);
-      });
-    });
-
-    describe('changeRegionName', () => {
-      it('change regionName', () => {
-        const regionName = '수원';
-
-        const initialState = {
-          regionName: '',
-        };
-
-        const state = reducer(initialState, changeRegionName(regionName));
-
-        expect(state.regionName).toBe('수원');
-      });
-    });
-
-    describe('changeCategoryId', () => {
-      it('change categoryId', () => {
-        const categoryId = 1;
-
-        const initialState = {
-          categoryId: '',
-        };
-
-        const state = reducer(initialState, changeCategoryId(categoryId));
-
-        expect(state.categoryId).toBe(1);
-      });
-    });
+    expect(state.regions).toHaveLength(1);
   });
 
-  context('존재하지 않는 액션 타입이면', () => {
-    it("state doesn't change", () => {
-      const state = reducer(undefined, {
-        type: 'notExistingAction',
-        payload: {
-          regionName: '수원',
-          categoryId: 1,
-        },
-      });
+  it('sets categories with new categories', () => {
+    const categories = [{ id: 1, name: '패스트푸드' }];
 
-      expect(state.regionName).toBe('');
-      expect(state.categoryId).toBe(0);
+    const initialState = {
+      categories: [],
+    };
+
+    const state = reducer(initialState, setCategories(categories));
+
+    expect(state.categories).toHaveLength(1);
+  });
+
+  it('sets restaurants with new restaurants', () => {
+    const restaurants = [
+      { id: 1, name: '버거킹' },
+      { id: 2, name: '입주집' },
+    ];
+
+    const initialState = {
+      restaurants: [],
+    };
+
+    const state = reducer(initialState, setRestaurants(restaurants));
+
+    expect(state.restaurants).toHaveLength(2);
+  });
+
+  it('change regionName', () => {
+    const regionName = '수원';
+
+    const initialState = {
+      regionName: '',
+    };
+
+    const state = reducer(initialState, changeRegionName(regionName));
+
+    expect(state.regionName).toBe('수원');
+  });
+
+  it('change categoryId', () => {
+    const categoryId = 1;
+
+    const initialState = {
+      categoryId: '',
+    };
+
+    const state = reducer(initialState, changeCategoryId(categoryId));
+
+    expect(state.categoryId).toBe(1);
+  });
+});
+
+describe('존재하지 않는 액션 타입', () => {
+  it("state doesn't change", () => {
+    const state = reducer(undefined, {
+      type: 'notExistingAction',
+      payload: {
+        regionName: '수원',
+        categoryId: 1,
+      },
     });
+
+    expect(state.regionName).toBe('');
+    expect(state.categoryId).toBe(0);
   });
 });
