1c15ec004da4cea202d520b3b9772feb39064871
jaeyooniverse <jaeyoon4819@naver.com>
2022-05-31 15:24:55 +0900

    FEAT - App 기능 추가
    App 기능을 추가하고, import 에러 해결하기 위해 CategoriesContainer,
    RegionsContainer, RestaurantsContainer export 설정
    
    완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { loadCategories, loadRegions } from './actions';
+
+import RegionsContainer from './containers/RegionsContainer';
+import CategoriesContainer from './containers/CategoriesContainer';
+import RestaurantsContainer from './containers/RestaurantsContainer';
+
 export default function App() {
-  return <></>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRegions());
+    dispatch(loadCategories());
+  }, []);
+
+  return (
+    <>
+      <RegionsContainer />
+      <CategoriesContainer />
+      <RestaurantsContainer />
+    </>
+  );
 }
