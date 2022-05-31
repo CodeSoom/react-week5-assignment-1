3e7924ebc4b72cc228ad1ec2cf6f650c338eca91
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-31 11:46:13 +0900

    카테고리 V표시 구현

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import { useState } from 'react';
 
 export default function Categories({ categories }) {
+  const [selected, setSelected] = useState();
+
+  const handleClickButton = ({ target: { name } }) => {
+    setSelected(name);
+  };
   return (
     <>
       <ul>
         {categories.map(({ id, name }) => (
-          <li key={id}>{name}</li>
+          <li key={id}>
+            <button
+              type="button"
+              name={name}
+              onClick={handleClickButton}
+            >
+              {`${name}${selected === name ? '(V)' : ''}`}
+            </button>
+          </li>
         ))}
       </ul>
     </>
--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -1,13 +1,23 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import Categories from './Categories';
 
 import { CATEGORIES } from './fixture';
 
 describe('Categories', () => {
+  const renderComponent = () => render(<Categories categories={CATEGORIES} />);
   it('render', () => {
-    const { container } = render(<Categories categories={CATEGORIES} />);
+    const { container } = renderComponent;
 
     expect(container).toHaveTextContent('한식');
+  });
+
+  it('카테고리를 클릭하면 V표시가 된다', () => {
+    const { container, getByRole } = renderComponent();
+
+    const button = getByRole('button', { name: '한식' });
+    fireEvent.click(button);
+
+    expect(container).toHaveTextContent('한식(V)');
   });
 });
