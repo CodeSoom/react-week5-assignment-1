a14cd7c260026538196145b521aee540d2d73e1f
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-31 10:58:07 +0900

    setCategories test code create

--- src/reducer.js
+++ src/reducer.js
@@ -2,7 +2,12 @@ const initialState = {};
 
 const defaultReducer = (state) => state;
 
-const reducers = {};
+const reducers = {
+  setCategories: (state, { payload: { categories } }) => ({
+    ...state,
+    categories,
+  }),
+};
 
 export default function reducer(state = initialState, action) {
   return reducers[action.type] || defaultReducer(state, action);
---
+++ src/reducer.test.js
@@ -0,0 +1,20 @@
+import reducer from './reducer';
+
+import { setCategories } from './action';
+
+describe('reducer', () => {
+  describe('setCategories', () => {
+    const previousState = {
+      categories: [],
+    };
+
+    it('카테고리를 저장한다', () => {
+      const { categories } = reducer(
+        previousState,
+        setCategories([{ id: 1, name: '서울' }]),
+      );
+
+      expect(categories.length).toBe(1);
+    });
+  });
+});
