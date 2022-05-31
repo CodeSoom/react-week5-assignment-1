f8048ed5d0d0cd38bd0d160d9458d13b18765f31
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-31 13:36:06 +0900

    App Componenets modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import Categories from './Categories';
+import { useDispatch } from 'react-redux';
+
+import { useEffect } from 'react';
+
+import { setCategories } from './action';
+import { CATEGORIES } from './fixture';
+
+import CategoriesContainer from './CategoriesContainer';
 
 export default function App() {
+  const dispatch = useDispatch;
+
+  const loadCategories = () => {
+    dispatch(setCategories(CATEGORIES));
+  };
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
   return (
     <>
-      <Categories />
+      <CategoriesContainer />
     </>
   );
 }
