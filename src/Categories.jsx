4aa9cde67b213974450a95883046e9175918cd2f
rukeni <rukeni@naver.com>
2022-05-31 15:26:21 +0900

    Add: Categories컴포넌트 추가

---
+++ src/Categories.jsx
@@ -0,0 +1,14 @@
+export default function Categories({ state, onClick }) {
+  return (
+    <>
+      {state.categories.map((category) => (
+        <li key={category.id}>
+          <button type="button" value={category.id} onClick={() => onClick(category.id)}>
+            {category.name}
+            {category.id === state.currentCategoryId && '(V)'}
+          </button>
+        </li>
+      ))}
+    </>
+  );
+}
