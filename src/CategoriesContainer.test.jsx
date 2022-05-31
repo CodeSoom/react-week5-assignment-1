d6af251f0acd81c859efd9365d03066eaabda82c
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-05-31 13:01:03 +0900

    CategoriesContainer test create

---
+++ src/CategoriesContainer.test.jsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
+
+import { CATEGORIES } from './fixture';
+
+import CategoriesContainer from './CategoriesContainer';
+
+jest.mock('react-redux');
+
+describe('CategoriesContainer', () => {
+  useSelector.mockImplementation((selector) => selector({
+    categories: CATEGORIES,
+  }));
+
+  it('render', () => {
+    const { container } = render(<CategoriesContainer />);
+
+    expect(container).toHaveTextContent('한식');
+  });
+});
