a43ed79f6d6faef7aed95fccf999a6bb0de37a0f
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-01 11:58:58 +0900

    FEAT - Categories 기능 추가
    Category 버튼의 UI를 담당하는 Category 컴포넌트 추가
    Regions 컴포넌트에서 사용했던 Item을 재사용한다.
    
    완료

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,3 +1,16 @@
-export default function Categories() {
-  return <></>;
+import Item from './Item';
+
+export default function Categories({ categories, changeCategoryId, onClick }) {
+  return (
+    <>
+      {categories.map((category) => (
+        <Item
+          key={category.id}
+          value={category}
+          selected={category.name === changeCategoryId?.name}
+          onClick={onClick}
+        />
+      ))}
+    </>
+  );
 }
