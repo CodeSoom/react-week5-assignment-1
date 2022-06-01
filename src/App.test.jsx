dcdd9e9483d238bafa1f8526b8ea69372f1907dd
Mika <solinepik@gmail.com>
2022-06-02 01:05:11 +0900

    feat: redux mock 테스트 코드 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,20 @@
 import { render } from '@testing-library/react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 describe('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    regions: [{ id: 0, name: '서울' }],
+  }));
+
   test('화면에 레스트랑 지역 목록이 나온다.', () => {
     const { getByRole, getAllByRole } = render((<App />));
 
