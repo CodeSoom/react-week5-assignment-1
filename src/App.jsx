a189d6fff2fdf32b41e6ac98bce2421a78d62044
Mika <solinepik@gmail.com>
2022-06-02 00:17:25 +0900

    feat: 레스토랑 지역 목록 화면 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+const restaurants = {
+  regions: ['서울', '대전', '대구', '부산', '광주', '강원도', '인천'],
+};
+
 export default function App() {
   return (
-    <></>
+    <div>
+      <ul>
+        {restaurants.regions.map((region) => (
+          <li key={region}>
+            <button type="button">{region}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
