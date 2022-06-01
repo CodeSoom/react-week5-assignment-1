f85d502fc698d94f063c7a0f2ecb596479059ce9
Mika <solinepik@gmail.com>
2022-06-02 01:55:23 +0900

    feat: 지역 목록을 가져오지 못한 경우 테스트 작성

--- src/components/Regions.test.jsx
+++ src/components/Regions.test.jsx
@@ -4,9 +4,22 @@ import regions from '../../fixture/regions';
 
 import Regions from './Regions';
 
-test('Regions', () => {
-  const { getByRole, getByText } = render(<Regions regions={regions} />);
+describe('Regions', () => {
+  context('지역 목록을 가져왔으면', () => {
+    test('화면에 지역들이 보인다.', () => {
+      const { getByRole, getByText } = render(<Regions regions={regions} />);
 
-  expect(getByRole('list')).toBeInTheDocument();
-  expect(getByText('서울')).toBeInTheDocument();
+      expect(getByRole('list')).toBeInTheDocument();
+      expect(getByText('서울')).toBeInTheDocument();
+    });
+  });
+
+  context('지역 목록을 가져오지 못했으면', () => {
+    test('지역 목록이 나오지 않고 에러 메세지가 나온다', () => {
+      const { getByRole, getByText } = render(<Regions regions={regions} />);
+
+      expect(getByRole('list')).not.toBeInTheDocument();
+      expect(getByText(/지역 목록을 가져오지 못했어요/));
+    });
+  });
 });
