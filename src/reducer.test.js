2a2da729debf412a20ff876f34598d4a200ff778
Mika <solinepik@gmail.com>
2022-06-02 02:23:41 +0900

    feat: reducer 테스트 작성

---
+++ src/reducer.test.js
@@ -0,0 +1,17 @@
+import reducer from './reducer';
+
+import { setRegions } from './actions';
+
+import regions from '../fixture/regions';
+
+describe('reducer', () => {
+  test('setRegions', () => {
+    const initialState = {
+      regions: [],
+    };
+
+    const state = reducer(initialState, setRegions(regions));
+
+    expect(state.regions).toHaveLength(2);
+  });
+});
