3bc4f1c8ef0211c48ae20fb3f050de3fdd98d8ef
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-01 14:22:47 +0900

    Fix #98- fetchRestaurants()
    regionName과 categoryId를 initialState를 제외하고 감싸주는 객체가 또 존재하는 것이 아니므로
    구조 분해 할당을 제거하여 api를 요청할 때 regionName과 categoryId가 undefined되는 오류를 해결한다.
    
    완료#98

--- src/services/api.js
+++ src/services/api.js
@@ -14,7 +14,7 @@ export async function fetchRegions() {
   return data;
 }
 
-export async function fetchRestaurants({ regionName, categoryId }) {
+export async function fetchRestaurants(regionName, categoryId) {
   const url = `https://eatgo-customer-api.ahastudio.com/restaurants?region=${regionName}&category=${categoryId}`;
   const response = await fetch(url);
   const data = await response.json();
