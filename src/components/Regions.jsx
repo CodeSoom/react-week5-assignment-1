b84f879c7f918c58af8d4a5bad7fe4e23838d33f
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-01 11:02:53 +0900

    FEAT - Regions 기능 추가
    지역 버튼을 출력하는 기능인 Regions 기능 추가
    Regions 하위에 Item 컴포넌트를 만들어 Categories에서도 재사용할 예정
    
    완료

--- src/components/Regions.jsx
+++ src/components/Regions.jsx
@@ -1,3 +1,16 @@
+import Item from './Item';
+
 export default function Regions({ regions, changeRegionName, onClick }) {
-  return <></>;
+  return (
+    <>
+      {regions.map((region) => (
+        <Item
+          key={region.id}
+          value={region}
+          selected={region.name === changeRegionName?.name}
+          onClick={onClick}
+        />
+      ))}
+    </>
+  );
 }
