7cfc367d2e5166500b8dcaf3aff015ac2fa18d59
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-01 14:18:09 +0900

    FEAT - RestaurantsContainer 기능 추가
    지역과 분류 버튼을 클릭했을 때, 클릭한 지역 버튼의 name을 selectRegionName으로 받고
    클릭한 분류 버튼의 id를 selectCategoryId로 받아, dispatch를 useEffect를 통해 진행한다.
    조건에 해당하는 식당을 Restaurants 컴포넌트에서 출력한다.
    
    완료

--- src/containers/RestaurantsContainer.jsx
+++ src/containers/RestaurantsContainer.jsx
@@ -1,3 +1,30 @@
+import { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { loadRestaurants } from '../actions';
+
+import Restaurants from '../components/Restaurants';
+
 export default function RestaurantsContainer() {
-  return <></>;
+  const dispatch = useDispatch();
+  const { restaurants, selectRegionName, selectCategoryId } = useSelector(
+    (state) => ({
+      restaurants: state.restaurants,
+      selectRegionName: state.regionName.name,
+      selectCategoryId: state.categoryId.id,
+    })
+  );
+
+  useEffect(() => {
+    if (selectRegionName && selectCategoryId) {
+      dispatch(loadRestaurants(selectRegionName, selectCategoryId));
+    }
+  }, [selectRegionName, selectCategoryId]);
+
+  return (
+    <>
+      <Restaurants restaurants={restaurants} />
+    </>
+  );
 }
