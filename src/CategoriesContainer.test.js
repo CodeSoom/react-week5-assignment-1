3e46be8336182cfb038d6c16bad1b7b2bfe0f789
Ga-Young Song <dolphin2440@naver.com>
2022-06-01 16:59:46 +0900

    Add test of CategoriesContainer

---
+++ src/CategoriesContainer.test.js
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
+
+import CategoriesContainer from './CategoriesContainer';
+
+jest.mock('react-redux');
+
+describe('CategoriesContainer', () => {
+  it('renders', () => {
+    useSelector.mockImplementation((selector) => selector({
+      categories: { id: 1, name: '한식' },
+    }));
+
+    const { container } = render(<CategoriesContainer />);
+
+    expect(container).toHaveTextContent('한식');
+  });
+});
