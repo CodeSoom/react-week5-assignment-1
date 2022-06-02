bc208478262fc2898e4883c3edaed78e01a60601
rukeni <rukeni@naver.com>
2022-06-02 14:21:22 +0900

    Test: Action Type으로 정의되지 않은 Action은 initialState를 반환해야한다.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,9 +1,25 @@
 import reducer, { initialState } from './reducer';
 
 describe('reducer', () => {
-  context('parameters that are not defined', () => {
+  context('without parameter', () => {
     it('returns the initial state', () => {
       const state = reducer();
+      expect(state).toEqual(initialState);
+    });
+  });
+
+  context('with action type that is not defined', () => {
+    it('returns the initial state', () => {
+      // const initialState = {
+      //   newId: 100,
+      //   restaurant: {
+      //     name: '',
+      //     address: '',
+      //     category: '',
+      //   },
+      //   restaurants: [],
+      // };
+      const state = reducer(initialState, { type: 'unknown' });
       expect(state).toEqual(initialState);
     });
   });
