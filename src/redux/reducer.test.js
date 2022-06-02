753e33091eed9fe985c6576fd4cdd11853df042d
Mika <solinepik@gmail.com>
2022-06-03 02:57:43 +0900

    feat: action이 없을 때 reducer 테스트 작성

--- src/redux/reducer.test.js
+++ src/redux/reducer.test.js
@@ -27,4 +27,14 @@ describe('reducer', () => {
 
     expect(state.errorMessage).toBe('지역 목록을 가져오지 못했어요. 잠시 후 다시 시도해주세요');
   });
+
+  test('action이 undefined면 initialState를 반환한다', () => {
+    const initialState = {
+      regions: [],
+      errorMessage: null,
+    };
+    const state = reducer(initialState, undefined);
+
+    expect(state).toEqual(initialState);
+  });
 });
