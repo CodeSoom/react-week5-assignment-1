bed7f42853a275e161e2009625bb5c9fe8fed2e6
rukeni <rukeni@naver.com>
2022-06-02 16:30:36 +0900

    Refactor: useState제거하고 redux 연동

--- src/App.jsx
+++ src/App.jsx
@@ -1,131 +1,47 @@
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCurrentRegionId, setCurrentCategoryId } from './actions';
+
 import Categories from './Categories';
 
 import Regions from './Regions';
 import Restaurants from './Restaurants';
 
 export default function App() {
-  const initialState = {
-    currentRegionId: Number.MIN_SAFE_INTEGER,
-    currentCategoryId: Number.MIN_SAFE_INTEGER,
-    regions: [
-      {
-        id: 1,
-        name: '서울',
-      },
-      {
-        id: 2,
-        name: '대전',
-      },
-      {
-        id: 3,
-        name: '대구',
-      },
-      {
-        id: 4,
-        name: '부산',
-      },
-      {
-        id: 5,
-        name: '광주',
-      },
-      {
-        id: 6,
-        name: '강원도',
-      },
-      {
-        id: 7,
-        name: '인천',
-      },
-      {
-        id: 8,
-        name: '제주',
-      },
-      {
-        id: 9,
-        name: '전주',
-      },
-      {
-        id: 10,
-        name: '순천',
-      },
-      {
-        id: 11,
-        name: '독도',
-      },
-    ],
-    categories: [
-      {
-        id: 1,
-        name: '한식',
-      },
-      {
-        id: 2,
-        name: '중식',
-      },
-      {
-        id: 3,
-        name: '일식',
-      },
-      {
-        id: 4,
-        name: '양식',
-      },
-      {
-        id: 5,
-        name: '분식',
-      },
-    ],
-    restaurants: [
-      {
-        id: 1,
-        categoryId: 1,
-        name: '양천주가',
-        address: '서울 강남구 123456',
-        information: '양천주가 in 서울 강남구 123456',
-      },
-      {
-        id: 6,
-        categoryId: 1,
-        name: '한국식 초밥',
-        address: '서울 강남구',
-        information: '한국식 초밥 in 서울 강남구',
-      },
-      {
-        id: 14,
-        categoryId: 1,
-        name: '김초밥',
-        address: '서울시 강남구 역삼동',
-        information: '김초밥 in 서울시 강남구 역삼동',
-      },
-      {
-        id: 15,
-        categoryId: 1,
-        name: '밀면넘어져요',
-        address: '서울시 강남구 역삼동',
-        information: '밀면넘어져요 in 서울시 강남구 역삼동',
-      },
-    ],
+  const dispatch = useDispatch();
+  const {
+    regions, categories, restaurants, currentRegionId, currentCategoryId,
+  } = useSelector((state) => ({
+    regions: state.regions,
+    categories: state.categories,
+    restaurants: state.restaurants,
+    currentRegionId: state.currentRegionId,
+    currentCategoryId: state.currentCategoryId,
+  }));
+
+  const handleClickRegion = (id) => {
+    dispatch(setCurrentRegionId(id));
   };
 
-  const [state, setState] = useState(initialState);
-
-  const handleClick = (event) => {
-    const { name, value } = event.target;
-    setState((prev) => ({
-      ...prev,
-      [name]: Number(value),
-    }));
+  const handleClickCategory = (id) => {
+    dispatch(setCurrentCategoryId(id));
   };
 
   return (
     <div>
-      <Regions state={state} onClick={handleClick} />
-      <Categories state={state} onClick={handleClick} />
+      <Regions
+        regions={regions}
+        currentRegionId={currentRegionId}
+        onClick={handleClickRegion}
+      />
+      <Categories
+        categories={categories}
+        currentCategoryId={currentCategoryId}
+        onClick={handleClickCategory}
+      />
       <Restaurants
-        restaurants={state.restaurants}
-        currentCategoryId={state.currentCategoryId}
-        currentRegionId={state.currentRegionId}
+        restaurants={restaurants}
+        currentCategoryId={currentCategoryId}
+        currentRegionId={currentRegionId}
       />
     </div>
   );
