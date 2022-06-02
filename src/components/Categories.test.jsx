291c08811951c89c7b4e5bdd25065fc753f78b56
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-02 22:13:55 +0900

    TEST - Categories.test.jsx
    category 버튼을 클릭하면 changeCategoryId action을 호출한 다음 (V) 표시를 추가하기 때문에
    'category 버튼을 클릭하면'과 같은 텍스트를 추가했는데 말씀하신 대로 fireEvent.Click과 같은
    코드가 없으니 'changeCategoryId'를 호출하면 호출한 category 버튼에 '(V)' 표시를 렌더한다로
    변경
    
    완료

--- src/components/Categories.test.jsx
+++ src/components/Categories.test.jsx
@@ -5,10 +5,9 @@ import Categories from './Categories';
 import categories from '../../fixtures/categories';
 
 describe('Categories', () => {
-  const renderCategories = (changeCategoryId) =>
-    render(
-      <Categories categories={categories} changeCategoryId={changeCategoryId} />
-    );
+  const renderCategories = (changeCategoryId) => render(
+    <Categories categories={categories} changeCategoryId={changeCategoryId} />,
+  );
 
   it('"category"버튼을 렌더한다', () => {
     const { getByRole } = renderCategories();
@@ -18,7 +17,7 @@ describe('Categories', () => {
     });
   });
 
-  it('"category"버튼을 클릭하면 "(V)"표시가 추가된다', () => {
+  it('"changeCategoryId를 호출하면 호출한 category 버튼에 "(V)"표시를 렌더한다', () => {
     const { getByText } = renderCategories(categories[0]);
 
     expect(getByText(`${categories[0].name}(V)`)).toBeInTheDocument();
