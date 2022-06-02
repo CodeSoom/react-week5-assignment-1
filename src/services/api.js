4c60071cd26e393c4bea9bfd6612f9b1e66902e7
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-02 10:41:42 +0900

    api Regions add

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,15 @@
 export async function fetchCategories() {
-  const URL = 'https://eatgo-customer-api.ahastudio.com/categories';
-  const response = await fetch(URL);
+  // 한식, 중식, 양식
+  const categories = 'https://eatgo-customer-api.ahastudio.com/categories';
+  const response = await fetch(categories);
+
+  return response.json();
+}
+
+export async function fetchRegions() {
+  // 서울, 대전, 대구
+  const regions = 'https://eatgo-customer-api.ahastudio.com/regions';
+  const response = await fetch(regions);
 
   return response.json();
 }
