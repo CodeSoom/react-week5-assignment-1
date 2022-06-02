cc7187b9b5370a1c53f128ee153f780e508ce291
rukeni <rukeni@naver.com>
2022-06-02 17:29:01 +0900

    Test: 각 관심사로 넘어간 테스트 로직 제거

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -12,57 +11,17 @@ jest.mock('react-redux');
 const dispatch = useDispatch();
 
 describe('App', () => {
-  beforeEach(() => {
-    useDispatch.mockImplementation(() => jest.fn());
+  useDispatch.mockImplementation(() => dispatch);
 
-    useSelector.mockImplementation((selector) => selector({
-      regions: state.regions,
-      categories: state.categories,
-      currentRegionId: state.currentRegionId,
-      currentCategoryId: state.currentCategoryId,
-    }));
-  });
-
-  it('renders regions list', () => {
+  useSelector.mockImplementation((selector) => selector({
+    categories: state.categories,
+    regions: state.regions,
+    restaurants: state.restaurants,
+    currentRegionId: state.currentRegionId,
+    currentCategoryId: state.currentCategoryId,
+  }));
+  it('renders regions', () => {
     render(<App />);
-    // screen.debug();
-    screen.getAllByRole('button');
-    // expect(state.regions.length).toHaveLength(screen.getAllByRole('button').length);
-    expect(screen.getByText('서울')).toHaveValue(String(state.regions[0].id));
-  });
-  context('when the user clicks a region', () => {
-    it('display region name with V sign in current region', async () => {
-      render(<App />);
-      const user = userEvent.setup();
-      const seoul = screen.getByText('서울');
-      await user.click(seoul);
-
-      expect(seoul).toHaveTextContent('서울(V)');
-    });
-  });
-  context('when the user clicks a category', () => {
-    it('display category name with V sign in current category', async () => {
-      render(<App />);
-      const user = userEvent.setup();
-      const koreanFoodStyle = screen.getByText('한식');
-      await user.click(koreanFoodStyle);
-
-      expect(koreanFoodStyle).toHaveTextContent('한식(V)');
-    });
-  });
-  context('with currentRegionId and currentCategoryId', () => {
-    it('display restaurants list', async () => {
-      render(<App />);
-      const user = userEvent.setup();
-      const seoul = screen.getByText('서울');
-
-      expect(screen.queryByText('양천주가')).toBeNull();
-
-      await user.click(seoul);
-      const koreanFoodStyle = screen.getByText('한식');
-      await user.click(koreanFoodStyle);
-
-      expect(screen.getByText('양천주가')).toBeInTheDocument();
-    });
+    expect(screen.getByText('서울')).toBeInTheDocument();
   });
 });
