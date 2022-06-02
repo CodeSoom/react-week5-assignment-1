323c9271641ba662145a3de9bbf76acaaa79c550
rukeni <rukeni@naver.com>
2022-06-02 18:56:47 +0900

    Add: Restaurants 비동기 호출 추가
    1. setRestaurants 액션 타입 추가(이전에 작성을 안했음)
    
    2. loadRestaurants() thunk 액션 함수 추가
    
    3. state.restaurants 빈배열로 수정
    
    4. useEffect로 비동기 호출 추가.
    -> 의존성 배열에 currentRegionId와 currentCategoryId를 추가하여 둘 중 하나가 변경되면 이펙트함수가 실행되도록 작성하였습니다.

--- src/Restaurants.jsx
+++ src/Restaurants.jsx
@@ -1,11 +1,21 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { loadRestaurants } from './actions';
 
 export default function Restaurants() {
+  const dispatch = useDispatch();
   const { restaurants, currentRegionId, currentCategoryId } = useSelector((state) => ({
     restaurants: state.restaurants,
     currentRegionId: state.currentRegionId,
     currentCategoryId: state.currentCategoryId,
   }));
+
+  useEffect(() => {
+    if (currentRegionId && currentCategoryId) {
+      dispatch(loadRestaurants());
+    }
+  }, [currentRegionId, currentCategoryId]);
 
   return (
     <>
--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,4 @@
-import { fetchCategories, fetchRegions } from './api';
+import { fetchCategories, fetchRegions, fetchRestaurants } from './api';
 
 export function setRegions(regions) {
   return {
@@ -36,6 +36,15 @@ export function setCurrentCategoryId(currentCategoryId) {
   };
 }
 
+export function setRestaurants(restaurants) {
+  return {
+    type: 'setRestaurants',
+    payload: {
+      restaurants,
+    },
+  };
+}
+
 export function loadRegions() {
   return async (dispatch) => {
     const response = await fetchRegions();
@@ -49,5 +58,14 @@ export function loadCategories() {
     const response = await fetchCategories();
     const categories = await response.json();
     dispatch(setCategories(categories));
+  };
+}
+
+export function loadRestaurants() {
+  return async (dispatch, getState) => {
+    const { currentRegionId, currentCategoryId } = getState();
+    const response = await fetchRestaurants(currentRegionId, currentCategoryId);
+    const restaurants = await response.json();
+    dispatch(setRestaurants(restaurants));
   };
 }
