e033c61e5c611b300067ec2bedcb4c1bf529330c
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-02 11:52:44 +0900

    Categories.test.jsx Context Add.

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -15,18 +15,22 @@ describe('Categories', () => {
     expect(container).toHaveTextContent('한식');
   });
 
-  it('카테고리를 클릭하면 V표시가 된다', () => {
-    const { container, getByRole } = renderComponent();
+  context('카테고리가 있을 때', () => {
+    it('카테고리를 클릭하면 V표시가 된다', () => {
+      const { container, getByRole } = renderComponent(CATEGORIES);
 
-    const button = getByRole('button', { name: '한식' });
-    fireEvent.click(button);
+      const button = getByRole('button', { name: '한식' });
+      fireEvent.click(button);
 
-    expect(container).toHaveTextContent('한식(V)');
+      expect(container).toHaveTextContent('한식(V)');
+    });
   });
 
-  it('카테고리가 없으면 메세지가 출력된다', () => {
-    const { container } = renderComponent([]);
+  context('카테고리가 없을때', () => {
+    it('카테고리가 존재하지 않다는 문구가 출력된다', () => {
+      const { container } = renderComponent([]);
 
-    expect(container).toHaveTextContent('카테고리가 존재하지 않습니다');
+      expect(container).toHaveTextContent('카테고리가 존재하지 않습니다');
+    });
   });
 });
