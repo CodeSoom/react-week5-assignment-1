1ecb6324a9997e87b6e70b1d086a899d6d61776c
rukeni <rukeni@naver.com>
2022-06-02 19:19:51 +0900

    Test: setRestaurants 리듀서 테스트 코드 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,7 +1,7 @@
 import reducer, { initialState } from './reducer';
 
 import {
-  setRegions, setCategories, setCurrentRegionId, setCurrentCategoryId,
+  setRegions, setCategories, setCurrentRegionId, setCurrentCategoryId, setRestaurants,
 } from './actions';
 
 import regions from '../fixtures/regions';
@@ -18,19 +18,11 @@ describe('reducer', () => {
 
   context('with undefined action type', () => {
     it('returns the initial state', () => {
-      // const initialState = {
-      //   newId: 100,
-      //   restaurant: {
-      //     name: '',
-      //     address: '',
-      //     category: '',
-      //   },
-      //   restaurants: [],
-      // };
       const state = reducer(initialState, { type: 'unknown' });
       expect(state).toEqual(initialState);
     });
   });
+
   describe('setRegions', () => {
     it('set new regions into regions', () => {
       const state = reducer(emptyState, setRegions(regions));
@@ -38,6 +30,7 @@ describe('reducer', () => {
       expect(state.regions).toEqual(regions);
     });
   });
+
   describe('setCategories', () => {
     it('set new categories into categories', () => {
       const state = reducer(emptyState, setCategories(categories));
@@ -45,6 +38,7 @@ describe('reducer', () => {
       expect(state.categories).toEqual(categories);
     });
   });
+
   describe('setCurrentRegionId', () => {
     it('set new currentRegionId', () => {
       const state = reducer(emptyState, setCurrentRegionId('1'));
@@ -52,11 +46,28 @@ describe('reducer', () => {
       expect(state.currentRegionId).toBe('1');
     });
   });
+
   describe('setCurrentCategoryId', () => {
     it('set new currentCategoryId', () => {
       const state = reducer(emptyState, setCurrentCategoryId('1'));
 
       expect(state.currentCategoryId).toBe('1');
+    });
+  });
+
+  describe('setCurrentCategoryId', () => {
+    it('set new currentCategoryId', () => {
+      const state = reducer(emptyState, setCurrentCategoryId('1'));
+
+      expect(state.currentCategoryId).toBe('1');
+    });
+  });
+
+  describe('setRestaurants', () => {
+    it('set new restaurants into restaurants', () => {
+      const state = reducer(emptyState, setRestaurants(regions));
+
+      expect(state.restaurants).toEqual(regions);
     });
   });
 });
