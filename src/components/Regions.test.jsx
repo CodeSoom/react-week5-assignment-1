773f463816e743fc11a814185e6e259cab77557d
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-02 22:18:43 +0900

    TEST - Regions.test.jsx
    region 버튼을 클릭하면 changeRegionName action을 호출한 다음 (V) 표시를 추가하기 때문에
    'region 버튼을 클릭하면'과 같은 텍스트를 추가했는데 말씀하신 대로 fireEvent.Click과 같은
    코드가 없으니 'changeRegion'를 호출하면 호출한 지역 버튼에 '(V)' 표시를 렌더한다로
    변경
    
    완료

--- src/components/Regions.test.jsx
+++ src/components/Regions.test.jsx
@@ -5,7 +5,8 @@ import Regions from './Regions';
 import regions from '../../fixtures/regions';
 
 describe('Regions', () => {
-  const renderRegions = (changeRegionName) => render(<Regions regions={regions} changeRegionName={changeRegionName} />);
+  const renderRegions = (changeRegionName) =>
+    render(<Regions regions={regions} changeRegionName={changeRegionName} />);
 
   it('"region"버튼을 렌더한다', () => {
     const { getByRole } = renderRegions();
@@ -15,7 +16,7 @@ describe('Regions', () => {
     });
   });
 
-  it('"region"버튼을 클릭하면 "(V)"표시가 추가된다', () => {
+  it('"changeRegionName"을 호출하면 호출한 지역 버튼에 "(V)"표시가 추가된다', () => {
     const { getByText } = renderRegions(regions[0]);
 
     expect(getByText(`${regions[0].name}(V)`)).toBeInTheDocument();
