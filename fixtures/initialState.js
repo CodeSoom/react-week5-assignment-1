a00a3df7f7e9c0d68e687e0ffe05d439db9290e1
rukeni <rukeni@naver.com>
2022-06-02 14:38:31 +0900

    Test: fixtures 데이터 분리
    fixture값이 장황해져서 분리하였습니다. 다음과 같이 분리하였습니다.
    
    1. categories
    2. regions
    3. restaurants
    4. initialState(초기 state)
    5. state(값이 있다고 가정한 state)

---
+++ fixtures/initialState.js
@@ -0,0 +1,7 @@
+export default {
+  currentRegionId: Number.MIN_SAFE_INTEGER,
+  currentCategoryId: Number.MIN_SAFE_INTEGER,
+  regions: [],
+  categories: [],
+  restaurants: [],
+};
--- fixtures/state.js
+++ fixtures/state.js
@@ -1,77 +1,11 @@
+import regions from './regions';
+import categories from './categories';
+import restaurants from './restaurants';
+
 export default {
-  regions: [
-    {
-      id: 1,
-      name: '서울',
-    },
-    {
-      id: 2,
-      name: '대전',
-    },
-    {
-      id: 3,
-      name: '대구',
-    },
-    {
-      id: 4,
-      name: '부산',
-    },
-    {
-      id: 5,
-      name: '광주',
-    },
-    {
-      id: 6,
-      name: '강원도',
-    },
-    {
-      id: 7,
-      name: '인천',
-    },
-  ],
-  categories: [
-    {
-      id: 1,
-      name: '한식',
-    },
-    {
-      id: 2,
-      name: '중식',
-    },
-    {
-      id: 3,
-      name: '일식',
-    },
-    {
-      id: 4,
-      name: '양식',
-    },
-    {
-      id: 5,
-      name: '분식',
-    },
-  ],
-  restaurants: [
-    {
-      id: 1,
-      categoryId: 1,
-      name: '양천주가',
-      address: '서울 강남구 123456',
-      information: '양천주가 in 서울 강남구 123456',
-    },
-    {
-      id: 6,
-      categoryId: 1,
-      name: '한국식 초밥',
-      address: '서울 강남구',
-      information: '한국식 초밥 in 서울 강남구',
-    },
-    {
-      id: 14,
-      categoryId: 1,
-      name: '김초밥',
-      address: '서울시 강남구 역삼동',
-      information: '김초밥 in 서울시 강남구 역삼동',
-    },
-  ],
+  currentRegionId: Number.MIN_SAFE_INTEGER,
+  currentCategoryId: Number.MIN_SAFE_INTEGER,
+  regions,
+  categories,
+  restaurants,
 };
