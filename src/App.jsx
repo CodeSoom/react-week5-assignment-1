bd7fc0009d5dfa6112137450e56c54e18659e4f0
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-02 14:58:10 +0900

    App.jsx modify
    Uncaught Error: Invalid hook call. Hooks can only be called inside of the body of a function component. This could happen for one of the following reasons:
    
    좋은 에러를 만났다.
    - 함수 뒤에 괄호를 넣어주자

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 
-import { loadCategories } from './action';
+import { loadCategories, loadRegions } from './action';
 
 import CategoriesContainer from './CategoriesContainer';
 
 export default function App() {
-  const dispatch = useDispatch;
+  const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadCategories());
+    dispatch(loadRegions());
   }, []);
 
   return (
