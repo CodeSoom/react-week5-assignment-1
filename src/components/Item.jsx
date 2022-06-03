e908104a319cce4e19438a2508a1ee0b95fbb73c
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-03 15:58:32 +0900

    Refactor #98 - Item.jsx Button이 value값을 몰라도 되도록 변경
    기존 코드는 value를 받아서 name만 사용하고 클릭했을 때 value를 그대로 또 되돌려주고 있기 때문에
    이름만 받고 버튼이 value 자체에 대해서 몰라도 괜찮도록 handleClick이라는 단순 핸들러 함수를
    만들어서 관심사를 분리한다.
    
    완료

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,9 +1,13 @@
 export default function Item({ value, selected, onClick }) {
   const { name } = value;
 
+  function handleClick() {
+    return onClick(value);
+  }
+
   return (
     <div>
-      <button type="button" onClick={() => onClick(value)}>
+      <button type="button" onClick={handleClick}>
         {name}
         {selected && '(V)'}
       </button>
