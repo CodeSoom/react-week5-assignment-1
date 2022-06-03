82bbff3f47efbd1730a7217a2ec1a8b6e2cfd59f
jodheeee <soso99700@gmail.com>
2022-06-03 17:17:27 +0900

    (ETC)-카테고리 목록 컨테이너 테스트 및 구현

---
+++ src/container/CategoriesContainer.test.jsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+
+import { useSelector } from 'react-redux';
+
+import CategoriesContainer from './CategoriesContainer';
+
+jest.mock('react-redux');
+
+describe('CategoriesContainer', () => {
+  it('CategoriesContainer render', () => {
+    useSelector.mockImplementation((selector) => selector({
+      categories: [{
+        id: 1,
+        name: '한식',
+      }],
+    }));
+    const { getByText } = render(<CategoriesContainer />);
+
+    expect(getByText('한식')).not.toBeNull();
+  });
+});
