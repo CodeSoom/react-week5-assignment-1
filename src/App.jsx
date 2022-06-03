701ae479f9b0b1c6fec75fb5687c06fb70da5ba7
jodheeee <soso99700@gmail.com>
2022-06-03 10:25:28 +0900

    (Green)-App 제목 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 export default function App() {
   return (
-    <></>
+    <h2>레스토랑 조회 구현하기</h2>
   );
 }
+
+/*
+레스토랑 지역 목록 보기: https://eatgo-customer-api.ahastudio.com/regions
+레스토랑 카테고리 목록 보기: https://eatgo-customer-api.ahastudio.com/categories
+레스토랑 목록 보기: https://eatgo-customer-api.ahastudio.com/restaurants?region=${regionName}&category=${categoryId}
+*/
