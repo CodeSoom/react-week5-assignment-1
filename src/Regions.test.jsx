96838c261018153cff7975ded035830cb489bacf
jodheeee <soso99700@gmail.com>
2022-06-03 15:21:23 +0900

    (Red)-지역 목록 테스트 작성

--- src/Regions.test.jsx
+++ src/Regions.test.jsx
@@ -1,24 +1,17 @@
 import { render } from '@testing-library/react';
 
+import { useDispatch } from 'react-redux';
+
 import Regions from './Regions';
 
+jest.mock('react-redux');
+
 describe('Regions', () => {
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+
   it('render Regions', () => {
-    const regions = [
-      {
-        id: 1,
-        name: '서울',
-      },
-      {
-        id: 2,
-        name: '대전',
-      },
-      {
-        id: 3,
-        name: '대구',
-      },
-    ];
-    const { getByText } = render(<Regions regions={regions} />);
+    const { getByText } = render(<Regions />);
 
     expect(getByText(/서울/)).not.toBeNull();
     expect(getByText(/대전/)).not.toBeNull();
