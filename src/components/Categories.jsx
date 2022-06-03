1786cc35308047fb7ecf0c87fac04adae57f3747
Mika <solinepik@gmail.com>
2022-06-04 03:17:57 +0900

    feat: Categories 컴포넌트 구현

---
+++ src/components/Categories.jsx
@@ -0,0 +1,10 @@
+export default function Categories() {
+  return (
+    <div>
+      <ul>
+        <li><button type="button">한식</button></li>
+        <li><button type="button">중식</button></li>
+      </ul>
+    </div>
+  );
+}
