0a15b30dea7888aad4e75d681440280a1cb456b1
jodheeee <soso99700@gmail.com>
2022-06-03 17:17:53 +0900

    (ETC)-카테고리 목록 APP 테스트를 위한 작성

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { useDispatch } from 'react-redux';
 import { loadCategories, loadRegions } from './modules/actions';
 
 import RegionsContainer from './container/RegionsContainer';
+import CategoriesContainer from './container/CategoriesContainer';
 
 export default function App() {
   const dispatch = useDispatch();
@@ -19,6 +20,7 @@ export default function App() {
       <h2>레스토랑 조회 구현하기</h2>
       <hr />
       <RegionsContainer />
+      <CategoriesContainer />
     </div>
   );
 }
