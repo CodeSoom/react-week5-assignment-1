af6162186c7b59c4ca44617b0673767e4c3c377c
jodheeee <soso99700@gmail.com>
2022-06-03 16:46:11 +0900

    (Green)-지역 목록 컨테이너 구현

---
+++ src/RegionsContainer.jsx
@@ -0,0 +1,11 @@
+import { useSelector } from 'react-redux';
+
+import Regions from './Regions';
+
+export default function RegionsContainer() {
+  const { regions } = useSelector((state) => ({
+    regions: state.regions,
+  }));
+
+  return <Regions regions={regions} />;
+}
