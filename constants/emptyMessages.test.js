6d8c04faf08452a9a638e357f86eeec9a6f93aa5
Mika <solinepik@gmail.com>
2022-06-04 03:14:54 +0900

    fix: describe-context-test 구조일 때 가독성을위해 it 으로 변경

--- constants/emptyMessages.test.js
+++ constants/emptyMessages.test.js
@@ -2,7 +2,7 @@ import { baseMessage } from './emptyMessages';
 
 describe('emptyMessages', () => {
   context('전달된 `text` 인자가 없으면', () => {
-    test('"목록"이 비어있다는 메세지를 반환한다', () => {
+    it('"목록"이 없다는 메세지를 반환한다', () => {
       const message = baseMessage();
 
       expect(message).toBe('아직 목록이 비어있어요');
@@ -10,7 +10,7 @@ describe('emptyMessages', () => {
   });
 
   context('전달된 `text` 인자가 있으면', () => {
-    test('`text`이 비어있다는 메세지를 반환한다', () => {
+    it('`text`이 없다는 메세지를 반환한다', () => {
       const text = '지역 목록';
       const message = baseMessage(text);
 
