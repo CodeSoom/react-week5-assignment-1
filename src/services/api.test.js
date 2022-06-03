4b2149293c1fab74a6ad04a953c1d585d5a462a3
Mika <solinepik@gmail.com>
2022-06-04 04:05:47 +0900

    feat: fetchCategories api test 작성

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -1,7 +1,8 @@
 import APIS from '../../constants/apis';
 import mockRegions from '../../fixture/regions';
+import mockCategories from '../../fixture/categories';
 
-import { fetchRegions } from './api';
+import { fetchRegions, fetchCategories } from './api';
 
 global.fetch = jest.fn();
 
@@ -10,12 +11,21 @@ beforeEach(() => {
 });
 
 describe('api', () => {
-  fetch.mockImplementationOnce(() => ({ json: () => Promise.resolve(mockRegions) }));
-
   test('fetchRegions', async () => {
+    fetch.mockImplementationOnce(() => ({ json: () => Promise.resolve(mockRegions) }));
+
     const regions = await fetchRegions();
 
     expect(regions).toHaveLength(regions.length);
     expect(fetch).toHaveBeenCalledWith(APIS.REGIONS);
+  });
+
+  test('fetchCategories', async () => {
+    fetch.mockImplementationOnce(() => ({ json: () => Promise.resolve(mockCategories) }));
+
+    const categories = await fetchCategories();
+
+    expect(categories).toHaveLength(categories.length);
+    expect(fetch).toHaveBeenCalledWith(APIS.CATEGORIES);
   });
 });
