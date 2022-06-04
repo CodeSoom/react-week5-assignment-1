ecbadcbc8b4a1ea5537e883181f130f9e2ca1a7f
ByeongSooLee-KR <djaxornwkd12@gmail.com>
2022-06-04 15:30:32 +0900

    Refacotor: refacotring button.test by addinmg more cased

--- src/components/Button.test.jsx
+++ src/components/Button.test.jsx
@@ -1,15 +1,40 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Button from './Button';
 
-test('Button', () => {
+describe('Button', () => {
+  const handleClick = jest.fn();
+
   const item = {
     id: 1,
     name: '한식',
   };
 
-  const { getByText } = render(<Button selected name={item.name} />);
+  context('with selected props', () => {
+    it('shows name and V text', () => {
+      const { getByText } = render(<Button selected name={item.name} />);
 
-  expect(getByText(/한식/)).not.toBeNull();
-  expect(getByText(/V/)).not.toBeNull();
+      expect(getByText(/V/)).not.toBeNull();
+
+      expect(getByText(/한식/)).not.toBeNull();
+    });
+
+    it('button clicked', () => {
+      const { getByText } = render(<Button selected name={item.name} onClick={handleClick} />);
+
+      expect(handleClick).not.toBeCalled();
+
+      fireEvent.click(getByText(/한식/));
+
+      expect(handleClick).toBeCalled();
+    });
+  });
+
+  context('without selected props', () => {
+    it("doesn'tshows V text", () => {
+      const { queryByText } = render(<Button selected={false} name={item.name} />);
+
+      expect(queryByText(/V/)).toBeNull();
+    });
+  });
 });
