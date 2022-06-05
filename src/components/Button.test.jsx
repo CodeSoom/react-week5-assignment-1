bdd30ec7e277b9fceffa693af68266d87b038b68
Mika <solinepik@gmail.com>
2022-06-05 23:27:03 +0900

    refactor: Button 테스트 name props -> children 변경

--- src/components/Button.test.jsx
+++ src/components/Button.test.jsx
@@ -2,26 +2,30 @@ import { fireEvent, render } from '@testing-library/react';
 
 import Button from './Button';
 
+const BUTTON_TEXT = '버튼';
+const ACTIVE_TEXT = '(V)';
+
 describe('Button', () => {
-  it('props로 받은 name을 자식으로 가진 버튼이 보인다.', () => {
-    const { getByRole } = render(<Button name="버튼" />);
+  it(`children props로 전달된 ${BUTTON_TEXT} 텍스트를 가진 버튼이 보인다.`, () => {
+    const { getByRole } = render(<Button>{BUTTON_TEXT}</Button>);
 
     expect(getByRole('button')).toBeInTheDocument();
+    expect(getByRole('button')).toHaveTextContent(BUTTON_TEXT);
   });
 
   it('버튼을 클릭하면 props로 받은 onClick이 호출된다.', () => {
     const onClick = jest.fn();
 
-    const { getByRole } = render(<Button name="버튼" onClick={onClick} />);
+    const { getByRole } = render(<Button onClick={onClick}>{BUTTON_TEXT}</Button>);
 
-    fireEvent(getByRole('button')).click();
+    fireEvent.click(getByRole('button'));
 
     expect(onClick).toBeCalled();
   });
 
-  it('버튼에 active props가 true면 버튼에 (V)표시가 추가된다.', () => {
-    const { getByRole } = render(<Button name="버튼" active />);
+  it(`active props가 true면 버튼에 ${ACTIVE_TEXT}표시가 추가된다.`, () => {
+    const { getByText } = render(<Button active>{BUTTON_TEXT}</Button>);
 
-    expect(getByRole('button')).toHaveTextContent(/(V)/g);
+    expect(getByText(ACTIVE_TEXT, { exact: false })).toBeInTheDocument();
   });
 });
