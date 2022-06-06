7aa56c2f4c5d65427b1968206546316a4a56d314
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-06 16:49:09 +0900

    강의 따라가기

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+/**
+ * 0. 지역, 분류 목록을 읽기
+ * 1. 지역 선택 - Regions <- API
+ * 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API
+ * 3. 식당 목록 - Restaurants <- API (with region, category)
+ */
 
-import { loadCategories, loadRegions } from './action';
-
-import CategoriesContainer from './CategoriesContainer';
-import RegionsContainer from './RegionsContainer';
+function RegionsContainer() {
+  return null;
+}
+function CategoriesContainer() {
+  return null;
+}
+function RestaurntsContainer() {
+  return null;
+}
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadCategories());
-    dispatch(loadRegions());
-  }, []);
-
   return (
     <>
-      <CategoriesContainer />
       <RegionsContainer />
+      <CategoriesContainer />
+      <RestaurntsContainer />
     </>
   );
 }
