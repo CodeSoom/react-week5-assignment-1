dad3903cd9e88d29936b073f89db0c15f503abb5
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-04 23:25:26 +0900

    refactor: App.jsx 가독성을 위한 코드 수정
    구조분해를 이용해 각 배열들 map 함수 속 콜백함수의 가독성을 높임
    
    레스토랑 목록을 예외케이스만 뒀던 걸 레스토랑 목록  map()으로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 
+import { regions, categories, restaurants } from '../__fixture__/restaurantsInfo';
+
 export default function App() {
-  // Todo: dummy를 server에서 받아온 data로 대체하기
-  const regions = [{ id: 1, name: '서울' }, { id: 2, name: '대전' }, { id: 3, name: '대구' }, { id: 4, name: '부산' }, { id: 5, name: '광주' }, { id: 6, name: '강원도' }, { id: 7, name: '인천' }, { id: 8, name: '제주' }, { id: 9, name: '전주' }, { id: 10, name: '순천' }, { id: 11, name: '독도' }];
-  const categories = [{ id: 1, name: '한식' }, { id: 2, name: '중식' }, { id: 3, name: '일식' }, { id: 4, name: '양식' }, { id: 5, name: '분식' }, { id: 6, name: '과자' }, { id: 7, name: '치킨' }, { id: 1003, name: '테스트' }, { id: 1034, name: '음료' }, { id: 1036, name: '사탕' }, { id: 1474, name: 'qq' }];
+  // Todo: __fixture__에서 가져온 목록들을 api server에서 받아온 data로 대체
   const [selectedRegion, setSelectedRegion] = useState('');
   const [selectedCategory, setSelectedCategory] = useState('');
 
@@ -14,28 +14,27 @@ export default function App() {
   return (
     <>
       <ul>
-        {regions.map((region) => (
-          <li key={region.id}>
-            <button type="button" onClick={() => handleClick(region.name, setSelectedRegion)}>
-              {region.name}
-              {selectedRegion === region.name && ('(V)')}
+        {regions.map(({ name, id }) => (
+          <li key={id}>
+            <button type="button" onClick={() => handleClick(name, setSelectedRegion)}>
+              {selectedRegion !== name ? (`${name}`) : (`${name}(V)`)}
             </button>
           </li>
         ))}
       </ul>
       <ul>
-        {categories.map((category) => (
-          <li key={category.id}>
-            <button type="button" onClick={() => handleClick(category.name, setSelectedCategory)}>
-              {category.name}
-              {selectedCategory === category.name && ('(V)')}
+        {categories.map(({ name, id }) => (
+          <li key={id}>
+            <button type="button" onClick={() => handleClick(name, setSelectedCategory)}>
+              {selectedCategory !== name ? (`${name}`) : (`${name}(V)`)}
             </button>
           </li>
         ))}
       </ul>
-      <section>
-        <p>지역과 카테고리를 입력해주세요!</p>
-      </section>
+      <ul>
+        {/* Todo: 불러올 restaurants가 없을 경우에 대한 예외 케이스 추가 */}
+        {restaurants.map(({ name, id }) => (<li key={id}>{name}</li>))}
+      </ul>
     </>
   );
 }
