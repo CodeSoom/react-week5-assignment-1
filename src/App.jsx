ed1b24547d5bcb5c7a6f1aace8621fb8b084248d
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-04 20:16:37 +0900

    App.jsx 구현
    작성한 테스트 코드가 통과될 수 있게 App.jsx의 ui를 구현했습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
 export default function App() {
+  const regions = ['서울', '대전', '대구', '부산', '광주', '강원도', '인천'];
+  const categories = ['한식', '중식', '일식', '양식', '분식'];
+
+  const handleClick = () => {
+    // Todo: handleClick 이벤트 작성
+  };
+
   return (
-    <></>
+    <>
+      <section>
+        {regions.map((region) => (
+          <button type="button" key={region} onClick={handleClick}>
+            {region}
+          </button>
+        ))}
+      </section>
+      <section>
+        {categories.map((category) => (
+          <button type="button" key={category} onClick={handleClick}>
+            {category}
+          </button>
+        ))}
+      </section>
+      <section>
+        <p>지역과 카테고리를 입력해주세요!</p>
+      </section>
+    </>
   );
 }
