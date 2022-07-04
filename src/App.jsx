5fdc037b62fe239755f1a15457f1c0cda363783e
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-04 20:58:00 +0900

    App 구현 - click event 추가
    작성한 테스트 코드를 따라 handleClick 함수를 작성했습니다.
    클릭 된 버튼에 v가 표시되도록 했습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
-export default function App() {
-  const regions = ['서울', '대전', '대구', '부산', '광주', '강원도', '인천'];
-  const categories = ['한식', '중식', '일식', '양식', '분식'];
+import { useState } from 'react';
 
-  const handleClick = () => {
-    // Todo: handleClick 이벤트 작성
+export default function App() {
+  // Todo: dummy를 server에서 받아온 data로 대체하기
+  const regions = [{ id: 1, name: '서울' }, { id: 2, name: '대전' }, { id: 3, name: '대구' }, { id: 4, name: '부산' }, { id: 5, name: '광주' }, { id: 6, name: '강원도' }, { id: 7, name: '인천' }, { id: 8, name: '제주' }, { id: 9, name: '전주' }, { id: 10, name: '순천' }, { id: 11, name: '독도' }];
+  const categories = [{ id: 1, name: '한식' }, { id: 2, name: '중식' }, { id: 3, name: '일식' }, { id: 4, name: '양식' }, { id: 5, name: '분식' }, { id: 6, name: '과자' }, { id: 7, name: '치킨' }, { id: 1003, name: '테스트' }, { id: 1034, name: '음료' }, { id: 1036, name: '사탕' }, { id: 1474, name: 'qq' }];
+  const [selectedRegion, setSelectedRegion] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  const handleClick = (name, setSelected) => {
+    setSelected(name);
   };
 
   return (
     <>
-      <section>
+      <ul>
         {regions.map((region) => (
-          <button type="button" key={region} onClick={handleClick}>
-            {region}
-          </button>
+          <li key={region.id}>
+            <button type="button" onClick={() => handleClick(region.name, setSelectedRegion)}>
+              {region.name}
+              {selectedRegion === region.name && ('(V)')}
+            </button>
+          </li>
         ))}
-      </section>
-      <section>
+      </ul>
+      <ul>
         {categories.map((category) => (
-          <button type="button" key={category} onClick={handleClick}>
-            {category}
-          </button>
+          <li key={category.id}>
+            <button type="button" onClick={() => handleClick(category.name, setSelectedCategory)}>
+              {category.name}
+              {selectedCategory === category.name && ('(V)')}
+            </button>
+          </li>
         ))}
-      </section>
+      </ul>
       <section>
         <p>지역과 카테고리를 입력해주세요!</p>
       </section>
