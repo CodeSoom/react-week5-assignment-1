7a0a44fad449160102db40786d6dc8f25381c6fe
Woo Dy <kjop7419@naver.com>
2022-07-04 21:28:26 +0900

    test (setRegions) 테스트 코드 작성

--- src/store/reducer.test.js
+++ src/store/reducer.test.js
@@ -1,9 +1,9 @@
-import { setCategories } from './actions';
+import { setCategories, setRegions } from './actions';
 
 import reducer from './reducer';
 
 describe('setCategories', () => {
-  it('카테고리가 변경됩니다.', () => {
+  it('카테고리리스트가 변경됩니다.', () => {
     const categories = [
       { id: 1, name: '한식' },
     ];
@@ -15,5 +15,19 @@ describe('setCategories', () => {
     const state = reducer(initialState, setCategories(categories));
 
     expect(state.categories).toHaveLength(1);
+  });
+
+  it('지역리스트가 변경됩니다.', () => {
+    const regions = [
+      { id: 101, name: '부산' },
+    ];
+
+    const initialState = {
+      regions: [],
+    };
+
+    const state = reducer(initialState, setRegions(regions));
+
+    expect(state.regions).toHaveLength(1);
   });
 });
