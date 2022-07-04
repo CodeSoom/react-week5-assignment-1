6828b63df4145bbfc50d4ec6815728938d3d5647
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-04 20:24:55 +0900

    App.jsx에 버튼 클릭할 때의 테스트코드 추가
    handleClick 함수 호출하는 테스트 코드 작성
    선택된 버튼에 v표시되는 테스트 코드 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import App from './App';
 
@@ -6,6 +6,8 @@ describe('<App/>', () => {
   const renderApp = () => ((
     render(<App />)
   ));
+
+  const handleClick = jest.fn();
 
   it('지역 목록이 보임.', () => {
     const { getByText } = renderApp();
@@ -21,5 +23,23 @@ describe('<App/>', () => {
     const categories = ['한식', '중식', '일식', '양식', '분식'];
 
     categories.map((category) => expect(getByText(`${category}`)).toBeInTheDocument());
+  });
+
+  describe('버튼을 클릭할 때', () => {
+    it('handleClcik 함수 호출됨', () => {
+      const { getByRole } = renderApp();
+
+      fireEvent.click(getByRole('button'));
+
+      expect(handleClick).toBeCalled();
+    });
+
+    it('선택된 버튼에 V표시가 보임', () => {
+      const { getByText, container } = renderApp();
+
+      fireEvent.click(getByText('한식'));
+
+      expect(container).toHaveTextContent('한식(V)');
+    });
   });
 });
