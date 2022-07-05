38ee60bb889330fefdb5bacd7ae89d0c4e8213b2
Woo Dy <kjop7419@naver.com>
2022-07-05 19:28:43 +0900

    test (Category) Category 컴포넌트 테스트를 작성하였습니다.

---
+++ src/components/Category.test.jsx
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+
+import Category from './Category';
+
+describe('Category', () => {
+  it('카테고리 목록이 보여집니다.', () => {
+    const categories = {
+      id: 1, name: '한식',
+    };
+
+    const { container } = render((
+      <Category
+        categories={categories}
+      />
+    ));
+
+    expect(container).toHaveTextContent('한식');
+  });
+});
