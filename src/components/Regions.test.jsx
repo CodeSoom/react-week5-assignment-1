3c56e547af318f112a09d6f5811917d1ac7d398b
Woo Dy <kjop7419@naver.com>
2022-07-05 19:33:50 +0900

    test(Regions) 테스트 코드 작성

---
+++ src/components/Regions.test.jsx
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+
+import Regions from './Regions';
+
+describe('Regions', () => {
+  it('지역 목록이 보여집니다.', () => {
+    const regions = {
+      id: 1, name: '대구',
+    };
+
+    const { container } = render((
+      <Regions
+        name={regions.name}
+      />
+    ));
+
+    expect(container).toHaveTextContent('대구');
+  });
+});
