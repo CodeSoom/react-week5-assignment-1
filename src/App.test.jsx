d67c6e46dc6429df5e5ee08d5f2ff63998be830f
seungwoo Kim <kyh196201@gmail.com>
2022-07-06 01:24:43 +0900

    App 컴포넌트에서 loadRegions 호출에 대한 테스트 추가
    - 처음 앱이 실행될 때 loadRegions를 실행하는지 체크하는 테스트 코드를 추가했습니다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,23 +1,44 @@
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import given from 'given2';
 
-import App from './App';
-
 import REGIONS from './fixtures/regions';
 
+import {
+  loadRegions,
+} from './store/async-actions';
+
+import App from './App';
+
 jest.mock('react-redux');
+jest.mock('./services/api');
+jest.mock('./store/async-actions');
 
 describe('<App />', () => {
   given('regions', () => []);
+
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
 
   useSelector.mockImplementation((selector) => selector({
     regions: given.regions,
   }));
 
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   const renderApp = () => render((<App />));
+
+  it('load regions initially', () => {
+    renderApp();
+
+    expect(dispatch).toBeCalled();
+    expect(loadRegions).toBeCalledTimes(1);
+  });
 
   context('with regions', () => {
     it('renders regions', () => {
