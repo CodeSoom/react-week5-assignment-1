1251cb943f1357e0e33436eb71b3b445fd8073cf
2scent <ad7549@gmail.com>
2022-07-05 18:54:17 +0900

    Refactor api service

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,11 @@
-export const fetchRegions = async () => {
-  const url = 'https://eatgo-customer-api.ahastudio.com/regions';
-  const response = await fetch(url);
+const fetchApi = async (api) => {
+  const response = await fetch(`https://eatgo-customer-api.ahastudio.com${api}`);
   const data = await response.json();
   return data;
 };
 
-export const fetchCategories = async () => {
-  const url = 'https://eatgo-customer-api.ahastudio.com/categories';
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-};
+export const fetchRegions = async () => fetchApi('/regions');
 
-export const fetchRestaurants = async (region, category) => {
-  const url = `https://eatgo-customer-api.ahastudio.com/restaurants?region=${region.name}&category=${category.id}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-};
+export const fetchCategories = async () => fetchApi('/categories');
+
+export const fetchRestaurants = async (region, category) => fetchApi(`/restaurants?region=${region.name}&category=${category.id}`);
