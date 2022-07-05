c88f3d0aede2fef096c74e27b4fccabd0f429c21
minsuk <alstjr0183@naver.com>
2022-07-06 02:02:21 +0900

    feat: app 구현 app.test를 토대로 app을 구현하였습니다

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import React, { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { loadRegions } from './actions-async';
+
+import Regions from './Regions';
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  const { isLoading } = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(loadRegions());
+  }, []);
+
+  if (isLoading) {
+    return <div>로딩중입니다!</div>;
+  }
+
   return (
-    <></>
+    <div>
+      <Regions />
+    </div>
   );
 }
