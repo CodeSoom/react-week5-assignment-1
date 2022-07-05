f3be4947dea4577794e92bf8cbbd23748e9749de
seungwoo Kim <kyh196201@gmail.com>
2022-07-06 00:09:22 +0900

    Reducer setRegions 테스트 통과

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,6 +2,15 @@ export const initialState = {
   regions: [],
 };
 
-const reducer = (state = initialState) => state;
+const reducer = (state = initialState, action) => {
+  if (action.type === 'setRegions') {
+    return {
+      ...state,
+      regions: action.payload.regions,
+    };
+  }
+
+  return state;
+};
 
 export default reducer;
