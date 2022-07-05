f889ec90943c66d2355d96b0f03b41d26eaee96d
seungwoo Kim <kyh196201@gmail.com>
2022-07-06 01:25:24 +0900

    App 컴포넌트 테스트 통과
    - 최초 App이 렌더링될 때 loadRegions를 호출했습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
+
+import {
+  loadRegions,
+} from './store/async-actions';
+
 import RegionsContainer from './containers/RegionsContainer';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRegions());
+  }, []);
+
   return (
     <>
       <RegionsContainer />
