92592032874ef839db76729325a7fb885f054d64
Woo Dy <kjop7419@naver.com>
2022-07-06 20:21:11 +0900

    fix(App) RegionsContainer 와 CategoriesContainer 연결 둘 다 컨테이너에서 불러오기 때문에 공백을 두지 않았습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import CategoriesContainer from './container/CategoriesContainer';
+import RegionsContainer from './container/RegionsContainer';
+
 export default function App() {
   return (
-    <></>
+    <>
+      <RegionsContainer />
+      <CategoriesContainer />
+    </>
   );
 }
