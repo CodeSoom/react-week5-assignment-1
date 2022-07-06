b58f2627c11513757dbe04b23ea256ef30bc340b
Dave <dave.lee@stagefive.com>
2022-07-06 21:42:36 +0900

    [leehuun][react-week5-assignment-1]
     - 잘못 커밋된 내용 수정.

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -5,12 +5,10 @@ import { getRegions } from './actions';
 describe('reducer', () => {
   describe('getRegions', () => {
     it('regions의 리스트를 반환한다.', () => {
-      const prevState = initialState;
+      const prevState = { ...initialState };
 
-      prevState.region = 'sdfsdf';
-
-      console.log(prevState, initialState);
       const state = reducer(prevState, getRegions());
+
       expect(state.regions).not.toHaveLength(0);
     });
   });
