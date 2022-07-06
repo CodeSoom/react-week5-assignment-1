80540651f99d9de53ddabcfe57696042a2805260
Woo Dy <kjop7419@naver.com>
2022-07-06 22:46:46 +0900

    fix(api) api 함수 정의

---
+++ src/services/api.js
@@ -0,0 +1,17 @@
+const baseURL = 'https://eatgo-customer-api.ahastudio.com';
+
+export async function fetchRegions() {
+  const url = `${baseURL}/regions`;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data;
+}
+
+export async function fetchCategories() {
+  const url = `${baseURL}/categories`;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data;
+}
