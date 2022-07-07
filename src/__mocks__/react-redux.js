e0728c99a8eaaf5d7e6097f999b212da013f2fa4
Woo Dy <kjop7419@naver.com>
2022-07-07 20:07:42 +0900

    fix(async-actions) 비동기 mocks 추가

--- src/__mocks__/react-redux.js
+++ src/__mocks__/react-redux.js
@@ -1,3 +1,10 @@
+import categories from '../fixtures/categories';
+import regions from '../fixtures/regions';
+
 export const useDispatch = jest.fn();
 
 export const useSelector = jest.fn();
+
+export const fetchRegions = jest.fn(async () => regions);
+
+export const fetchCategories = jest.fn(async () => categories);
