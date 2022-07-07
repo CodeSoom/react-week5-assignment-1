e70c69d8aa8c6c65518023afd7929d5025da83de
minsuk <alstjr0183@naver.com>
2022-07-08 01:02:02 +0900

    feat: Regions.test - 렌더링 케이스 추가 지역이 선택되었을때 선택된 지역에 (V) 가 뜨는 케이스를 작성해주었습니다

--- src/Regions.test.jsx
+++ src/Regions.test.jsx
@@ -1,27 +1,35 @@
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
-
-import { regions } from './fixtures/restaurant';
+import { region, regions } from './fixtures/restaurant';
 
 import Regions from './Regions';
-
-jest.mock('react-redux');
 
 describe('Regions', () => {
   const handleClickSelectRegion = jest.fn();
 
-  useSelector.mockImplementation((selector) => selector({
-    regions,
-  }));
-
-  const renderRegions = () => render((
-    <Regions regions={regions} onClickSelectRegion={handleClickSelectRegion} />
+  const renderRegions = (paramRegion) => render((
+    <Regions region={paramRegion} regions={regions} onClickSelectRegion={handleClickSelectRegion} />
   ));
 
   it('regions가 렌더링된다', () => {
     const { container } = renderRegions();
 
     expect(container).toHaveTextContent(regions[0].name);
+  });
+
+  context('region이 없을때', () => {
+    it('(V) 표시가 보이지않는다', () => {
+      const { container } = renderRegions('');
+
+      expect(container).not.toHaveTextContent('(V)');
+    });
+  });
+
+  context('region이 있을때', () => {
+    it('(V) 표시가 렌더링된다', () => {
+      const { container } = renderRegions(region);
+
+      expect(container).toHaveTextContent('(V)');
+    });
   });
 });
