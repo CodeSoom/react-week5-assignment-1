7480d0e93a12def482dcaef09329a6e6d66aed0d
Woo Dy <kjop7419@naver.com>
2022-07-07 22:36:39 +0900

    test(RegionsContainer) async 테스트 코드 작성

--- src/container/RegionsContainer.test.jsx
+++ src/container/RegionsContainer.test.jsx
@@ -1,6 +1,8 @@
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { getRegions } from '../store/async-actions';
 
 import regions from '../fixtures/regions';
 
@@ -9,14 +11,34 @@ import RegionsContainer from './RegionsContainer';
 jest.mock('react-redux');
 
 describe('RegionsContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
   useSelector.mockImplementation((selector) => selector({
     regions,
   }));
 
+  function renderRegionsContainer() {
+    return render(
+      <RegionsContainer />,
+    );
+  }
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('regions API를 호출합니다.', () => {
+    const { container } = renderRegionsContainer();
+
+    expect(dispatch).toHaveBeenCalledWith(getRegions());
+    regions.forEach(({ name }) => {
+      expect(container).toHaveTextContent(name);
+    });
+  });
+
   it('지역 목록이 보여집니다.', () => {
-    const { container } = render((
-      <RegionsContainer />
-    ));
+    const { container } = renderRegionsContainer();
 
     expect(container).toHaveTextContent('부산');
     expect(container).toHaveTextContent('대구');
