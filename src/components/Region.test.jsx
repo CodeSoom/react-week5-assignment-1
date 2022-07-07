067973cda398abef3c2c4555259432d537b223a6
kyh196201 <kyh196201@gmail.com>
2022-07-07 17:36:05 +0900

    Region 테스트 코드 추가
    - 클릭 이벤트에 대한 테스트 코드를 추가했습니다.

--- src/components/Region.test.jsx
+++ src/components/Region.test.jsx
@@ -1,11 +1,17 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import Region from './Region';
 
 import REGIONS from '../fixtures/regions';
 
 describe('<Region />', () => {
-  const renderRegion = (region) => render((<Region region={region} />));
+  const handleClick = jest.fn();
+
+  const renderRegion = (region) => render((<Region region={region} onClick={handleClick} />));
+
+  beforeEach(() => {
+    handleClick.mockClear();
+  });
 
   it('render region name', () => {
     const region = REGIONS[0];
@@ -13,5 +19,17 @@ describe('<Region />', () => {
     const { container } = renderRegion(region);
 
     expect(container).toHaveTextContent(region.name);
+  });
+
+  it('listen click event', () => {
+    const region = REGIONS[0];
+
+    const { getByRole } = renderRegion(region);
+
+    expect(handleClick).not.toBeCalled();
+
+    fireEvent.click(getByRole('button'));
+
+    expect(handleClick).toBeCalledWith(region.id);
   });
 });
