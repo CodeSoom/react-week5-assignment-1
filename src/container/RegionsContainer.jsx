179eada159403fb99d332762739255195d7f968f
Woo Dy <kjop7419@naver.com>
2022-07-07 22:42:50 +0900

    fix(RegionsContainer) 테스트 통과

--- src/container/RegionsContainer.jsx
+++ src/container/RegionsContainer.jsx
@@ -1,11 +1,22 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
 
 import Regions from '../components/Regions';
 
+import { getRegions } from '../store/async-actions';
+
 export default function RegionsContainer() {
+  const dispatch = useDispatch();
+
   const { regions } = useSelector((state) => ({
     regions: state.regions,
   }));
+
+  useEffect(() => {
+    dispatch(getRegions());
+  }, []);
+
   return (
     <Regions
       regions={regions}
