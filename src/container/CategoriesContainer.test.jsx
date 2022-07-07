9e68ef15d9f3fa8731168c1dae0b9a5c390b7750
Woo Dy <kjop7419@naver.com>
2022-07-07 22:58:00 +0900

    refactor(CategoriesContainer) Api 호출 테스트 작성

--- src/container/CategoriesContainer.test.jsx
+++ src/container/CategoriesContainer.test.jsx
@@ -1,22 +1,44 @@
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import CategoriesContainer from './CategoriesContainer';
 
 import categories from '../fixtures/categories';
 
+import { getCategories } from '../store/async-actions';
+
 jest.mock('react-redux');
 
 describe('CategoriesContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
   useSelector.mockImplementation((selector) => selector({
     categories,
   }));
 
+  function renderCategoriesContainer() {
+    return render(
+      <CategoriesContainer />,
+    );
+  }
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('categories API를 호출합니다.', () => {
+    const { container } = renderCategoriesContainer();
+
+    expect(dispatch).toHaveBeenCalledWith(getCategories());
+    categories.forEach(({ name }) => {
+      expect(container).toHaveTextContent(name);
+    });
+  });
+
   it('카테고리 목록이 보여집니다.', () => {
-    const { container } = render((
-      <CategoriesContainer />
-    ));
+    const { container } = renderCategoriesContainer();
 
     expect(container).toHaveTextContent('한식');
     expect(container).toHaveTextContent('양식');
