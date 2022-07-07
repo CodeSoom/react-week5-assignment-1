6d366a5d0bd36cd52033cd805f870304bc78116c
2scent <ad7549@gmail.com>
2022-07-07 20:58:37 +0900

    Add test for listening click event

--- src/Regions.test.jsx
+++ src/Regions.test.jsx
@@ -1,21 +1,40 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import regions from '../fixtures/regions';
 
 import Regions from './Regions';
 
 describe('Regions', () => {
+  const handleClick = jest.fn();
+
+  const renderRegions = () => render((
+    <Regions
+      regions={regions}
+      onClick={handleClick}
+    />
+  ));
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('renders regions', () => {
-    const { container } = render((
-      <Regions
-        regions={regions}
-      />
-    ));
+    const { container } = renderRegions();
 
     regions
       .map((region) => region.name)
       .forEach(
         (regionName) => expect(container).toHaveTextContent(regionName),
       );
+  });
+
+  it('listens click event', () => {
+    const region = regions[2];
+
+    const { getByRole } = renderRegions();
+
+    fireEvent.click(getByRole('button', { name: region.name }));
+
+    expect(handleClick).toHaveBeenCalledWith(region);
   });
 });
