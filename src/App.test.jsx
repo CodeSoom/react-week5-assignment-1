f5aea159d5c6838367a5b7eae81f84eadac1f482
2scent <ad7549@gmail.com>
2022-07-07 19:40:25 +0900

    Edit App test for rendering fetched regions

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,33 @@
 import { render } from '@testing-library/react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import regions from '../fixtures/regions';
+
+import { loadRegions } from './async-actions';
 
 import App from './App';
 
+jest.mock('react-redux');
+jest.mock('./async-actions');
+
 describe('App', () => {
-  it('renders regions', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((state) => state({
+    regions,
+  }));
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders fetched regions', () => {
     const { container } = render(<App />);
 
+    expect(dispatch).toHaveBeenCalledWith(loadRegions());
     regions
       .map((region) => region.name)
       .forEach(
