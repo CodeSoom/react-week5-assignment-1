056bbb80db6ab8d6b6b0b89c4ccebb2e9d9354b4
Woo Dy <kjop7419@naver.com>
2022-07-07 23:01:52 +0900

    fix(CategoriesContainer) 테스트 통과

--- src/container/CategoriesContainer.jsx
+++ src/container/CategoriesContainer.jsx
@@ -1,11 +1,22 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { getCategories } from '../store/async-actions';
 
 import Categories from '../components/Categories';
 
 export default function CategoriesContainer() {
+  const dispatch = useDispatch();
+
   const { categories } = useSelector((state) => ({
     categories: state.categories,
   }));
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, []);
+
   return (
     <Categories
       categories={categories}
