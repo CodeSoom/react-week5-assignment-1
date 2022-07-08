f14489152d4ed9a7c37bb914f17cd47a1e7360cd
kyh196201 <kyh196201@gmail.com>
2022-07-08 19:46:08 +0900

    Regions 테스트 추가
    - 지역 버튼 클릭에 대한 테스트 코드를 추가했습니다.

--- src/components/Regions.test.jsx
+++ src/components/Regions.test.jsx
@@ -1,11 +1,20 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import Regions from './Regions';
 
 import REGIONS from '../fixtures/regions';
 
 describe('<Regions />', () => {
-  const renderRegions = (regions) => render((<Regions regions={regions} />));
+  const handleClickRegion = jest.fn();
+
+  const renderRegions = (regions) => render((<Regions
+    regions={regions}
+    onClickRegion={handleClickRegion}
+  />));
+
+  beforeEach(() => {
+    handleClickRegion.mockClear();
+  });
 
   context('without regions', () => {
     it('renders "지역을 불러오지 못했습니다."', () => {
@@ -21,6 +30,20 @@ describe('<Regions />', () => {
 
       REGIONS.forEach((region) => {
         expect(container).toHaveTextContent(region.name);
+      });
+    });
+
+    describe('click region', () => {
+      it('calls handleClickRegion', () => {
+        const region = REGIONS[0];
+
+        const { getByText } = renderRegions(REGIONS);
+
+        expect(handleClickRegion).not.toBeCalled();
+
+        fireEvent.click(getByText(region.name));
+
+        expect(handleClickRegion).toBeCalledWith(region.id);
       });
     });
   });
