138398c03d38b2a174cb8d43cf8aa245de2e775e
2scent <ad7549@gmail.com>
2022-07-08 15:35:10 +0900

    Edit App for rendering categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import categories from '../fixtures/categories';
+
 import RegionsContainer from './RegionsContainer';
 
 const App = () => (
   <>
     <RegionsContainer />
+    <ol>
+      {categories.map(({ id, name }) => (
+        <li key={id}>{name}</li>
+      ))}
+    </ol>
   </>
 );
 
