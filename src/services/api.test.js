a9ddbcecde0139faaccfd1b8775b318a4f5e63d5
Woo Dy <kjop7419@naver.com>
2022-07-08 22:47:16 +0900

    refactor(api) mockResolvedValue 이 부분에 대해 좀 이해가 안되는 부분이 있어..혹시 참고할만한 자료 있을까요!?...ㅠㅠ

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -1,13 +1,9 @@
-import regions from '../fixtures/regions';
-
 import { baseURL, fetchCategories, fetchRegions } from './api';
 
 describe('api', () => {
   describe('fetchRegions', () => {
     beforeEach(() => {
-      global.fetch = jest.fn(() => Promise.resolve({
-        json: () => Promise.resolve(regions),
-      }));
+      global.fetch = jest.fn().mockResolvedValue({ json: () => {} });
     });
 
     it('지역 목록 Api를 호출합니다.', async () => {
@@ -21,7 +17,7 @@ describe('api', () => {
     it('카테고리 목록 Api를 호출합니다.', async () => {
       await fetchCategories();
 
-      expect(fetch).toHaveBeenCalledWith(`${baseURL}/categories`);
+      expect(fetch).toHaveBeenCalledWith(`${baseURL}/regions`);
     });
   });
 });
