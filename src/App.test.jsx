63a78ab89ac3ae5ff460ab3502f2234bb7985958
Woo Dy <kjop7419@naver.com>
2022-07-09 01:05:05 +0900

    test(categories, restaurants) 테스트 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,12 +2,13 @@ import { render } from '@testing-library/react';
 
 import { useSelector, useDispatch } from 'react-redux';
 
-import { getRegions } from './store/async-actions';
+import { getCategories, getRegions, getRestaurants } from './store/async-actions';
 
 import App from './App';
 
 import regions from './fixtures/regions';
 import categories from './fixtures/categories';
+import restaurants from './fixtures/restaurants';
 
 jest.mock('react-redux');
 jest.mock('./store/async-actions');
@@ -20,6 +21,7 @@ describe('App', () => {
   useSelector.mockImplementation((selector) => selector({
     regions,
     categories,
+    restaurants,
   }));
 
   beforeEach(() => {
@@ -36,7 +38,28 @@ describe('App', () => {
     const { container } = renderApp();
 
     expect(dispatch).toHaveBeenCalledWith(getRegions());
+
     regions.forEach(({ name }) => {
+      expect(container).toHaveTextContent(name);
+    });
+  });
+
+  it('categories API를 호출합니다.', () => {
+    const { container } = renderApp();
+
+    expect(dispatch).toHaveBeenLastCalledWith(getCategories());
+
+    categories.forEach(({ name }) => {
+      expect(container).toHaveTextContent(name);
+    });
+  });
+
+  it('restaurants API를 호출합니다.', () => {
+    const { container } = renderApp();
+
+    expect(dispatch).toHaveBeenCalledWith(getRestaurants());
+
+    restaurants.forEach(({ name }) => {
       expect(container).toHaveTextContent(name);
     });
   });
