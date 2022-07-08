8c94ebe793ef9fe7406ce4bc905644e16967c4d8
bruceoh-rtm <brucegwanhooh@rtm.ai>
2022-07-08 17:06:26 +0900

    feat: redux 추가

---
+++ src/actions.js
@@ -0,0 +1,47 @@
+import { fetchCategories } from './services/api';
+
+export function setRestaurants(restaurants) {
+  return {
+    type: 'setRestaurants',
+    payload: {
+      restaurants,
+    },
+  };
+}
+
+export function addRestaurant() {
+  return {
+    type: 'addRestaurant',
+  };
+}
+
+export function changeRestaurantField({ name, value }) {
+  return {
+    type: 'changeRestaurantField',
+    payload: {
+      name,
+      value,
+    },
+  };
+}
+
+export function setCategories(categories) {
+  return {
+    type: 'setCategories',
+    payload: { categories },
+  };
+}
+
+export function loadCategories() {
+  return async (dispatch) => {
+    const categories = await fetchCategories();
+    dispatch(setCategories(categories));
+  };
+}
+
+export function loadRestaurants() {
+  return async (dispatch) => {
+    const restaurants = [];
+    dispatch(setRestaurants(restaurants));
+  };
+}
---
+++ src/reducer.js
@@ -0,0 +1,56 @@
+const initialRestaurant = {
+  name: '',
+  category: '',
+  address: '',
+};
+
+export const initialState = {
+  newId: 100,
+  restaurants: [],
+  restaurant: initialRestaurant,
+  categories: [],
+};
+
+export default function reducer(state = initialState, action) {
+  if (action.type === 'setRestaurants') {
+    const { restaurants } = action.payload;
+    return {
+      ...state,
+      restaurants,
+    };
+  }
+
+  if (action.type === 'changeRestaurantField') {
+    const { name, value } = action.payload;
+    return {
+      ...state,
+      restaurant: {
+        ...state.restaurant,
+        [name]: value,
+      },
+    };
+  }
+
+  if (action.type === 'addRestaurant') {
+    const { newId, restaurant, restaurants } = state;
+    return {
+      ...state,
+      newId: newId + 1,
+      restaurants: [
+        ...restaurants,
+        { ...restaurant, id: newId },
+      ],
+      restaurant: initialRestaurant,
+    };
+  }
+
+  if (action.type === 'setCategories') {
+    const { categories } = action.payload;
+    return {
+      ...state,
+      categories,
+    };
+  }
+
+  return state;
+}
