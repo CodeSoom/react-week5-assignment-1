642c1c9fc9bec30d33bb7f80a62ee1a2872a6910
minsuk <alstjr0183@naver.com>
2022-07-08 22:08:09 +0900

    feat: test 데이터 추가 fixtures - resturant - restaurants 테스트 데이터를 추가했습니다

--- src/fixtures/restaurant.js
+++ src/fixtures/restaurant.js
@@ -4,4 +4,10 @@ export const categories = [{ id: 1, name: '한식' }, { id: 2, name: '중식' }]
 
 export const region = '서울';
 
-export const category = '중식';
+export const category = '한식';
+
+export const restaurants = [{
+  id: 6, categoryId: 1, name: '한국식 초밥', address: '서울 강남구', information: '한국식 초밥 in 서울 강남구',
+}, {
+  id: 14, categoryId: 1, name: '김초밥', address: '서울시 강남구 역삼동', information: '김초밥 in 서울시 강남구 역삼동',
+}];
