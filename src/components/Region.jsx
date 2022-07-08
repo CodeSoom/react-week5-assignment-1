8c0f5bd620da15802f9f7df6756311c7fd0f884a
kyh196201 <kyh196201@gmail.com>
2022-07-08 20:23:05 +0900

    Region 테스트 통과
    - 선택되었는지를 판단하기 위해 selected props를 추가했습니다.
    - 선택되었을 경우 (V) 문구를 표시하도록 수정했습니다.

--- src/components/Region.jsx
+++ src/components/Region.jsx
@@ -1,10 +1,12 @@
-export default function Region({ region, onClick }) {
+export default function Region({ region, onClick, selected }) {
   return (
     <button
       type="button"
       onClick={() => onClick(region.id)}
     >
       { region.name }
+      {' '}
+      { selected ? '(V)' : '' }
     </button>
   );
 }
