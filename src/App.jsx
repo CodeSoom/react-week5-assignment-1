b3cb4837bf56c8ebc1b7c0fe0cf1bcb2437312e7
bruceoh-rtm <brucegwanhooh@rtm.ai>
2022-07-08 17:09:44 +0900

    feat: app 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
+
+import RestaurantsContainer from './RestaurantsContainer';
+// import RestaurantsCreateContainer from './RestaurantsCreateContainer';
+import CategoriesContainer from './CategoriesContainer';
+
+import { loadCategories, loadRestaurants } from './actions';
+
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadCategories());
+    dispatch(loadRestaurants());
+  }, []);
+
   return (
-    <></>
+    <div>
+      <h1>Restaurants</h1>
+      <CategoriesContainer />
+      <RestaurantsContainer />
+      {/* <RestaurantsCreateContainer /> */}
+    </div>
   );
 }
