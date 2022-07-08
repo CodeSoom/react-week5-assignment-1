75963b80976ebd356d43053d98522bafd96f8ac3
Woo Dy <kjop7419@naver.com>
2022-07-08 22:34:04 +0900

    refactor(reducer.test) fixture 활용

--- src/store/reducer.test.js
+++ src/store/reducer.test.js
@@ -1,3 +1,6 @@
+import categories from '../fixtures/categories';
+import regions from '../fixtures/regions';
+
 import { setCategories, setRegions } from './actions';
 
 import reducer, { initailState } from './reducer';
@@ -15,33 +18,25 @@ describe('setCategories', () => {
 
   describe('setCategories', () => {
     it('카테고리리스트가 변경됩니다.', () => {
-      const categories = [
-        { id: 1, name: '한식' },
-      ];
-
       const initialState = {
         categories: [],
       };
 
       const state = reducer(initialState, setCategories(categories));
 
-      expect(state.categories).toHaveLength(1);
+      expect(state.categories).toHaveLength(3);
     });
   });
 
   describe('setRegions', () => {
     it('지역리스트가 변경됩니다.', () => {
-      const regions = [
-        { id: 101, name: '부산' },
-      ];
-
       const initialState = {
         regions: [],
       };
 
       const state = reducer(initialState, setRegions(regions));
 
-      expect(state.regions).toHaveLength(1);
+      expect(state.regions).toHaveLength(3);
     });
   });
 });
