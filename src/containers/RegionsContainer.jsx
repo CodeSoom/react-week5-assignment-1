01d9d0beaf923997990ad28eb6e50106b799828a
kyh196201 <kyh196201@gmail.com>
2022-07-08 19:58:38 +0900

    RegionsContainer 컴포넌트 테스트 통과
    - 지역 버튼 클릭 시 selectRegion 액션을 dispatch하도록 구현했습니다.

--- src/containers/RegionsContainer.jsx
+++ src/containers/RegionsContainer.jsx
@@ -1,13 +1,24 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { selectRegion } from '../store/actions';
 
 import Regions from '../components/Regions';
 
 export default function RegionsContainer() {
   const regions = useSelector((state) => state.regions);
 
+  const dispatch = useDispatch();
+
+  const handleClickRegion = (regionId) => {
+    dispatch(selectRegion(regionId));
+  };
+
   return (
     <>
-      <Regions regions={regions} />
+      <Regions
+        regions={regions}
+        onClickRegion={handleClickRegion}
+      />
     </>
   );
 }
