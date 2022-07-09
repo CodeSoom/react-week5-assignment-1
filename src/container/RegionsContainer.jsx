78ba41b239a80152ea232c85c313f5201bd8173c
Woo Dy <kjop7419@naver.com>
2022-07-09 11:28:23 +0900

    fix(RegionsContainer) 테스트 통과

--- src/container/RegionsContainer.jsx
+++ src/container/RegionsContainer.jsx
@@ -3,23 +3,31 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import Regions from '../components/Regions';
+import { selectRegion } from '../store/actions';
 
 import { getRegions } from '../store/async-actions';
 
 export default function RegionsContainer() {
   const dispatch = useDispatch();
 
-  const { regions } = useSelector((state) => ({
-    regions: state.regions,
-  }));
-
   useEffect(() => {
     dispatch(getRegions());
   }, []);
 
+  const { regions, selectedRegion } = useSelector((state) => ({
+    regions: state.regions,
+    selectedRegion: state.selectedRegion,
+  }));
+
+  const handleClickRegion = (region) => {
+    dispatch(selectRegion(region));
+  };
+
   return (
     <Regions
       regions={regions}
+      selectedRegion={selectedRegion}
+      onClick={handleClickRegion}
     />
   );
 }
