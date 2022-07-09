5e7f8bc70b7fb8a5288a57fd49412bb404f5d4c7
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-09 22:21:04 +0900

    test: (green) App.test.jsx useSelector.mockImplementation 추가
    테스트코드 통과를 위해 useSelector mock function 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,18 @@
 import { render } from '@testing-library/react';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { useDispatch } from 'react-redux';
 import App from './App';
 
 describe('<App/>', () => {
   const dispatch = jest.fn();
 
   useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    regions: [
+      { id: 1, name: '서울' },
+    ],
+  }));
 
   const renderApp = () => ((
     render(<App />)
