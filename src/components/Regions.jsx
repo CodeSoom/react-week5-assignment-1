1e121609b9ff705b51b8ba100f72fb27d9c90eed
Woo Dy <kjop7419@naver.com>
2022-07-09 11:29:03 +0900

    fix(Regions) 테스트 통과

--- src/components/Regions.jsx
+++ src/components/Regions.jsx
@@ -1,11 +1,12 @@
-export default function Regions({ regions }) {
+export default function Regions({ regions, selectedRegion, onClick }) {
   return (
     <ul>
       {
         regions.map((region) => (
           <li key={region.id}>
-            <button type="button">
+            <button type="button" onClick={() => onClick(region.name)}>
               {region.name}
+              {selectedRegion === region.name && '(V)'}
             </button>
           </li>
         ))
