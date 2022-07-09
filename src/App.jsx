c3f7a4f883bfcfcfb7c610d4e7b22a46ffa9f0eb
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-09 17:08:47 +0900

    refactor: (green) App 컴포넌트에서 dispatch 호출 테스트코드 통과를 위한 리팩토링
    react-redux 폴더에 useDispatch mock 함수 추가
    App.jsx에서 디스패치 호출

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { loadInitialData } from './action';
+
 import CategoryContainer from './CategoryContainer';
 import RegionContainer from './RegionContainer';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadInitialData());
+  }, []);
+
   return (
     <>
       <RegionContainer />
