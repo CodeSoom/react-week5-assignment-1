8dbe81ca1cad25842af85270ab5b102d5d5859f9
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-09 17:07:20 +0900

    test: (red) App컴포넌트에서 dispatch 호출을 위한 테스트코드 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,38 +1,19 @@
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
+import { useDispatch } from 'react-redux';
 import App from './App';
 
-import { regions, categories } from '../__fixture__/restaurantsInfo';
-
 describe('<App/>', () => {
-  const regionNames = regions.map((region) => region.name);
-  const categoryNames = categories.map((category) => category.name);
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
 
   const renderApp = () => ((
     render(<App />)
   ));
 
-  it('지역 목록이 보임.', () => {
-    const { getByText } = renderApp();
-    regionNames.forEach((async (name) => {
-      await expect(getByText(name)).not.toBeNull();
-    }));
-  });
-
-  it('카테고리 목록이 보임.', () => {
-    const { getByText } = renderApp();
-    categoryNames.forEach((async (name) => {
-      await expect(getByText(name)).not.toBeNull();
-    }));
-  });
-
-  it('선택된 버튼에 V표시가 보임', () => {
-    const { getByText, container } = renderApp();
-
-    setTimeout(() => {
-      fireEvent.click(getByText('한식'));
-
-      expect(container).toHaveTextContent('한식(V)');
-    }, 100);
+  it('dispatch 호출', () => {
+    renderApp();
+    expect(dispatch).toBeCalled();
   });
 });
