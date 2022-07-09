58506d3408a3b098c41a94bda2c90b00a2ec60c1
Woo Dy <kjop7419@naver.com>
2022-07-09 22:27:54 +0900

    test(Categories) handleClickCategory 테스트 추가

--- src/components/Categories.test.jsx
+++ src/components/Categories.test.jsx
@@ -1,18 +1,42 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import categories from '../fixtures/categories';
 
 import Categories from './Categories';
 
 describe('Categories', () => {
-  it('카테고리 목록이 보여집니다.', () => {
-    const { container } = render((
+  const defaultCategory = categories[0].id;
+
+  const handleClickCategory = jest.fn();
+
+  function renderCategories({ selectedCategory } = {}) {
+    return render(
       <Categories
         categories={categories}
-      />
-    ));
+        selectedCategory={selectedCategory}
+        onClick={handleClickCategory}
+      />,
+    );
+  }
+
+  it('카테고리 목록이 보여집니다.', () => {
+    const { container } = renderCategories();
 
     categories.forEach(({ name }) => {
       expect(container).toHaveTextContent(name);
     });
+  });
+
+  it('category를 클릭하면 handleClcikCategory가 호출됩니다.', () => {
+    const { getByRole } = renderCategories();
+
+    fireEvent.click(getByRole('button', { id: defaultCategory }));
+
+    expect(handleClickCategory).toHaveBeenCalledWith(defaultCategory);
+  });
+
+  it('선택되었으면 (V)가 함께 보여집니다.', () => {
+    const { getByRole } = renderCategories({ selectedCategory: defaultCategory });
+
+    expect(getByRole('button', { name: `${defaultCategory}(V)` })).toBeInTheDocument();
   });
 });
