26ac96938c030cc893f6a3283fc5282f0bbec326
Woo Dy <kjop7419@naver.com>
2022-07-09 11:28:47 +0900

    test(Regions) 테스트코드 작성

--- src/components/Regions.test.jsx
+++ src/components/Regions.test.jsx
@@ -1,19 +1,43 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Regions from './Regions';
 
 import regions from '../fixtures/regions';
 
 describe('Regions', () => {
-  it('지역 목록이 보여집니다.', () => {
-    const { container } = render((
+  const defaultRegion = regions[0].name;
+
+  const handleClickRegion = jest.fn();
+
+  function renderRegion({ selectedRegion } = {}) {
+    return render(
       <Regions
         regions={regions}
-      />
-    ));
+        selectedRegion={selectedRegion}
+        onClick={handleClickRegion}
+      />,
+    );
+  }
+
+  it('지역 목록이 보여집니다.', () => {
+    const { container } = renderRegion();
 
     regions.forEach(({ name }) => {
       expect(container).toHaveTextContent(name);
     });
+  });
+
+  it('region을 클릭하면 handleClickRegion이 호출됩니다', () => {
+    const { getByRole } = renderRegion();
+
+    fireEvent.click(getByRole('button', { name: defaultRegion }));
+
+    expect(handleClickRegion).toHaveBeenCalledWith(defaultRegion);
+  });
+
+  it('선택되었으면 (V)가 함께 보여집니다.', () => {
+    const { getByRole } = renderRegion({ selectedRegion: defaultRegion });
+
+    expect(getByRole('button', { name: `${defaultRegion}(V)` })).toBeInTheDocument();
   });
 });
