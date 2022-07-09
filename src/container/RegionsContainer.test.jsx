befc5f1e0aa12e24446a578a4cd6dde2ab12d3f2
Woo Dy <kjop7419@naver.com>
2022-07-09 10:46:41 +0900

    test(selectRegion) 테스트 추가

--- src/container/RegionsContainer.test.jsx
+++ src/container/RegionsContainer.test.jsx
@@ -1,8 +1,10 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
 import { getRegions } from '../store/async-actions';
+
+import { selectRegion } from '../store/actions';
 
 import regions from '../fixtures/regions';
 
@@ -14,10 +16,13 @@ jest.mock('../store/async-actions');
 describe('RegionsContainer', () => {
   const dispatch = jest.fn();
 
+  const selectedRegion = regions[0];
+
   useDispatch.mockImplementation(() => dispatch);
 
   useSelector.mockImplementation((selector) => selector({
     regions,
+    selectedRegion,
   }));
 
   function renderRegionsContainer() {
@@ -38,5 +43,23 @@ describe('RegionsContainer', () => {
     regions.forEach(({ name }) => {
       expect(container).toHaveTextContent(name);
     });
+  });
+
+  it('특정 지역을 선택하면 selectRegion이 dispatch와 함께 호출됩니다.', () => {
+    const region = regions[0];
+
+    const { getByRole } = renderRegionsContainer();
+
+    fireEvent.click(getByRole('button', { name: region.name }));
+
+    expect(dispatch).toHaveBeenCalledWith(selectRegion());
+  });
+
+  it('특정 지역을 선택하면 V가 선택된 지역에 보여집니다.', () => {
+    const { getByRole } = renderRegionsContainer();
+
+    expect(getByRole('button'),
+      { name: `${selectedRegion.name}(V)` })
+      .toBeInTheDocument();
   });
 });
