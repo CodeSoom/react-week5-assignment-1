c362dabcea1fd84ae361ed3bb1a26e77e351cfd5
Woo Dy <kjop7419@naver.com>
2022-07-09 10:20:26 +0900

    fix(RestaurantsContainer) 테스트 통과

--- src/container/RestaurantsContainer.jsx
+++ src/container/RestaurantsContainer.jsx
@@ -1,3 +1,25 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getRestaurants } from '../store/async-actions';
+
 export default function RestaurantsContainer() {
-  return <div>RestaurantsContainer</div>;
+  const dispatch = useDispatch();
+
+  const { region, categoryId, restaurants } = useSelector((state) => ({
+    region: state.selectedRegion,
+    categoryId: state.categoryId,
+    restaurants: state.restaurants,
+  }));
+
+  useEffect(() => {
+    dispatch(getRestaurants({ region, categoryId }));
+  }, []);
+
+  return (
+    <ul>
+      {restaurants.map(({ id, name }) => (
+        <li key={id}>{name}</li>
+      ))}
+    </ul>
+  );
 }
