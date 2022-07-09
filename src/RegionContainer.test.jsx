79552baf6ecb531f7a1bf62621283ef53d6a35b6
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-09 19:15:03 +0900

    test: (refactor) 기존 지역버튼 클릭시 기대효과를 handle click 함수 호출에서dispatch 호출로 변경
    redux를 사용하게 돼서 기존 handleClick 함수 호출보단 dispatch 호출이 의미있어 변경

--- src/RegionContainer.test.jsx
+++ src/RegionContainer.test.jsx
@@ -1,21 +1,29 @@
 import { fireEvent, render } from '@testing-library/react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import RegionContainer from './RegionContainer';
 
 describe('<RegionContainer/>', () => {
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    regions: [
+      { id: 1, name: '서울' },
+    ],
+  }));
+
   const handleClick = jest.fn();
 
   const renderRegionContainer = () => ((
     render(<RegionContainer onClick={handleClick} />)
   ));
 
-  it('클릭하면 handleClick 호출.', async () => {
-    const { getByText } = await renderRegionContainer();
+  it('클릭하면 dispatch 호출.', () => {
+    const { getByText } = renderRegionContainer();
 
-    setTimeout(() => {
-      fireEvent.click(getByText('서울'));
+    fireEvent.click(getByText('서울'));
 
-      expect(handleClick).toBeCalled();
-    }, 100);
+    expect(dispatch).toBeCalled();
   });
 });
