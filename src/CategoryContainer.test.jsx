5125494a1880c54f14cd47e4dba795205a89b7a0
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-10 10:50:31 +0900

    test: (red) 클릭시 dispatch 호출로 변경
    기존 handleClick 함수 호출에서 dispatch 호출로 변경

--- src/CategoryContainer.test.jsx
+++ src/CategoryContainer.test.jsx
@@ -1,20 +1,29 @@
 import { fireEvent, render } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { categories } from '../__fixture__/restaurantsInfo';
 
 import CategoryContainer from './CategoryContainer';
 
 describe('<Category/>', () => {
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    categories,
+  }));
+
   const handleClick = jest.fn();
 
   const renderCategoryContainer = () => ((
     render(<CategoryContainer onClick={handleClick} />)
   ));
-  it('클릭하면 handleClick 호출.', async () => {
-    const { getByText } = await renderCategoryContainer();
 
-    setTimeout(() => {
-      fireEvent.click(getByText('한식'));
+  it('클릭하면 dispatch 호출.', () => {
+    const { getByText } = renderCategoryContainer();
 
-      expect(handleClick).toBeCalled();
-    }, 100);
+    fireEvent.click(getByText('한식'));
+
+    expect(dispatch).toBeCalled();
   });
 });
