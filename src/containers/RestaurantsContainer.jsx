e4873d27a00f4eb4c0f0584aeb90bdea830a8b21
seungwoo Kim <kyh196201@gmail.com>
2022-07-10 20:42:53 +0900

    RestaurantsContainer 테스트 통과
    - 선택된 지역, 카테고리가 변경되면 레스토랑을 조회하도록 구현했습니다.

--- src/containers/RestaurantsContainer.jsx
+++ src/containers/RestaurantsContainer.jsx
@@ -1,11 +1,27 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+import { loadRestaurants } from '../store/async-actions';
 
 import Restaurants from '../components/Restaurants';
 
 export default function RestaurantsContainer() {
-  const { restaurants } = useSelector((state) => ({
+  const dispatch = useDispatch();
+
+  const { restaurants, selectedRegionId, selectedCategoryId } = useSelector((state) => ({
     restaurants: state.restaurants,
+    selectedRegionId: state.selectedRegionId,
+    selectedCategoryId: state.selectedCategoryId,
   }));
+
+  useEffect(() => {
+    if (selectedRegionId === null || selectedCategoryId === null) {
+      return;
+    }
+
+    dispatch(loadRestaurants());
+  }, [selectedRegionId, selectedCategoryId]);
 
   return (
     <Restaurants restaurants={restaurants} />
