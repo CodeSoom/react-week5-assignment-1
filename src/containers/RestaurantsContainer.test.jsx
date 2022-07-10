75555edcb3905026bba86a46343f1c051859fb0a
seungwoo Kim <kyh196201@gmail.com>
2022-07-10 20:42:17 +0900

    RestaurantsContainer 테스트 추가
    - 선택된 지역, 카테고리가 있을 경우 레스토랑을 조회하는 것에 대한
    테스트를 추가했습니다.

--- src/containers/RestaurantsContainer.test.jsx
+++ src/containers/RestaurantsContainer.test.jsx
@@ -1,19 +1,34 @@
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import given from 'given2';
+
+import { loadRestaurants } from '../store/async-actions';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
 import RESTAURANTS from '../fixtures/restaurants';
+import REGIONS from '../fixtures/regions';
+import CATEGORIES from '../fixtures/categories';
 
 jest.mock('react-redux');
+jest.mock('../store/async-actions');
 
 describe('<RestaurantsContainer />', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
     restaurants: given.restaurants,
+    selectedRegionId: given.selectedRegionId,
+    selectedCategoryId: given.selectedCategoryId,
   }));
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
   const renderRestaurantsContainer = () => render(<RestaurantsContainer />);
 
@@ -36,6 +51,17 @@ describe('<RestaurantsContainer />', () => {
       const { getByText } = renderRestaurantsContainer();
 
       expect(getByText('레스토랑을 불러오지 못했습니다.')).toBeInTheDocument();
+    });
+  });
+
+  context('with selectedRegionId and selectedCategoryId', () => {
+    given('selectedRegionId', () => REGIONS[0].id);
+    given('selectedselectedCategoryIdRegionId', () => CATEGORIES[0].id);
+
+    it('dispatch load restaurants', () => {
+      renderRestaurantsContainer();
+
+      expect(loadRestaurants).toBeCalled();
     });
   });
 });
