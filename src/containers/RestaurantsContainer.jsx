5769551f99d8ef255c0c04ae3d65c88510fb28a1
seungwoo Kim <kyh196201@gmail.com>
2022-07-10 19:23:51 +0900

    RestaurantsContainer 리팩터링
    - 내부 UI에 관련된 로직을 제거하고, Restaurants 컴포넌트를 렌더링했습니다.

--- src/containers/RestaurantsContainer.jsx
+++ src/containers/RestaurantsContainer.jsx
@@ -1,27 +1,13 @@
 import { useSelector } from 'react-redux';
 
+import Restaurants from '../components/Restaurants';
+
 export default function RestaurantsContainer() {
   const { restaurants } = useSelector((state) => ({
     restaurants: state.restaurants,
   }));
 
-  if (!restaurants || !restaurants.length) {
-    return (
-      <p>
-        레스토랑을 불러오지 못했습니다.
-      </p>
-    );
-  }
-
   return (
-    <ul>
-      {
-        restaurants.map((restaurant) => (
-          <li key={restaurant.id}>
-            {restaurant.name}
-          </li>
-        ))
-      }
-    </ul>
+    <Restaurants restaurants={restaurants} />
   );
 }
