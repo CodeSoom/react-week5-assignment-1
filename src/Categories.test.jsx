d1edcf8b7430a0baf59403a58533996efac4ee03
hanna244 <shhn0509@gmail.com>
2022-08-02 03:54:47 +0900

    Categories 테스트 추가

---
+++ src/Categories.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Categories from './Categories';
+
+jest.mock('react-redux');
+
+test('Categories', () => {
+  const categories = [{ id: 1, name: '한식' }];
+
+  const { getByText } = render(<Categories categories={categories} />);
+
+  expect(getByText(/한식/)).not.toBeNull();
+});
