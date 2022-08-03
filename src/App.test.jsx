a3a2da2bcb65a82ae2573038537d6a1582e3c0ff
Heejae Chang <heejae0704@gmail.com>
2022-08-03 16:08:38 +0900

    App 기본형태 테스트 작성 (RED)

---
+++ src/App.test.jsx
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+function customRender() {
+  return render(<App />)
+}
+
+describe('App', () => {
+  test('renders addresses', () => {
+    const { queryByText } = customRender();
+
+    expect(queryByText('서울')).not.toBeNull();
+  });
+  
+  test('renders categories', () => {
+    const { queryByText } = customRender();
+
+    expect(queryByText('한식')).not.toBeNull();
+  })
+
+  test('renders restaurants', () => {
+    const { queryByText } = customRender();
+
+    expect(queryByText('양천주가')).not.toBeNull();
+  })
+});
