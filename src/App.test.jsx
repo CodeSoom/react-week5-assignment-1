8cafd8e3d454fcd5c8c39d0a9fa1ebaa0e21ef59
hanna244 <shhn0509@gmail.com>
2022-08-03 23:50:41 +0900

    App test 리팩토링 - state 기본 값 전달

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -8,7 +8,7 @@ jest.mock('react-redux');
 
 test('App', () => {
   useSelector.mockImplementation((selector) => selector({
-    categories: [{ id: 1, name: '한식' }],
+    categories: [],
   }));
 
   const { getByText } = render(<App />);
