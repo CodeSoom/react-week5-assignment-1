b9bca19e73feac3ed5e56e149da098111f4a6a55
Heejae Chang <heejae0704@gmail.com>
2022-08-03 16:12:23 +0900

    App 컴포넌트 기본형태 구현 (GREEN)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 export default function App() {
   return (
-    <></>
+    <>
+      <div>
+        <ul>
+          <li>서울</li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>한식</li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>양천주가</li>
+        </ul>
+      </div>
+    </>
   );
 }
