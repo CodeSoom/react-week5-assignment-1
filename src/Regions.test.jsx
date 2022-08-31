4095ee73cf7e580cb739c2258e2dc6bd881abcba
0-Chan <kyc4421194@gmail.com>
2022-08-31 21:56:41 +0900

    :green_circle: Add Regions / Categories tests

---
+++ src/Regions.test.jsx
@@ -0,0 +1,28 @@
+import { render, fireEvent } from '@testing-library/react';
+
+import Regions from './Regions';
+
+import { regions } from '../__fixtures__/list';
+
+describe('<Regions />', () => {
+  it('shows regions', () => {
+    const { queryByText } = render(
+      <Regions regions={regions} />,
+    );
+
+    regions.forEach((region) => {
+      expect(queryByText(region.name)).not.toBeNull();
+    });
+  });
+
+  it('clicks each button to check it\'s selected', () => {
+    const { getByRole, queryByText } = render(
+      <Regions regions={regions} />,
+    );
+
+    regions.forEach((region) => {
+      fireEvent.click(getByRole('button', { name: region.name }));
+      expect(queryByText(region.name)).toBeEnabled();
+    });
+  });
+});
