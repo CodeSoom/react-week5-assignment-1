5cafe96e10e004ebdb3b0775e25f1bb640cc0832
0-Chan <kyc4421194@gmail.com>
2022-09-02 18:41:37 +0900

    :recycle: Refector <Regions /> test using given2
    Same as Categories.test.js.

--- src/Regions.test.jsx
+++ src/Regions.test.jsx
@@ -1,18 +1,27 @@
 import { render } from '@testing-library/react';
 
+import given from 'given2';
+
 import Regions from './Regions';
 
 import { regions } from '../__fixtures__/data';
 
 describe('<Regions />', () => {
+  given('regions', () => regions);
+  given('selectedRegion', () => undefined);
+
+  const renderRegions = () => render(
+    <Regions
+      regions={given.regions}
+      selectedRegion={given.selectedRegion}
+    />,
+  );
+
   context('without selectedRegion', () => {
+    given('selectedRegion', () => undefined);
+
     it('renders only regions', () => {
-      const { container } = render(
-        <Regions
-          regions={regions}
-          selectedRegion=""
-        />,
-      );
+      const { container } = renderRegions();
 
       regions.forEach((region) => {
         expect(container).toHaveTextContent(region.name);
@@ -21,15 +30,11 @@ describe('<Regions />', () => {
   });
 
   context('with selectedRegion', () => {
+    given('selectedRegion', () => regions[0].name);
     it('renders region with check mark(V)', () => {
-      const { getByText } = render(
-        <Regions
-          regions={regions}
-          selectedRegion="부산"
-        />,
-      );
+      const { getByText } = renderRegions();
 
-      expect(getByText('부산(V)')).not.toBeNull();
+      expect(getByText('서울(V)')).not.toBeNull();
     });
   });
 });
