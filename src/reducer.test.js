1f46668e0753cc2d67aa50f4b51bd04d6b7f4a97
0-Chan <kyc4421194@gmail.com>
2022-09-02 13:57:55 +0900

    :red_circle: Add a test for selectCategory

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -7,18 +7,35 @@ import {
 import { regions, categories } from '../__fixtures__/data';
 
 describe('reducer', () => {
-  describe('selecteRegion', () => {
-    it('updates region selected', () => {
+  describe('selectRegion', () => {
+    it('updates selectedRegion ', () => {
       const initialState = {
         selectedRegion: {
           id: '',
-          name: '서울',
+          name: '',
         },
       };
 
       const state = reducer(initialState, selectRegion(regions[0]));
 
+      expect(state.selectedRegion.id).toBe(1);
       expect(state.selectedRegion.name).toBe('서울');
+    });
+  });
+
+  describe('selectCategory', () => {
+    it('updates selectedCategory', () => {
+      const initialState = {
+        selectedCategory: {
+          id: '',
+          name: '',
+        },
+      };
+
+      const state = reducer(initialState, selectCategory(categories[0]));
+
+      expect(state.selectedCategory.id).toBe(1);
+      expect(state.selectedCategory.name).toBe('한식');
     });
   });
 });
