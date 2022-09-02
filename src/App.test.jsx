f6071e6f276bed75774d8af56cddecfd418f640f
0-Chan <kyc4421194@gmail.com>
2022-09-02 12:54:56 +0900

    :adhesive_bandage: Remove context from app.test.jsx
    That was unnecessary.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,18 +5,16 @@ import App from './App';
 import { regions, categories } from '../__fixtures__/data';
 
 describe('<App />', () => {
-  context('render App', () => {
-    it('shows regions and categories name', () => {
-      const { container } = render((
-        <App />
-      ));
+  it('renders regions and categories name', () => {
+    const { container } = render((
+      <App />
+    ));
 
-      regions.forEach((region) => {
-        expect(container).toHaveTextContent(region.name);
-      });
-      categories.forEach((category) => {
-        expect(container).toHaveTextContent(category.name);
-      });
+    regions.forEach((region) => {
+      expect(container).toHaveTextContent(region.name);
+    });
+    categories.forEach((category) => {
+      expect(container).toHaveTextContent(category.name);
     });
   });
 });
