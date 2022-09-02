08350a21a604938150dfb08655af0729e5e1a276
0-Chan <kyc4421194@gmail.com>
2022-09-02 16:58:37 +0900

    :red_circle: Fix <Categories /> test to use <Button />

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -1,34 +1,59 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import Categories from './Categories';
 
 import { categories } from '../__fixtures__/data';
 
+const renderCategory = (c, s) => render(
+  <Categories
+    categories={c}
+    selectedCategory={s}
+  />,
+);
+
 describe('<Categories />', () => {
-  it('shows categories', () => {
-    const { queryByText } = render(
-      <Categories
-        categories={categories}
-        selectedRegion="양식"
-      />,
-    );
+  context('without selectedCategory', () => {
+    it('renders categories', () => {
+      const { container } = renderCategory(categories);
 
-    categories.forEach((category) => {
-      expect(queryByText(category.name)).not.toBeNull();
+      categories.forEach((category) => {
+        expect(container).toHaveTextContent(category.name);
+      });
     });
   });
 
-  it('clicks each button to check it\'s selected', () => {
-    const { getByRole, queryByText } = render(
-      <Categories
-        categories={categories}
-        selectedRegion="양식"
-      />,
-    );
+  context('with selectedCategory', () => {
+    it('renders category with (V) mark', () => {
+      const { container } = renderCategory(categories, categories[0].name);
 
-    categories.forEach((category) => {
-      fireEvent.click(getByRole('button', { name: category.name }));
-      expect(queryByText(category.name)).toBeEnabled();
+      expect(container).toHaveTextContent('한식(V)');
     });
   });
+
+  // it('shows categories', () => {
+  //   const { queryByText } = render(
+  //     <Categories
+  //       categories={categories}
+  //       selectedRegion="양식"
+  //     />,
+  //   );
+
+  //   categories.forEach((category) => {
+  //     expect(queryByText(category.name)).not.toBeNull();
+  //   });
+  // });
+
+  // it('clicks each button to check it\'s selected', () => {
+  //   const { getByRole, queryByText } = render(
+  //     <Categories
+  //       categories={categories}
+  //       selectedRegion="양식"
+  //     />,
+  //   );
+
+  //   categories.forEach((category) => {
+  //     fireEvent.click(getByRole('button', { name: category.name }));
+  //     expect(queryByText(category.name)).toBeEnabled();
+  //   });
+  // });
 });
