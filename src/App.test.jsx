4a16f0d50e7f52c722ff51e55253a6a7406f0c4f
0-Chan <kyc4421194@gmail.com>
2022-09-03 19:45:09 +0900

    :green_circle: Fix <App /> test to have initial state

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { render } from '@testing-library/react';
 
@@ -9,9 +9,19 @@ import { regions, categories } from '../__fixtures__/data';
 jest.mock('../__mocks__/react-redux');
 jest.mock('./services/api');
 
+
 describe('<App />', () => {
+  const fixtureState = {
+    regions,
+    selectedRegion: '',
+    categories,
+    selectedCategory: '',
+  };
+
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
   useSelector.mockImplementation((selector) => selector({
-    categories: {},
+    categories: fixtureState.categories,
   }));
 
   it('renders regions and categories name', () => {
