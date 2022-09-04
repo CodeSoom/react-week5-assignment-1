31df928d9949ba7d4d402fe2668a09c922342dbe
0-Chan <kyc4421194@gmail.com>
2022-09-04 12:31:21 +0900

    :red_circle: Add a test for <RegionsContainer />
    Going to add onClick to pass the test.

--- src/RegionsContainer.test.jsx
+++ src/RegionsContainer.test.jsx
@@ -1,10 +1,12 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
 
 import RegionsContainer from './RegionsContainer';
 
 import { regions } from '../__fixtures__/data';
+
+const handleClick = jest.fn();
 
 describe('<RegionsContainer />', () => {
   it('renders regions', () => {
@@ -20,5 +22,22 @@ describe('<RegionsContainer />', () => {
     regions.forEach((region) => {
       expect(container).toHaveTextContent(region.name);
     });
+  });
+
+  it('clicks each button to check it\'s selected', () => {
+    useSelector.mockImplementation((selector) => selector({
+      regions,
+      selectedRegion: '',
+    }));
+
+    const { getByText } = render((
+      <RegionsContainer />
+    ));
+
+    regions.forEach((region) => {
+      fireEvent.click(getByText(region.name));
+    });
+
+    expect(handleClick).toBeCalledTimes(regions.length);
   });
 });
