af9a20424d4d8ebd9ab707d7a87de3a08c361ff6
0-Chan <kyc4421194@gmail.com>
2022-09-05 08:25:31 +0900

    :recycle: Refector containers test to have 100% coverage

--- src/CategoriesContainer.test.jsx
+++ src/CategoriesContainer.test.jsx
@@ -1,42 +1,74 @@
-import { useDispatch, useSelector } from 'react-redux';
-
 import { render, fireEvent } from '@testing-library/react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import CategoriesContainer from './CategoriesContainer';
+
 import { selectCategory } from './actions';
-
-import { categories } from '../__fixtures__/data';
-
-jest.mock('react-redux');
+import { categories, region } from '../__fixtures__/data';
 
 describe('<CategoriesContainer />', () => {
-  useSelector.mockImplementation((selector) => selector({
-    categories,
-    selectedRegion: '',
-    selectedCategory: '',
-  }));
-  it('renders categories', () => {
-    const { container } = render((
-      <CategoriesContainer />
-    ));
+  const dispatch = jest.fn();
 
-    categories.forEach((category) => {
-      expect(container).toHaveTextContent(category.name);
+  const KOREAN_FOOD = categories[0];
+
+  beforeEach(() => {
+    dispatch.mockClear();
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
+  context('without selectedCategory & selectedRegion', () => {
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        categories,
+        selectedRegion: {
+          id: undefined,
+          name: undefined,
+        },
+      }));
+    });
+
+    it('renders categories', () => {
+      const { container } = render((
+        <CategoriesContainer />
+      ));
+
+      categories.forEach((category) => {
+        expect(container).toHaveTextContent(category.name);
+      });
+    });
+
+    it('clicks each button to check it\'s selected', () => {
+      const { getByText, container } = render((
+        <CategoriesContainer />
+      ));
+
+      expect(container).toHaveTextContent(KOREAN_FOOD.name);
+
+      categories.forEach((category) => {
+        fireEvent.click(getByText(category.name));
+        expect(dispatch).toBeCalledWith(selectCategory(category));
+      });
     });
   });
 
-  it('clicks each button to check it\'s selected', () => {
-    const dispatch = jest.fn();
+  context('with selectedCategory & selectedRegion', () => {
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        categories,
+        selectedCategory: KOREAN_FOOD,
+        selectedRegion: region,
+      }));
+    });
 
-    useDispatch.mockImplementation(() => dispatch);
+    it('load restaurants', () => {
+      const { getByText } = render((
+        <CategoriesContainer />
+      ));
 
-    const { getByText } = render((
-      <CategoriesContainer />
-    ));
+      fireEvent.click(getByText(`${KOREAN_FOOD.name}(V)`));
 
-    categories.forEach((category) => {
-      fireEvent.click(getByText(category.name));
-      expect(dispatch).toBeCalledWith(selectCategory(category));
+      expect(typeof dispatch.mock.calls[1][0]).toBe('function');
     });
   });
 });
--- src/RegionsContainer.test.jsx
+++ src/RegionsContainer.test.jsx
@@ -4,38 +4,69 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import RegionsContainer from './RegionsContainer';
 
-import { regions } from '../__fixtures__/data';
 import { selectRegion } from './actions';
+import { regions, category } from '../__fixtures__/data';
 
 describe('<RegionsContainer />', () => {
-  useSelector.mockImplementation((selector) => selector({
-    regions,
-    selectedRegion: '',
-    selectedCategory: '',
-  }));
+  const dispatch = jest.fn();
 
-  it('renders regions', () => {
-    const { container } = render((
-      <RegionsContainer />
-    ));
+  const SEOUL = regions[0];
 
-    regions.forEach((region) => {
-      expect(container).toHaveTextContent(region.name);
+  beforeEach(() => {
+    dispatch.mockClear();
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
+  context('without selectedRegion & selectedCategory', () => {
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        regions,
+        selectedCategory: {
+          id: undefined,
+          name: undefined,
+        },
+      }));
+    });
+
+    it('renders regions', () => {
+      const { container } = render((
+        <RegionsContainer />
+      ));
+
+      regions.forEach((region) => {
+        expect(container).toHaveTextContent(region.name);
+      });
+    });
+
+    it('clicks each button to check it\'s selected', () => {
+      const { getByText } = render((
+        <RegionsContainer />
+      ));
+
+      regions.forEach((region) => {
+        fireEvent.click(getByText(region.name));
+        expect(dispatch).toBeCalledWith(selectRegion(region));
+      });
     });
   });
 
-  it('clicks each button to check it\'s selected', () => {
-    const dispatch = jest.fn();
+  context('with selectedRegion & selectedCategory', () => {
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        regions,
+        selectedRegion: SEOUL,
+        selectedCategory: category,
+      }));
+    });
 
-    useDispatch.mockImplementation(() => dispatch);
+    it('load restaurants', () => {
+      const { getByText } = render((
+        <RegionsContainer />
+      ));
 
-    const { getByText } = render((
-      <RegionsContainer />
-    ));
+      fireEvent.click(getByText(`${SEOUL.name}(V)`));
 
-    regions.forEach((region) => {
-      fireEvent.click(getByText(region.name));
-      expect(dispatch).toBeCalledWith(selectRegion(region));
+      expect(typeof dispatch.mock.calls[1][0]).toBe('function');
     });
   });
 });
