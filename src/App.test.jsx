75a7a552a5c16cdcebdbf2402f052e8a3305aade
task11 <task11@naver.com>
2022-10-11 23:19:22 +0900

    refactor: App 테스트코드 리팩토링

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,21 +1,41 @@
 import { render } from '@testing-library/react';
 
-import App from "./App";
+import { useDispatch, useSelector } from 'react-redux';
+
+import regions from '../fixtures/region';
+import categories from '../fixtures/categories';
+
+import App from './App';
+
+jest.mock('react-redux');
 
 describe('App', () => {
   it('Shows regions', () => {
+    const dispatch = jest.fn();
+
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      regions,
+      categories,
+      restaurants: [],
+    }));
+
     const { queryByText } = render((
       <App />
     ));
+
+    expect(dispatch).toBeCalledWith(({
+      type: 'setRegions',
+      payload: { regions },
+    }));
+
+    expect(dispatch).toBeCalledWith(({
+      type: 'setCategories',
+      payload: { categories },
+    }));
 
     expect(queryByText(/서울/)).not.toBeNull();
-  });
-
-  it('Shows categories', () => {
-    const { queryByText } = render((
-      <App />
-    ));
-
     expect(queryByText(/한식/)).not.toBeNull();
   });
 });
