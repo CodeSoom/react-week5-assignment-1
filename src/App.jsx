2e388a9c266828aca567a7b0c96e5f63eaf393e3
task11 <task11@naver.com>
2022-10-11 20:21:23 +0900

    feat: App 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+
 export default function App() {
+
+
   return (
-    <></>
+    <div>
+      <div>
+        <ul>
+          <li>
+            <button>
+              서울
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <button>
+              한식
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
