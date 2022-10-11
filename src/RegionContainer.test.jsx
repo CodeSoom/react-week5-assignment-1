a83b297d9636c8d823bcbbebcce6c7d5e3a4f766
task11 <task11@naver.com>
2022-10-11 23:21:45 +0900

    refactor: RegionContainer 컴포넌트 리팩토링

--- src/RegionContainer.test.jsx
+++ src/RegionContainer.test.jsx
@@ -1,15 +1,23 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import regions from '../fixtures/region';
 
 import RegionContainer from './RegionContainer';
 
+jest.mock('react-redux');
+
 describe('RegionContainer', () => {
   it('shows region', () => {
+    useSelector.mockImplementation((selector) => selector({
+      regions,
+      categories: [],
+      restaurants: [],
+    }));
+
     const { queryByText } = render((
-      <RegionContainer
-        regions={regions}
-      />
+      <RegionContainer />
     ));
 
     expect(queryByText(/서울/)).not.toBeNull();
