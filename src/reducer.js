e271f1fa4f582af760e84b69799d39fe31dea96f
task11 <task11@naver.com>
2022-10-11 23:19:06 +0900

    feat: reducer setRegion/Categories 구현

--- src/reducer.js
+++ src/reducer.js
@@ -5,5 +5,23 @@ const initialState = {
 };
 
 export default function reducer(state = initialState, action) {
+  if (action.type === 'setRegions') {
+    const { regions } = action.payload;
+
+    return {
+      ...state,
+      regions,
+    };
+  }
+
+  if (action.type === 'setCategories') {
+    const { categories } = action.payload;
+
+    return {
+      ...state,
+      categories,
+    };
+  }
+
   return state;
 }
