a7cd25af4a7ba452f8569565b07d698bfa9e1757
task11 <task11@naver.com>
2022-10-11 23:20:15 +0900

    refactor: CategoriesContainer 컴포넌트 테스트코드 리팩토링

--- src/CategoriesContainer.test.jsx
+++ src/CategoriesContainer.test.jsx
@@ -1,15 +1,22 @@
 import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
 
-import categories from '../fixtures/region';
+import categories from '../fixtures/categories';
 
 import CategoriesContainer from './CategoriesContainer';
 
+jest.mock('react-redux');
+
 describe('CategoriesContainer', () => {
   it('shows region', () => {
+    useSelector.mockImplementation((selector) => selector({
+      regions: [],
+      categories,
+      restaurants: [],
+    }));
+
     const { queryByText } = render((
-      <CategoriesContainer
-        categories={categories}
-      />
+      <CategoriesContainer />
     ));
 
     expect(queryByText(/한식/)).not.toBeNull();
