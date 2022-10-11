847f28938b01ae2cb46abdaa3f82c4f113a662a1
task11 <task11@naver.com>
2022-10-11 20:30:52 +0900

    feat: mock data

---
+++ fixtures/region.js
@@ -0,0 +1,20 @@
+const regions = [
+  {
+    id: 1,
+    name: '서울',
+  },
+  {
+    id: 2,
+    name: '대전',
+  },
+  {
+    id: 3,
+    name: '대구',
+  },
+  {
+    id: 4,
+    name: '부산',
+  },
+];
+
+export default regions;
