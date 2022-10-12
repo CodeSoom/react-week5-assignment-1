ea770f7e7b23c5317457db45c191b98f7e1b769e
task11 <task11@naver.com>
2022-10-12 18:08:04 +0900

    feat: restaurants 조회 api call 구현

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,21 @@
-const RegionListApi = 'https://eatgo-customer-api.ahastudio.com/regions';
-
-const CategoryListApi = 'https://eatgo-customer-api.ahastudio.com/categories';
-
 export async function fetchRegions() {
+  const RegionListApi = 'https://eatgo-customer-api.ahastudio.com/regions';
   const response = await fetch(RegionListApi);
   const data = await response.json();
   return data;
 }
 
 export async function fetchCategories() {
+  const CategoryListApi = 'https://eatgo-customer-api.ahastudio.com/categories';
   const response = await fetch(CategoryListApi);
   const data = await response.json();
+  return data;
+}
+
+export async function fetchRestaurants(regionName, categoryId) {
+  const RestaurntApi = `https://eatgo-customer-api.ahastudio.com/restaurants?region=${regionName}&category=${categoryId}`;
+  const response = await fetch(RestaurntApi);
+  const data = await response.json();
+
   return data;
 }
