f2a506e4c0a6e7af12c20555ff26c2d18cb34656
task11 <task11@naver.com>
2022-10-12 16:43:49 +0900

    feat: 선택한 categories 텍스트 변경을 위한 디스패치 테스트 작성

--- src/CategoriesContainer.test.jsx
+++ src/CategoriesContainer.test.jsx
@@ -1,5 +1,5 @@
-import { render } from '@testing-library/react';
-import { useSelector } from 'react-redux';
+import { fireEvent, render } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import categories from '../fixtures/categories';
 
@@ -20,5 +20,32 @@ describe('CategoriesContainer', () => {
     ));
 
     expect(queryByText(/한식/)).not.toBeNull();
+  });
+
+  it('dispatchs select region', () => {
+    const dispatch = jest.fn();
+
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      regions: [],
+      categories,
+      restaurants: [],
+      selectRegion: 0,
+      selectCategory: 0,
+    }));
+
+    dispatch.mockImplementation(() => dispatch);
+
+    const { queryByText } = render((
+      <CategoriesContainer />
+    ));
+
+    fireEvent.click(queryByText(/한식/));
+
+    expect(dispatch).toBeCalledWith({
+      type: 'setSelectCategoryId',
+      payload: { selectCategoryId: 1 },
+    });
   });
 });
