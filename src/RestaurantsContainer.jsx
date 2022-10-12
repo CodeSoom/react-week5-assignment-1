d53fe3fbb15bc3ceb08b075dd231e0730b0758ff
task11 <task11@naver.com>
2022-10-12 18:11:48 +0900

    feat: RestaurantsContainer 구현

---
+++ src/RestaurantsContainer.jsx
@@ -0,0 +1,13 @@
+import { useSelector } from 'react-redux';
+
+import Restaurants from './Restaurants';
+
+export default function RestaurantsContainer() {
+  const { restaurants } = useSelector((state) => ({
+    restaurants: state.restaurants,
+  }));
+
+  return (
+    <Restaurants restaurants={restaurants} />
+  );
+}
