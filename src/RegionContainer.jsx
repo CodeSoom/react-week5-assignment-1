7e225a44d8649258f3b3a90fb3e689e79d8ee76d
task11 <task11@naver.com>
2022-10-12 16:29:37 +0900

    feat: RegionContainer dispatch 구현

--- src/RegionContainer.jsx
+++ src/RegionContainer.jsx
@@ -1,13 +1,24 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSelectRegion } from './actions';
 
 import Regions from './Regions';
 
 export default function RegionContainer() {
-  const { regions } = useSelector((state) => ({
+  const dispatch = useDispatch();
+  const { regions, selectRegionId } = useSelector((state) => ({
     regions: state.regions,
+    selectRegionId: state.selectRegionId,
   }));
 
+  const handleClick = (regionId) => {
+    dispatch(setSelectRegion(regionId));
+  };
+
   return (
-    <Regions regions={regions} />
+    <Regions
+      regions={regions}
+      handleClick={handleClick}
+      selectRegionId={selectRegionId}
+    />
   );
 }
