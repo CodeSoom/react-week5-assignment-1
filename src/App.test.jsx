58997d8293e159ad50e150c272f2c6adedb50bd4
nada1587 <nada1587@naver.com>
2022-10-14 18:56:39 +0900

    - ListContainer 테스트 코드 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,15 +9,14 @@ jest.mock('./services/api');
 
 describe('App', () => {
   useSelector.mockImplementation((selector) => selector({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 11, name: '한식' },
-    ],
+    regions: [{ id: 1, name: '서울' }],
+    categories: [{ id: 11, name: '한식' }],
     restaurants: [
       {
-        id: 101, name: '마녀식당', type: '일식', address: '서울시 강남구',
+        id: 101,
+        name: '마녀식당',
+        type: '일식',
+        address: '서울시 강남구',
       },
     ],
   }));
--- src/ListContainer.test.jsx
+++ src/ListContainer.test.jsx
@@ -2,35 +2,42 @@ import { render } from '@testing-library/react';
 
 import { useSelector, useDispatch } from 'react-redux';
 
+import given from 'given2';
+
 import ListContainer from './ListContainer';
 
 jest.mock('react-redux');
 jest.mock('./services/api');
 
+const listItems = {
+  regions: [{ id: 1, name: '서울' }],
+  categories: [{ id: 11, name: '한식' }],
+  restaurants: [
+    {
+      id: 101,
+      name: '마녀식당',
+      type: '일식',
+      address: '서울시 강남구',
+    },
+  ],
+};
+
 describe('ListContainer', () => {
+  const renderListContainer = () => render((<ListContainer />));
+
+  given('listItems', () => []);
   useSelector.mockImplementation((selector) => selector({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 11, name: '한식' },
-    ],
-    restaurants: [
-      {
-        id: 101, name: '마녀식당', type: '일식', address: '서울시 강남구',
-      },
-    ],
+    listItems: given.listItems,
   }));
 
   const dispatch = jest.fn();
-
   useDispatch.mockImplementation(() => dispatch);
 
-  it('change regions list', () => {
-    const { getByText } = render((
-      <ListContainer />
-    ));
+  it('renders listItems', () => {
+    given('listItems', () => listItems);
 
-    expect(getByText('서울')).not.toBeNull();
+    const { container } = renderListContainer();
+
+    expect(container).not.toBeNull();
   });
 });
