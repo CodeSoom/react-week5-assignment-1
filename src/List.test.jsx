090a1421975032736047df65a3f723119a7fd3da
nada1587 <nada1587@naver.com>
2022-10-14 19:05:51 +0900

    - List 테스트 코드 작성

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -2,35 +2,49 @@ import { render } from '@testing-library/react';
 
 import { useDispatch } from 'react-redux';
 
+import given from 'given2';
+
 import List from './List';
 
 jest.mock('react-redux');
 jest.mock('./services/api');
 
-describe('List', () => {
-  const dispatch = jest.fn();
+const listItems = {
+  regions: [{ id: 1, name: '서울' }],
+  categories: [{ id: 11, name: '한식' }],
+  restaurants: [
+    {
+      id: 101,
+      name: '마녀식당',
+      type: '일식',
+      address: '서울시 강남구',
+    },
+  ],
+};
 
+describe('List', () => {
+  const renderList = () => render((<List />));
+  given('listItems', () => []);
+
+  const dispatch = jest.fn();
   useDispatch.mockImplementation(() => dispatch);
 
-  it('renders regions list', () => {
-    const regions = [
-      { id: 1, name: '서울' },
-    ];
-    const { getByText } = render((
-      <List listItems={regions} />
-    ));
+  context('with listItems', () => {
+    it('renders regions list', () => {
+      given('listItems', () => listItems);
 
-    expect(getByText('서울')).not.toBeNull();
+      const { getByText } = renderList();
+
+      expect(getByText('서울')).toBe();
+      expect(getByText('한식')).toBe();
+    });
   });
 
-  it('renders categories list', () => {
-    const categories = [
-      { id: 11, name: '한식' },
-    ];
-    const { getByText } = render((
-      <List listItems={categories} />
-    ));
+  context('without listItems', () => {
+    it('renders categories list', () => {
+      const { container } = renderList();
 
-    expect(getByText('한식')).not.toBeNull();
+      expect(container).toBeNull();
+    });
   });
 });
