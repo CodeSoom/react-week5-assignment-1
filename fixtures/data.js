37f9eaffcfdd1c4b3091da31c88bf148fe36edae
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-10 07:18:03 +0900

    fixtures 폴더 생성 및 테스트용 데이터 생성
    테스트용 데이터를 생성하였습니다

---
+++ fixtures/data.js
@@ -0,0 +1,20 @@
+export const regions = [
+  { id: 1, name: '서울' },
+  { id: 2, name: '부산' },
+  { id: 3, name: '대전' },
+  { id: 4, name: '전주' },
+  { id: 5, name: '제주도' },
+];
+
+export const categories = [
+  { id: 1, name: '한식' },
+  { id: 2, name: '분식' },
+  { id: 3, name: '중식' },
+  { id: 4, name: '일식' },
+  { id: 5, name: '양식' },
+];
+
+export const restaurants = [
+  { id: 1, name: '김밥천국' },
+  { id: 2, name: '김밥지옥' },
+];
