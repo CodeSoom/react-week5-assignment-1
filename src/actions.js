b39d5de9d2b992555dae851853819977797d285a
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-10 07:21:12 +0900

    상태 관리할 actions 생성
    카테고리, 지역, 레스토랑을 관리할 수 있는 액션들을 모아 작업했습니다

---
+++ src/actions.js
@@ -0,0 +1,53 @@
+import {
+  fetchRestaurants,
+  fetchRegions,
+  fetchCategories,
+} from './services/api';
+
+export function setRestaurants(restaurants) {
+  return {
+    type: 'setRestaurants',
+    payload: {
+      restaurants,
+    },
+  };
+}
+
+export function setRegions(regions) {
+  return {
+    type: 'setRegions',
+    payload: {
+      regions,
+    },
+  };
+}
+
+export function setCategories(categories) {
+  return {
+    type: 'setCategories',
+    payload: {
+      categories,
+    },
+  };
+}
+
+export function loadRestaurants() {
+  return async (dispatch) => {
+    const restaurants = await fetchRestaurants();
+    dispatch(setRestaurants(restaurants));
+  };
+}
+
+export function loadRegions() {
+  return async (dispatch) => {
+    const regions = await fetchRegions();
+    dispatch(setRegions(regions));
+  };
+}
+
+export function loadCategories() {
+  return async (dispatch) => {
+    const categories = await fetchCategories();
+    dispatch(setCategories(categories));
+  };
+}
