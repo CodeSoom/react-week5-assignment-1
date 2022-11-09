fb9e84b762fa2823733daa1e1fcf9f2ed178eab9
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-10 07:19:02 +0900

    RegionsContainer 생성
    지역을 보여줄 RegionsContainer를 생성하였습니다

---
+++ src/RegionsContainer.jsx
@@ -0,0 +1,15 @@
+import { useSelector } from 'react-redux';
+
+import Regions from './Regions';
+
+export default function RegionContainer() {
+  const { regions } = useSelector((state) => ({
+    regions: state.regions,
+  }));
+
+  return (
+    <Regions
+      regions={regions}
+    />
+  );
+}
