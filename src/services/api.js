ed1f355cf9e250770aff4788e9d693dd20c6fe39
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-10 07:30:53 +0900

    api 파일 생성 및 코드 작성
    지역, 목록, 레스토랑의 api 데이터를 받아올 수 있도록 async await을 이용하여 구현하였습니다

---
+++ src/services/api.js
@@ -0,0 +1,24 @@
+export async function fetchRestaurants() {
+  const url =
+    'https://eatgo-customer-api.ahastudio.com/restaurants?region=${regionName}&category=${categoryId}';
+  const response = await fetch(url);
+  const data = response.json();
+
+  return data;
+}
+
+export async function fetchRegions() {
+  const url = 'https://eatgo-customer-api.ahastudio.com/regions';
+  const response = await fetch(url);
+  const data = response.json();
+
+  return data;
+}
+
+export async function fetchCategories() {
+  const url = 'https://eatgo-customer-api.ahastudio.com/categories';
+  const response = await fetch(url);
+  const data = response.json();
+
+  return data;
+}
