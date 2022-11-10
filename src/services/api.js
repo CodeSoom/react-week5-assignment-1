26d7cf82165303c2e54a06058c24fd5bd8573564
hyeji07 <zindex_ann@naver.com>
2022-11-11 08:04:27 +0900

    api 파일 생성
    regions, categories data불러오기

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+const url = 'https://eatgo-customer-api.ahastudio.com';
+
+export async function fetchRegions() {
+  const response = await fetch(`${url}/regions`);
+  const data = await response.json();
+
+  return data;
+}
+
+export async function fetchCategories() {
+  const response = await fetch(`${url}/categories`);
+  const data = await response.json();
+
+  return data;
+}
