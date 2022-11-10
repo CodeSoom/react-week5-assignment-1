8ac65b7eb2455f37d55b9694a69987c8cd79e7ac
sejineeee <sejinee0918@gmail.com>
2022-11-10 18:20:19 +0900

    test: App 테스트에 Categories 추가
    categories fixtures 테스트용 데이터 중 한식이 있는지 테스트

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import App from './App';
 
 import regions from '../fixtures/regions';
+import categories from '../fixtures/categories';
 
 jest.mock('react-redux');
 
@@ -19,11 +20,13 @@ describe('App', () => {
 
   useSelector.mockImplementation((selector) => selector({
     regions,
+    categories,
   }));
 
   it('App이 렌더링된다.', () => {
     const { getByText } = renderApp();
 
     expect(getByText('서울')).not.toBeNull();
+    expect(getByText('한식')).not.toBeNull();
   });
 });
