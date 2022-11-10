9f6d68d3fe219de25fa18ffaf0917d92ceb9ee53
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-10 22:39:15 +0900

    Region.test 생성 및 테스트 코드 구현
    지역 목록 여부, handleClick 호출, 아이디 여부에 따른 테스트를 추가하였습니다

---
+++ src/Region.test.jsx
@@ -0,0 +1,58 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import Region from './Region';
+
+import { regions } from '../fixtures/data';
+
+jest.mock('react-redux');
+
+describe('Region', () => {
+  const handleClick = jest.fn();
+
+  const renderCategory = ({ region, isSelected = false }) => render((
+    <Region
+      region={region}
+      isSelected={isSelected}
+      onClick={handleClick}
+    />
+  ));
+
+  it('카테고리 목록이 랜더링된다', () => {
+    const region = regions[0];
+
+    const { container } = renderCategory({ region });
+
+    expect(container).toHaveTextContent(region.name);
+  });
+
+  it('카테고리를 선택하면 handleClick이 호출한다', () => {
+    const region = regions[0];
+
+    renderCategory({ region });
+
+    fireEvent.click(screen.getByRole('button'));
+
+    expect(handleClick).toBeCalledWith(region.id);
+  });
+
+  context('카테고리를 선택하면', () => {
+    it('선택한 카테고리에 "V"가 랜더링된다', () => {
+      const region = regions[0];
+
+      const { container } = renderCategory({ region, isSelected: true });
+
+      expect(container).toHaveTextContent(`${region.name}(V)`);
+    });
+  });
+
+  context('카테고리를 선택하지 않은 목록에는', () => {
+    it('"V"가 랜더링되지 않는다', () => {
+      const region = regions[0];
+
+      const { container } = renderCategory({ region, isSelected: false });
+
+      expect(container).toHaveTextContent(`${region.name}`);
+      expect(container).not.toHaveTextContent('(V)');
+    });
+  });
+});
