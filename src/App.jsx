4c84df43dff7c4d5d92800e1a1557030e6927194
sejineeee <sejinee0918@gmail.com>
2022-11-10 18:25:22 +0900

    App 파일에 CategoriesContainer 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import RegionsContainer from './RegionsContainer';
+import CategoriesContainer from './CategoriesContainer';
 
 export default function App() {
   return (
     <>
       <RegionsContainer />
+      <CategoriesContainer />
     </>
   );
 }
