0dbff900910507b93a8a884dba28fa91a90372e9
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-10 22:26:12 +0900

    RegionsContainer handleClick 이벤트 dispatch 추가
    지역 목록 중 하나를 선택했을 때, V 표시를 랜더링하기 위하여 handleClick 이벤트를 추가하였습니다

--- src/RegionsContainer.jsx
+++ src/RegionsContainer.jsx
@@ -1,15 +1,26 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Regions from './Regions';
 
+import { selectRegion } from './actions';
+
 export default function RegionContainer() {
-  const { regions } = useSelector((state) => ({
+  const dispatch = useDispatch();
+
+  const { regions, selectedRegionId } = useSelector((state) => ({
     regions: state.regions,
+    selectedRegionId: state.selectedRegionId,
   }));
+
+  function handleClickCheck(region) {
+    dispatch(selectRegion(region));
+  }
 
   return (
     <Regions
       regions={regions}
+      selectedRegionId={selectedRegionId}
+      onClick={handleClickCheck}
     />
   );
 }
