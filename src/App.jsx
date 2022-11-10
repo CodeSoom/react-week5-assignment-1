e06b09494824f9396c49012157c2eb34f3b9304b
hyeji07 <zindex_ann@naver.com>
2022-11-11 08:44:01 +0900

    App에 dispatch설정, CategoriesContainer컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
+
+import {
+  loadRegions,
+  loadCategories,
+  loadRestaurants,
+} from './actions';
+
 import RegionsContainer from './RegionsContainer';
+import CategoriesContainer from './CategoriesContainer';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRegions());
+    dispatch(loadCategories());
+    dispatch(loadRestaurants());
+  }, []);
+
   return (
     <div>
       <RegionsContainer />
+      <CategoriesContainer />
     </div>
   );
 }
---
+++ src/RegionsContainer.jsx
@@ -0,0 +1,5 @@
+export default function RegionsContainer() {
+  return (
+    <div />
+  );
+}
