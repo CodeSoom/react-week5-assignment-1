de7f21b2c4cb7a9c9a3cc2425930c0394d8b4afd
hyeji07 <zindex_ann@naver.com>
2022-11-11 08:19:49 +0900

    mock생성, fixtures테스트data생성

---
+++ fixtures/data.js
@@ -0,0 +1,16 @@
+export const regions = [
+  { id: 1, name: '서울' },
+  { id: 2, name: '대전' },
+  { id: 3, name: '대구' },
+];
+
+export const categories = [
+  { id: 1, name: '한식' },
+  { id: 2, name: '중식' },
+  { id: 3, name: '일식' },
+];
+
+export const restaurants = [
+  { id: 1, name: '맛나' },
+  { id: 2, name: '맛나요' },
+];
