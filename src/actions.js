856e06f91df6817d23eb593e9417ad5aa11a1ba7
sejineeee <sejinee0918@gmail.com>
2022-11-10 18:04:27 +0900

    categories에 대한 action 추가
    setCategories action 구현
    loadCategories 함수 구현
    fetchCategories import 하여 loadCategories 함수에서 async await 사용하여 호출

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,4 @@
-import { fetchRegions } from './services/api';
+import { fetchRegions, fetchCategories } from './services/api';
 
 export const setRegions = (regions) => ({
   type: 'setRegions',
@@ -7,7 +7,19 @@ export const setRegions = (regions) => ({
   },
 });
 
+export const setCategories = (categories) => ({
+  type: 'setCategories',
+  payload: {
+    categories,
+  },
+});
+
 export const loadRegions = () => async (dispatch) => {
   const regions = await fetchRegions();
   dispatch(setRegions(regions));
+};
+
+export const loadCategories = () => async (dispatch) => {
+  const categories = await fetchCategories();
+  dispatch(setCategories(categories));
 };
