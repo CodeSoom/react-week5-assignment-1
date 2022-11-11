e49716de3f59566a6d84209162022b02d0822d5f
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-11 17:03:35 +0900

    reducer.test에 selectCategory, selectRegion 추가
    선택한 카테고리와 지역으로 변경을 해주는 액션을 테스트 추가하였습니다

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,6 +1,12 @@
 import reducer from './reducer';
 
-import { setRegions, setCategories, setRestaurants } from './actions';
+import {
+  setRegions,
+  setCategories,
+  setRestaurants,
+  selectCategory,
+  selectRegion,
+} from './actions';
 
 import { regions, categories, restaurants } from '../fixtures/data';
 
@@ -9,6 +15,8 @@ describe('reducer', () => {
     regions: [],
     categories: [],
     restaurants: [],
+    selectedRegion: '',
+    selectedCategoryId: null,
   };
 
   describe('setRegions', () => {
@@ -31,7 +39,36 @@ describe('reducer', () => {
     it('레스토랑 목록을 변경해준다', () => {
       const state = reducer(initialState, setRestaurants(restaurants));
 
-      expect(state.restaurants).toHaveLength(2);
+      expect(state.restaurants).toHaveLength(3);
+    });
+  });
+
+  describe('selectCategory', () => {
+    it('selectedCategoryId를 선택한 카테고리의 아이디를 반경해준다', () => {
+      const state = reducer(
+        { selectedCategoryId: null },
+        selectCategory(categories[0]),
+      );
+
+      expect(state.selectedCategoryId).toEqual(categories[0]);
+    });
+  });
+
+  describe('selectRegion', () => {
+    it('selectedRegion을 선택한 지역으로 반경해준다', () => {
+      const state = reducer({ selectedRegion: '' }, selectRegion(regions[0]));
+
+      expect(state.selectedRegion).toEqual(regions[0]);
+    });
+  });
+
+  describe('아무런 값이 없을 때', () => {
+    it('아무런 동작을 하지 않는다', () => {
+      const state = reducer();
+
+      expect(state.regions).toStrictEqual([]);
+      expect(state.categories).toStrictEqual([]);
+      expect(state.restaurants).toHaveLength(0);
     });
   });
 });
