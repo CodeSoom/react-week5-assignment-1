b3d48fa760d5fb5500146def0c3e1280bd620558
sejineeee <sejinee0918@gmail.com>
2022-11-11 17:41:14 +0900

    선택된 name, category에 대한 action 추가, loadRestaurant 함수 구현
    selectedRegion, selectedCategory, selectedRestaurant action 추가
    loadRestaurant 함수에서 async await 이용하여 fetchSelected 함수 호출하여 데이터 받아옴
    dispatch 호출하여 selectedRestaurant 액션에 받아온 데이터 전달

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,4 @@
-import { fetchRegions, fetchCategories } from './services/api';
+import { fetchRegions, fetchCategories, fetchSelected } from './services/api';
 
 export const setRegions = (regions) => ({
   type: 'setRegions',
@@ -14,6 +14,27 @@ export const setCategories = (categories) => ({
   },
 });
 
+export const selectedRegion = (regionName) => ({
+  type: 'selectedRegion',
+  payload: {
+    selectedRegion: regionName,
+  },
+});
+
+export const selectedCategory = (categoryId) => ({
+  type: 'selectedCategory',
+  payload: {
+    selectedCategory: categoryId,
+  },
+});
+
+export const selectedRestaurant = (restaurant) => ({
+  type: 'selectedRestaurant',
+  payload: {
+    selectedRestaurant: restaurant,
+  },
+});
+
 export const loadRegions = () => async (dispatch) => {
   const regions = await fetchRegions();
   dispatch(setRegions(regions));
@@ -22,4 +43,9 @@ export const loadRegions = () => async (dispatch) => {
 export const loadCategories = () => async (dispatch) => {
   const categories = await fetchCategories();
   dispatch(setCategories(categories));
+};
+
+export const loadRestaurant = (regionName, categoryId) => async (dispatch) => {
+  const selecteRestaurant = await fetchSelected(regionName, categoryId);
+  dispatch(selectedRestaurant(selecteRestaurant));
 };
