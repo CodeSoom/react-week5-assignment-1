d791da4767f38b306732caf1017237151f9f4a80
sejineeee <sejinee0918@gmail.com>
2022-11-11 17:19:03 +0900

    test: 지역을 선택했을 때, 선택된 지역에 체크 표시 테스트 추가
    선택된 지역에 체크 표시 테스트 구현하였으나 실패
    getByText에 대전을 찾을 수 없음.
    클릭 이벤트 전에 체크 표시가 붙은 값 반환으로 인해 테스트 오동작

--- src/Regions.test.jsx
+++ src/Regions.test.jsx
@@ -1,18 +1,38 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Regions from './Regions';
 
 import regions from '../fixtures/regions';
 
 describe('Regions', () => {
-  function renderRegions() {
-    return render((<Regions regions={regions} />));
+  const handleClick = jest.fn();
+
+  function renderRegions({ regionLists, selectedRegionName }) {
+    return render((<Regions
+      regions={regionLists}
+      onClick={handleClick}
+      selectedRegionName={selectedRegionName}
+    />));
   }
 
   it('Regions가 렌더링된다.', () => {
-    const { getByText } = renderRegions();
+    const { getByText } = renderRegions({
+      regionLists: regions,
+    });
 
     expect(getByText('서울')).not.toBeNull();
     expect(getByText('대전')).not.toBeNull();
+  });
+
+  it('지역을 클릭하면 선택된 지역에 체크 표시가 붙는다.', () => {
+    const { getByText } = renderRegions({
+      regionLists: regions,
+      selectedRegionName: '대전',
+    });
+
+    fireEvent.click(getByText('대전'));
+
+    expect(handleClick).toBeCalled();
+    expect(getByText('대전✅')).not.toBeNull();
   });
 });
