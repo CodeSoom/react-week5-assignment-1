8a2465c4ea1933ef3d9026c1b970f5a65a75939d
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-11 16:45:24 +0900

    RestaurantsContaienr 컴포넌트 생성 및 구현
    레스토랑 목록이 보일 수 있게 레스토랑 관련 관심사를 분리하여 생성하였습니다.
    선택한 지역과, 카테고리 아이디가 있을 시에만 loadRestaurnats를 호출하여 상태를 변화시킬 수 있도록 하였습니다.

---
+++ src/RestaurantsContainer.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+import Restaurants from './Restaurants';
+
+import { loadRestaurants } from './actions';
+
+export default function RestaurantsContainer() {
+  const dispatch = useDispatch();
+
+  const { restaurants, selectedRegion, selectedCategoryId } = useSelector((state) => ({
+    selectedRegion: state.selectedRegion,
+    selectedCategoryId: state.selectedCategoryId,
+    restaurants: state.restaurants,
+  }));
+
+  useEffect(() => {
+    if (selectedRegion === null || selectedCategoryId === null) {
+      return;
+    }
+
+    dispatch(loadRestaurants(selectedRegion, selectedCategoryId));
+  }, [selectedRegion, selectedCategoryId]);
+
+  return (
+    <Restaurants
+      restaurants={restaurants}
+    />
+  );
+}
