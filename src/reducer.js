c59cbc039fad9b3b36435fa6ed437711535d275e
hyeji07 <zindex_ann@naver.com>
2022-11-11 10:34:24 +0900

    reducer작성
    각 action별 상태를 관리 하도록 작성

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,40 @@
-const reducer = () => {};
+const initialState = {
+  resions: [],
+  restaurants: [],
+  categories: [],
+};
 
-export default reducer;
+const actionCreators = {
+  setRegions: (state, action) => {
+    const { resions } = action.payload;
+
+    return {
+      ...state,
+      resions,
+    };
+  },
+
+  setCategories: (state, action) => {
+    const { categories } = action.payload;
+
+    return {
+      ...state,
+      categories,
+    };
+  },
+
+  setRestaurants: (state, action) => {
+    const { restaurants } = action.payload;
+
+    return {
+      ...state,
+      restaurants,
+    };
+  },
+};
+
+export default function reducer(state = initialState, action) {
+  return !action || !actionCreators[action.type]
+    ? state
+    : actionCreators[action.type](state, action);
+}
