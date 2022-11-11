38fdcf9c26089539b322898dcb7fa5bdea0155c1
sejineeee <sejinee0918@gmail.com>
2022-11-11 16:53:21 +0900

    test: Categories 테스트에 클릭했을 때와 체크표시 테스트 추가
    카테고리를 클릭했을 때, id 값을 전달하는 것을 테스트
    카테고리를 클릭했을 때, 선택된 카테고리에 체크 표시가 붙는 것 테스트
    renderCategories에 인자로 id 값을 전달하여 확인하고 싶었으나,
    fireEvent가 실행되기 이전에 함수가 호출되는 사항으로 테스트 오작동

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -1,17 +1,43 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Categories from './Categories';
 
 import categories from '../fixtures/categories';
 
 describe('Categories', () => {
-  function renderCategories() {
-    return render((<Categories categories={categories} />));
+  const handleClick = jest.fn();
+
+  function renderCategories(categoryId) {
+    return render((<Categories
+      categories={categories}
+      onClick={handleClick}
+      selectedCategory={categoryId}
+    />));
   }
 
   it('Categories가 렌더링된다.', () => {
     const { getByText } = renderCategories();
 
     expect(getByText('한식')).not.toBeNull();
-  })
-})
+  });
+
+  it('분류 목록을 클릭했을 때, onClick 함수에 id 값을 전달한다.', () => {
+    const { getByText } = renderCategories();
+
+    fireEvent.click(getByText('중식'));
+
+    expect(handleClick).toBeCalledWith(2);
+  });
+
+  it('분류 목록을 클릭했을 때, 선택된 분류에 체크 표시가 붙는다.', () => {
+    const { getByText } = renderCategories(3);
+
+    expect(handleClick).not.toBeCalled();
+
+    fireEvent.click(getByText('일식✅'));
+
+    expect(handleClick).toBeCalled();
+
+    expect(getByText('일식✅')).not.toBeNull();
+  });
+});
