ed86aed681f31a73c78c6cd36553ad5be8d09d08
sejineeee <sejinee0918@gmail.com>
2022-11-11 17:21:40 +0900

    지역 선택시 체크표시 되도록 구현
    selectedRegionName을 Props로 전달 받음
    단락평가 이용하여 selectedName과 region.name이 같으면 체크 표시 반환

--- src/Regions.jsx
+++ src/Regions.jsx
@@ -1,8 +1,23 @@
-export default function Regions({ regions, onClick }) {
+export default function Regions({
+  regions,
+  onClick,
+  selectedRegionName,
+}) {
   return (
     <div>
       {
-        regions.map((region) => <button type="button" key={region.id} onClick={() => onClick(region.name)}>{region.name}</button>)
+        regions.map((region) => (
+          <button
+            type="button"
+            key={region.id}
+            onClick={() => onClick(region.name)}
+          >
+            {region.name}
+            {
+              selectedRegionName === region.name && '✅'
+            }
+          </button>
+        ))
       }
     </div>
   );
