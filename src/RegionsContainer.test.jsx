1547e363861abe6b5661025a4201ed0c91f8d2df
hyeji07 <zindex_ann@naver.com>
2022-11-11 10:23:26 +0900

    RegionsContainer test작성

---
+++ src/RegionsContainer.test.jsx
@@ -0,0 +1,23 @@
+import { useSelector } from 'react-redux';
+
+import { render } from '@testing-library/react';
+
+import RegionsContainer from './RegionsContainer';
+
+jest.mock('react-redux');
+
+describe('RegionsContainer', () => {
+  it('지역이 랜더링된다', () => {
+    useSelector.mockImplementation((selector) => selector({
+      regions: [
+        { id: 1, name: '서울' },
+      ],
+    }));
+
+    const { getByText } = render((
+      <RegionsContainer />
+    ));
+
+    expect(getByText(/서울/)).not.toBeNull();
+  });
+});
