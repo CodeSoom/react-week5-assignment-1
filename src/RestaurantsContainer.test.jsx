9d37e3ba14eea84bbc51967aff7fcebc3082adac
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-11 16:48:55 +0900

    RestaurantsContainer.test 테스트 파일 생성 및 테스트
    선택한 지역과 카테고리 데이터 여부를 나누어 랜더링 되는 것을 확인하는 테스트 코드를 구성하였습니다

---
+++ src/RestaurantsContainer.test.jsx
@@ -0,0 +1,54 @@
+import { render, screen } from '@testing-library/react';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+import RestaurantsContainer from './RestaurantsContainer';
+
+import { restaurants, categories, regions } from '../fixtures/data';
+
+jest.mock('react-redux');
+
+describe('RestaurantsContainer', () => {
+  const dispatch = jest.fn();
+
+  const selectedCategory = categories[0];
+  const selectedRegion = regions[0];
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  const renderRestaurantsContainer = () => render((
+    <RestaurantsContainer />
+  ));
+
+  context('선택한 지역과 카테고리 데이터가 있을 시', () => {
+    const mockSelector = () => useSelector.mockImplementation(
+      (selector) => selector({
+        restaurants,
+        selectedRegion,
+        selectedCategory,
+      }),
+    );
+
+    it('dispatch가 실행한다', () => {
+      mockSelector();
+      renderRestaurantsContainer();
+
+      expect(dispatch).toBeCalled();
+    });
+
+    it('레스토랑 목록이 랜더링된다', () => {
+      mockSelector();
+      renderRestaurantsContainer();
+
+      expect(screen.queryByText('양천주가')).not.toBeNull();
+    });
+  });
+
+  context('선택한 지역 혹은 카테고리 아이디가 없을 시', () => {
+    it('레스토랑이 없어요! 문구가 랜더링된다', () => {
+      const { container } = renderRestaurantsContainer([]);
+
+      expect(container).toHaveTextContent('레스토랑이 없어요!');
+    });
+  });
+});
