dfde37c85084ed8fd41072c10b79cd0e2e45c3f2
sejineeee <sejinee0918@gmail.com>
2022-11-11 17:38:38 +0900

    test: review 사항 반영하여 테스트명 변경, setCategories 액션에 대한 테스트 추가
    review 사항 반영하여 리듀서 관점에서 테스트명 받아온다에서 저장한다로 변경
    setCategories 액션에 대한 테스트 추가
    selectedRegion 액션은 현재 진행 중인 상태이므로 완성한 후 다시 푸시할 예정

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,11 +1,13 @@
 import reducer from './reducer';
 
+import { setCategories, setRegions } from './actions';
+
 import regions from '../fixtures/regions';
-import { setRegions } from './actions';
+import categories from '../fixtures/categories';
 
 describe('reducer', () => {
   describe('setRegions', () => {
-    it('지역에 대한 데이터를 받아온다.', () => {
+    it('지역 데이터를 저장한다.', () => {
       const initialState = {
         regions: [],
       };
@@ -13,6 +15,24 @@ describe('reducer', () => {
       const state = reducer(initialState, setRegions(regions));
 
       expect(state.regions).toHaveLength(3);
+    });
+  });
+
+  describe('setCategories', () => {
+    it('카테고리 데이터를 저장한다.', () => {
+      const initialState = {
+        categories: [],
+      };
+
+      const state = reducer(initialState, setCategories(categories));
+
+      expect(state.categories).toHaveLength(3);
+    });
+  });
+
+  describe('selectedRegion', () => {
+    it('선택된 지역의 이름을 저장한다.', () => {
+
     });
   });
 });
