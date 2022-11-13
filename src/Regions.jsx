c245c2028057f8268c7947addd90a0da314099f0
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-13 14:42:30 +0900

    Regions 컴포넌트 관심사 분리
    선택된 지역을 관리하는 것은 Region 컴포넌트에서 전체적으로 관리할 수 있도록 Regions에서는 목록만 넘길 수 있게 관심사 분리를 하였습니다

--- src/Regions.jsx
+++ src/Regions.jsx
@@ -1,14 +1,6 @@
 import Region from './Region';
 
 export default function Regions({ regions, selectedRegion, onClick }) {
-  const isSelected = (region) => {
-    if (selectedRegion === null) {
-      return false;
-    }
-
-    return region.name === selectedRegion;
-  };
-
   if (!regions?.length) {
     return <p>지역 목록이 없어요!</p>;
   }
@@ -20,7 +12,7 @@ export default function Regions({ regions, selectedRegion, onClick }) {
           <Region
             key={region.id}
             region={region}
-            isSelected={isSelected(region)}
+            selectedRegion={selectedRegion}
             onClick={onClick}
           />
         ))
