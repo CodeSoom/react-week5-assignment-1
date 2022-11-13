88e6e07e9435b41c4fd36b77dc0c70c022617948
sejineeee <sejinee0918@gmail.com>
2022-11-13 16:13:48 +0900

    review: 각 리듀서 테스트 구현, 테스트명 변경
    저장한다에서 변경한다로 변경
    selected에 대한 테스트 구현
    action이 없을 경우, 정의된 action type이 없을 경우 테스트 추가

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,13 +1,20 @@
 import reducer from './reducer';
 
-import { setCategories, setRegions } from './actions';
+import {
+  setCategories,
+  setRegions,
+  selectedRegion,
+  selectedCategory,
+  selectedRestaurant,
+} from './actions';
 
 import regions from '../fixtures/regions';
 import categories from '../fixtures/categories';
+import restaurants from '../fixtures/restaurants';
 
 describe('reducer', () => {
   describe('setRegions', () => {
-    it('지역 데이터를 저장한다.', () => {
+    it('지역 목록을 변경한다.', () => {
       const initialState = {
         regions: [],
       };
@@ -19,7 +26,7 @@ describe('reducer', () => {
   });
 
   describe('setCategories', () => {
-    it('카테고리 데이터를 저장한다.', () => {
+    it('카테고리 목록을 변경한다.', () => {
       const initialState = {
         categories: [],
       };
@@ -31,8 +38,66 @@ describe('reducer', () => {
   });
 
   describe('selectedRegion', () => {
-    it('선택된 지역의 이름을 저장한다.', () => {
+    it('selectedRegionName을 선택된 지역명으로 변경한다.', () => {
+      const initialState = {
+        selectedRegionName: '',
+      };
 
+      const state = reducer(initialState, selectedRegion('서울'));
+
+      expect(state.selectedRegionName).toEqual('서울');
+    });
+  });
+
+  describe('selectedCategory', () => {
+    it('selectedCategoryId를 선택된 카테고리 id로 변경한다.', () => {
+      const initialState = {
+        selectedCategoryId: '',
+      };
+
+      const state = reducer(initialState, selectedCategory(3));
+
+      expect(state.selectedCategoryId).toEqual(3);
+    });
+  });
+
+  describe('selectedRestaurant', () => {
+    it('레스토랑의 정보를 변경한다.', () => {
+      const initialState = {
+        selectedRestaurant: [],
+      };
+
+      const state = reducer(initialState, selectedRestaurant(restaurants));
+
+      expect(state.selectedRestaurant).toHaveLength(2);
+    });
+  });
+
+  describe('정의된 action type이 없을 경우', () => {
+    it('아무 일도 일어나지 않는다.', () => {
+      const initialState = {
+        regions: [],
+        categories: [],
+        selectedRestaurant: [],
+        selectedRegionName: '',
+        selectedCategoryId: '',
+      };
+
+      const action = {
+        type: 'none action type',
+      };
+
+      const state = reducer(initialState, action);
+
+      expect(state).toBe(initialState);
+    });
+  });
+
+  describe('action이 없을 경우', () => {
+    it('아무 일도 일어나지 않는다.', () => {
+      const state = reducer();
+
+      expect(state.regions).toBe([]);
     });
   });
 });
