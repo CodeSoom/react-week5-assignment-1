6bf648523ef022b9b48b1cbf4cceee59ce2bc77a
sejineeee <sejinee0918@gmail.com>
2022-11-13 16:01:15 +0900

    review: RORO 패턴 적용,  체크 표시 UI 테스트
    RORO 패턴 적용, 체크 표시 UI 테스트 구현

--- src/Categories.test.jsx
+++ src/Categories.test.jsx
@@ -7,36 +7,37 @@ import categories from '../fixtures/categories';
 describe('Categories', () => {
   const handleClick = jest.fn();
 
-  function renderCategories(categoryId) {
+  function renderCategories({ categoryLists, selectedCategoryId }) {
     return render((<Categories
-      categories={categories}
+      categories={categoryLists}
       onClick={handleClick}
-      selectedCategory={categoryId}
+      selectedCategoryId={selectedCategoryId}
     />));
   }
 
   it('Categories가 렌더링된다.', () => {
-    const { getByText } = renderCategories();
+    const { getByText } = renderCategories({
+      categoryLists: categories,
+    });
 
     expect(getByText('한식')).not.toBeNull();
   });
 
   it('분류 목록을 클릭했을 때, onClick 함수에 id 값을 전달한다.', () => {
-    const { getByText } = renderCategories();
+    const { getByText } = renderCategories({
+      categoryLists: categories,
+    });
 
     fireEvent.click(getByText('중식'));
 
     expect(handleClick).toBeCalledWith(2);
   });
 
-  it('분류 목록을 클릭했을 때, 선택된 분류에 체크 표시가 붙는다.', () => {
-    const { getByText } = renderCategories(3);
-
-    expect(handleClick).not.toBeCalled();
-
-    fireEvent.click(getByText('일식✅'));
-
-    expect(handleClick).toBeCalled();
+  it('선택된 분류에 체크표시가 붙는다.', () => {
+    const { getByText } = renderCategories({
+      categoryLists: categories,
+      selectedCategoryId: 3,
+    });
 
     expect(getByText('일식✅')).not.toBeNull();
   });
