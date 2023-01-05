af7da972d8c984afc9feadb74b2dda6c9927094e
Ubermensch0608 <melancholyee28@gmail.com>
2023-01-06 05:30:16 +0900

    Refactor: 카테고리 컴포넌트 리팩토링
    카테고리 컴포넌트 렌더링과 액션 함수 실행 여부를 확인하는 테스트 코드를 추가하였습니다.

--- src/Category.test.jsx
+++ src/Category.test.jsx
@@ -1,16 +1,38 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 
 import Category from './Category';
+import categories from './fixtures/categories';
 
 describe('Category', () => {
-  it('checkCategory 액션이 실행된다.', () => {
-    const category = { id: 1, name: '한식' };
-    const checkCategory = jest.fn();
+  const checkCategory = jest.fn();
 
-    render(<Category category={category} onCheckCategory={checkCategory} />);
+  it('카테고리가 화면에 렌더 된다.', () => {
+    categories.forEach((category) => {
+      render(
+        <Category
+          category={category}
+          onCheckCategory={checkCategory}
+        />,
+      );
+    });
+
+    expect(screen.getByRole('button', { name: '한식' }));
+    expect(screen.getByRole('button', { name: '일식' }));
+    expect(screen.getByRole('button', { name: '중식' }));
+  });
+
+  it('카테고리 체크 액션이 실행 된다.', () => {
+    categories.forEach((category) => {
+      render(
+        <Category
+          category={category}
+          onCheckCategory={checkCategory}
+        />,
+      );
+    });
 
     fireEvent.click(screen.getByRole('button', { name: '한식' }));
 
-    expect(checkCategory).toBeCalled();
+    expect(checkCategory).toBeCalledTimes(1);
   });
 });
